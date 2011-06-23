733b4f0e47607d60d98e8cdc7a49d4ef225fefd6
ainara-galdona <>
2011-06-23 19:44:21 +0200

    stock_delivery_qty

--- avanzosc_stock_delivery_qty/stock_picking.py
+++ avanzosc_stock_delivery_qty/stock_picking.py
@@ -19,3 +19,301 @@
 #    along with this program.  If not, see http://www.gnu.org/licenses/.
 #
 ##############################################################################
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+import time
+
+from osv import osv, fields
+from tools.translate import _
+
+import decimal_precision as dp
+import netsvc
+
+class stock_move(osv.osv):
+    _inherit = 'stock.move'
+    
+    _columns = {                
+                'product_qty':fields.float('Measured Qty', digits_compute=dp.get_precision('Product UoM'), required=True,states={'done': [('readonly', True)]}),
+                'sup_qty':fields.float('Supplied Qty', digits_compute=dp.get_precision('Product UoM'), required=True, states={'done': [('readonly', True)]}),
+                }
+    
+    
+    def onchange_qty(self, cr, uid, fields, sup_qty, context=None):
+        res = {}
+        if sup_qty:
+            res = {
+                'product_qty': sup_qty,            
+                }
+        return {'value': res}
+    
+stock_move()
+
+class purchase_order(osv.osv):
+    _inherit = 'purchase.order'
+    
+    def action_picking_create(self,cr, uid, ids, *args):        
+        lines = self.pool.get('stock.move')        
+        pick_id = super(purchase_order, self).action_picking_create(cr, uid, ids)
+        ids = lines.search(cr, uid,[('picking_id','=',pick_id)])
+        for line_id in ids:
+            line_obj = lines.browse(cr, uid, line_id)
+            qty = line_obj.product_qty
+            lines.write(cr, uid, [line_id], {'sup_qty':qty})
+        return pick_id
+    
+purchase_order()
+
+class sale_order(osv.osv):
+    _inherit = 'sale.order'
+    
+    def action_ship_create(self, cr, uid, ids, *args):
+        wf_service = netsvc.LocalService("workflow")
+        picking_id = False
+        move_obj = self.pool.get('stock.move')
+        proc_obj = self.pool.get('procurement.order')
+        company = self.pool.get('res.users').browse(cr, uid, uid).company_id
+        for order in self.browse(cr, uid, ids, context={}):
+            proc_ids = []
+            output_id = order.shop_id.warehouse_id.lot_output_id.id
+            picking_id = False
+            for line in order.order_line:
+                proc_id = False
+                date_planned = datetime.now() + relativedelta(days=line.delay or 0.0)
+                date_planned = (date_planned - timedelta(days=company.security_lead)).strftime('%Y-%m-%d %H:%M:%S')
+
+                if line.state == 'done':
+                    continue
+                move_id = False
+                if line.product_id and line.product_id.product_tmpl_id.type in ('product', 'consu'):
+                    location_id = order.shop_id.warehouse_id.lot_stock_id.id
+                    if not picking_id:
+                        pick_name = self.pool.get('ir.sequence').get(cr, uid, 'stock.picking.out')
+                        picking_id = self.pool.get('stock.picking').create(cr, uid, {
+                            'name': pick_name,
+                            'origin': order.name,
+                            'type': 'out',
+                            'state': 'auto',
+                            'move_type': order.picking_policy,
+                            'sale_id': order.id,
+                            'address_id': order.partner_shipping_id.id,
+                            'note': order.note,
+                            'invoice_state': (order.order_policy=='picking' and '2binvoiced') or 'none',
+                            'company_id': order.company_id.id,
+                        })
+                    move_id = self.pool.get('stock.move').create(cr, uid, {
+                        'name': line.name[:64],
+                        'picking_id': picking_id,
+                        'product_id': line.product_id.id,
+                        'date': date_planned,
+                        'date_expected': date_planned,
+                        'product_qty': line.product_uom_qty,
+                        'sup_qty' : line.product_uom_qty,
+                        'product_uom': line.product_uom.id,
+                        'product_uos_qty': line.product_uos_qty,
+                        'product_uos': (line.product_uos and line.product_uos.id)\
+                                or line.product_uom.id,
+                        'product_packaging': line.product_packaging.id,
+                        'address_id': line.address_allotment_id.id or order.partner_shipping_id.id,
+                        'location_id': location_id,
+                        'location_dest_id': output_id,
+                        'sale_line_id': line.id,
+                        'tracking_id': False,
+                        'state': 'draft',
+                        #'state': 'waiting',
+                        'note': line.notes,
+                        'company_id': order.company_id.id,
+                    })
+
+                if line.product_id:
+                    proc_id = self.pool.get('procurement.order').create(cr, uid, {
+                        'name': line.name,
+                        'origin': order.name,
+                        'date_planned': date_planned,
+                        'product_id': line.product_id.id,
+                        'product_qty': line.product_uom_qty,
+                        'product_uom': line.product_uom.id,
+                        'product_uos_qty': (line.product_uos and line.product_uos_qty)\
+                                or line.product_uom_qty,
+                        'product_uos': (line.product_uos and line.product_uos.id)\
+                                or line.product_uom.id,
+                        'location_id': order.shop_id.warehouse_id.lot_stock_id.id,
+                        'procure_method': line.type,
+                        'move_id': move_id,
+                        'property_ids': [(6, 0, [x.id for x in line.property_ids])],
+                        'company_id': order.company_id.id,
+                    })
+                    proc_ids.append(proc_id)
+                    self.pool.get('sale.order.line').write(cr, uid, [line.id], {'procurement_id': proc_id})
+                    if order.state == 'shipping_except':
+                        for pick in order.picking_ids:
+                            for move in pick.move_lines:
+                                if move.state == 'cancel':
+                                    mov_ids = move_obj.search(cr, uid, [('state', '=', 'cancel'),('sale_line_id', '=', line.id),('picking_id', '=', pick.id)])
+                                    if mov_ids:
+                                        for mov in move_obj.browse(cr, uid, mov_ids):
+                                            move_obj.write(cr, uid, [move_id], {'product_qty': mov.product_qty, 'product_uos_qty': mov.product_uos_qty})
+                                            proc_obj.write(cr, uid, [proc_id], {'product_qty': mov.product_qty, 'product_uos_qty': mov.product_uos_qty})
+
+            val = {}
+
+            if picking_id:
+                wf_service.trg_validate(uid, 'stock.picking', picking_id, 'button_confirm', cr)
+
+            for proc_id in proc_ids:
+                wf_service.trg_validate(uid, 'procurement.order', proc_id, 'button_confirm', cr)
+
+            if order.state == 'shipping_except':
+                val['state'] = 'progress'
+                val['shipped'] = False
+
+                if (order.order_policy == 'manual'):
+                    for line in order.order_line:
+                        if (not line.invoiced) and (line.state not in ('cancel', 'draft')):
+                            val['state'] = 'manual'
+                            break
+            self.write(cr, uid, [order.id], val)
+        return True
+sale_order()
+
+class stock_picking(osv.osv):
+    _inherit = 'stock.picking'
+    
+    def action_invoice_create(self, cr, uid, ids, journal_id=False,
+            group=False, type='out_invoice', context=None):
+        """ Creates invoice based on the invoice state selected for picking.
+        @param journal_id: Id of journal
+        @param group: Whether to create a group invoice or not
+        @param type: Type invoice to be created
+        @return: Ids of created invoices for the pickings
+        """
+        if context is None:
+            context = {}
+
+        invoice_obj = self.pool.get('account.invoice')
+        invoice_line_obj = self.pool.get('account.invoice.line')
+        address_obj = self.pool.get('res.partner.address')
+        invoices_group = {}
+        res = {}
+        inv_type = type
+        for picking in self.browse(cr, uid, ids, context=context):
+            if picking.invoice_state != '2binvoiced':
+                continue
+            payment_term_id = False
+            partner =  picking.address_id and picking.address_id.partner_id
+            if not partner:
+                raise osv.except_osv(_('Error, no partner !'),
+                    _('Please put a partner on the picking list if you want to generate invoice.'))
+
+            if not inv_type:
+                inv_type = self._get_invoice_type(picking)
+
+            if inv_type in ('out_invoice', 'out_refund'):
+                account_id = partner.property_account_receivable.id
+                payment_term_id = self._get_payment_term(cr, uid, picking)
+            else:
+                account_id = partner.property_account_payable.id
+
+            address_contact_id, address_invoice_id = \
+                    self._get_address_invoice(cr, uid, picking).values()
+            address = address_obj.browse(cr, uid, address_contact_id, context=context)
+
+            comment = self._get_comment_invoice(cr, uid, picking)
+            if group and partner.id in invoices_group:
+                invoice_id = invoices_group[partner.id]
+                invoice = invoice_obj.browse(cr, uid, invoice_id)
+                invoice_vals = {
+                    'name': (invoice.name or '') + ', ' + (picking.name or ''),
+                    'origin': (invoice.origin or '') + ', ' + (picking.name or '') + (picking.origin and (':' + picking.origin) or ''),
+                    'comment': (comment and (invoice.comment and invoice.comment+"\n"+comment or comment)) or (invoice.comment and invoice.comment or ''),
+                    'date_invoice':context.get('date_inv',False),
+                    'user_id':uid
+                }
+                invoice_obj.write(cr, uid, [invoice_id], invoice_vals, context=context)
+            else:
+                invoice_vals = {
+                    'name': picking.name,
+                    'origin': (picking.name or '') + (picking.origin and (':' + picking.origin) or ''),
+                    'type': inv_type,
+                    'account_id': account_id,
+                    'partner_id': address.partner_id.id,
+                    'address_invoice_id': address_invoice_id,
+                    'address_contact_id': address_contact_id,
+                    'comment': comment,
+                    'payment_term': payment_term_id,
+                    'fiscal_position': partner.property_account_position.id,
+                    'date_invoice': context.get('date_inv',False),
+                    'company_id': picking.company_id.id,
+                    'user_id':uid
+                }
+                cur_id = self.get_currency_id(cr, uid, picking)
+                if cur_id:
+                    invoice_vals['currency_id'] = cur_id
+                if journal_id:
+                    invoice_vals['journal_id'] = journal_id
+                invoice_id = invoice_obj.create(cr, uid, invoice_vals,
+                        context=context)
+                invoices_group[partner.id] = invoice_id
+            res[picking.id] = invoice_id
+            for move_line in picking.move_lines:
+                if move_line.state == 'cancel':
+                    continue
+                origin = move_line.picking_id.name or ''
+                if move_line.picking_id.origin:
+                    origin += ':' + move_line.picking_id.origin
+                if group:
+                    name = (picking.name or '') + '-' + move_line.name
+                else:
+                    name = move_line.name
+
+                if inv_type in ('out_invoice', 'out_refund'):
+                    account_id = move_line.product_id.product_tmpl_id.\
+                            property_account_income.id
+                    if not account_id:
+                        account_id = move_line.product_id.categ_id.\
+                                property_account_income_categ.id
+                else:
+                    account_id = move_line.product_id.product_tmpl_id.\
+                            property_account_expense.id
+                    if not account_id:
+                        account_id = move_line.product_id.categ_id.\
+                                property_account_expense_categ.id
+
+                price_unit = self._get_price_unit_invoice(cr, uid,
+                        move_line, inv_type)
+                discount = self._get_discount_invoice(cr, uid, move_line)
+                tax_ids = self._get_taxes_invoice(cr, uid, move_line, inv_type)
+                account_analytic_id = self._get_account_analytic_invoice(cr, uid, picking, move_line)
+
+                #set UoS if it's a sale and the picking doesn't have one
+                uos_id = move_line.product_uos and move_line.product_uos.id or False
+                if not uos_id and inv_type in ('out_invoice', 'out_refund'):
+                    uos_id = move_line.product_uom.id
+
+                account_id = self.pool.get('account.fiscal.position').map_account(cr, uid, partner.property_account_position, account_id)
+                invoice_line_id = invoice_line_obj.create(cr, uid, {
+                    'name': name,
+                    'origin': origin,
+                    'invoice_id': invoice_id,
+                    'uos_id': uos_id,
+                    'product_id': move_line.product_id.id,
+                    'account_id': account_id,
+                    'price_unit': price_unit,
+                    'discount': discount,
+                    'quantity': move_line.sup_qty or move_line.product_uos_qty or move_line.product_qty,
+                    'invoice_line_tax_id': [(6, 0, tax_ids)],
+                    'account_analytic_id': account_analytic_id,
+                }, context=context)
+                self._invoice_line_hook(cr, uid, move_line, invoice_line_id)
+
+            invoice_obj.button_compute(cr, uid, [invoice_id], context=context,
+                    set_total=(inv_type in ('in_invoice', 'in_refund')))
+            self.write(cr, uid, [picking.id], {
+                'invoice_state': 'invoiced',
+                }, context=context)
+            self._invoice_hook(cr, uid, picking, invoice_id)
+        self.write(cr, uid, res.keys(), {
+            'invoice_state': 'invoiced',
+            }, context=context)
+        return res
+     
+stock_picking()
