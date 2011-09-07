3a6e001fe322ae17e5457de696f96aed0dce3c09
ainara-galdona <ainaragaldona@avanzosc.com>
2011-09-07 14:06:01 +0200

    ting_sales_tree_grid_v6 modified

--- ting_sales_tree_grid_v6.0/sale.py
+++ ting_sales_tree_grid_v6.0/sale.py
@@ -32,8 +32,8 @@ class sale_order_line(osv.osv):
     def product_id_change(self, cr, uid, ids, pricelist, product, qty=0,
             uom=False, qty_uos=0, uos=False, name='', partner_id=False,
             lang=False, update_tax=True, date_order=False, packaging=False, fiscal_position=False, flag=False):
-        if not  partner_id:
-            raise osv.except_osv(_('No Customer Defined !'), _('You have to select a customer in the sale form !\nPlease set one customer before choosing a product.'))
+#        if not  partner_id:
+#            raise osv.except_osv(_('No Customer Defined !'), _('You have to select a customer in the sale form !\nPlease set one customer before choosing a product.'))
         warning = {}
         product_uom_obj = self.pool.get('product.uom')
         partner_obj = self.pool.get('res.partner')
