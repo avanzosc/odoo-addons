9e92d91d047e4c537a927b4694c87159ca48f3b4
ainara-galdona <ainaragaldona@avanzosc.com>
2011-09-07 14:04:43 +0200

    tree_grid modified

--- tree_grid/sale.py
+++ tree_grid/sale.py
@@ -32,8 +32,8 @@ class sale_order_line(osv.osv):
     def product_id_change(self, cr, uid, ids, pricelist, product, qty=0,
             uom=False, qty_uos=0, uos=False, name='', partner_id=False,
             lang=False, update_tax=True, date_order=False, packaging=False, fiscal_position=False, flag=False):
-        if not  partner_id:
-            raise osv.except_osv(_('No Customer Defined !'), _('You have to select a customer in the sale form !\nPlease set one customer before choosing a product.'))
+#        if not partner_id:
+#            raise osv.except_osv(_('No Customer Defined !'), _('You have to select a customer in the sale form !\nPlease set one customer before choosing a product.'))
         warning = {}
         product_uom_obj = self.pool.get('product.uom')
         partner_obj = self.pool.get('res.partner')
@@ -175,3 +175,78 @@ class sale_order_line(osv.osv):
         'qty_avl': fields.function(_get_real_stock, method=True, string='Real Stock'),
     }
 sale_order_line()
+
+
+class purchase_order_line(osv.osv):
+    _inherit="purchase.order.line"
+    
+    def product_id_change(self, cr, uid, ids, pricelist, product, qty, uom,
+            partner_id, date_order=False, fiscal_position=False, date_planned=False,
+            name=False, price_unit=False, notes=False):
+#        if not pricelist:
+#            raise osv.except_osv(_('No Pricelist !'), _('You have to select a pricelist or a supplier in the purchase form !\nPlease set one before choosing a product.'))
+#        if not  partner_id:
+#            raise osv.except_osv(_('No Partner!'), _('You have to select a partner in the purchase form !\nPlease set one partner before choosing a product.'))
+        if not product:
+            return {'value': {'price_unit': price_unit or 0.0, 'name': name or '',
+                'notes': notes or'', 'product_uom' : uom or False}, 'domain':{'product_uom':[]}}
+        res = {}
+        prod= self.pool.get('product.product').browse(cr, uid, product)
+
+        product_uom_pool = self.pool.get('product.uom')
+        lang=False
+        if partner_id:
+            lang=self.pool.get('res.partner').read(cr, uid, partner_id, ['lang'])['lang']
+        context={'lang':lang}
+        context['partner_id'] = partner_id
+
+        prod = self.pool.get('product.product').browse(cr, uid, product, context=context)
+        prod_uom_po = prod.uom_po_id.id
+        if not uom:
+            uom = prod_uom_po
+        if not date_order:
+            date_order = time.strftime('%Y-%m-%d')
+        qty = qty or 1.0
+        seller_delay = 0
+
+        prod_name = self.pool.get('product.product').name_get(cr, uid, [prod.id], context=context)[0][1]
+        res = {}
+        for s in prod.seller_ids:
+            if s.name.id == partner_id:
+                seller_delay = s.delay
+                if s.product_uom:
+                    temp_qty = product_uom_pool._compute_qty(cr, uid, s.product_uom.id, s.min_qty, to_uom_id=prod.uom_id.id)
+                    uom = s.product_uom.id #prod_uom_po
+                temp_qty = s.min_qty # supplier _qty assigned to temp
+                if qty < temp_qty: # If the supplier quantity is greater than entered from user, set minimal.
+                    qty = temp_qty
+                    res.update({'warning': {'title': _('Warning'), 'message': _('The selected supplier has a minimal quantity set to %s, you cannot purchase less.') % qty}})
+        qty_in_product_uom = product_uom_pool._compute_qty(cr, uid, uom, qty, to_uom_id=prod.uom_id.id)
+        price = self.pool.get('product.pricelist').price_get(cr,uid,[pricelist],
+                    product, qty_in_product_uom or 1.0, partner_id, {
+                        'uom': uom,
+                        'date': date_order,
+                        })[pricelist]
+        dt = (datetime.now() + relativedelta(days=int(seller_delay) or 0.0)).strftime('%Y-%m-%d %H:%M:%S')
+
+
+        res.update({'value': {'price_unit': price, 'name': prod_name,
+            'taxes_id':map(lambda x: x.id, prod.supplier_taxes_id),
+            'date_planned': date_planned or dt,'notes': notes or prod.description_purchase,
+            'product_qty': qty,
+            'product_uom': uom}})
+        domain = {}
+
+        taxes = self.pool.get('account.tax').browse(cr, uid,map(lambda x: x.id, prod.supplier_taxes_id))
+        fpos = fiscal_position and self.pool.get('account.fiscal.position').browse(cr, uid, fiscal_position) or False
+        res['value']['taxes_id'] = self.pool.get('account.fiscal.position').map_tax(cr, uid, fpos, taxes)
+
+        res2 = self.pool.get('product.uom').read(cr, uid, [uom], ['category_id'])
+        res3 = prod.uom_id.category_id.id
+        domain = {'product_uom':[('category_id','=',res2[0]['category_id'][0])]}
+        if res2[0]['category_id'][0] != res3:
+            raise osv.except_osv(_('Wrong Product UOM !'), _('You have to select a product UOM in the same category than the purchase UOM of the product'))
+
+        res['domain'] = domain
+        return res
+purchase_order_line()
