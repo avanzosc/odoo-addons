de9f1d1153d6ef94b3753cca480f76983f7ba8ac
dani-ds <danielcampos@avanzosc.com>
2011-12-29 14:26:12 +0100

    [ADD] crm_claim_add_seq

---
+++ crm_claim_add_seq/__openerp__.py
@@ -0,0 +1,41 @@
+
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution    
+#    Copyright (C) 2004-2010 Tiny SPRL (http://tiny.be). All Rights Reserved
+#    
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+##############################################################################
+{
+    "name": "crm_claim_add_seq",
+    "version": "1.0",
+    "depends": ["base",
+                "crm_claim"
+                ],
+    "author": "Iker Coranti for Avanzosc",
+    "category": "CRM & SRM",
+    "description": """
+    This module provide : Add sequence for claims
+    """,
+    "init_xml": [],
+    'update_xml': ["crm_claim_add_seq_view.xml",
+                   "claim_sequence.xml"],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+#    'certificate': 'certificate',
+}
