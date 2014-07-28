71e55817b15a359e4f7e9bf1fcf78b9b02265a55
oihane <oihanecruce@gmail.com>
2014-07-28 17:28:53 +0200

    [ADD] <project_task_sequence> new module

---
+++ project_task_sequence/__openerp__.py
@@ -0,0 +1,45 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2008-2014 AvanzOSC S.L. (Oihane) All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+##############################################################################
+
+{
+    "name": "Sequence for Tasks",
+    "version": "1.0",
+    "depends": [
+        "project",
+    ],
+    "author": "AvanzOSC",
+    "contributors": [
+        "Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>",
+    ],
+    "category": "Custom Module",
+    "website": "http://www.avanzosc.es",
+    "complexity": "normal",
+    "summary": "Task sequence generator",
+    "description": """
+    This module provide :
+    """,
+    "data": [
+        "data/task_sequence.xml",
+        "views/project_view.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
