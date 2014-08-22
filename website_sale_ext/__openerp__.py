f2d8618c37741a1627a6ee55e1c17a2c0f281056
oihane <oihanecruce@gmail.com>
2014-08-22 09:39:47 +0200

    Update __openerp__.py

--- website_sale_ext/__openerp__.py
+++ website_sale_ext/__openerp__.py
@@ -1,9 +1,9 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
 #
-#    OpenERP, Open Source Management Solution    
+#    OpenERP, Open Source Management Solution
 #    Copyright (C) 2008-2014 AvanzOSC S.L. (Oihane) All Rights Reserved
-#    
+#
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published
 #    by the Free Software Foundation, either version 3 of the License, or
