cf3ad7d4c7b17e81fb2f2707081b2cf6b83bb0e6
alfredo <alfredo@alfredo-Aspire-5738.(none)>
2015-02-04 11:15:20 +0100

    [IMP crm_claim_sequence]

--- crm_claim_sequence/__init__.py
+++ crm_claim_sequence/__init__.py
@@ -1,19 +1,16 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ##############################################################################
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see http://www.gnu.org/licenses/.
-#
+# For copyright and license notices, see __openerp__.py file in root directory
 ##############################################################################
-
 from . import models
+from openerp import SUPERUSER_ID
+
+
+def assign_old_sequences(cr, registry):
+    claim_obj = registry['crm.claim']
+    sequence_obj = registry['ir.sequence']
+    claim_ids = claim_obj.search(cr, SUPERUSER_ID, [], order="id")
+    for claim_id in claim_ids:
+        claim_obj.write(cr, SUPERUSER_ID, claim_id,
+                        {'sequence': sequence_obj.get(
+                            cr, SUPERUSER_ID, 'crm.claim')})
--- crm_claim_sequence/__openerp__.py
+++ crm_claim_sequence/__openerp__.py
@@ -1,6 +1,10 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
 #
+# Copyright (c)
+#    2015 Serv. Tec. Avanzados - Pedro M. Baeza (http://www.serviciosbaeza.com)
+#    2015 AvanzOsc (http://www.avanzosc.es)
+#
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published
 #    by the Free Software Foundation, either version 3 of the License, or
@@ -9,34 +13,32 @@
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+#    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see http://www.gnu.org/licenses/.
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-
 {
     "name": "Sequence for Claims",
     "version": "1.0",
+    "author": "OdooMRP team",
+    "website": "http://www.odoomrp.com",
+    "contributors": [
+        "Pedro M. Baeza <pedro.baeza@serviciosbaeza.com",
+        "Ana Juaristi <ajuaristo@gmail.com>",
+        "Iker Coranti <ikercoranti@avanzosc.com>",
+        "Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>",
+        "Alfredo de la Fuente <alfredodelafuente@avanzosc.es>",
+    ],
+    "category": "CRM & SRM",
     "depends": [
         "crm",
     ],
-    "author": "Avanzosc, S.L.",
-    "contributors": [
-        "Iker Coranti <ikercoranti@avanzosc.com>",
-        "Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>",
-    ],
-    "category": "CRM & SRM",
-    "website": "http://www.avanzosc.es",
-    "summary": "Claim sequence generator",
-    "description": """
-    This module adds a sequence for claims
-    """,
     "data": [
         "views/crm_claim_view.xml",
         "data/claim_sequence.xml",
     ],
     "installable": True,
-    "auto_install": False,
+    "post_init_hook": "assign_old_sequences",
 }
