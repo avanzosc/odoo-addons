b9c50237869757e21ade776fec27576bfdb28c70
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-03-25 20:48:43 +0100

    [IMP] Expand AUTHORS

--- sale_order_line_view/__openerp__.py
+++ sale_order_line_view/__openerp__.py
@@ -22,7 +22,9 @@
     "depends": [
         "sale",
     ],
-    "author": "OdooMRP team",
+    "author": "OdooMRP team,"
+              "AvanzOSC,"
+              "Serv. Tecnol. Avanzados - Pedro M. Baeza",
     "contributors": [
         "Mikel Arregi <mikelarregi@avanzosc.es>"
         "Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>",
