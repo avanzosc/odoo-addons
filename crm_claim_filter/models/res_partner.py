f70dc1ae629a2c8d1de39a56cf85b0d48603556c
oihane <oihanecruce@gmail.com>
2015-12-16 16:56:04 +0100

    [FIX] crm_claim_filter: fixed name_search behaviour

--- crm_claim_filter/models/res_partner.py
+++ crm_claim_filter/models/res_partner.py
@@ -1,21 +1,24 @@
 # -*- coding: utf-8 -*-
 # (c) 2015 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from openerp import models, api
+
+from openerp import models
 
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    @api.model
-    def name_search(self, name='', args=None, operator='ilike', limit=100):
+    def name_search(self, cr, uid, name, args=None, operator='ilike',
+                    context=None, limit=100):
         if args is None:
             args = []
         partners = super(ResPartner, self).name_search(
-            name, args=args, operator=operator, limit=limit)
+            cr, uid, name, args=args, operator=operator, context=context,
+            limit=limit)
         ids = [x[0] for x in partners]
         if name:
-            ids2 = self.search(
-                [('vat', operator, name)] + args, limit=limit).ids
-            ids = list(set(ids + ids2))
-        return self.browse(ids).name_get()
+            ids = self.search(cr, uid,
+                              ['|', ('id', 'in', ids),
+                               ('vat', operator, name)],
+                              limit=limit, context=context)
+        return self.name_get(cr, uid, ids, context=context)
