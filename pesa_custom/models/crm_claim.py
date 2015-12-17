547b1c0f9a2698703258365b191bbb0b50361c12
esthermm <esthermartin001@gmail.com>
2015-12-17 11:49:41 +0100

    [MOD] pesa_custom: travis required changes, added test

--- pesa_custom/models/crm_claim.py
+++ pesa_custom/models/crm_claim.py
@@ -12,7 +12,7 @@ class CrmClaim(models.Model):
     real_line_id = fields.Many2one(comodel_name='real.line',
                                    string='Real line')
     journey_id = fields.Many2one(comodel_name='journey',
-                                    string='Journey')
+                                 string='Journey')
     schedule_id = fields.Many2one(comodel_name='schedule', string='Schedule')
     driver_id = fields.Many2one(comodel_name='res.users', string='Driver')
     book_number = fields.Integer(string='Book number')
