089cc5f23dbf8ca5caff85d22010cde3968e4856
esthermm <esthermartin001@gmail.com>
2015-12-18 11:32:05 +0100

    [ADD] New module crm_claim_call

---
+++ crm_claim_call/README.rst
@@ -0,0 +1,20 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+==============
+Crm Claim Call
+==============
+
+This module creates a shortcut on claims to access it's own phonecalls. It also
+creates the inverse shortcut in phonecalls so that if there is not claim, there
+will be created a new one taking partner and phone from the phonecall object.
+
+Credits
+=======
+
+
+Contributors
+------------
+* Esther Martín <esthermartin@avanzosc.es>
+* Ana Juaristi <anajuaristi@avanzosc.es>
---
+++ crm_claim_call/__openerp__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# (c) 2015 Esther Martín - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Crm Claim Call",
+    "version": "8.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "contributors": [
+        "Esther Martín <esthermartin@avanzosc.es>",
+        "Ana Juaristi <anajuaristi@avanzosc.es>",
+    ],
+    "depends": [
+        "crm",
+        "crm_claim",
+    ],
+    "category": "Customer Relationship Management",
+    "data": [
+        "views/crm_claim_view.xml",
+        "views/crm_phonecall_view.xml",
+    ],
+    "installable": True
+}
