03ec4f81d2b0cb23cb1093c8d2d47053a606060f
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2016-01-11 16:38:34 +0100

    [ADD] event_registration_partner: show the partner in event registrations.

---
+++ event_registration_partner/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# (c) 2015 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Event Registration Partner",
+    "version": "8.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Avanzosc, S.L.",
+    "website": "http://www.avanzosc.es",
+    "contributors": [
+        "Ana Juaristi <anajuaristi@avanzosc.es>",
+        "Alfredo de la Fuente <alfredodelafuente@avanzosc.es>",
+    ],
+    "category": "Event Management",
+    "depends": [
+        "event",
+    ],
+    "data": [
+        "views/event_view.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
