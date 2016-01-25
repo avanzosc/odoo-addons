11a58924c204cb80cde1e139632577c702c17c66
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2016-01-25 14:41:15 +0100

    [ADD] sale_service_recurrence_configurator: Define whether a product is recurrent, and defined sales order lines, months, weeks and days.

---
+++ sale_service_recurrence_configurator/__openerp__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# (c) 2016 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    'name': 'Sale Service Recurrence Configurator',
+    'version': '8.0.1.1.0',
+    'category': 'Website',
+    'license': "AGPL-3",
+    'author': "AvanzOSC",
+    'website': "http://www.avanzosc.es",
+    'contributors': [
+        "Ana Juaristi <anajuaristi@avanzosc.es>",
+        "Alfredo de la Fuente <alfredodelafuente@avanzosc.es",
+    ],
+    'depends': [
+        'product',
+        'website_quote',
+    ],
+    "data": [
+        "views/sale_quote_view.xml",
+        "views/sale_order_view.xml",
+        "views/product_product_view.xml",
+    ],
+    "installable": True,
+}
---
+++ sale_service_recurrence_configurator/models/product_product.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# (c) 2016 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+from openerp import models, fields
+
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    recurring_service = fields.Boolean(
+        string='Recurring Service', default=False)
