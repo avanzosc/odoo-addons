9730be1062aaf75d0c485fec8ca55f99b420c3b2
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2016-02-15 16:49:27 +0100

    [ADD] purchase_order_line_with_sale_account: The contract of the sale order will be, to the line of the purchase order.

---
+++ purchase_order_line_with_sale_account/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# (c) 2016 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Purchase Order Line With Sale Account",
+    'version': '8.0.1.1.0',
+    'license': "AGPL-3",
+    'author': "AvanzOSC",
+    'website': "http://www.avanzosc.es",
+    'contributors': [
+        "Ana Juaristi <anajuaristi@avanzosc.es>",
+        "Alfredo de la Fuente <alfredodelafuente@avanzosc.es",
+    ],
+    "category": "Procurements",
+    "depends": [
+        'sale',
+        'procurement_purchase_no_grouping',
+    ],
+    "data": [],
+    "installable": True,
+}
