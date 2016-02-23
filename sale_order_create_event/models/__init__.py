9b6e318c643357ed17d3d061c8c348f6de9dc9f7
alfredo <alfredo@alfredo-Aspire-5738.(none)>
2016-02-23 20:08:53 +0100

    [FIX] sale_order_create_event: Fix programing.

--- sale_order_create_event/models/__init__.py
+++ sale_order_create_event/models/__init__.py
@@ -3,4 +3,5 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 from . import sale_order
 from . import project
-from . import event_track
+from . import event
+from . import res_partner
