c6f8cdb51ffd399b3bc32b5e64a919eebd9823f7
alfredo <alfredo@alfredo-Aspire-5738.(none)>
2016-02-23 21:02:51 +0100

    [FIX] sale_order_create_event: Fix programing.

--- sale_order_create_event/tests/test_sale_order_create_event.py
+++ sale_order_create_event/tests/test_sale_order_create_event.py
@@ -10,15 +10,16 @@ class TestSaleOrderCreateEvent(common.TransactionCase):
         super(TestSaleOrderCreateEvent, self).setUp()
         self.task_model = self.env['project.task']
         self.sale_model = self.env['sale.order']
+        self.account_model = self.env['account.analytic.account']
+        self.project_model = self.env['project.project']
         self.procurement_model = self.env['procurement.order']
         account_vals = {'name': 'account procurement service project',
                         'date_start': '2016-01-15',
                         'date': '2016-02-28'}
-        self.account = self.env['account.analytic.account'].create(
-            account_vals)
+        self.account = self.account_model.create(account_vals)
         project_vals = {'name': 'project procurement service project',
                         'analytic_account_id': self.account.id}
-        self.project = self.env['project.project'].create(project_vals)
+        self.project = self.project_model.create(project_vals)
         service_product = self.env.ref('product.product_product_consultant')
         service_product.write({'performance': 5.0,
                                'recurring_service': True})
@@ -27,6 +28,7 @@ class TestSaleOrderCreateEvent(common.TransactionCase):
             (6, 0,
              [self.ref('procurement_service_project.route_serv_project')])]
         sale_vals = {
+            'name': 'sale order 1',
             'partner_id': self.ref('base.res_partner_1'),
             'partner_shipping_id': self.ref('base.res_partner_1'),
             'partner_invoice_id': self.ref('base.res_partner_1'),
@@ -49,6 +51,17 @@ class TestSaleOrderCreateEvent(common.TransactionCase):
             'thursday': True}
         sale_vals['order_line'] = [(0, 0, sale_line_vals)]
         self.sale_order = self.sale_model.create(sale_vals)
+        account_vals = {'name': 'account procurement service project 2',
+                        'date_start': '2016-01-15',
+                        'date': '2016-02-28'}
+        self.account2 = self.account_model.create(account_vals)
+        project_vals = {'name': 'project procurement service project 2',
+                        'analytic_account_id': self.account2.id}
+        self.project2 = self.project_model.create(project_vals)
+        sale_vals.update({'name': 'sale order 2',
+                          'project_id': self.account2.id,
+                          'project_by_task': 'yes'})
+        self.sale_order2 = self.sale_model.create(sale_vals)
 
     def test_sale_order_create_event(self):
         self.sale_order.action_button_confirm()
@@ -58,3 +71,12 @@ class TestSaleOrderCreateEvent(common.TransactionCase):
         self.assertNotEqual(
             len(self.project.tasks[0].sessions), 0,
             'Sessions no generated')
+
+    def test_sale_order_create_event_project_by_task(self):
+        self.sale_order2.action_button_confirm()
+        self.project2.tasks[0]._calc_num_sessions()
+        self.project2.tasks[0].show_sessions_from_task()
+        self.project2.tasks[0].button_recalculate_sessions()
+        self.assertNotEqual(
+            len(self.project2.tasks[0].sessions), 0,
+            'Sessions no generated 2')
