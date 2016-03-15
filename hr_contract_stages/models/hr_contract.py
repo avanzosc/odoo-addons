bc5fc7929f9d59c1ebf54f80a6fb1374c3026adb
Esther Martín <esthermartin001@gmail.com>
2016-03-15 13:27:58 +0100

    [ADD} hr_contract_stages: new module

---
+++ hr_contract_stages/models/hr_contract.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# (c) 2015 Esther Martín - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from openerp import api, fields, models
+
+
+class HrContract(models.Model):
+    _inherit = 'hr.contract'
+
+    @api.multi
+    def _default_stage_id(self):
+        stages = self.env['hr.contract.stage'].search([])
+        return stages and stages[0] or False
+
+    contract_stage_id = fields.Many2one(
+        comodel_name='hr.contract.stage', string='Stage',
+        default=_default_stage_id)
