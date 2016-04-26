df7bef911196203792780a7e557c4b5f36f6a288
Esther Martín <esthermartin001@gmail.com>
2016-04-26 11:56:06 +0200

    [ADD] new module: stock_account_payment_term

---
+++ stock_account_payment_term/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# © 2015 Esther Martín - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Stock Account Payment Term",
+    "summary": "",
+    "version": "8.0.1.0.0",
+    "category": "Custom Module",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "contributors": [
+        "Esther Martín <esthermartin@avanzosc.es>",
+    ],
+    "depends": [
+        "stock_account",
+    ],
+    "data": [
+    ],
+    "installable": True,
+}
