9fffcbb58dc6cbb1c2ef534949747edf789ae2ed
oihane <oihanecruce@gmail.com>
2016-05-16 11:41:18 +0200

    [IMP] mrp_bom_structure_show_child_with_manual_standard_cost: new dependency

--- mrp_bom_structure_show_child_with_manual_standard_cost/models/mrp_bom_line.py
+++ mrp_bom_structure_show_child_with_manual_standard_cost/models/mrp_bom_line.py
@@ -1,26 +1,34 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in root directory
-##############################################################################
-from openerp import models, fields, api
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+from openerp import api, fields, models
 import openerp.addons.decimal_precision as dp
 
 
 class MrpBomLine(models.Model):
     _inherit = 'mrp.bom.line'
 
-    @api.one
+    @api.multi
     def _compute_manual_standard_cost(self):
-        self.manual_standard_cost = (
-            self.product_id.manual_standard_cost or
-            self.product_template.manual_standard_cost)
+        for line in self:
+            if line.product_id:
+                line.manual_standard_cost =\
+                    line.product_id.manual_standard_cost
+            else:
+                try:
+                    line.manual_standard_cost =\
+                        line.product_tmpl_id.manual_standard_cost
+                except:
+                    line.manual_standard_cost = 0.0
 
-    @api.one
+    @api.depends('child_line_ids')
     def _compute_childs_manual_standard_cost(self):
-        self.childs_manual_standard_cost = 0
-        for line in self.child_line_ids:
-            self.childs_manual_standard_cost += (
-                line.manual_standard_cost + line.childs_manual_standard_cost)
+        for bom_line in self:
+            childs_manual_standard_cost = 0.0
+            for line in bom_line.child_line_ids:
+                childs_manual_standard_cost += (
+                    line.manual_standard_cost +
+                    line.childs_manual_standard_cost)
+            bom_line.childs_manual_standard_cost = childs_manual_standard_cost
 
     manual_standard_cost = fields.Float(
         string='Manual Standard Cost', compute='_compute_manual_standard_cost',
