b96539a716363b4b52eea8d70ac4abb2016c2f9b
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2016-05-24 16:22:32 +0200

    [ADD] qc_inspection_from_event: Go to inspections from event form

---
+++ qc_inspection_from_event/__openerp__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# © 2016 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Qc Inspection From Event",
+    "version": "8.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "contributors": [
+        "Ana Juaristi <anajuaristi@avanzosc.es>",
+        "Alfredo de la Fuente <alfredodelafuente@avanzosc.es",
+    ],
+    "category": "Quality control",
+    "depends": [
+        "event",
+        "quality_control",
+    ],
+    "data": [
+        "data/qc_inspection_from_event_data.xml",
+        "views/event_event_view.xml",
+    ],
+    "installable": True,
+}
