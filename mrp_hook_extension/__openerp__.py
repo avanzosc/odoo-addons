ab8a85038a2f995124ad1ec8ae4c122feba0255d
mikelarre <mikelarregi@avanzosc.es>
2016-07-21 16:17:19 +0200

    [ADD] mrp_hook_extension

---
+++ mrp_hook_extension/__openerp__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# © 2015 Mikel Arregi Etxaniz - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "MRP Hooks Extension",
+    "version": "1.0",
+    "depends": [
+        "mrp_hook",
+    ],
+    "author": "OdooMRP team, "
+              "AvanzOSC, "
+              "Serv. Tecnol. Avanzados - Pedro M. Baeza",
+    "website": "http://www.odoomrp.com",
+    "contributors": [
+        "Mikel Arregi <mikelarregi@avanzosc.es>",
+        "Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>",
+        "Ana Juaristi <anajuaristi@avanzosc.es>",
+    ],
+    "category": "Hidden",
+    "summary": "",
+    "data": [
+         
+    ],
+    "installable": True,
+}
