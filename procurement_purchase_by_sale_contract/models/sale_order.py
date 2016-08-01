9f0589ee0c8f1b8b9e565d4c8c356f7594566c29
oihane <oihanecruce@gmail.com>
2016-08-01 17:14:26 +0200

    [IMP] procurement_purchase_by_sale_contract: project only required when product category is grouped by sale contract (#257)

--- procurement_purchase_by_sale_contract/models/sale_order.py
+++ procurement_purchase_by_sale_contract/models/sale_order.py
@@ -10,6 +10,21 @@ class SaleOrder(models.Model):
 
     @api.multi
     def action_button_confirm(self):
-        if not self.project_id:
-            raise exceptions.Warning(_('You must enter the project/contract'))
+        for order in self:
+            groupings = order.mapped(
+                'order_line.product_id.categ_id.procured_purchase_grouping')
+            try:
+                groupings |= order.mapped(
+                    'order_line.product_tmpl_id.categ_id.'
+                    'procured_purchase_grouping')
+            except:
+                pass
+            check_project = False
+            for grouping in groupings:
+                check_project = grouping == 'sale_contract'
+                if check_project:
+                    break
+            if check_project and not order.project_id:
+                raise exceptions.Warning(
+                    _('You must enter the project/contract'))
         return super(SaleOrder, self).action_button_confirm()
