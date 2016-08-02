d2c205bd536f9413e0f7dce236d006b80f7753e4
mikelarre <mikelarregi@avanzosc.es>
2016-08-02 10:15:51 +0200

    [ADD] New module mrp_supplier_price

---
+++ mrp_supplier_price/__openerp__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# © 2016 Mikel Arregi Etxaniz - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Mrp Supplier Price",
+    "version": "8.0.2.0.2",
+    "license": "AGPL-3",
+    "depends": [
+        "mrp",
+    ],
+    "author": "OdooMRP team, "
+              "AvanzOSC, ",
+    "website": "http://www.odoomrp.com",
+    "contributors": [
+        "Mikel Arregi <mikelarregi@avanzosc.es>",
+        "Ana Juaristi <anajuaristi@avanzosc.es>",
+        "Odoo Community Association (OCA)",
+    ],
+    "category": "",
+    "summary": "",
+    "data": [
+             "views/mrp_production_view.xml"
+    ],
+    "installable": True,
+}
