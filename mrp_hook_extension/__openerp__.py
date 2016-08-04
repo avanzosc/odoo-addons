7e470e4f6d3214309c5b9462a3b7e2890e87b11f
mikelarre <mikelarregi@avanzosc.es>
2016-08-04 16:21:27 +0200

    [ADD] New module mrp_hook_extension

---
+++ mrp_hook_extension/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# © 2015 Mikel Arregi Etxaniz - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "MRP Hooks Extension",
+    "version": "8.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "mrp_hook",
+    ],
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "contributors": [
+        "Mikel Arregi <mikelarregi@avanzosc.es>",
+        "Ana Juaristi <anajuaristi@avanzosc.es>",
+    ],
+    "category": "Hidden",
+    "data": [
+    ],
+    "installable": True,
+}
