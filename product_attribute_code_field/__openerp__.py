10b979fe82bcae7c873af046a1adbd78e146cdf5
mikelarre <mikelarregi@avanzosc.es>
2016-08-04 16:05:55 +0200

    [ADD] New module product_attribute_code_field

---
+++ product_attribute_code_field/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# © 2016 Mikel Arregi Etxaniz - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Product attribute code field",
+    "version": "8.0.2.0.2",
+    "license": "AGPL-3",
+    "depends": [
+        "product",
+    ],
+    "author": "OdooMRP team, "
+              "AvanzOSC, "
+              "Serv. Tecnol. Avanzados - Pedro M. Baeza",
+    "website": "http://www.odoomrp.com",
+    "contributors": [
+        "Mikel Arregi <mikelarregi@avanzosc.es>",
+        "Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>",
+        "Ana Juaristi <anajuaristi@avanzosc.es>",
+    ],
+    "category": "product",
+    "summary": "",
+    "data": ["views/product_attribute_view.xml"],
+    "installable": True,
+}
