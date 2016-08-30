707c83c66a5923b07b50e98b0970f964d199796e
oihane <oihanecruce@gmail.com>
2016-08-30 13:45:39 +0200

    [ADD] New module quality_control_stock_inventory (#283)

---
+++ quality_control_stock_inventory/__openerp__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# © 2016 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Quality Control - Stock Inventory",
+    "version": "8.0.1.0.0",
+    "category": "Quality control",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "contributors": [
+        "Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>",
+        "Ana Juaristi <anajuaristi@avanzosc.es>",
+    ],
+    "depends": [
+        "quality_control",
+        "quality_control_stock",
+    ],
+    "data": [
+        "data/quality_control_stock_inventory_data.xml",
+        "views/stock_inventory_view.xml",
+    ],
+    "installable": True,
+}
