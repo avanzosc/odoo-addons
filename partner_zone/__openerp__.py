7f00b10e306100c1ef1701d256a96f0db15261dd
Esther Martín <esthermartin001@gmail.com>
2016-09-05 12:56:02 +0200

    [MOD] rename module

---
+++ partner_zone/__openerp__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# © 2016 Esther Martín - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Zone for partner",
+    "version": "8.0.1.0.0",
+    "category": "Generic Modules",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "contributors": [
+        "Esther Martín <esthermartin@avanzosc.es>",
+        "Ana Juaristi <anajuaristi@avanzosc.es>",
+    ],
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "views/partner_view.xml",
+        "security/ir.model.access.csv",
+    ],
+    "demo": [
+        "demo/zone_demo.xml"
+    ],
+    "installable": True,
+}
