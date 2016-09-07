de0234b1d89eea0c4de6bbff6ccfb7ee30df9bf1
agaldona <ainaragaldona@avanzosc.es>
2016-09-07 13:46:46 +0200

    [ADD] stock_picking_transfer_limit: new module to control the transfer line qty

---
+++ stock_picking_transfer_limit/wizard/stock_transfer_details.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# (c) 2016 Ainara Galdona - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from openerp import models, fields, api, _
+
+
+class StockTransferDetails(models.TransientModel):
+
+    _inherit = 'stock.transfer_details'
+
+    @api.model
+    def default_get(self, fields):
+        res = super(StockTransferDetails, self).default_get(fields=fields)
+        for item in res.get('item_ids', []):
+            item['origin_qty'] = item.get('quantity', 0)
+        return res
+
+
+class StockTransferDetailsItems(models.TransientModel):
+
+    _inherit = 'stock.transfer_details_items'
+
+    origin_qty = fields.Float(string="Origin qty")
+
+    @api.multi
+    @api.onchange('quantity')
+    def onchange_quantity(self):
+        self.ensure_one()
+        res = {}
+        if self.quantity > self.origin_qty:
+            self.quantity = self.origin_qty
+            res = {'warning': {
+                'title': _('Error in quantity'),
+                'message': (_('The quantity must be lower than %s') %
+                            self.origin_qty)}}
+        return res
