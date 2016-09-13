18690f2b3d5ff405700fe70b707f6b80773d3cec
Esther Martín <esthermartin001@gmail.com>
2016-09-13 12:26:20 +0200

    [MOD] hr_calendar_zone: remove domain + test, add group by

--- hr_calendar_zone/models/resource_calendar.py
+++ hr_calendar_zone/models/resource_calendar.py
@@ -2,7 +2,7 @@
 # © 2016 Esther Martín - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import api, fields, models
+from openerp import fields, models
 
 
 class ResourceCalendarAttendance(models.Model):
@@ -12,10 +12,3 @@ class ResourceCalendarAttendance(models.Model):
         comodel_name='res.partner.zone', string='Zone')
     employee_id = fields.Many2one(
         comodel_name='hr.employee', string='Employee')
-
-    @api.onchange('employee_id')
-    def onchange_employee_id(self):
-        if self.employee_id:
-            return {'domain': {'zone_id': [
-                    ('id', 'in',
-                     self.employee_id.address_home_id.zone_ids.ids)]}}
