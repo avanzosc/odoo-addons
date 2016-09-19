c2d8d6cb6fb595c9408a4ace04724ab59c69f481
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2016-09-19 15:11:15 +0200

    [ADD] stock_information_sale_forecast: Show sale forectas in stock information.

---
+++ stock_information_sale_forecast/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# (c) 2016 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Stock Information Sale Forecast",
+    "version": "8.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "contributors": [
+        "Ana Juaristi <anajuaristi@avanzosc.es>",
+        "Alfredo de la Fuente <alfredodelafuente@avanzosc.es>",
+    ],
+    'category': 'Warehouse Management',
+    'depends': ['stock_information',
+                'procurement_sale_forecast',
+                ],
+    "data": [
+        "views/stock_information_view.xml",
+    ],
+    'installable': True,
+}
---
+++ stock_information_sale_forecast/models/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# (c) 2016 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+from . import stock_information
+from . import procurement_sale_forecast_line
