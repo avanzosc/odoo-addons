a97c5eb4c93eb33dc165f921196ccfc91c83b08a
oihane <oihanecruce@gmail.com>
2016-10-31 16:28:09 +0100

    [ADD] New module project_task_tag

---
+++ project_task_tag/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# © 2016 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Project Task Tags",
+    "version": "8.0.1.0.0",
+    "category": "Project Management",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "contributors": [
+        "Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>",
+        "Ana Juaristi <anajuaristi@avanzosc.es>",
+    ],
+    "depends": [
+        "project",
+    ],
+    "data": [
+        "views/project_task_view.xml",
+    ],
+    "installable": True,
+}
