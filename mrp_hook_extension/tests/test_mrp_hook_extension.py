3ce5f36aee59825e5b39198f31f8f838f92b9fce
oihane <oihanecruce@gmail.com>
2016-11-02 15:14:47 +0100

    [IMP] mrp_hook_extension: add test extending mrp_hook tests

---
+++ mrp_hook_extension/tests/test_mrp_hook_extension.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# © 2016 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from openerp.addons.mrp_hook.tests.test_mrp_hook import TestMrpHook
+
+
+class TestMrpHookExtension(TestMrpHook):
+
+    def setUp(self):
+        super(TestMrpHookExtension, self).setUp()
