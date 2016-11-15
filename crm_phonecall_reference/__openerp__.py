409fa79541cf68ad683a6e3ba482d81d80ee5def
Esther Martín <esthermartin001@gmail.com>
2016-11-15 11:19:09 +0100

    [ADD] new module crm_phonecall_reference

---
+++ crm_phonecall_reference/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# (c) 2016 Esther Martín - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "References on phonecalls",
+    "version": "8.0.1.0.0",
+    "author": "AvanzOSC",
+    "website": "www.avanzosc.es",
+    "category": "Customer Relationship Management",
+    "license": "AGPL-3",
+    "contributors": [
+        "Esther Martín <esthermartin@avanzosc.es>",
+        "Ana Juaristi <anajuaristi@avanzosc.es>",
+        ],
+    "depends": [
+        "crm_claim"
+        ],
+    "data": [
+        "views/crm_claim_view.xml"],
+    "installable": True
+}
