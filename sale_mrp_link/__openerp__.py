6f8bc9a5cf97f32793357b0113740f9d6609d961
oihane <oihanecruce@gmail.com>
2016-11-18 12:51:23 +0100

    [FIX] sale_mrp_link: error writting uom qty in sale order lines

--- sale_mrp_link/__openerp__.py
+++ sale_mrp_link/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
-    "name": "Sale Mrp Link",
+    "name": "Sale MRP Link",
     "version": "8.0.1.0.0",
     "category": "Sales Management",
     "license": "AGPL-3",
