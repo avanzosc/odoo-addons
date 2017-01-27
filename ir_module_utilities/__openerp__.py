716deacbf53bf928154f55ca9dc04e94922e565c
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2017-01-27 12:15:15 +0100

    [ADD] ir_module_module: Group modules by state.

---
+++ ir_module_utilities/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# (c) 2017 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Module utilities",
+    "version": "8.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "contributors": [
+        "Ana Juaristi <anajuaristi@avanzosc.es>",
+        "Alfredo de la Fuente <alfredodelafuente@avanzosc.es>",
+    ],
+    "category": "Hidden",
+    "depends": [
+        "base"
+    ],
+    "data": [
+        "views/ir_module_view.xml",
+    ],
+    "installable": True,
+}
