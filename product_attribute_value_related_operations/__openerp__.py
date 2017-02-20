58ce8c059c8985c7525ee9a17e752f9e0c049084
mikelarre <mikelarregi@avanzosc.es>
2017-02-20 16:48:00 +0100

    [ADD] product_attribute_value_related_operations

---
+++ product_attribute_value_related_operations/__openerp__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# © 2017 Mikel Arregi Etxaniz - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Product value related operations",
+    "version": "8.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "mrp_operations_extension",
+    ],
+    "author": "OdooMRP team, "
+              "AvanzOSC, "
+              "Odoo Community Association (OCA)",
+    "website": "http://www.odoomrp.com",
+    "contributors": [
+        "Mikel Arregi <mikelarregi@avanzosc.es>",
+        "Ana Juaristi <anajuaristi@avanzosc.es>",
+    ],
+    "category": "",
+    "summary": "",
+    "data": [
+        "views/product_attribute_value_view.xml",
+    ],
+    "installable": True,
+}
