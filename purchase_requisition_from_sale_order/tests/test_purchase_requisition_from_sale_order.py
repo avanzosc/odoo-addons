6119cc1647f706f47ee12e28aa5d29639d89e8bc
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2017-03-01 09:03:28 +0100

    [IMP] purchase_requisition_from_sale_order: Remove the "Calls for bids" tab in sales orders, and create a shortcut.

--- purchase_requisition_from_sale_order/tests/test_purchase_requisition_from_sale_order.py
+++ purchase_requisition_from_sale_order/tests/test_purchase_requisition_from_sale_order.py
@@ -25,6 +25,10 @@ class TestPurchaseRequisitionFromSaleOrder(common.TransactionCase):
 
     def test_purchase_requisition_from_sale_order(self):
         self.sale_order.button_create_purchase_requision_from_sale_order()
+        self.sale_order._compute_count_requisitions()
+        self.assertEqual(
+            self.sale_order.count_requisitions, 1,
+            'Purchase requisition not generated')
         self.assertNotEqual(len(self.sale_order.purchase_requisition_ids),
                             0, 'Purchase requisition no generated')
         requisition = self.sale_order.purchase_requisition_ids[0]
@@ -36,3 +40,8 @@ class TestPurchaseRequisitionFromSaleOrder(common.TransactionCase):
             requisition.line_ids[0].sale_order_line_id.id,
             self.sale_order.order_line[0].id,
             'Purchase requisition line without sale order line info')
+        result = self.sale_order.show_calls_for_bids()
+        domain = result.get('domain')
+        self.assertEqual(
+            domain[0][2],
+            self.sale_order.mapped('purchase_requisition_ids').ids)
