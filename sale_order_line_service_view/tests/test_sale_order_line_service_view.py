d008e05683fa24b8ad0dbb9290083232b9ea34bc
oihane <oihanecruce@gmail.com>
2017-05-08 10:59:43 +0200

    [IMP] sale_order_line_service_view: get type from product and make compatible with variants

--- sale_order_line_service_view/tests/test_sale_order_line_service_view.py
+++ sale_order_line_service_view/tests/test_sale_order_line_service_view.py
@@ -9,16 +9,7 @@ class TestSaleOrderLineServiceView(common.TransactionCase):
     def setUp(self):
         super(TestSaleOrderLineServiceView, self).setUp()
         self.sale_model = self.env['sale.order']
-        self.account_model = self.env['account.analytic.account']
-        self.project_model = self.env['project.project']
         self.wiz_model = self.env['wiz.delete.sale.line']
-        account_vals = {'name': 'sale order line sevice view',
-                        'date_start': '2025-01-15',
-                        'date': '2025-02-28'}
-        self.account = self.account_model.create(account_vals)
-        project_vals = {'name': 'project 1',
-                        'analytic_account_id': self.account.id}
-        self.project = self.project_model.create(project_vals)
         service_product = self.env.ref('product.product_product_consultant')
         sale_vals = {
             'name': 'sale order 1',
@@ -26,9 +17,8 @@ class TestSaleOrderLineServiceView(common.TransactionCase):
             'partner_shipping_id': self.ref('base.res_partner_1'),
             'partner_invoice_id': self.ref('base.res_partner_1'),
             'pricelist_id': self.env.ref('product.list0').id,
-            'project_id': self.account.id}
+        }
         sale_line_vals = {
-            'partner_id': self.ref('base.res_partner_1'),
             'product_id': service_product.id,
             'name': service_product.name,
             'product_uom_qty': 7,
@@ -40,12 +30,17 @@ class TestSaleOrderLineServiceView(common.TransactionCase):
 
     def test_sale_order_line_service_view(self):
         wiz = self.wiz_model.with_context(
-            {'active_ids': [self.sale_order.id]}).create({})
-        vals = ['lines']
-        wiz.with_context(
-            {'active_ids': [self.sale_order.id]}).default_get(vals)
-        for line in wiz.lines:
-            line.delete_record = True
+            active_ids=self.sale_order.ids).create({})
+        wiz.lines.write({'delete_record': True})
         wiz.button_delete_sale_lines()
         self.assertEqual(
             len(self.sale_order.order_line), 0, 'Sale order with line')
+
+    def test_sale_order_line_service(self):
+        self.assertEquals(len(self.sale_order.order_line), 1)
+        for line in self.sale_order.order_line:
+            line.invalidate_cache()
+        self.assertEquals(self.sale_order.order_line,
+                          self.sale_order.service_order_line |
+                          self.sale_order.no_service_order_line)
+        self.assertEquals(len(self.sale_order.service_order_line), 1)
