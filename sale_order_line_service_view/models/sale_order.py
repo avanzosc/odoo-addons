44c29f554683d9ba73a7ee24e55136a0b7f9920d
oihane <oihanecruce@gmail.com>
2017-05-09 09:50:33 +0200

    [IMP] sale_order_line_service_view: get type from product and make compatible with variants (#454)

--- sale_order_line_service_view/models/sale_order.py
+++ sale_order_line_service_view/models/sale_order.py
@@ -25,9 +25,19 @@ class SaleOrderLine(models.Model):
     @api.depends('product_id')
     def _compute_product_type(self):
         for line in self:
-            line.product_type = ''
             if line.product_id:
                 line.product_type = line.product_id.type
+            else:
+                try:
+                    line.product_type = line.product_tmpl_id.type
+                except:
+                    pass
 
-    product_type = fields.Char(
-        string='Product type', compute='_compute_product_type', store=True)
+    @api.model
+    def _get_selection_product_type(self):
+        return self.env['product.product'].fields_get(
+            allfields=['type'])['type']['selection']
+
+    product_type = fields.Selection(
+        selection='_get_selection_product_type', string='Product type',
+        compute='_compute_product_type', store=True)
