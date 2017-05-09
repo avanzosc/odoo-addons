d0a521772b57225a3ded29314492a862b0bb6f5a
oihane <oihanecruce@gmail.com>
2017-05-09 16:54:57 +0200

    [REM] hr_* modules moved to hr-addons (#462)

--- hr_timesheet_invoice_by_partner/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 Ainara Galdona - AvanzOSC
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-
-from . import account_analytic_line
-from . import res_config
