230c77c70e426596a1464d8b205c5b96a69d1503
oihane <oihanecruce@gmail.com>
2017-05-23 12:58:50 +0200

    [IMP] product_training_plan: add code to product plan view and plan name search (#472)

--- product_training_plan/models/product.py
+++ product_training_plan/models/product.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-# (c) 2017 Alfredo de la Fuente - AvanzOSC
+# Copyright (c) 2017 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from openerp import fields, models
+from openerp import api, fields, models
 
 
 class ProductTemplate(models.Model):
@@ -9,8 +9,7 @@ class ProductTemplate(models.Model):
 
     product_template_training_ids = fields.One2many(
         comodel_name='product.training.plan', copy=True,
-        inverse_name='product_tmpl_id', string='Training plan',
-        domain=[('product_id', '=', False)])
+        inverse_name='product_tmpl_id', string='Training plans')
 
 
 class ProductProduct(models.Model):
@@ -18,7 +17,18 @@ class ProductProduct(models.Model):
 
     product_training_ids = fields.One2many(
         comodel_name='product.training.plan', inverse_name='product_id',
-        string='Training plan', copy=True)
+        string='Training plans', copy=True)
+    template_training_ids = fields.Many2many(
+        comodel_name='product.training.plan', string='Template training plans',
+        compute='_compute_template_training_ids')
+
+    @api.depends('product_tmpl_id',
+                 'product_tmpl_id.product_template_training_ids')
+    def _compute_template_training_ids(self):
+        for record in self:
+            record.template_training_ids =\
+                record.product_tmpl_id.product_template_training_ids.filtered(
+                    lambda p: not p.product_id)
 
 
 class ProductTrainingPlan(models.Model):
@@ -28,9 +38,13 @@ class ProductTrainingPlan(models.Model):
     _order = 'product_tmpl_id, product_id, sequence asc'
 
     product_tmpl_id = fields.Many2one(
-        comodel_name='product.template', string='Product template')
+        comodel_name='product.template', string='Product template',
+        required=True)
     product_id = fields.Many2one(
         comodel_name='product.product', string='Product')
-    sequence = fields.Integer(string="Sequence")
+    sequence = fields.Integer(string='Sequence')
     training_plan_id = fields.Many2one(
-        comodel_name='training.plan', string='Training Plan')
+        comodel_name='training.plan', string='Training Plan',
+        required=True)
+    training_plan_code = fields.Char(
+        string='Code', related='training_plan_id.code')
--- product_training_plan/models/training_plan.py
+++ product_training_plan/models/training_plan.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
-# (c) 2017 Alfredo de la Fuente - AvanzOSC
+# Copyright (c) 2017 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from openerp import fields, models, api
+from openerp import api, fields, models
+from openerp.models import expression
 
 
 class TrainingPlanCategory(models.Model):
@@ -25,20 +26,20 @@ class TrainingPlan(models.Model):
             id = False
         return id
 
-    name = fields.Char(string="Description", required=True)
-    code = fields.Char(string="Sequence", default="/")
+    name = fields.Char(string='Description', required=True)
+    code = fields.Char(string='Code', default='/')
     category_id = fields.Many2one(
         comodel_name='training.plan.category', string='Category',
         default=_get_default_category_id)
     tag_ids = fields.Many2many(
         comodel_name='training.plan.category', string='Tags')
-    planification = fields.Text(string="Planification")
-    resolution = fields.Text(string="Resolution")
+    planification = fields.Text(string='Planification')
+    resolution = fields.Text(string='Resolution')
     html_info = fields.Html(string='Description', translate=True)
     url = fields.Char(string='URL')
     product_training_plan_ids = fields.One2many(
         comodel_name='product.training.plan',
-        inverse_name='training_plan_id', string='Product training plan')
+        inverse_name='training_plan_id', string='Training plan products')
     other_info_ids = fields.Many2many(
         comodel_name='training.plan.other.info', string='Other info',
         relation='training_plan_other_info_rel', column1='training_plan_id',
@@ -47,7 +48,7 @@ class TrainingPlan(models.Model):
 
     _sql_constraints = [
         ('training_plan_unique_sequence', 'UNIQUE (code)',
-         'The training plan sequence must be unique!'),
+         'The training plan code must be unique!'),
     ]
 
     @api.model
@@ -66,12 +67,24 @@ class TrainingPlan(models.Model):
             self.env.ref('product_training_plan.training_plan_sequence').id))
         return super(TrainingPlan, self).copy(default)
 
+    @api.model
+    def name_search(self, name='', args=None, operator='ilike', limit=100):
+        results = super(TrainingPlan, self).name_search(
+            name=name, args=args, operator=operator, limit=limit)
+        if not args:
+            args = []
+        domain = expression.OR([[('name', operator, name)],
+                                [('code', operator, name)]])
+        domain = expression.AND([domain, args or []])
+        more_results = self.search(domain, limit=limit)
+        return more_results and more_results.name_get() or results
+
 
 class TrainingPlanOtherinfo(models.Model):
     _name = 'training.plan.other.info'
     _description = 'Training plan other info'
     _order = 'sequence asc'
 
-    sequence = fields.Integer(string="Sequence")
+    sequence = fields.Integer(string='Sequence')
     training_plan_id = fields.Many2one(
         comodel_name='training.plan', string='Training plan')
