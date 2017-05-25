0b65ffab7d6e6b754cfd560e2d583fa98ee3555a
agaldona <ainaragaldona@avanzosc.es>
2017-05-25 12:30:38 +0200

    [ADD] account_invoice_merge_stock_picking: new glue module between account_invoice_merge and stock_picking_invoice_link

---
+++ account_invoice_merge_stock_picking/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Ainara Galdona - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Account Invoice Merge Stock Picking",
+    "version": "8.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "contributors": [
+        "Ainara Galdona <ainaragaldona@avanzosc.es>",
+    ],
+    "category": "Accounting & Finance",
+    "depends": [
+        "account_invoice_merge",
+        "stock_picking_invoice_link"
+    ],
+    "data": [
+    ],
+    "installable": True,
+    "autoinstall": True,
+}
