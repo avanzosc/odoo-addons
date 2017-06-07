f96ae6a2f45f8d17ea1f1b5ecb6037fb610f2f9a
agaldona <ainaragaldona@avanzosc.es>
2017-06-07 13:02:19 +0200

    [IMP] quality_control_by_product: filter trigger lines new condition

--- quality_control_by_product/models/qc_trigger.py
+++ quality_control_by_product/models/qc_trigger.py
@@ -24,14 +24,23 @@ class QcTriggerProductLine(models.Model):
                 tests |= parents
         return tests
 
+    def clean_trigger_lines(self, trigger_lines):
+        cleaned_trigger_lines = []
+        tr_lines_recordset = self.env['qc.trigger.product_line']
+        for tl in trigger_lines:
+            tr_lines_recordset |= tl
+        parents_lst = self.get_parent_tests(tr_lines_recordset)
+        for line in trigger_lines:
+            if ((line.test not in parents_lst) and not
+                tr_lines_recordset.filtered(
+                    lambda x: x.test.parent_id == line.test.parent_id and
+                    x.partners and x.id != line.id)):
+                cleaned_trigger_lines.append(line)
+        return cleaned_trigger_lines
+
     def get_trigger_line_for_product(self, trigger, product, partner=False):
         trigger_lines = super(
             QcTriggerProductLine,
             self).get_trigger_line_for_product(trigger, product,
                                                partner=partner)
-        lines = self.env['qc.trigger.product_line']
-        for line in trigger_lines:
-            lines |= line
-        parent_tests = self.get_parent_tests(
-            lines.filtered(lambda x: x.test.parent_id))
-        return set(lines.filtered(lambda x: x.test not in parent_tests))
+        return self.clean_trigger_lines(trigger_lines)
