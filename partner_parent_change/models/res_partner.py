3cef2e413d173247e1aedfee184d821c8cf6539c
oihane <oihanecruce@gmail.com>
2017-06-12 17:31:57 +0200

    [ADD] partner_parent_change: new module

---
+++ partner_parent_change/models/res_partner.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright © 2017 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from openerp import _, api, exceptions, models
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    @api.multi
+    def write(self, values):
+        if 'parent_id' in values and not self.env.context.get('change_parent'):
+            raise exceptions.Warning(
+                _('You can\'t change the parent, please use the wizard.'))
+        return super(ResPartner, self).write(values)
