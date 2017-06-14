a27ca5c104a5feb762b59adbe0ab2518af717d1b
oihane <oihanecruce@gmail.com>
2017-06-14 11:36:59 +0200

    [REM] mrp_* & quality_* modules moved to mrp-addons

--- mrp_production_no_split_finished_product_move/tests/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# (c) 2015 Alfredo de la Fuente - AvanzOSC
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-
-from . import test_mrp_production_no_split_finished_product_move
--- product_attribute_value_related_operations/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# © 2017 Mikel Arregi Etxaniz - AvanzOSC
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from . import product_attribute
-from . import mrp_bom
