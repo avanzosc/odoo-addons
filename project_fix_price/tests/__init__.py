504424cf6223ed23e9f9ddbdb00cdf51cf414bbb
oihane <oihanecruce@gmail.com>
2017-06-14 17:18:38 +0200

    [REM] project_* modules moved to project-addons

--- project_fix_price/tests/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# (c) 2017 Esther Martín - Avanzosc S.L.
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import test_fix_price_project
--- project_partner_event_registration/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# (c) 2016 Oihane Crucelaegui - AvanzOSC
-# (c) 2016 Mikel Arregi - AvanzOSC
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from . import project
-from . import event
