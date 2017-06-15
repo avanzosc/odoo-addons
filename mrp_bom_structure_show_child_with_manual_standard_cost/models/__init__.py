ee0b6f07ea7c24689f9692a43aadc5f894d203b7
oihane <oihanecruce@gmail.com>
2017-06-15 15:00:07 +0200

    [REM] mrp_* & quality_* modules moved to mrp-addons (#485)

--- mrp_bom_structure_show_child_with_manual_standard_cost/models/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from . import mrp_bom_line
--- quality_control_by_product/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# © 2016 Ainara Galdona - AvanzOSC
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-
-from . import partner
-from . import product
-from . import qc_test
-from . import qc_inspection
-from . import qc_trigger
