623a93c5e969e35ea668fa2a66029b4bed03dd39
oihane <oihanecruce@gmail.com>
2017-06-15 09:06:51 +0200

    [REM] project_* modules moved to project-addons (#486)

--- project_events_operations/wizard/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# (c) 2016 Mikel Arregi - AvanzOSC
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from . import events_copy
-from . import events_date_modify
-from . import events_timetable_unify
--- project_partner_event_registration/tests/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-# (c) 2017 Alfredo de la Fuente - AvanzOSC
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from . import test_project_partner_event_registration
