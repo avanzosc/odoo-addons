973c29515f80de7d3c21deef3945e77ae161d406
oihane <oihanecruce@gmail.com>
2017-06-20 13:08:09 +0200

    [REM] sale_* related modules moved to sale-addons

--- sale_order_renovate_contract/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# (c) 2017 Alfredo de la Fuente - AvanzOSC
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from . import sale_order
-from . import account_analytic_account
