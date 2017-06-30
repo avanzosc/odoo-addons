12998a618832620df9864475601f737da3571067
agaldona <ainaragaldona@avanzosc.es>
2017-06-30 10:38:57 +0200

    [ADD] stock_inventory_negative_quants: nuevo modulo para eliminar quants negativos al hacer inventario

---
+++ stock_inventory_negative_quants/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Ainara Galdona - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Stock Inventory Negative Quants",
+    "version": "8.0.1.0.0",
+    "category": "Warehouse Management",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "contributors": [
+        "Ainara Galdona <ainaragaldona@avanzosc.es>",
+    ],
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        "views/stock_inventory_view.xml",
+        "wizard/stock_change_product_qty_view.xml"
+    ],
+    "installable": True,
+}
