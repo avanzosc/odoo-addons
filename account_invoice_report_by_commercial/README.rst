1c7a6755ed74dab05ddb1e8dfb3ea8bd98ba368a
agaldona <ainaragaldona@avanzosc.es>
2017-07-03 15:55:30 +0200

    [ADD] account_invoice_report_by_comercial: new module to print commercial data in invoice report header (#491)

---
+++ account_invoice_report_by_commercial/README.rst
@@ -0,0 +1,17 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+====================================
+Account Invoice Report By Commercial
+====================================
+
+In invoice reports header will print commercial data instead of the company's one. 
+
+Credits
+=======
+
+Contributors
+------------
+* Ainara Galdona <ainaragaldona@avanzosc.es>
+* Ana Juaristi <anajuaristi@avanzosc.es>
---
+++ account_invoice_report_by_commercial/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Ainara Galdona - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Account Invoice Report By Commercial",
+    "version": "8.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "contributors": [
+        "Ainara Galdona <ainaragaldona@avanzosc.es>",
+        "Ana Juaristi <anajuaristi@avanzosc.es>",
+    ],
+    "category": "Accounting & Finance",
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "views/layouts.xml",
+        "views/account_invoice_report.xml"
+    ],
+    "installable": True,
+}
