aca6a080937f94ba6f94f9a026569a4f0b53666a
oihane <oihanecruce@gmail.com>
2017-09-11 10:19:25 +0200

    [IMP] product_sequence_by_category: added tests (#504)
    * [IMP] product_sequence_by_category: added tests
    
    * [IMP] product_sequence_by_category: add rewritting action

--- product_sequence_by_category/__openerp__.py
+++ product_sequence_by_category/__openerp__.py
@@ -1,21 +1,14 @@
 # -*- coding: utf-8 -*-
-# (c) 2015 Oihane Crucelaegui - AvanzOSC
+# Copyright 2015-2017 Oihane Crucelaegui - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
     "name": "Product Sequence by Category",
     "version": "8.0.1.0.0",
-    "category": "Custom Module",
+    "category": "Product Management",
     "license": "AGPL-3",
-    "author": "OdooMRP team, "
-              "AvanzOSC, "
-              "Serv. Tecnol. Avanzados - Pedro M. Baeza",
-    "website": "http://www.odoomrp.com",
-    "contributors": [
-        "Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>",
-        "Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>",
-        "Ana Juaristi <anajuaristi@avanzosc.es>",
-    ],
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
     "depends": [
         "product",
     ],
