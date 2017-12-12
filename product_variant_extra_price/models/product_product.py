837996eaeee2f05d98a6a823b949d564d8c4efef
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2017-12-12 17:15:24 +0100

    [IMP] product_variant_extra_price: Two new price in product.template

--- product_variant_extra_price/models/product_product.py
+++ product_variant_extra_price/models/product_product.py
@@ -5,6 +5,15 @@ from openerp import fields, models
 import openerp.addons.decimal_precision as dp
 
 
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    price1 = fields.Float(
+        string='First price', digits=dp.get_precision('Product Price'))
+    price2 = fields.Float(
+        string='Second price', digits=dp.get_precision('Product Price'))
+
+
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
