bb0e08d53d31163890f47c6c10a02fc338c4b5dd
Oihane Crucelaegui <oihanecruce@gmail.com>
2018-02-05 15:36:46 +0100

    [IMP] contract_specification: add applicable models

--- contract_specification/models/contract_condition_template.py
+++ contract_specification/models/contract_condition_template.py
@@ -1,7 +1,15 @@
 # Copyright 2017 Oihane Crucelaegui - AvanzOSC
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl
 
-from odoo import fields, models
+from odoo import api, fields, models
+from odoo.addons.base.res.res_request import referenceable_models
+
+
+@api.model
+def template_type_selection(self):
+    return [('quotation', 'Quotation'),
+            ('order', 'Order'),
+            ('both', 'Both')]
 
 
 class ContractConditionTemplate(models.Model):
@@ -10,6 +18,10 @@ class ContractConditionTemplate(models.Model):
     _inherit = ['mail.thread']
 
     name = fields.Char(string='Title', translate=True, required=True)
+    tmpl_model = fields.Selection(
+        selection=referenceable_models, string='Applies to model')
+    tmpl_type = fields.Selection(
+        selection=template_type_selection, string='Type', default='both')
     condition_ids = fields.Many2many(
         comodel_name='contract.condition', string='Conditions',
         relation='rel_condition_template', column1='template_id',
