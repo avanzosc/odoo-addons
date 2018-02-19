31b4c4f69f9911e6b8dc57d75253834503107baa
agaldona <ainaragaldona@avanzosc.es>
2018-02-19 10:03:11 +0100

    [FIX] sale_product_max_discount: corregir test

--- sale_product_max_discount/tests/test_sale_product_max_discount.py
+++ sale_product_max_discount/tests/test_sale_product_max_discount.py
@@ -30,7 +30,8 @@ class TestSaleProductMaxDiscount(common.TransactionCase):
         self.product.min_margin = 10
         self.product.lst_price = 15
         self.product.standard_price = 5
-        self.assertAlmostEqual(self.product.max_discount, 57)
+        result = round((((15 - 5) - (10 / 100 * 15)) / 15 * 100), 2)
+        self.assertAlmostEqual(self.product.max_discount, result)
 
     def test_sale_order_exceed_max_discount(self):
         self.product.min_margin = 10
