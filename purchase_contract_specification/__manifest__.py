e8594bc280cfc64ff3b9d1100053bc7650c4479e
oihane <oihanecruce@gmail.com>
2018-03-05 14:53:09 +0100

    [IMP] purchase_contract_specification: execute condition_id onchange

--- purchase_contract_specification/__manifest__.py
+++ purchase_contract_specification/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Purchase Contract Specification',
-    'version': '11.0.1.0.0',
+    'version': '11.0.1.0.1',
     'category': 'Purchases',
     'author': 'AvanzOsc',
     'license': 'AGPL-3',
