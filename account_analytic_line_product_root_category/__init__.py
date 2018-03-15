1646dd78f57e508ce59ef104a3741565be9dde01
mikelarre <mikelarregi@avanzosc.es>
2018-03-15 16:37:06 +0100

    [ADD] account_analytic_line_product_root_category

---
+++ account_analytic_line_product_root_category/__init__.py
@@ -0,0 +1,3 @@
+# © Copyright 2018 Mikel Arregi Etxaniz - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+from . import models
---
+++ account_analytic_line_product_root_category/__manifest__.py
@@ -0,0 +1,22 @@
+# © Copyright 2018 Mikel Arregi Etxaniz - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Root category of product in analytic line",
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "account",
+    ],
+    "author": "AvanzOSC, ",
+    "website": "http://www.avanzosc.es",
+    "contributors": [
+        "Mikel Arregi <mikelarregi@avanzosc.es>",
+        "Ana Juaristi <anajuaristi@avanzosc.es>",
+    ],
+    "category": "Account",
+    "summary": "",
+    "data": [
+        "views/account_analytic_line_view.xml",
+    ],
+    "installable": True,
+}
