7c745fc0d8ee24a80c43d6295c2d0d732de3435e
mikelarre <mikelarre@users.noreply.github.com>
2018-03-27 17:19:34 +0200

    [11.0][ADD] account_analytic_line_product_root_category (#570)
    * [ADD] account_analytic_line_product_root_category
    
    * [IMP] account_analytic_ine_product_root_category: better description

---
+++ account_analytic_line_product_root_category/models/account_analityc_line.py
@@ -0,0 +1,23 @@
+# © Copyright 2018 Mikel Arregi Etxaniz - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+from openerp import api, fields, models
+
+
+class AccountAnalyticLine(models.Model):
+    _inherit = 'account.analytic.line'
+
+    product_root_category = fields.Many2one(
+        comodel_name='product.category', compute='_compute_get_root_category',
+        store=True, string="Root category")
+
+    def _get_root_category(self, category):
+        if not category.parent_id:
+            return category
+        else:
+            return self._get_root_category(category.parent_id)
+
+    @api.depends('product_id')
+    def _compute_get_root_category(self):
+        for line in self:
+            line.product_root_category = line._get_root_category(
+                line.product_id.categ_id)
