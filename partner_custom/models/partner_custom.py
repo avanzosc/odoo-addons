0b49496101c694ade30de9eb02a7d6a573fd21aa
Maite Esnal <maiteesnal@avanzosc.es>
2018-05-09 12:12:15 +0200

    [ADD]This module customizes contacts area adding two new sheets and many fields on them.

--- partner_custom/models/partner_custom.py
+++ partner_custom/models/partner_custom.py
@@ -3,46 +3,41 @@
 from odoo import fields, models, _
 
 
-class Partner(models.Model):
+class ResPartner(models.Model):
 
-
-    _inherit ='res.partner'
+    _inherit = 'res.partner'
     comertial_implantation = fields.One2many(commodel_name="res.partner",
-                                              string="Comertial implantation")
+                                             string="Comertial implantation")
     productive_implantation = fields.One2many(commodel_name="res.partner",
-        string="Productive implantation")
+                                              string="Productive implantation")
     asociated = fields.Many2one(commodel_name="res.partner",
                                 string="Asociated")
     asociated_type = fields.Many2one(commodel_name="res.partner",
-                                    string="Asociated type")
+                                     string="Asociated type")
     nature_entity = fields.Many2one(commodel_name="res.partner",
                                     string="Nature entity")
-
-
-class PartnerPartner(models.Model):
-
-    _inherit = 'res.partner'
     high_date = fields.Date("High date")
     low_date = fields.Date("Low date")
-    asociated = fields.Selection([('Yes', _('Yes')), ('No', _('No')),
-        ('Asociated potential', _('Asociated potential'))], string="Asociated",
-        default="Yes")
-    asociated_type = fields.Selection([('Partner', _('Partner')),
-        ('Parter/Junior', _('Partner/Junior')),
-        ('Strategic Partner', _('Strategic Partner')),
-        ('Strategic Partner Junior', _('Strategic Partner Junior'))],
-        string="Asociated type", default="Partner")
-    nature_entity = fields.Selection([('Company', _('Company')),
-        ('Training center', _('Training center')),
-        ('Research center', _('Research center')),
-        ('Organism', _('Organism'))], string="Nature of entity",
+    asociated = fields.Selection(
+        [('Yes', _('Yes')), ('No', _('No')), ('Asociated potential',
+         _('Asociated potential'))], string="Asociated", default="Yes")
+    asociated_type = fields.Selection(
+        [('Partner', _('Partner')),
+         ('Parter/Junior', _('Partner/Junior')), ('Strategic Partner',
+         _('Strategic Partner')), ('Strategic Partner Junior',
+         _('Strategic Partner Junior'))], string="Asociated type",
+        default="Partner")
+    nature_entity = fields.Selection(
+        [('Company', _('Company')), ('Training center', _('Training center')),
+         ('Research center', _('Research center')),
+         ('Organism', _('Organism'))], string="Nature of entity",
         default="Company")
     SME = fields.Boolean(default=True, string="SME")
     nature_of_sector = fields.Char(string="Nature of sector")
     group_of_control = fields.Boolean(default=True, string="Group of control")
-    doyou = fields.Boolean(default=True,
-        string="Do you have participation of NO SMEs or venture capital"
-        "entities in your shareholding?")
+    doyou = fields.Boolean(default=True, string="Do you have participation of"
+                           "NO SMEs or venture capital entities in your"
+                           "shareholding?")
     number_of_employees = fields.Integer(string="Number of employees")
     country_id = fields.Many2one(commodel_name="res.country",
                                  string='Select country', select=True)
@@ -50,10 +45,6 @@ class PartnerPartner(models.Model):
                                              string="Comertial implantation")
     productive_implantation = fields.Many2one(comodel_name='res.partner',
                                               string="Productive implantation")
-
-
-class PartnerEconomic(models.Model):
-    _inherit = 'res.partner'
     date = fields.Date(string="Date")
     real_total_turnover = fields.Integer(string="Real total turnover")
     real_number_employees = fields.Integer(string="Real number of employees")
