e44798f6974104379bd386e64ba3df6622573a1f
oihane <oihanecruce@gmail.com>
2018-05-10 16:11:04 +0200

    [ADD] partner_characterization: new module (#583)
    * [ADD]This module customizes contacts area adding two new sheets and many fields on them.
    
    * [ADD]This module customizes contacts area adding two new sheets and many fields on them.
    
    * [ADD]This module customizes contacts area adding two new sheets and many fields on them.
    
    * [ADD] partner_characterization: new module
    
    * [IMP] partner_characterization: rename field and translation

---
+++ partner_characterization/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2018 Maite Esnal - AvanzOSC
+# Copyright 2018 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Partner Characterization",
+    "version": "11.0.1.0.0",
+    "category": "Customers, Vendors, Partners,...",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "contacts",
+    ],
+    "data": [
+        "views/partner_view.xml",
+    ],
+    "installable": True,
+}
