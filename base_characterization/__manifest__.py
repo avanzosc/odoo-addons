aa20d72d0814f04aed0e0d6606998a79d49cb5d5
Xanti Pablo <38457813+xantipablo@users.noreply.github.com>
2018-05-10 23:09:48 +0200

    [ADD] New module base_characterization (#582)

---
+++ base_characterization/__manifest__.py
@@ -0,0 +1,31 @@
+# Copyright 2018 Xanti Pablo - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Base Characterization",
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+    ],
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "category": "Hidden",
+    "data": [
+        "security/ir.model.access.csv",
+        "views/res_team_view.xml",
+        "views/res_character_view.xml",
+        "views/res_target_view.xml",
+        "views/res_area_view.xml",
+        "views/res_area_type_view.xml",
+        "views/res_committee_view.xml",
+        "views/res_structure_view.xml",
+        "views/res_opportunity_space_view.xml",
+        "views/res_area_specialization_view.xml",
+        "views/res_activity_view.xml",
+        "views/res_activity_type_view.xml",
+        "views/res_space_view.xml",
+        "data/base_characterization_data.xml"
+    ],
+    "installable": True,
+}
