d82bf51f876bbfe498e4511caa3c1a119564ce05
Oihane Crucelaegui <oihanecruce@gmail.com>
2018-05-11 14:10:24 +0200

    [IMP] partner_characterization: some changes in views

--- partner_characterization/__manifest__.py
+++ partner_characterization/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Partner Characterization",
-    "version": "11.0.1.0.0",
+    "version": "11.0.1.0.1",
     "category": "Customers, Vendors, Partners,...",
     "license": "AGPL-3",
     "author": "AvanzOSC",
