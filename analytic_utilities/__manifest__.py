1424d05868f91cdc7068bb6bd3f8a7be8840dbdf
Oihane Crucelaegui <oihanecruce@gmail.com>
2018-05-16 15:48:31 +0200

    [ADD] New module analytic_utilities

---
+++ analytic_utilities/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2018 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Analytic Accounting Utilities",
+    "version": "11.0.1.0.0",
+    "category": "Accounting",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "analytic",
+    ],
+    "data": [
+        "views/account_analytic_line_view.xml",
+    ],
+    "installable": True,
+}
