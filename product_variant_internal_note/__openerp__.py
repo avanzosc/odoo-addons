f3ecb09474937435df7f43e847d43cf5de4bb0cf
Oihane Crucelaegui <oihanecruce@gmail.com>
2018-05-17 16:36:47 +0200

    [ADD] New module product_variant_internal_note

---
+++ product_variant_internal_note/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Product Variant Internal Note",
+    "version": "8.0.1.0.0",
+    "category": "Sales Management",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "contributors": [
+        "Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>",
+        "Ana Juaristi <anajuaristi@avanzosc.es>",
+    ],
+    "depends": [
+        "product",
+    ],
+    "data": [
+        "views/product_product_view.xml",
+    ],
+    "installable": True,
+}
