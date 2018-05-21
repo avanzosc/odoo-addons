777309209e8ad7dcbe17f591ee4123204ef89f1d
Oihane Crucelaegui <oihanecruce@gmail.com>
2018-05-21 12:13:54 +0200

    [MIG] partner_group: Migration to 11.0

--- partner_group/__manifest__.py
+++ partner_group/__manifest__.py
@@ -1,24 +1,18 @@
-# -*- coding: utf-8 -*-
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
-    "name": "Partner group",
-    "version": "8.0.1.0.0",
+    "name": "Partner Group",
+    "version": "11.0.1.0.0",
     "license": "AGPL-3",
     "author": "OdooMRP team, "
               "AvanzOSC, "
               "Serv. Tecnol. Avanzados - Pedro M. Baeza, "
               "Odoo Community Association (OCA)",
     "website": "http://www.odoomrp.com",
-    "contributors": [
-        "Daniel Campos <danielcampos@avanzosc.es>",
-        "Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>",
-        "Ana Juaristi <anajuaristi@avanzosc.es>",
-    ],
     "depends": [
-        "base",
+        "contacts",
     ],
-    "category": "Generic Modules",
+    "category": "Tools",
     "data": [
         "views/res_partner_view.xml",
     ],
--- partner_group/models/partner.py
+++ partner_group/models/partner.py
@@ -1,12 +1,12 @@
-# -*- coding: utf-8 -*-
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import models, fields
+from odoo import models, fields
 
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    partner_group = fields.Many2one('res.partner', 'Partner Group')
-    is_group = fields.Boolean('Is a Group',
-                              help="Check if the partner is a group")
+    partner_group = fields.Many2one(
+        comodel_name='res.partner', string='Partner Group')
+    is_group = fields.Boolean(
+        string='Is a Group', help="Check if the partner is a group")
