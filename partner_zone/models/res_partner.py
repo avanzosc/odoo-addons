28418f832afd93cfe13f9abddd23c6241177a7e3
Heru Prasetyo Utomo <hprasetyou@hotmail.com>
2018-05-21 14:20:26 +0200

    [MIG] partner_zoneS: Migration to 11.0

--- partner_zone/models/res_partner.py
+++ partner_zone/models/res_partner.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
 # © 2016 Esther Martín - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import fields, models
+from odoo import fields, models
 
 
 class ResPartnerZone(models.Model):
