8470f75e0f312052b4a3c7b112589240dd4d6e6d
Miguel Urbistondo <mikelurbistondo@avanzosc.es>
2018-07-11 13:36:45 +0200

    [ADD] event_characterization: With this module you can characterize different parameters for events

---
+++ event_characterization/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2018 Mikel Urbistondo - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Event Characterization",
+    "version": "11.0.1.0.0",
+    "category": "Marketing",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "base_characterization",
+        "event",
+    ],
+    "data": [
+        "views/event_event_views.xml",
+    ],
+    "installable": True,
+}
---
+++ event_characterization/models/event_event.py
@@ -0,0 +1,27 @@
+from odoo import fields, models
+
+
+class EventEvent(models.Model):
+    # partner characterization
+    _inherit = 'event.event'
+
+    area_ids = fields.Many2many(
+        string='Areas', comodel_name='res.partner.area',
+        relation='rel_event_area', column1='event_id',
+        column2='area_id', copy=False)
+    committee_ids = fields.Many2many(
+        string='Committees', comodel_name='res.committee',
+        relation='rel_event_committee', column1='event_id',
+        column2='committee_id', copy=False)
+    team_ids = fields.Many2many(
+        string='Teams', comodel_name='res.team',
+        relation='rel_event_team', column1='event_id',
+        column2='team_id', copy=False)
+    structure_ids = fields.Many2many(
+        string='Structures', comodel_name='res.structure',
+        relation='rel_event_structure', column1='event_id',
+        column2='structure_id', copy=False)
+    main_contact = fields.Boolean(string='Main Contact')
+    assembly = fields.Boolean(string='Assembly')
+    joint = fields.Boolean(string='Joint')
+    bidding = fields.Boolean(string='Bidding')
