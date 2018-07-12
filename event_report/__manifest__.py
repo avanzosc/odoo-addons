02e8e8f22d6bd6574c52893b8b4a4f09deb8e01c
Gotzon Imaz <gotzonimaz@avanzosc.es>
2018-07-12 12:42:52 +0200

    [MIG] event_report: Migration to 11.0

--- event_report/__manifest__.py
+++ event_report/__manifest__.py
@@ -1,20 +1,19 @@
-# -*- coding: utf-8 -*-
 # © 2016 Esther Martín - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
     "name": "Reports for events",
-    "version": "8.0.1.0.0",
+    "version": "11.0.1.0.0",
     "category": "Marketing",
     "license": "AGPL-3",
     "author": "AvanzOSC",
     "website": "http://www.avanzosc.es",
     "contributors": [
         "Esther Martín <esthermartin@avanzosc.es>",
-    ],
+        "Gotzon Imaz <gotzonimaz@avanzosc.es>",
+    ], 
     "depends": [
         "event",
-        "report",
         "website_event_track",
     ],
     "data": [
