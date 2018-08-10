3d367de1c02959755f58c4742b8c7221e91b7e2d
Oihane Crucelaegui <oihanecruce@gmail.com>
2018-08-10 12:36:21 +0200

    [IMP] account_utilities: filtering bank accounts

--- account_utilities/models/account_invoice.py
+++ account_utilities/models/account_invoice.py
@@ -30,3 +30,22 @@ class AccountInvoice(models.Model):
 
     payment_date = fields.Date(compute='_compute_payment_date',
                                string='Payment Date', store=True)
+
+    @api.multi
+    def onchange_partner_id(
+            self, type, partner_id, date_invoice=False,
+            payment_term=False, partner_bank_id=False, company_id=False):
+        res = super(AccountInvoice, self).onchange_partner_id(
+            type, partner_id, date_invoice=date_invoice,
+            payment_term=payment_term, partner_bank_id=partner_bank_id,
+            company_id=company_id)
+        p = self.env['res.partner'].browse(partner_id or False)
+        domain = res.setdefault('domain', {})
+        if type in ('in_invoice', 'out_refund'):
+            bank_ids = p.commercial_partner_id.bank_ids
+            if bank_ids:
+                domain['partner_bank_id'] = [('id', 'in', bank_ids.ids)]
+            else:
+                domain['partner_bank_id'] = [
+                    ('partner_id', '=', p.commercial_partner_id.id)]
+        return res
