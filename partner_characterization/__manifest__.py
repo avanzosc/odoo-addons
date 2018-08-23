50299ebb9b68c80e868f4ff390812b601e137c2d
oihane <oihanecruce@gmail.com>
2018-08-23 15:20:48 +0200

    [IMP] partner_characterization: change partner view (#619)
    * [IMP] partner_characterization: change partner view
    
    * [IMP] partner_characterization: some name change, added new field

--- partner_characterization/__manifest__.py
+++ partner_characterization/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Partner Characterization",
-    "version": "11.0.2.1.0",
+    "version": "11.0.2.2.0",
     "category": "Customers, Vendors, Partners,...",
     "license": "AGPL-3",
     "author": "AvanzOSC",
