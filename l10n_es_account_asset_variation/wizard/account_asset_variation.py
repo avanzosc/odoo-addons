a451ef6d44e71e204ffd34f6565936c10b0de756
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2018-09-03 10:55:07 +0200

    [FIX] l10n_es_account_asset_variation: fix calculations.

--- l10n_es_account_asset_variation/wizard/account_asset_variation.py
+++ l10n_es_account_asset_variation/wizard/account_asset_variation.py
@@ -3,7 +3,6 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from openerp import models, fields, api
-from dateutil.relativedelta import relativedelta
 
 
 class AccountAssetVariation(models.TransientModel):
@@ -21,26 +20,4 @@ class AccountAssetVariation(models.TransientModel):
         asset_id = self.env.context.get('active_id')
         asset = self.env['account.asset.asset'].browse(asset_id)
         asset.with_context(wiz=self).compute_depreciation_board()
-        if asset.depreciation_line_ids:
-            max_line = max(asset.depreciation_line_ids, key=lambda x: x.id)
-            amount = round(
-                (asset.purchase_value * max_line.method_percentage) / 100, 2)
-            if amount < max_line.amount:
-                max_line.write({'amount': amount,
-                                'remaining_value': max_line.amount - amount})
-                new_line = max_line.copy()
-                new_date = fields.Date.from_string(
-                    max_line.depreciation_date) + relativedelta(
-                    months=asset.method_period)
-                deprec_value = max_line.depreciated_value + max_line.amount
-                new_line.write({'amount': max_line.remaining_value,
-                                'remaining_value': 0.00,
-                                'depreciation_date': new_date,
-                                'depreciated_value': deprec_value})
-                max_line = max(asset.depreciation_line_ids, key=lambda x: x.id)
-                lines = asset.depreciation_line_ids.filtered(
-                    lambda x: x.id != max_line.id)
-                perc = sum(lines.mapped('method_percentage'))
-                i = len(asset.depreciation_line_ids) - 1
-                asset.depreciation_line_ids[i].method_percentage = 100 - perc
         return True
