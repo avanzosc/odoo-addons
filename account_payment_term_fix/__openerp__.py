ec3a0fa2793bfda2db1883dd15faaeab4f0fdfcc
Oihane Crucelaegui <oihanecruce@gmail.com>
2018-11-07 17:46:16 +0100

    [IMP] account_payment_term_fix: new module to fix adding a month to due date

---
+++ account_payment_term_fix/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Account Payment Term FIX",
+    "version": "8.0.1.0.0",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "category": "Accounting & Finance",
+    "license": "AGPL-3",
+    "depends": [
+        "account",
+    ],
+    "data": [
+    ],
+    "installable": True,
+    "auto_install": True,
+}
