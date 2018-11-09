80c514d6b0aa81c7508f9cf572189c8dbe57c395
oihane <oihanecruce@gmail.com>
2018-11-09 10:04:42 +0100

    [FIX] account_analytic_billing_plan: fix some errors (#650)

--- account_analytic_billing_plan/__openerp__.py
+++ account_analytic_billing_plan/__openerp__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Account Analytic Billing Plan",
-    "version": "8.0.1.0.0",
+    "version": "8.0.1.1.0",
     "license": "AGPL-3",
     "author": "AvanzOSC",
     "website": "http://www.avanzosc.es",
