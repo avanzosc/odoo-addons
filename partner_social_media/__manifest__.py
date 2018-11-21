64a8188fac95edd178313ae97d62a4ad35c1c2f1
Oihane Crucelaegui <oihanecruce@gmail.com>
2018-11-21 14:54:08 +0100

    [ADD] New module partner_social_media

---
+++ partner_social_media/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2018 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Partner Social Media",
+    "version": "11.0.1.0.0",
+    "category": "Customers, Vendors, Partners,...",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "views/res_partner_view.xml",
+    ],
+    "installable": True,
+}
