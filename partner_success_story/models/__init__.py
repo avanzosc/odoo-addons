4f2384b094bc00ba68ba26468fd144821d4b8848
Oihane Crucelaegui <oihanecruce@gmail.com>
2018-12-05 16:07:06 +0100

    [ADD] partner_success_story: new module

---
+++ partner_success_story/models/__init__.py
@@ -0,0 +1,5 @@
+# Copyright 2018 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from . import res_partner
+from . import res_partner_success_story
