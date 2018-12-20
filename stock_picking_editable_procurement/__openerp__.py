8639cbbfb42dea66f71b86fb3a99b99baf9b9a93
mikelarre <mikelarregi@avanzosc.es>
2018-12-20 18:29:00 +0100

    [ADD] stock_picking_editable_procurement

---
+++ stock_picking_editable_procurement/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# © Copyright 2018 Mikel Arregi Etxaniz - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Editable picking-procurement relation",
+    "version": "8.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "stock",
+    ],
+    "author": "OdooMRP team, "
+              "AvanzOSC, "
+              "Odoo Community Association (OCA)",
+    "website": "http://www.odoomrp.com",
+    "contributors": [
+        "Mikel Arregi <mikelarregi@avanzosc.es>",
+        "Ana Juaristi <anajuaristi@avanzosc.es>",
+    ],
+    "category": "",
+    "summary": "",
+    "data": [
+    ],
+    "installable": True,
+}
