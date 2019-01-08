0309ee9264b07695d417f03a5ad19fb3a2abe7fd
Jorge Camacho <jcamacho@trey.es>
2019-01-08 12:09:13 +0100

    [ADD][mass_mailing_characterization] Allow to add posts to mass_mailing

---
+++ mass_mailing_characterization/README.rst
@@ -0,0 +1,29 @@
+.. image:: https://img.shields.io/badge/license-AGPL--3-blue.png
+   :target: https://www.gnu.org/licenses/agpl
+   :alt: License: AGPL-3
+
+=============================
+Mass mailing characterization
+=============================
+
+With this module you can characterize different parameters for mass mailing
+
+Bug Tracker
+===========
+
+Bugs are tracked on `GitHub Issues
+<https://github.com/avanzosc/odoo-addons/issues>`_. In case of trouble, please
+check there if your issue has already been reported. If you spotted it first,
+help us smash it by providing detailed and welcomed feedback.
+
+Credits
+=======
+
+Contributors
+------------
+
+* Roberto Lizana <roberto@trey.es>
+* Jorge Camacho <jorge@trey.es>
+
+Do not contact contributors directly about support or help with technical
+issues.
---
+++ mass_mailing_characterization/models/mail.py
@@ -0,0 +1,68 @@
+# Copyright 2019 Roberto Lizana - Trey, Jorge Camacho - Trey
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+from odoo import fields, models, api
+
+
+class MassMailing(models.Model):
+    _inherit = 'mail.mass_mailing'
+
+    area_ids = fields.Many2many(
+        string='Areas', comodel_name='res.partner.area',
+        relation='rel_mass_mailing_area', column1='mass_mailing_id',
+        column2='area_id', copy=False)
+    post_state = fields.Selection(
+        selection=[
+            ('draft', 'Draft'),
+            ('done', 'Done')],
+        string='Post state',
+        default='draft')
+    post_ids = fields.Many2many(
+        comodel_name='blog.post',
+        relation='mail_mass_mailing2blog_post_rel',
+        column1='mailing_id',
+        column2='post_id')
+    post_line_ids = fields.One2many(
+        comodel_name='mail.mass_mailing.post_line',
+        inverse_name='mailing_id',
+        string='Posts')
+    featured_post_ids = fields.Many2many(
+        comodel_name='blog.post',
+        relation='mail_mass_mailing2featured_blog_post_rel',
+        column1='mailing_id',
+        column2='post_id')
+
+    @api.multi
+    def action_post_state_draft(self):
+        self.post_state = 'draft'
+
+    @api.multi
+    def action_post_state_done(self):
+        post_exists_ids = [l.post_id.id for l in self.post_line_ids]
+        to_create = [p for p in self.post_ids if p.id not in post_exists_ids]
+        for post in to_create:
+            self.env['mail.mass_mailing.post_line'].create({
+                'mailing_id': self.id,
+                'post_id': post.id,
+                'sequence': 10})
+        to_delete = [
+            p for p in self.post_line_ids
+            if p.post_id.id not in self.post_ids.ids]
+        for post in to_delete:
+            post.unlink()
+        self.post_state = 'done'
+
+
+class MassMailingPostLine(models.Model):
+    _name = 'mail.mass_mailing.post_line'
+
+    sequence = fields.Integer(
+        string='Sequence')
+    post_id = fields.Many2one(
+        comodel_name='blog.post',
+        string='Post')
+    area_ids = fields.Many2many(
+        related='post_id.area_ids',
+        readonly=True)
+    mailing_id = fields.Many2one(
+        comodel_name='mail.mass_mailing',
+        string='LABEL')
