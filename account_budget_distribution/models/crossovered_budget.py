a2bd4bbd27c07d8e823f892b221a8dbd1778ad23
Oihane Crucelaegui <oihanecruce@gmail.com>
2019-01-13 20:09:51 +0100

    [IMP] account_budget_distribution: wizard for distribution

--- account_budget_distribution/models/crossovered_budget.py
+++ account_budget_distribution/models/crossovered_budget.py
@@ -1,27 +1,12 @@
 # Copyright 2019 Oihane Crucelaegui - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class CrossoveredBudget(models.Model):
     _inherit = 'crossovered.budget'
 
-    total_planned_amount = fields.Float(
-        string='Total Planned Amount', store=True)
     summary_ids = fields.One2many(
         comodel_name='crossovered.budget.summary',
         inverse_name='crossovered_budget_id', string='Summary Lines')
-
-    @api.multi
-    def button_divide_planned_amount(self):
-        for budget in self.filtered('crossovered_budget_line'):
-            try:
-                budget_lines = budget.crossovered_budget_line.filtered(
-                    lambda l: not l.general_budget_id.expenses)
-            except Exception:
-                budget_lines = budget.crossovered_budget_line
-            budget_lines.write({
-                'planned_amount': (
-                        budget.total_planned_amount / len(budget_lines))
-            })
--- account_budget_distribution/wizards/crossovered_budget_distribution.py
+++ account_budget_distribution/wizards/crossovered_budget_distribution.py
@@ -10,18 +10,41 @@ class CrossoveredBudgetDistribution(models.TransientModel):
 
     budget_id = fields.Many2one(comodel_name='crossovered.budget')
     line_ids = fields.One2many(
-        comodel_name='crossovered.budget.distribution.line')
+        comodel_name='crossovered.budget.distribution.line',
+        inverse_name='distribution_id')
 
     @api.model
     def default_get(self, fields):
+        context = self.env.context
         res = super(CrossoveredBudgetDistribution, self).default_get(fields)
+        if context.get('active_model') == 'crossovered.budget':
+            budget = self.env[context.get('active_model')].browse(
+                context.get('active_id'))
+            res.update({
+                'budget_id': budget.id,
+                'line_ids': [
+                    (0, 0, {'budget_post_id': x.general_budget_id.id,
+                            'planned_amount': x.planned_amount})
+                    for x in budget.summary_ids],
+            })
         return res
 
-    # @api.multi
-    # def button_distribute_amount(self):
-    #     for wiz in self:
-    #         for budget_post in wiz.mapped('line_ids.budget_post_id'):
-
+    @api.multi
+    def button_distribute_amount(self):
+        for wiz in self:
+            for line in wiz.line_ids:
+                budget_lines = wiz.budget_id.crossovered_budget_line.filtered(
+                    lambda l: l.general_budget_id == line.budget_post_id)
+                try:
+                    budget_lines = budget_lines.filtered(
+                        lambda l: not l.general_budget_id.expenses)
+                except Exception:
+                    pass
+                if budget_lines:
+                    budget_lines.write({
+                        'planned_amount': (
+                            line.planned_amount / len(budget_lines))
+                    })
 
 
 class CrossoveredBudgetDistributionLine(models.TransientModel):
