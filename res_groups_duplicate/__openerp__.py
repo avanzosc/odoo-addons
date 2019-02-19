da73b70e7d5001e14d54074abc94858bd83bffd5
oihane <oihanecruce@gmail.com>
2019-02-19 16:52:13 +0100

    [ADD] res_groups_duplicate (#693)

---
+++ res_groups_duplicate/__openerp__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# © Copyright 2018 Mikel Arregi Etxaniz - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Duplicate group",
+    "version": "8.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+    ],
+    "author": "OdooMRP team, "
+              "AvanzOSC, "
+              "Odoo Community Association (OCA)",
+    "website": "http://www.odoomrp.com",
+    "contributors": [
+        "Mikel Arregi <mikelarregi@avanzosc.es>",
+        "Ana Juaristi <anajuaristi@avanzosc.es>",
+    ],
+    "category": "",
+    "summary": "",
+    "data": [
+        "views/res_groups_view.xml"
+    ],
+    "installable": True,
+}
