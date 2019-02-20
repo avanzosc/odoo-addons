8cbf3d11c79cc4e256a19a8aac7ca526c590998e
mikelarre <mikelarregi@avanzosc.es>
2019-02-20 16:20:05 +0100

    [MIG] product_attribute_types

---
+++ product_attribute_types/__manifest__.py
@@ -0,0 +1,30 @@
+# Copyright 2015 Oihane Crucelaegui - AvanzOSC
+# Copyright 2015-2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# Copyright 2018 Mikel Arregi Etxaniz - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Product attribute types",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "product",
+    ],
+    "author": "OdooMRP team,"
+              "AvanzOSC,"
+              "Tecnativa,"
+              "Serv. Tecnol. Avanzados - Pedro M. Baeza,"
+              "Odoo Community Association (OCA)",
+    "contributors": [
+        "Mikel Arregi <mikelarregi@avanzosc.es>",
+        "Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>",
+        "Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>",
+        "Ana Juaristi <ajuaristio@gmail.com>"
+    ],
+    "category": "Product Management",
+    "website": "http://www.odoomrp.com",
+    'data': [
+        "views/product_view.xml",
+    ],
+    'installable': True,
+}
