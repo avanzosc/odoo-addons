7d17c858a6fc5bee49997e20afccc9179b9360aa
oihane <oihanecruce@gmail.com>
2019-02-26 15:57:41 +0100

    [IMP][mass_mailing_characterization] Add posts to mass mailing by area (#698)

--- mass_mailing_characterization/__manifest__.py
+++ mass_mailing_characterization/__manifest__.py
@@ -2,23 +2,25 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
-    "name": "Mass Mailing Characterization",
-    "version": "11.0.1.0.0",
-    "category": "Marketing",
-    "license": "AGPL-3",
-    "author": "Trey (www.trey.es)",
-    "website": "https://www.trey.es",
-    "depends": [
-        "mass_mailing",
-        "web",
-        "website_blog",
-        "website_blog_characterization",
+    'name': 'Mass Mailing Characterization',
+    'version': '11.0.1.0.0',
+    'category': 'Marketing',
+    'license': 'AGPL-3',
+    'author': 'Trey (www.trey.es)',
+    'website': 'https://www.trey.es',
+    'depends': [
+        'base_characterization',
+        'mass_mailing',
+        'web',
+        'website_blog',
+        'website_blog_characterization',
     ],
-    "data": [
-        "security/ir.model.access.csv",
-        "views/blog_post_view.xml",
-        "views/mail_mass_mailing_view.xml",
+    'data': [
+        'security/ir.model.access.csv',
+        'views/blog_post_view.xml',
+        'views/mail_mass_mailing_view.xml',
+        'views/res_partner_area_view.xml',
         'views/web.xml'
     ],
-    "installable": True,
+    'installable': True,
 }
