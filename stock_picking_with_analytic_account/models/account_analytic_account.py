7b833a2d79a55e659042425bfc98cd2e7619a7e3
Oihane Crucelaegui <oihanecruce@gmail.com>
2019-03-04 11:36:49 +0100

    [FIX] remove unnecessary encoding

--- stock_picking_with_analytic_account/models/account_analytic_account.py
+++ stock_picking_with_analytic_account/models/account_analytic_account.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright (c) 2017 Alfredo de la fuente <alfredodelafuente@avanzosc.es>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from odoo import models, fields
