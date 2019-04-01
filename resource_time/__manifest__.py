d7659a2d2508735b38d10667edab0dc93d5ca81d
Oihane Crucelaegui <oihanecruce@gmail.com>
2019-04-01 16:43:11 +0200

    [IMP] resource_time: readme and translations

--- resource_time/__manifest__.py
+++ resource_time/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    "name": "Resource",
+    "name": "Resource Time",
     "version": "12.0.1.0.0",
     "category": "Technical Settings",
     "license": "AGPL-3",
