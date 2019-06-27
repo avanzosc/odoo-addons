97f396e30412ff3275f8e380d3a1e2e12a3540dc
mikelarre <mikelarregi@avanzosc.es>
2019-06-27 16:43:46 +0200

    [ADD] ir_attachment_download

---
+++ ir_attachment_download/__manifest__.py
@@ -0,0 +1,25 @@
+# Copyright 2019 Mikel Arregi Etxaniz - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Attachments download",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "base", "web"
+    ],
+    "author": "OdooMRP team, "
+              "AvanzOSC, "
+              "Odoo Community Association (OCA)",
+    "website": "http://www.odoomrp.com",
+    "contributors": [
+        "Mikel Arregi <mikelarregi@avanzosc.es>",
+        "Ana Juaristi <anajuaristi@avanzosc.es>",
+    ],
+    "category": "",
+    "summary": "",
+    "data": [
+        "wizard/attach_download_action_view.xml",
+        "views/ir_model_view.xml",
+    ],
+    "installable": True,
+}
---
+++ ir_attachment_download/models/ir_attachment.py
@@ -0,0 +1,54 @@
+# Copyright 2019 Mikel Arregi Etxaniz - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+from odoo import api, exceptions, fields, models, _
+from io import BytesIO
+import zipfile
+import base64
+
+
+class IrAttachment(models.Model):
+    _inherit = "ir.attachment"
+
+    def _generate_zip(self, files):
+        mem_zip = BytesIO()
+        with zipfile.ZipFile(mem_zip, mode="w",
+                             compression=zipfile.ZIP_DEFLATED) as zf:
+            for f in files:
+                zf.writestr(f[0], f[1])
+        zip_file = mem_zip.getvalue()
+        mem_zip.close()
+        return zip_file
+
+    @api.model
+    def _generate_zip_from_attachments(self, records,
+                                       att_fields=None):
+        files = []
+        if att_fields:
+            att_fields = self.env["ir.model.fields"].browse(att_fields)
+            for attach in records:
+                for field in att_fields:
+                    if attach[field.name]:
+                        file = base64.b64decode(attach[field.name])
+                        files.append((attach.name, file))
+        else:
+            for attach in self.env['ir.attachment'].search(
+                    [('res_model', '=', str(records._name)),
+                     ('res_id', '=', records._ids)]):
+                file = base64.b64decode(attach.datas)
+                files.append((attach.name, file))
+        if files:
+            zip_file = self._generate_zip(files)
+            file_name = "%s/%s" % ((self.env["ir.model"].search(
+                [("model", "=", self._context.get("active_model"))]).name
+                                    or ""), fields.Datetime.now())
+            attach_id = self.create({'datas': base64.b64encode(zip_file),
+                                     'name': file_name})
+            file_url = "/web/binary/saveas?model=ir.attachment&field=datas&" \
+                       "filename_field=datas_fname&id=%s" % str(attach_id.id)
+            return {
+                'type': 'ir.actions.act_url',
+                'url': file_url,
+                'target': 'new'
+            }
+        else:
+            raise exceptions.Warning(_("No files to download"))
