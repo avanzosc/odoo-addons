c36aed6531598f350dcfdca21dec14dedda8789b
Thor <alfredodelafuente@avanzosc.es>
2019-07-18 15:41:11 +0200

    [ADD] account_invoice_report_utilities: New field "Invoice printing note" in account tax, and in account fiscal position.

---
+++ account_invoice_report_utilities/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2019 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Account Invoice Report Utilities",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "category": "Accounting & Finance",
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "views/account_tax_view.xml",
+        "views/account_fiscal_position_view.xml",
+        "report/account_invoice_report.xml",
+    ],
+    "installable": True,
+}
