2f4e5ed22db040c554aac0bdf961001057a9a806
mikelarre <mikelarregi@avanzosc.es>
2019-07-30 14:03:06 +0200

    [ADD] sale_order_line_product_configurator_by_category

---
+++ sale_order_line_product_configurator_by_category/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2019 Mikel Arregi Etxaniz - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Sale line product configurator by category",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "sale",
+    ],
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "category": "",
+    "data": [
+        "security/ir.model.access.csv",
+        "views/product_category_view.xml",
+        "views/product_view.xml",
+        "views/sale_order_view.xml",
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+}
