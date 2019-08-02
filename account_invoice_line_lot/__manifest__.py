0ca807e5ede1289653c37bef5318a8bfe73aa4ae
Thor <alfredodelafuente@avanzosc.es>
2019-08-02 11:51:32 +0200

    [ADD] account_invoice_line_lot: Bring the picking lots to the customer invoice lines.

---
+++ account_invoice_line_lot/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2019 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Account Invoice Line Lot",
+    "version": "12.0.1.0.0",
+    "category": "Invoices & Payments",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "sale_management",
+        "stock_account"
+    ],
+    "data": [
+        "views/account_invoice_view.xml",
+    ],
+    "installable": True,
+}
