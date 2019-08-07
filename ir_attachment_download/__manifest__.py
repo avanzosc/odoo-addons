563b77b03e3b61184b3b851b83c77168483db246
Oihane Crucelaegui <oihanecruce@gmail.com>
2019-08-07 16:29:43 +0200

    [MIG] ir_attachment_download: Migration to 11.0

--- ir_attachment_download/__manifest__.py
+++ ir_attachment_download/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
     "name": "Attachments download",
-    "version": "12.0.1.0.0",
+    "version": "11.0.1.0.0",
     "license": "AGPL-3",
     "depends": [
         "base",
