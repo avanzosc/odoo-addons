8ad247a1d39a18bb11bc44e73a4669d64c7b2e23
oihane <oihanecruce@gmail.com>
2019-08-08 10:18:31 +0200

    [MIG] ir_attachment_download: Migration to 11.0 (#759)
    * [ADD] ir_attachment_download (#740)
    
    * [ADD] ir_attachment_download
    
    * [FIX] ir_attachment_download: test
    
    * [FIX] ir_attachment_download_test: test pep8
    
    * [IMP] ir_attachment_download: enable downloading + fix tests
    
    * [MIG] ir_attachment_download: Migration to 11.0

---
+++ ir_attachment_download/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2019 Mikel Arregi Etxaniz - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Attachments download",
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+        "web",
+    ],
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "category": "Tools",
+    "data": [
+        "wizard/attach_download_action_view.xml",
+        "views/ir_model_view.xml",
+    ],
+    "installable": True,
+}
