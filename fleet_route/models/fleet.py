0143f64636a92ffa894eba20b261e04fb4cec3d8
Oihane Crucelaegui <oihanecruce@gmail.com>
2019-09-23 17:50:37 +0200

    [IMP] fleet_route: change names

--- fleet_route/models/fleet.py
+++ fleet_route/models/fleet.py
@@ -1,31 +1,31 @@
 # Copyright 2019 Mentxu Isuskitza - AvanzOSC
 # Copyright 2019 Oihana Larrañaga - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class FleetItinerary(models.Model):
-    _name = 'fleet.itinerary'
-    _description = 'Itinerary'
+    _name = 'fleet.route.stop'
+    _description = 'Route Stop'
 
     name = fields.Char(string='Description', required=True)
-    stop_id = fields.Many2one(
-        string='Stop', comodel_name='res.partner',
+    location_id = fields.Many2one(
+        string='Location', comodel_name='res.partner',
         domain=lambda self: [
             ('category_id', 'in',
              self.env.ref('fleet_route.stop_location_partner_cat').id)])
     street = fields.Char(
-        string='Street', related='stop_id.street')
+        string='Street', related='location_id.street')
     city = fields.Char(
-        string='City', related='stop_id.city')
+        string='City', related='location_id.city')
     state_id = fields.Many2one(
         string='State', comodel_name='res.country.state',
-        related='stop_id.state_id')
+        related='location_id.state_id')
     country_id = fields.Many2one(
         string='Country', comodel_name='res.country',
-        related='stop_id.country_id')
+        related='location_id.country_id')
     comment = fields.Text(
-        string='Internal notes', related='stop_id.comment')
+        string='Internal notes', related='location_id.comment')
     departure_estimated_time = fields.Float(
         string='Departure estimated time')
     return_estimated_time = fields.Float(
@@ -54,6 +54,9 @@ class FleetRoute(models.Model):
     driver_id = fields.Many2one(
         string='Driver', comodel_name='res.partner',
         related='vehicle_id.driver_id')
+    driver_commercial_id = fields.Many2one(
+        comodel_name='res.partner',
+        related='driver_id.commercial_partner_id')
     seats = fields.Integer(
         string='Seats', related='vehicle_id.seats')
     manager_id = fields.Many2one(
@@ -64,12 +67,13 @@ class FleetRoute(models.Model):
     substitute_id = fields.Many2one(
         string='Substitute manager', comodel_name='hr.employee')
     substitute_phone_mobile = fields.Char(
-        string='Phone/mobile', compute='_compute_substitute_phone_mobile',
+        string='Substitute phone/mobile',
+        compute='_compute_substitute_phone_mobile',
         store=True)
     timetable = fields.Many2one(
         string='Timetable', comodel_name='resource.calendar')
-    itinerary_ids = fields.One2many(
-        string='Itineraries', comodel_name='fleet.itinerary',
+    stop_ids = fields.One2many(
+        string='Stops', comodel_name='fleet.route.stop',
         inverse_name='route_id')
 
     @api.depends('manager_id', 'manager_id.work_phone',
