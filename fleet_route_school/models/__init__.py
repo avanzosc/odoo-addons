05d7c982c78e09b4bfe92c8bc18c7865250205da
oihane <oihanecruce@gmail.com>
2019-10-10 14:59:21 +0200

    [ADD] New module fleet_route_school (#774)
    * [ADD] New module fleet_route_school
    
    * [IMP] fleet_route_school: access rights

---
+++ fleet_route_school/models/__init__.py
@@ -0,0 +1,3 @@
+from . import fleet_route_stop
+from . import fleet_route_stop_passenger
+from . import fleet_vehicle
---
+++ fleet_route_school/models/fleet_route_stop_passenger.py
@@ -0,0 +1,16 @@
+# Copyright 2019 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class FleetRouteStopPassenger(models.Model):
+    _name = 'fleet.route.stop.passenger'
+    _description = 'Passenger'
+
+    partner_id = fields.Many2one(
+        comodel_name='res.partner', string='Passenger', required=True)
+    stop_id = fields.Many2one(
+        comodel_name='fleet.route.stop', string='Route Stop', required=True)
+    start_date = fields.Date()
+    end_date = fields.Date()
