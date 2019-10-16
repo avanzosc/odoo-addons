d1aa83763438844590adcf1e2a5502299d623085
oihane <oihanecruce@gmail.com>
2019-10-16 10:13:09 +0200

    [IMP] partner_characterization: improve with views, add pivot, restrictions (#779)

--- partner_characterization/tests/test_partner_characterization.py
+++ partner_characterization/tests/test_partner_characterization.py
@@ -1,6 +1,7 @@
 # Copyright 2018 Oihane Crucelaegui - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
+from odoo import fields
 from odoo.tests.common import SavepointCase
 
 
@@ -8,12 +9,17 @@ class TestPartnerCharacterization(SavepointCase):
     @classmethod
     def setUpClass(cls):
         super(TestPartnerCharacterization, cls).setUpClass()
+        cls.today = fields.Date.today()
         cls.activity = cls.env['res.activity'].create({
             'name': 'Activity Name',
         })
         cls.activity_type1 = cls.env['res.activity.type'].create({
             'name': 'Type Name',
             'activity_id': cls.activity.id,
+        })
+        cls.economic_data = cls.env['res.partner.economic_data'].create({
+            'partner_id': cls.env.ref('base.main_partner').id,
+            'economic_date': cls.today,
         })
 
     def test_name_get(self):
@@ -26,3 +32,7 @@ class TestPartnerCharacterization(SavepointCase):
             (self.activity_type1.id, '{} / {}'.format(
                 self.activity_type1.activity_id.name,
                 self.activity_type1.name)))
+        current_year = fields.Date.from_string(self.today).year
+        self.assertEqual(
+            '[{}] {}'.format(current_year, self.economic_data.partner_id.name),
+            self.economic_data.display_name)
