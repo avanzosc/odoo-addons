daaa6436f6e1f02e7195215cb03c3f2de976434c
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2019-11-18 11:54:18 +0100

    [FIX] stock_move_utilities: Fix product form.

--- stock_move_utilities/__openerp__.py
+++ stock_move_utilities/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
     "name": "Stock Move Utilities",
-    "version": "8.0.1.0.0",
+    "version": "8.0.1.1.0",
     "license": "AGPL-3",
     "depends": [
         "stock",
