5c00370daa5f0dde084b410aa7760351e8329f83
Oihane Crucelaegui <oihanecruce@gmail.com>
2019-11-27 12:54:43 +0100

    [ADD] New module product_usability: root category in categories

---
+++ product_usability/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2019 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Product Usability",
+    "version": "12.0.1.0.0",
+    "category": "Hidden",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "product",
+    ],
+    "data": [
+        "views/product_category_view.xml",
+    ],
+    "installable": True,
+}
