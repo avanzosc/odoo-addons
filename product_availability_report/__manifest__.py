d1c307034d4645d0f699782a5fc555a770aaa429
Thor <alfredodelafuente@avanzosc.es>
2020-02-19 11:36:50 +0100

    [FIX] product_availability_report: Put "compute_sudo".

--- product_availability_report/__manifest__.py
+++ product_availability_report/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
     "name": "Product Availability Report",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.1.0",
     "license": "AGPL-3",
     "depends": [
         "stock",
