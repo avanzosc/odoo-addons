5f75db436fa1d50e3d764c0e1b57c6471578efbc
Thor <alfredodelafuente@avanzosc.es>
2020-04-07 13:13:21 +0200

    [IMP] account_invoice_company_bank_currency: Take into account the currency of the invoice for the bank domain.

--- account_invoice_company_bank_currency/models/account_invoice.py
+++ account_invoice_company_bank_currency/models/account_invoice.py
@@ -1,6 +1,6 @@
 # Copyright 2020 Alfredo de la fuente - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from odoo import models
+from odoo import models, api
 
 
 class AccountInvoice(models.Model):
@@ -19,3 +19,15 @@ class AccountInvoice(models.Model):
         if my_bank:
             return my_bank
         return bank
+
+    @api.onchange('currency_id')
+    def onchange_currency_id(self):
+        bank_obj = self.env['res.partner.bank']
+        self.partner_bank_id = False
+        if self.company_id.partner_id and self.currency_id:
+            cond = [('partner_id', '=', self.company_id.partner_id.id),
+                    ('company_id', '=', self.company_id.id),
+                    ('currency_id', '=', self.currency_id.id)]
+            my_bank = bank_obj.search(cond, limit=1)
+            if my_bank:
+                self.partner_bank_id = my_bank.id
