6fc6b7f45854d11318ff27c94279683aa56714b3
Thor <alfredodelafuente@avanzosc.es>
2020-04-08 08:51:29 +0200

    [ADD] purchase_order_date_ready_forwarded: Two new fields in purchase order: "Date ready",and "Date forwarded".

---
+++ purchase_order_date_ready_forwarded/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2020 Adrian Revilla - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Purchase Order Date Ready Fordwarded",
+    "version": "12.0.1.0.0",
+    "category": "Purchases",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "contributors": [
+        "Adrian Revilla <adrianrevilla@avanzosc.es>",
+        "Ana Juaristi <anajuaristi@avanzosc.es>",
+    ],
+    "depends": [
+        "purchase",
+    ],
+    "data": [
+        "report/purchase_order_reports.xml",
+        "views/purchase_order_views.xml",
+    ],
+    "installable": True,
+}
