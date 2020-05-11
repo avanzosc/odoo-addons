5a7878b4d148fc93e26378cf700b38908ed779d4
Oihane Crucelaegui <oihanecruce@gmail.com>
2020-05-11 16:08:40 +0200

    [IMP] res_partner_student_fields: fix dependencies

--- res_partner_student_fields/__manifest__.py
+++ res_partner_student_fields/__manifest__.py
@@ -2,19 +2,17 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Res partner student fields',
-    'version': '12.0.1.0.0',
-    'depends': [
-        "base",
-        "education",
+    "name": "Res partner student fields",
+    "version": "12.0.1.0.0",
+    "category": "Tools",
+    "depends": [
+        "contacts_school",
     ],
-    'author':  "AvanzOSC",
-    'license': "AGPL-3",
-    'summary': '''Res partner student fields''',
-    'website': 'http://www.avanzosc.es',
-    'data': [
-        'views/res_partner_view.xml',
-        ],
-    'installable': True,
-    'auto_install': False,
+    "author":  "AvanzOSC",
+    "license": "AGPL-3",
+    "website": "http://www.avanzosc.es",
+    "data": [
+        "views/res_partner_view.xml",
+    ],
+    "installable": True,
 }
--- res_partner_student_fields/models/res_partner.py
+++ res_partner_student_fields/models/res_partner.py
@@ -3,12 +3,11 @@
 
 from odoo import fields, models
 
+
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
-    
-    nickname = fields.Char(string='Nickname')
-    priest = fields.Many2one(string='Priest', comodel_name='hr.employee')
-    university = fields.Char(string='University')
-    career = fields.Char(string='Career')
-    
-    
+    _inherit = "res.partner"
+
+    nickname = fields.Char(string="Nickname")
+    priest_id = fields.Many2one(string="Priest", comodel_name="hr.employee")
+    university = fields.Char(string="University")
+    career = fields.Char(string="Career")
