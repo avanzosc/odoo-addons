ef82fc89907a6410a8d44304c70a8649f775feeb
Adrian Revilla <adrianrg1996@gmail.com>
2020-05-11 17:46:18 +0200

    [ADD] res_partner_student_fields (#866)
    * [ADD] res_partner_student_fields
    
    * [IMP] res_partner_student_fields: fix dependencies
    
    Co-authored-by: Oihane Crucelaegui <oihanecruce@gmail.com>

---
+++ res_partner_student_fields/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright (c) 2019 Adrian Revilla <adrianrevilla@avanzosc.es> - Avanzosc S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Res partner student fields",
+    "version": "12.0.1.0.0",
+    "category": "Tools",
+    "depends": [
+        "contacts_school",
+    ],
+    "author":  "AvanzOSC",
+    "license": "AGPL-3",
+    "website": "http://www.avanzosc.es",
+    "data": [
+        "views/res_partner_view.xml",
+    ],
+    "installable": True,
+}
---
+++ res_partner_student_fields/models/res_partner.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2019 Adrian Revilla <adrianrevilla@avanzosc.es> - Avanzosc S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    nickname = fields.Char(string="Nickname")
+    priest_id = fields.Many2one(string="Priest", comodel_name="hr.employee")
+    university = fields.Char(string="University")
+    career = fields.Char(string="Career")
