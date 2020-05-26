4f9a07ad6b00efec33275281d17f903ebcc11d47
Adrian Revilla <adrianrg1996@gmail.com>
2020-05-26 18:14:54 +0200

    [ADD] purchase_partner_company (#876)
    * [ADD] purchase_partner_company
    
    * [IMP] purchase_partner_company: cleanup
    
    Co-authored-by: Oihane Crucelaegui <oihanecruce@gmail.com>

---
+++ purchase_partner_company/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright (c) 2020 Adrian Revilla <adrianrevilla@avanzosc.es> - Avanzosc S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Purchase Partner Company",
+    "version": "12.0.1.0.0",
+    "depends": [
+        "purchase",
+    ],
+    "author":  "AvanzOSC",
+    "license": "AGPL-3",
+    "website": "http://www.avanzosc.es",
+    "data": [
+        "views/purchase_partner_company_view.xml",
+    ],
+    "installable": True,
+}
