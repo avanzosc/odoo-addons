de6df9be9f2dc203fa43e53052abdfe5dd405cd2
Adrian Revilla <adrianrg1996@gmail.com>
2020-06-09 09:31:12 +0200

    [ADD] Education group mail list (#851)
    * [FIX] education_group_mail_list
    
    * [IMP] education_group_mail_list: some cleaning
    
    Co-authored-by: Oihane Crucelaegui <oihanecruce@gmail.com>

---
+++ education_group_mail_list/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright (c) 2019 Adrian Revilla <adrianrevilla@avanzosc.es> - Avanzosc S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Education Group Mail List",
+    "version": "12.0.1.0.0",
+    "category": "Marketing",
+    "depends": [
+        "education",
+        "mass_mailing",
+        "mass_mailing_partner",
+    ],
+    "author":  "AvanzOSC",
+    "license": "AGPL-3",
+    "website": "http://www.avanzosc.es",
+    "data": [
+        "views/education_group_view.xml",
+        "views/mail_mass_mailing_list_view.xml",
+    ],
+    "installable": True,
+}
---
+++ education_group_mail_list/models/__init__.py
@@ -0,0 +1,2 @@
+from . import mail_mass_mailing_list
+from . import education_group
