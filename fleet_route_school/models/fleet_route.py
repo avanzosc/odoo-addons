a86632aec116159b1f7834346b1b5caec247e8c2
oihane <oihanecruce@gmail.com>
2020-06-16 11:57:14 +0200

    [IMP] fleet_route_school: passenger can be 'other children' (#890)

--- fleet_route_school/models/fleet_route.py
+++ fleet_route_school/models/fleet_route.py
@@ -13,11 +13,16 @@ class FleetRoute(models.Model):
     passenger_ids = fields.Many2many(
         comodel_name="res.partner", string="Passengers",
         compute="_compute_passenger_ids", compute_sudo=True)
+    passenger_count = fields.Integer(
+        string="Passenger Count",
+        compute="_compute_passenger_ids", compute_sudo=True)
 
     @api.multi
     @api.depends("stop_ids", "stop_ids.passenger_ids",
                  "stop_ids.passenger_ids.partner_id")
     def _compute_passenger_ids(self):
         for route in self:
-            route.passenger_ids = route.mapped(
+            passenger_ids = route.mapped(
                 "stop_ids.passenger_ids.partner_id")
+            route.passenger_ids = passenger_ids
+            route.passenger_count = len(passenger_ids)
