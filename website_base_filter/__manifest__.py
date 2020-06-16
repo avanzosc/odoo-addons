8b8a0ba7fb63dc230bd04d4cd1538c9cc4d43a29
adrian <adrianrg1996@gmail.com>
2020-06-16 11:13:09 +0200

    [ADD] website_base_filter

---
+++ website_base_filter/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2029 Adrian Revilla - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Website base filter",
+    "version": "12.0.1.0.0",
+    "category": "website",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "portal",
+        "website",
+    ],
+    "data": [
+        "views/website_base_filter_template.xml",
+    ],
+    "installable": True,
+}
