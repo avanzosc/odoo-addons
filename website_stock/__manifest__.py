4cbea2f5b46db1db19f74184451fdd8b45daa691
adrian <adrianrg1996@gmail.com>
2020-07-14 11:55:25 +0200

    [ADD] website_stock

---
+++ website_stock/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2029 Adrian Revilla - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Website stock",
+    "version": "12.0.1.0.0",
+    "category": "website",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "portal",
+        "website",
+        "stock",
+        "website_base_filter"
+    ],
+    "data": [
+        "views/website_stock_template.xml"
+    ],
+    "installable": True,
+}
