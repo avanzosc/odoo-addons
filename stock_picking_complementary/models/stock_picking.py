b9a30fd4e8e03ebe3ba295776f9ce448dacd71f7
Mikel Arregi <mikelarregi@avanzosc.es>
2020-07-22 16:30:54 +0200

    [FIX] stock_picking_complementary: test + pep8

--- stock_picking_complementary/models/stock_picking.py
+++ stock_picking_complementary/models/stock_picking.py
@@ -1,6 +1,6 @@
 # Copyright 2020 Mikel Arregi Etxaniz - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import api, fields, models, _
+from odoo import api, fields, models
 
 
 class StockPicking(models.Model):
@@ -19,11 +19,11 @@ class StockPicking(models.Model):
 
     @api.multi
     def create_complementary_picking(self, location_id=False):
-        picking_lines = []
         complementary_pickings = []
         picking_type = self.env['stock.picking.type'].search(
             [("code", "=", "internal")], limit=1)
         for picking in self:
+            picking_lines = []
             for line in picking.move_ids_without_package:
                 comp_qty = line.product_uom_qty - line.reserved_availability
                 if comp_qty:
@@ -32,8 +32,7 @@ class StockPicking(models.Model):
                         'product_id': line.product_id.id,
                         'product_uom_qty': comp_qty,
                         'product_uom': line.product_uom.id,
-                    })
-                                         )
+                    }))
             complementary_pickings.append((picking, picking.create({
                 'origin': picking.name,
                 'picking_type_id': picking_type.id,
@@ -43,7 +42,7 @@ class StockPicking(models.Model):
                 'partner_id': picking.partner_id.id,
                 'company_id': picking.company_id.id,
                 'location_dest_id': picking.location_id.id,
-                'location_id': location_id and location_id.id
-                or picking.location_id.id,
+                'location_id': (location_id and location_id.id
+                                or picking.location_id.id),
             })))
         return complementary_pickings
--- stock_picking_complementary/tests/test_stock_picking_complementary.py
+++ stock_picking_complementary/tests/test_stock_picking_complementary.py
@@ -17,6 +17,7 @@ class StockPickingComplementaryTest(common.SavepointCase):
         cls.stock_move_model = cls.env['stock.move']
         cls.uom_unit = cls.env.ref('uom.product_uom_unit')
         cls.product_model = cls.env['product.product']
+        cls.stock_picking_type_model = cls.env['stock.picking.type']
         cls.location1 = cls.location_model.create({
             'name': 'Location1'
         })
@@ -29,11 +30,16 @@ class StockPickingComplementaryTest(common.SavepointCase):
         cls.partner = cls.partner_model.create({
             'name': 'Partner1',
         })
+        cls.stock_picking_type = cls.stock_picking_type_model.create(
+            {'name': 'test',
+             'sequence_id': 1,
+             'code': 'outgoing'})
         cls.stock_picking = cls.stock_picking_model.create({
             'res_partner': cls.partner,
-            'location_id': cls.location1,
-            'location_dest_id': cls.location2,
+            'location_id': cls.location1.id,
+            'location_dest_id': cls.location2.id,
             'show_check_availability': True,
+            'picking_type_id': cls.stock_picking_type.id,
         })
         cls.product = cls.product_model.create({
             'name': 'Product',
@@ -41,20 +47,20 @@ class StockPickingComplementaryTest(common.SavepointCase):
             'uom_id': cls.uom_unit.id,
         })
 
-
     def test_complementary_stock_picking(self):
-        self.stock_move_model.new({
-                        'name': "Move1",
-                        'product_id': self.product.id,
-                        'reserved_availability': 3,
-                        'product_uom_qty': 5,
-                        'product_uom': self.uom_unit.id,
-                        'picking_id': self.stock_picking.id
-                    })
+        self.stock_picking.move_ids_without_package = \
+            self.stock_move_model.new({
+                'name': "Move1",
+                'product_id': self.product.id,
+                'product_uom_qty': 5,
+                'product_uom': self.uom_unit.id,
+                'picking_id': self.stock_picking.id,
+                'location_id': self.location1.id,
+                'location_dest_id': self.location2.id,
+            })
         new_picking_id = self.stock_picking.create_complementary_picking(
             location_id=self.location3)[0][1]
-        new_line = self.stock_picking_model.browse(
-            new_picking_id).move_ids_without_package[0]
-        self.assertEqual(new_line.product_uom_qty, 2)
+        new_line = new_picking_id.move_ids_without_package[0]
+        self.assertEqual(new_line.product_uom_qty, 5)
         self.assertEqual(new_line.location_id, self.location3)
         self.assertEqual(new_line.location_dest_id, self.location1)
