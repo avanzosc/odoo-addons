14eb1a3391ceda495579c5d354fab6e50d511fea
Mikel Arregi <mikelarregi@avanzosc.es>
2020-08-06 15:27:24 +0200

    [ADD] account_invoice_lines_recalculate

---
+++ account_invoice_lines_recalculate/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2020 Mikel Arregi Etxaniz - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Invoice lines recalculate",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "account",
+    ],
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "category": "Tools",
+    "data": [
+        "views/account_invoice_view.xml",
+        "views/product_category_view.xml",
+        "views/product_product_view.xml",
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+}
---
+++ account_invoice_lines_recalculate/models/account_invoice.py
@@ -0,0 +1,25 @@
+# Copyright 2020 Mikel Arregi Etxaniz - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import api, fields, models
+
+
+class AccountInvoiceLine(models.Model):
+    _inherit = "account.invoice.line"
+
+    product_discount_percentage = fields.Float(
+        related="product_id.discount_percentage")
+    product_exclude_discount_categ = fields.Boolean(
+        related="product_id.categ_id.discounts_exclude")
+
+    @api.multi
+    def recalculate_invoice_line(self):
+        for invoice_line in self:
+            subtotal = sum(invoice_line.invoice_id.invoice_line_ids.filtered(
+                lambda x: not x.product_id.categ_id.discounts_exclude and
+                    x.product_id.discount_percentage == 0).mapped(
+                'price_subtotal'))
+            discount_percentage = invoice_line.product_id.discount_percentage
+            exclude = invoice_line.product_id.categ_id.discounts_exclude
+            if exclude and discount_percentage > 0:
+                invoice_line.price_unit = subtotal * (
+                        discount_percentage / 100) * -1
