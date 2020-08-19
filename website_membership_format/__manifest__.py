2b5ff84da1a140529cf7b1db08fd0b2332ef4833
adrian <adrianrg1996@gmail.com>
2020-08-19 10:03:23 +0200

    [ADD] website_membership_format

---
+++ website_membership_format/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2020 Adrian Revilla - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Website Membership Format",
+    "version": "13.0.1.0.0",
+    "category": "website",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "portal",
+        "purchase",
+        "website_membership",
+        "geonames_delivery_zone_link"
+    ],
+    "data": [
+        "views/website_membership_format_template.xml"
+    ],
+    "installable": True,
+}
