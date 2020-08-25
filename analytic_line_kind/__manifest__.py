93a49451639070172a99b21d96664d7a13907b73
Oihane Crucelaegui <oihanecruce@gmail.com>
2020-08-25 12:04:13 +0200

    [MIG] analytic_line_kind: Migration to 13.0

--- analytic_line_kind/__manifest__.py
+++ analytic_line_kind/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
     "name": "Analytic Line Classification",
-    "version": "11.0.1.1.1",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "depends": [
         "account",
