a198d1a585d92d98725c3c59227f57eca09deefe
Mikel Arregi <mikelarregi@avanzosc.es>
2020-08-28 14:07:39 +0200

    [ADD] mrp_workorder_moves_view

---
+++ mrp_workorder_moves_view/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2020 Mikel Arregi Etxaniz - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Workorder moves in view",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "mrp",
+    ],
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "category": "Manufacturing",
+    "data": [
+        "views/mrp_workorder_view.xml"
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+}
