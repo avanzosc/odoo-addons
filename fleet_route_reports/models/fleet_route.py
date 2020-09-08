01f2a66e6b60cbf336d4171f0e4b6a296e1c6398
oihane <oihanecruce@gmail.com>
2020-09-08 14:27:15 +0200

    [IMP] fleet_route_reports: add issues after all stop dates (#970)

--- fleet_route_reports/models/fleet_route.py
+++ fleet_route_reports/models/fleet_route.py
@@ -1,7 +1,6 @@
 # Copyright 2020 Adrian Revilla - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from odoo import models, api
-from datetime import datetime
+from odoo import api, fields, models
 
 
 class FleetRoute(models.Model):
@@ -14,4 +13,36 @@ class FleetRoute(models.Model):
 
     def _get_actual_date(self):
         self.ensure_one()
-        return datetime.now().strftime('%Y-%m-%d')
+        return fields.Date.context_today(self).strftime('%Y-%m-%d')
+
+    def current_issues(self):
+        self.ensure_one()
+        route_stops = self.mapped("stop_ids")
+        today = fields.Date.context_today(self)
+        issue_obj = self.env["fleet.route.support"]
+        low_issues = issue_obj.search([
+            ("date", "=", today),
+            ("low_stop_id", "in", route_stops.ids),
+        ])
+        high_issues = issue_obj.search([
+            ("date", "=", today),
+            ("high_stop_id", "in", route_stops.ids),
+        ])
+        notes = issue_obj.search([
+            ("type", "=", "note"),
+            ("date", "=", today),
+            ("student_id", "in", self.mapped("passenger_ids").ids),
+        ])
+        return low_issues, high_issues, notes
+
+    def current_issue_by_type(self, type=False):
+        if not type:
+            return
+        low_issues, high_issues, notes = self.current_issues()
+        if type == "low":
+            return low_issues
+        elif type == "high":
+            return high_issues
+        elif type == "notes":
+            return notes
+        return low_issues | high_issues | notes
