6ee069ec2097cfcfb89625e3fccc3f925261daa9
adrian <adrianrg1996@gmail.com>
2020-10-08 07:20:44 +0200

    [ADD] website_sale_optional_products_delete

---
+++ website_sale_optional_products_delete/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright (c) 2020 Adrian Revilla <adrianrevilla@avanzosc.es> - Avanzosc S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Website Sale Optional Products Delete',
+    'version': '12.0.1.0.0',
+    "category": "Website",
+    'depends': [
+        'sale_management'
+    ],
+    'author':  "AvanzOSC",
+    'license': "AGPL-3",
+    'website': 'http://www.avanzosc.es',
+    'data': [
+        'views/sale_order_template.xml'
+        ],
+    'installable': True,
+    'auto_install': False,
+}
