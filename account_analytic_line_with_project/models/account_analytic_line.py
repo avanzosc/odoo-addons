204df9d1634dcd1cd6e48987bb6420482896d101
Thor <alfredodelafuente@avanzosc.es>
2020-10-29 13:55:57 +0100

    [ADD] account_analytic_line_with_project: Apunte analítico siempre informado con proyecto.

---
+++ account_analytic_line_with_project/models/account_analytic_line.py
@@ -0,0 +1,27 @@
+# Copyright 2020 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+from odoo import models, api
+
+
+class AccountAnalyticLine(models.Model):
+    _inherit = 'account.analytic.line'
+
+    @api.model
+    def create(self, vals):
+        if ('account_id' in vals and vals.get('account_id', False) and
+           ('project_id' not in vals or not vals.get('project_id', False))):
+            cond = [('analytic_account_id', '=',  vals.get('account_id'))]
+            project = self.env['project.project'].search(cond, limit=1)
+            if len(project) == 1:
+                vals['project_id'] = project.id
+        return super(AccountAnalyticLine, self).create(vals)
+
+    @api.multi
+    def write(self, vals):
+        if ('account_id' in vals and vals.get('account_id', False) and
+           ('project_id' not in vals or not vals.get('project_id', False))):
+            cond = [('analytic_account_id', '=',  vals.get('account_id'))]
+            project = self.env['project.project'].search(cond, limit=1)
+            if len(project) == 1:
+                vals['project_id'] = project.id
+        return super(AccountAnalyticLine, self).write(vals)
