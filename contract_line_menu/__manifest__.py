81eb3c5edf4177e2b463cbead355ab8785f9150b
Thor <alfredodelafuente@avanzosc.es>
2020-11-16 15:28:21 +0100

    [ADD] contract_line_menu: New menu for "contrac line" object.

---
+++ contract_line_menu/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2020 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Contract Line Menu",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "contract",
+    ],
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "category": "Contract Management",
+    "data": [
+        "views/contract_line_views.xml",
+    ],
+    "installable": True,
+}
