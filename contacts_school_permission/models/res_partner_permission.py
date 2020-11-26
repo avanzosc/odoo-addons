d3449a4b5ace418d2b06ff85280a4f01cc77ec7c
Leire Martinez de Santos <leiremartinez@avanzosc.es>
2020-11-26 16:11:18 +0100

    [IMP] contacts_school_permission: added new fields

--- contacts_school_permission/models/res_partner_permission.py
+++ contacts_school_permission/models/res_partner_permission.py
@@ -35,7 +35,6 @@ class ResPartnerPermission(models.Model):
         required=True)
     type_description = fields.Text(
         string='Type Description', related='type_id.description', store=True)
-    description = fields.Text(string='Description')
     state = fields.Selection(
         selection=[('yes', 'Signed'),
                    ('no', 'Refused'),
@@ -47,15 +46,22 @@ class ResPartnerPermission(models.Model):
     end_date = fields.Date(string='End Date')
     attachment_doc = fields.Binary(string='Attached Document')
     signature = fields.Binary(string='Signature', attachment=True)
+    signature_status = fields.Selection(
+        selection=[('yes', 'Signed'),
+                   ('no', 'Refused')],
+        string='Signature Status')
     signature_date = fields.Date(string='Signature Date')
     signature_2 = fields.Binary(string='Signature 2', attachment=True)
+    signature_status_2 = fields.Selection(
+        selection=[('yes', 'Signed'),
+                   ('no', 'Refused')],
+        string='Signature Status 2')
     signature_date_2 = fields.Date(string='Signature Date 2')
-    comments = fields.Text(string="Comments")
+    description = fields.Text(string="Comments")
     refuser_ids = fields.Many2many(
         comodel_name='res.partner', string='Refuser Signers',
-        relation='rel_res_partner_refusers',
-        column1='permission_id', column2='refuser_id',
-    )
+        compute="_compute_refuser_ids",
+        domain="[('id', 'in', allowed_signer_ids)]")
 
     @api.depends('partner_id', 'partner_id.child2_ids',
                  'partner_id.child2_ids.relation',
@@ -68,13 +74,24 @@ class ResPartnerPermission(models.Model):
                 ).mapped('responsible_id'))
 
     @api.depends('signature', 'signature_2',
-                 'signer_id', 'signer_id_2')
+                 'signer_id', 'signer_id_2',
+                 'signature_status', 'signature_status_2')
     def _compute_signer_ids(self):
         for record in self:
-            if record.signature:
+            if record.signature and record.signature_status == 'yes':
                 record.signer_ids |= record.signer_id
-            if record.signature_2:
+            if record.signature_2 and record.signature_status_2 == 'yes':
                 record.signer_ids |= record.signer_id_2
+
+    @api.depends('signature', 'signature_2',
+                 'signer_id', 'signer_id_2',
+                 'signature_status', 'signature_status_2')
+    def _compute_refuser_ids(self):
+        for record in self:
+            if record.signature and record.signature_status == 'no':
+                record.refuser_ids |= record.signer_id
+            if record.signature_2 and record.signature_status_2 == 'no':
+                record.refuser_ids |= record.signer_id_2
 
     @api.multi
     def _get_report_base_filename(self):
