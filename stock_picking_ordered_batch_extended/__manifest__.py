a25c4f9564954fc726d8fe4503de1747e276d0f0
Tu Nombre <you@example.com>
2021-02-15 12:42:28 +0100

    [ADD] stock_picking_ordered_batch_extended

---
+++ stock_picking_ordered_batch_extended/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2021 Daniel Campos - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Stock Picking Ordered Batch Extended",
+    "version": "12.0.1.0.0",
+    "author": "AvanzOSC",
+    "license": "AGPL-3",
+    "category": 'Hidden',
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "stock_picking_ordered",
+        "stock_picking_batch_extended",
+    ],
+    'data': [
+        "views/stock_picking_ordered_batch_extended_view.xml",
+    ],
+    'auto_install': True,
+}
