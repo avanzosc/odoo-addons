d6fd2c7ce5dd8f28ec205d1c0cb954d14eb99cb3
Tu Nombre <you@example.com>
2021-04-19 13:59:55 +0200

    [ADD] education_center:Creates a boolean that describes if one contact is a school or not.

---
+++ education_center/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2021 Berezi - Iker - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Education Center",
+    'version': '14.0.1.0.0',
+    "author": "Avanzosc",
+    "category": "Sales/CRM",
+    "depends": [
+        "partner_contact_type",
+    ],
+    "data": [
+        "views/res_partner_views.xml",
+        "views/res_partner_type_views.xml",
+    ],
+    "license": "AGPL-3",
+    'installable': True,
+}
