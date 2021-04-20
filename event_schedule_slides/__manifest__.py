2f3d592ead3ba5b1f2977b60dc2a9f3d03f08daf
Tu Nombre <you@example.com>
2021-04-20 11:56:16 +0200

    [ADD] Event_schedule_slides: Events in slides.

---
+++ event_schedule_slides/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2021 Berezi - Iker - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Event Schedule Slides",
+    'version': '14.0.1.0.0',
+    "author": "Avanzosc",
+    "category": "Website/eLearning",
+    "depends": [
+        "event_schedule",
+        "website_slides"
+    ],
+    "data": [
+        "views/event_event_views.xml",
+        "views/slide_channel_views.xml"
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": True,
+}
