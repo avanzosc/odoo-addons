dde69834c7a4e198970f44f9b510b7c8afe63fbd
Berezi <82805447+Berezi@users.noreply.github.com>
2021-04-21 09:21:49 +0200

    [ADD] partner_contact_type: Creats a new object called contact type. (#1061)

---
+++ partner_contact_type/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2021 Berezi - Iker - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Partner Contact type",
+    'version': '14.0.1.0.0',
+    "author": "Avanzosc",
+    "category": "Sales/CRM",
+    "depends": [
+        "contacts",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/res_partner_views.xml",
+        "views/res_partner_type_views.xml",
+    ],
+    "license": "AGPL-3",
+    'installable': True,
+}
