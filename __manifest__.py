ba249f7224d94f3eef07631e43114c5ec0a5ecb5
Tu Nombre <you@example.com>
2021-04-21 09:02:55 +0200

    [ADD] event_track_analytic: Project in event, and analytic lines in event and event tracks.

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2021 Berezi - Iker - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Event Track Analytic",
+    'version': '14.0.1.0.0',
+    "author": "Avanzosc",
+    "category": "Marketing/Events",
+    "depends": [
+        "project",
+        "analytic",
+        "website_event_track"
+    ],
+    "data": [
+        "views/event_event_views.xml",
+        "views/event_track_views.xml",
+        "views/account_analytic_line_views.xml",
+    ],
+    "license": "AGPL-3",
+    'installable': True,
+}
