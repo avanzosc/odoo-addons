6535b841791442c360f0a9881281ed1af718cc92
Berezi <82805447+Berezi@users.noreply.github.com>
2021-04-21 09:21:49 +0200

    [ADD] partner_contact_type: Creats a new object called contact type. (#1061)

---
+++ partner_contact_type/models/res_partner.py
@@ -0,0 +1,10 @@
+# Copyright 2021 Berezi - Iker - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import models, fields
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    contact_type_id = fields.Many2one(
+        string='Contact type', comodel_name='res.partner.type')
---
+++ partner_contact_type/models/res_partner_type.py
@@ -0,0 +1,10 @@
+# Copyright 2021 Berezi - Iker - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import models, fields
+
+
+class ResPartnerType(models.Model):
+    _name = 'res.partner.type'
+    _description = 'Partner types'
+
+    name = fields.Char(string="Description", required=True)
