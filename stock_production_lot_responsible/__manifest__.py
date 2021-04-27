f8d4718f63161a608793a69e540f3d228c0e30a5
Tu Nombre <you@example.com>
2021-04-27 13:34:34 +0200

    [ADD] stock_production_lot_responsible

---
+++ stock_production_lot_responsible/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2021 Berezi - Iker - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Stock Production Lot Responsible",
+    'version': '14.0.1.0.0',
+    "author": "Avanzosc",
+    "category": "Inventory",
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        "views/stock_production_lot_views.xml",
+        "views/stock_quant_views.xml",
+    ],
+    "license": "AGPL-3",
+    'installable': True,
+}
