067ffab8c6094e724b01df3219a4edf50d262270
Leire Martinez de Santos <leiremartinez@avanzosc.es>
2021-04-27 16:04:04 +0200

    [ADD] website_portal_filter: add filters for portal my/account

---
+++ website_portal_filter/controllers/portal.py
@@ -0,0 +1,119 @@
+
+from odoo import fields, http, _
+from dateutil.relativedelta import relativedelta
+from odoo.http import request
+from odoo.addons.portal.controllers.portal import pager as portal_pager
+from datetime import date, datetime, timedelta
+from odoo.addons.portal.controllers.portal import CustomerPortal
+
+
+class CustomerPortal(CustomerPortal):
+
+    def _get_date_filters(self):
+        return {
+            'all': {'input': 'all', 'label': _('All dates')},
+            'today': {'input': 'today', 'label': _('Today'),
+                      'domain': [("confirmation_date", '=', date.today())]},
+            'week': {'input': 'week', 'label': _('Last week'),
+                     'domain': [('confirmation_date', '<=', date.today()),
+                                ('confirmation_date', '>=', date.today()
+                                 - timedelta(weeks=1))]},
+            'month': {'input': 'month', 'label': _('This month'),
+                      'domain': [('is_current_month', '=', 1)]},
+            'year': {'input': 'year', 'label': _('This year'),
+                     'domain': [('is_current_year', '=', 1)]}
+            }
+
+    def filter_data(self, args):
+        domain = []
+
+        if 'customer' in args and args.get('customer') != 'all':
+            domain += [('partner_id', '=', int(args.get('customer')))]
+
+        if 'search' in args and args.get('search') != '':
+            search = args.get('search')
+            domain += ['|', ('name', 'ilike', search),
+                       ('partner_id.name', 'ilike', search)]
+
+        if 'date' in args and args.get('date') != 'all':
+            _date = args.get('date')
+            date_filters = self._get_date_filters()
+            domain += date_filters[_date]['domain']
+
+        return domain
+
+    @http.route(['/my/orders', '/my/orders/page/<int:page>'],
+                type='http', auth="user", website=True)
+    def portal_my_orders(self, page=1, date_begin=None, date_end=None,
+                         sortby=None, search_in='all', **kw):
+        res = super(CustomerPortal, self).portal_my_orders(
+            page, date_begin, date_end, sortby, **kw)
+        SaleOrder = request.env['sale.order']
+
+        user_id = request.env.user
+        domain = [
+            ('state', 'in', ['sale', 'done'])
+        ]
+        if user_id.is_commercial != 'all':
+            domain += [('partner_id', '=', request.env.user.partner_id.id)]
+
+        all_orders = SaleOrder.sudo().search(domain)
+
+        order_partner_ids = None
+        if all_orders and user_id.is_commercial != 'no':
+            order_partner_ids = all_orders.mapped('partner_id')
+
+        domain += self.filter_data(kw)
+
+        searchbar_inputs = {
+            'all': {'input': 'all', 'label': _('Search in All')},
+        }
+        # default sortby order
+        if not sortby:
+            sortby = 'date'
+        sort_order = res.qcontext.get('searchbar_sortings')[sortby]['order']
+
+        filtered_orders = self.filter_by_date(all_orders, kw)
+        domain += [('id', 'in', filtered_orders.ids)]
+
+        # count for pager
+        order_count = SaleOrder.sudo().search_count(domain)
+
+        # pager
+        pager = portal_pager(
+            url="/my/orders",
+            url_args={'date_begin': date_begin, 'date_end': date_end,
+                      'sortby': sortby},
+            total=order_count,
+            page=page,
+            step=self._items_per_page
+        )
+        # content according to pager and archive selected
+        orders = SaleOrder.sudo().search(
+            domain, order=sort_order, limit=self._items_per_page,
+            offset=pager['offset'])
+        request.session['my_orders_history'] = orders.ids[:100]
+
+        date_filters = self._get_date_filters()
+        res.qcontext.update({
+            'orders': orders,
+            'pager': pager,
+            'search_in': search_in,
+            'search': kw.get('search'),
+            'date':  kw.get('date'), #if 'date' in kw else 'all',
+            'customer': kw.get('customer'), # if 'customer' in kw else 'all',
+            'searchbar_inputs': searchbar_inputs,
+            'searchbar_filters': [],
+            'order_partner_ids': order_partner_ids,
+            'filterby': kw.get('filterby'),
+            'date_filters': date_filters,
+        })
+        return res
+
+    def filter_by_date(self, orders, args):
+        if 'date' in args and args.get('date') != 'all':
+            if args.get('date') == 'month':
+                orders = orders.filtered(lambda l: l.is_current_month == 1)
+            if args.get('date') == 'year':
+                orders = orders.filtered(lambda l: l.is_current_year == 1)
+        return orders
---
+++ website_portal_filter/models/sale.py
@@ -0,0 +1,32 @@
+# Copyright 2020 Leire Martinez de Santos - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import models, fields, api
+from datetime import datetime
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    is_current_year = fields.Boolean(compute='_check_current_year',
+                                     string="Is current year")
+    is_current_month = fields.Boolean(compute='_check_current_month',
+                                      string="Is current month")
+
+    @api.multi
+    @api.depends('confirmation_date')
+    def _check_current_year(self):
+        today = datetime.today()
+        for res in self:
+            if res.confirmation_date and\
+                    today.year == res.confirmation_date.year:
+                res.is_current_year = True
+
+    @api.multi
+    @api.depends('confirmation_date', 'is_current_year')
+    def _check_current_month(self):
+        today = datetime.today()
+        for res in self:
+            if res.is_current_year and \
+                    today.month == res.confirmation_date.month:
+                res.is_current_month = True
