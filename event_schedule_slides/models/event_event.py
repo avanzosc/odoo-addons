771a12abfaa7bd6bc21ba72a26c7b5cf703fdeca
Oihane Crucelaegui <oihanecruce@gmail.com>
2021-04-27 14:39:47 +0200

    [FIX] event_schedule_slides: column misspelled

--- event_schedule_slides/models/event_event.py
+++ event_schedule_slides/models/event_event.py
@@ -1,11 +1,15 @@
 # Copyright 2021 Berezi - Iker - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import models, fields
+from odoo import fields, models
 
 
 class EventEvent(models.Model):
-    _inherit = 'event.event'
+    _inherit = "event.event"
 
     slides_ids = fields.Many2many(
-        string='Courses', comodel_name='slide.channel',
-        relation='rel_event_slides', columm1='event_ids', columm2='slides_ids')
+        string="Courses",
+        comodel_name="slide.channel",
+        relation="rel_event_slides",
+        column1="event_id",
+        column2="slides_id",
+    )
--- event_schedule_slides/models/slide_channel.py
+++ event_schedule_slides/models/slide_channel.py
@@ -1,11 +1,15 @@
 # Copyright 2021 Berezi - Iker - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import models, fields
+from odoo import fields, models
 
 
 class SlideChannel(models.Model):
-    _inherit = 'slide.channel'
+    _inherit = "slide.channel"
 
     event_ids = fields.Many2many(
-        string='Event', comodel_name='event.event',
-        relation='rel_event_slides', columm1='slides_ids', columm2='event_ids')
+        string="Event",
+        comodel_name="event.event",
+        relation="rel_event_slides",
+        column1="slides_id",
+        column2="event_id",
+    )
