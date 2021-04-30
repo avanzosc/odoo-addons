8404e2a9f4c745a8454e2996cdf3399d0f6398a6
Leire Martinez de Santos <leiremartinez@avanzosc.es>
2021-04-30 09:10:30 +0200

    [ADD] event_attendee_birthdate: add birthdate and age to event attendee

---
+++ event_attendee_birthdate/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2021 Leire Martinez de Santos - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    'name': "Event Partner Birthdate",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    'category': 'website',
+    'version': '14.0.1.0.0',
+    'depends': ['event', 'website_event'],
+    'data': [
+        'views/event_templates.xml',
+        'views/event_views.xml'
+    ]
+}
