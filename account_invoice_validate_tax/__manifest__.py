97611e9228153f616a0fa2781e48d2d10cb4cbd7
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2021-05-03 15:15:16 +0200

    [ADD] account_invoice_validate_tax: When validating an invoice, validate that taxes have been entered on its lines. (#1088)

---
+++ account_invoice_validate_tax/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2021 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Account Invoice Validate Tax",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "category": "Invoicing Management",
+    "depends": [
+        "account",
+    ],
+    "data": [
+    ],
+    "installable": True,
+    "auto_install": False,
+}
---
+++ account_invoice_validate_tax/models/account_invoice.py
@@ -0,0 +1,18 @@
+# Copyright 2021 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+from odoo import models, api, _
+from odoo.exceptions import ValidationError
+
+
+class AccountInvoice(models.Model):
+    _inherit = 'account.invoice'
+
+    @api.multi
+    def action_invoice_open(self):
+        for invoice in self:
+            for line in invoice.invoice_line_ids:
+                if not line.invoice_line_tax_ids:
+                    message = _(u"You must introduce tax to the line with "
+                                "description: {}").format(line.name)
+                    raise ValidationError(message)
+        return super(AccountInvoice, self).action_invoice_open()
