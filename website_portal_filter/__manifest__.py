a08fdd3cb663751c1085590dc18de408337aba07
leiremartinezdesantos <72156354+leiremartinezdesantos@users.noreply.github.com>
2021-05-11 16:57:02 +0200

    [ADD] website_portal_filter: add filters for portal my/account (#1080)
    * [ADD] website_portal_filter: add filters for portal my/account
    
    * [FIX] website_portal_filter: fix coding
    
    * [IMP] website_portal_filter: fix log messages
    
    * [IMP] website_portal_filter: fix dependencies
    
    * [IMP] website_portal_filter: fix dependencies
    
    * [IMP] website_portal_filter: change base domain filter

---
+++ website_portal_filter/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2021 Leire Martinez de Santos - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Website Portal Filters",
+    "version": "12.0.1.0.0",
+    "category": "website",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "sale",
+        "purchase",
+        "portal",
+        "website",
+    ],
+    "data": [
+        "views/website_sale_filter_template.xml",
+    ],
+    "installable": True,
+}
