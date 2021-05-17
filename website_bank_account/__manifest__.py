3ea389eb68ca458bd7071b9f891451af82f7eeac
leiremartinezdesantos <72156354+leiremartinezdesantos@users.noreply.github.com>
2021-05-17 09:34:54 +0200

    [ADD] website_bank_account: require bank account field in payment process (#1094)

---
+++ website_bank_account/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2021 Leire Martinez de Santos - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    'name': "Website Bank Account",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    'category': 'website',
+    'version': '14.0.1.0.0',
+    'depends': ['website_sale'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/templates.xml',
+    ]
+}
---
+++ website_bank_account/controllers/main.py
@@ -0,0 +1,65 @@
+
+from odoo import http, tools, _
+from odoo.http import request
+from odoo.addons.website_sale.controllers.main import WebsiteSale
+
+
+class WebsiteSale(WebsiteSale):
+
+    @http.route()
+    def checkout(self, **post):
+        order = request.website.sale_get_order()
+        if not order.partner_id.bank_ids:
+            return request.redirect('/shop/address?partner_id=' + str(order.partner_id.id))
+
+        return super(WebsiteSale, self).checkout(**post)
+
+    def checkout_form_validate(self, mode, all_form_values, data):
+        error, error_message = super(WebsiteSale, self).checkout_form_validate(mode, all_form_values, data)
+
+        user = request.env['res.users'].browse(request.session.uid)
+        partner = user.partner_id
+
+        # account validation
+        if data.get('bank_acc') and not self.is_unique_account(data.get('bank_acc') and not partner.bank_ids):
+            error["bank_acc"] = 'error'
+            error_message.append(_('Invalid Bank Account! Bank account already registered.'))
+
+        return error, error_message
+
+    def is_unique_account(self, account):
+        bank_ids = request.env['res.partner.bank'].sudo().search([])
+        for bank in bank_ids:
+            if account == bank.acc_number:
+                return False
+        return True
+
+    @http.route()
+    def address(self, **kw):
+        res = super(WebsiteSale, self).address(**kw)
+        order = res.qcontext.get('website_sale_order')
+        user = request.env['res.users'].browse(request.session.uid)
+        partner = order.partner_id if order else user.partner_id
+
+        acc_error = False
+        error = res.qcontext.get('error')
+        if error and 'bank_acc' in error:
+            acc_error = True
+        values = {}
+        if partner:
+            bank_ids = partner.bank_ids
+            if bank_ids:
+                values.update({'bank_acc_nr': bank_ids[0].acc_number})
+            if 'bank_acc' in kw and not bank_ids:
+                bank_account = kw.get('bank_acc')
+                partner_bank_acc = request.env['res.partner.bank'].sudo().search([
+                    ('partner_id', '=', partner.id),
+                    ('acc_number', '=', bank_account)
+                ])
+                if not partner_bank_acc and not acc_error:
+                    new_acc = request.env['res.partner.bank'].sudo().create(
+                        {'partner_id': partner.id,
+                         'acc_number': bank_account
+                         })
+        res.qcontext.update(values)
+        return res
