a893ffb8522261efc996e69a408822cdad3aaaaf
Avanzosc <leiremartinez@avanzosc.es>
2021-05-19 10:33:59 +0200

    [IMP] event_registration_generate_email: add optional email option

--- event_registration_generate_email/controllers/main.py
+++ event_registration_generate_email/controllers/main.py
@@ -9,12 +9,15 @@ class WebsiteEventController(WebsiteEventController):
     def _create_attendees_from_registration_post(self, event, registration_data):
         res = super(WebsiteEventController, self)._create_attendees_from_registration_post(event,registration_data)
         for registration in res:
-            name = registration.name
-            mail_name = name.replace(" ",".")
+            if not registration.email:
+                name = registration.name
+                mail_name = name.replace(" ",".")
 
-            company_id = request.env.user.company_id
-            user_default_domain = company_id.portal_user_default_domain if company_id else ''
+                company_id = request.env.user.company_id
+                user_default_domain = company_id.portal_user_default_domain if company_id else ''
 
-            registration.email = '%s@%s' % (mail_name.lower(), user_default_domain)
+                registration.email = mail_name.lower()
+                if user_default_domain:
+                    registration.email += '@%s' % user_default_domain
 
         return res
