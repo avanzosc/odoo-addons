617ceef45c420f082bf48dd5b47a06dc4ab065b8
Tu Nombre <you@example.com>
2021-05-19 17:02:48 +0200

    [IMP] account_banking_mandate_usability: add counts for bank_account and mandates

---
+++ account_banking_mandate_usability/models/res_partner.py
@@ -0,0 +1,23 @@
+# Copyright 2021 Daniel Campos - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from odoo import api, fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    bank_acc_count = fields.Integer(
+        compute="_compute_bank_acc_count", string="Number of Bank accounts",
+        readonly=True)
+
+    def _compute_bank_acc_count(self):
+        bank_acc_data = self.env["res.partner.bank"].read_group(
+            [("partner_id", "in", self.ids)], ["partner_id"], ["partner_id"]
+        )
+        mapped_data = {
+            bank_acc["partner_id"][0]: bank_acc["partner_id_count"]
+            for bank_acc in bank_acc_data
+        }
+        for partner in self:
+            partner.bank_acc_count = mapped_data.get(partner.id, 0)
