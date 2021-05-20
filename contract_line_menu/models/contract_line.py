340469f010b9e563e203383bbe2f08c9aad8b502
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2021-05-20 14:47:04 +0200

    [ADD] contract_line_menu: New menu for "Contract line" object. (#1108)

---
+++ contract_line_menu/models/contract_line.py
@@ -0,0 +1,21 @@
+# Copyright 2021 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import models, fields
+
+
+class ContractLine(models.Model):
+    _inherit = 'contract.line'
+    _order = "contract_type, partner_id, sequence,id"
+
+    contract_type = fields.Selection(
+        selection=[("sale", "Customer"), ("purchase", "Supplier")],
+        index=True, related='contract_id.contract_type', store=True)
+    partner_id = fields.Many2one(
+        string='Partner', comodel_name='res.partner',
+        related='contract_id.partner_id', store=True)
+    journal_id = fields.Many2one(
+        string="Journal", comodel_name="account.journal",
+        related='contract_id.journal_id', store=True)
+    pricelist_id = fields.Many2one(
+        string="Pricelist", comodel_name="product.pricelist",
+        related='contract_id.pricelist_id', store=True)
