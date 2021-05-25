6e162ac666dbb0238ba4cea320771c92684fc233
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2021-05-25 09:34:28 +0200

    [IMP] event_slides: Crear participante de evento en "participante" de curso. (#1116)

--- event_slides/models/__init__.py
+++ event_slides/models/__init__.py
@@ -1,2 +1,4 @@
 from . import event_event
 from . import slide_channel
+from . import event_registration
+from . import slide_channel_partner
---
+++ event_slides/models/event_registration.py
@@ -0,0 +1,23 @@
+# Copyright 2021 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import models
+
+
+class EventRegistration(models.Model):
+    _inherit = "event.registration"
+
+    def action_confirm(self):
+        super(EventRegistration, self).action_confirm()
+        for registration in self.filtered(lambda x: x.event_id.slides_ids):
+            registration.create_student_in_courses()
+
+    def create_student_in_courses(self):
+        for course in self.event_id.slides_ids:
+            vals = self._catch_values_for_slide_channel_partner(course)
+            self.env['slide.channel.partner'].create(vals)
+
+    def _catch_values_for_slide_channel_partner(self, course):
+        vals = {'partner_id': self.student_id.id,
+                'event_registration_id': self.id,
+                'channel_id': course.id}
+        return vals
