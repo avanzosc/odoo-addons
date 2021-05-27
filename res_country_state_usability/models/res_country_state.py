59d19d43226a647e2090975120e3bb3ac17d4554
oihane <oihanecruce@gmail.com>
2021-05-27 12:29:12 +0200

    [ADD] New module res_country_state_usability (#1118)

---
+++ res_country_state_usability/models/res_country_state.py
@@ -0,0 +1,23 @@
+# Copyright 2021 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, fields, models
+
+
+class ResCountryState(models.Model):
+    _inherit = "res.country.state"
+
+    active = fields.Boolean(
+        string="Active",
+        default=True,
+        help="Set active to false to hide the state without removing it.")
+
+    @api.multi
+    def write(self, vals):
+        res = super(ResCountryState, self).write(vals) if vals else True
+        if "active" in vals and vals["active"]:
+            # unarchiving a state does it on its country, too
+            self.with_context(active_test=False).mapped("country_id").write(
+                {"active": vals["active"]})
+        return res
+
