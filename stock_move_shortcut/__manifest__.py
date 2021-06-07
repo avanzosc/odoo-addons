d521e70bc0f5477ce8ff648dc4674f84eae8b9a2
Avanzosc <leiremartinez@avanzosc.es>
2021-06-07 14:06:28 +0200

    [ADD] stock_move_shortcut: add chortcut to lines

---
+++ stock_move_shortcut/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2021 Leire Martinez de Santos - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Stock Move Shortcut",
+    "version": "12.0.1.0.0",
+    "category": "Tools",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        "views/stock_move_view.xml"
+    ],
+    "installable": True,
+}
