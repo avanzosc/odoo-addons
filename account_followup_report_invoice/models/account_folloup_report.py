bf64a6a649ca09f6b85200a14f119ec0ee8fe555
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2021-06-09 07:08:31 +0200

    [IMP] account_folloup_report_invoice: Meter cabecera para factura. (#1139)

--- account_followup_report_invoice/models/account_folloup_report.py
+++ account_followup_report_invoice/models/account_folloup_report.py
@@ -1,10 +1,19 @@
 # Copyright 2021 Alfredo de la fuente - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from odoo import models
+from odoo import models, _
 
 
 class AccountFollowupReport(models.AbstractModel):
     _inherit = 'account.followup.report'
+
+    def _get_columns_name(self, options):
+        headers = super(AccountFollowupReport, self)._get_columns_name(options)
+        for header in headers:
+            if not header:
+                header.update(
+                    {'name': _('Invoice'),
+                     'style': 'text-align:center; white-space:nowrap;'})
+        return headers
 
     def _get_lines(self, options, line_id=None):
         lines = super(AccountFollowupReport, self)._get_lines(
