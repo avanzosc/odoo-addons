2341a34dec3b6a4676718d7447304a06bc1418c6
Leire Martinez de Santos <leiremartinez@avanzosc.es>
2021-06-09 10:01:50 +0200

    [IMP] event_registration_generate_email: require login on registration confirm

--- event_registration_generate_email/controllers/main.py
+++ event_registration_generate_email/controllers/main.py
@@ -1,20 +1,34 @@
 # Copyright 2021 Leire Martinez de Santos - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
+from odoo import http
 from odoo.http import request
 from odoo.addons.website_event.controllers.main import WebsiteEventController
 
+
 class WebsiteEventController(WebsiteEventController):
 
-    def _create_attendees_from_registration_post(self, event, registration_data):
-        res = super(WebsiteEventController, self)._create_attendees_from_registration_post(event,registration_data)
+    def _create_attendees_from_registration_post(
+            self, event, registration_data):
+        res = super(WebsiteEventController, self
+                    )._create_attendees_from_registration_post(
+            event, registration_data)
         for registration in res:
             name = registration.name
             mail_name = name.replace(" ",".")
 
             company_id = request.env.user.company_id
-            user_default_domain = company_id.portal_user_default_domain if company_id else ''
+            user_default_domain = company_id.portal_user_default_domain \
+                if company_id else ''
 
-            registration.email = '%s@%s' % (mail_name.lower(), user_default_domain)
+            registration.email = '%s@%s' % (mail_name.lower(),
+                                            user_default_domain)
 
         return res
+
+    @http.route()
+    def registration_confirm(self, event, **post):
+        if request.env.user.id == request.env.ref('base.public_user').id:
+            return request.redirect('/web/login')
+        return super(WebsiteEventController, self).registration_confirm(
+            event, **post)
