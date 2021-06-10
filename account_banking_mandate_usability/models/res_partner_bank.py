09a31cb18ac1fb55a2ba540dc75e275009ba448d
Oihane Crucelaegui <oihanecruce@gmail.com>
2021-06-10 12:47:36 +0200

    [FIX] Travis

--- account_banking_mandate_usability/models/res_partner_bank.py
+++ account_banking_mandate_usability/models/res_partner_bank.py
@@ -1,7 +1,7 @@
 # Copyright 2019 Oihane Crucelaegui - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, models
+from odoo import models
 
 
 class ResPartnerBank(models.Model):
