a572285516b612f6c04f87de3930b2c600fc9c05
Tu Nombre <you@example.com>
2021-06-17 14:05:42 +0200

    [ADD] slide_partner_menu: Put in the menu the table slide slide partner.

---
+++ slide_partner_menu/models/slide_slide.py
@@ -0,0 +1,30 @@
+# Copyright 2021 Berezi - Iker - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import models, fields, _
+
+
+class SlideSlide(models.Model):
+    _inherit = 'slide.slide'
+
+    def _compute_count_statistics(self):
+        for statistic in self:
+            statistic.count_statistic = len(
+                statistic.statistic_ids)
+
+    statistic_ids = fields.One2many(
+        string='Statistics', inverse_name='slide_id',
+        comodel_name='slide.slide.partner')
+    count_statistic = fields.Integer(
+        '# Statistics', compute='_compute_count_statistics')
+
+    def action_view_statistics(self):
+        context = self.env.context.copy()
+        context.update({'default_slide_id': self.id})
+        return {
+            'name': _("Statistics"),
+            'view_mode': 'tree,form',
+            'res_model': 'slide.slide.partner',
+            'domain': [('id', 'in', self.statistic_ids.ids)],
+            'type': 'ir.actions.act_window',
+            'context': context
+        }
