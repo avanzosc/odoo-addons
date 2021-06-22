b3e631f1efd13d0b4f0a4a2e026f3d10f3c6ad64
Berezi <82805447+Berezi@users.noreply.github.com>
2021-06-22 10:13:16 +0200

    [ADD] calendar_event_description: Description in calendar event tree view. (#1157)

---
+++ calendar_event_description/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2021 Berezi - Iker - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Calendar Event Description",
+    'version': '14.0.1.1.0',
+    "author": "Avanzosc",
+    "category": "Sales",
+    "depends": [
+        "calendar",
+    ],
+    "data": [
+        "views/calendar_event_views.xml",
+    ],
+    "license": "AGPL-3",
+    'installable': True,
+}
