59f2b772bf152a72f13042106ce0b5a93aba6d2a
Tu Nombre <you@example.com>
2021-06-22 13:54:48 +0200

    [ADD] employment_skills_hide_info: Hide skills information for some users.

---
+++ employment_skills_hide_info/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2021 Berezi - Iker - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Employment skills hide info",
+    'version': '14.0.1.0.0',
+    "author": "Avanzosc",
+    "category": "Sales/CRM",
+    "depends": [
+        "hr_skills"
+    ],
+    "data": [
+        "views/hr_employee_views.xml"
+    ],
+    "license": "AGPL-3",
+    'installable': True,
+}
