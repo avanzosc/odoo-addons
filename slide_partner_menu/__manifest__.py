e350e1552a2dc45691cbfcc8354c6d9684c91dec
Berezi <82805447+Berezi@users.noreply.github.com>
2021-06-23 15:07:11 +0200

    [ADD] slide_partner_menu: Put in the menu the table slide slide partner. (#1152)

---
+++ slide_partner_menu/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2021 Berezi - Iker - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Slide partner menu",
+    'version': '14.0.1.0.0',
+    "author": "Avanzosc",
+    "category": "Sales/CRM",
+    "depends": [
+        "website_slides",
+        "website_slides_survey",
+        "contacts"
+    ],
+    "data": [
+        "views/slide_slide_partner_views.xml",
+        "views/slide_channel_views.xml",
+        "views/res_partner_views.xml",
+        "views/slide_slide_views.xml"
+    ],
+    "license": "AGPL-3",
+    'installable': True,
+}
---
+++ slide_partner_menu/models/__init__.py
@@ -0,0 +1,4 @@
+from . import slide_slide_partner
+from . import slide_channel
+from . import slide_slide
+from . import res_partner
