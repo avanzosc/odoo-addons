a7c831573992afaf95768a8d5cbb2a5046f6a055
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2021-06-23 15:58:23 +0200

    [ADD] external_layout_standard_logo_size: Bigger logo size for "External layout standard" template. (#1156)

---
+++ external_layout_standard_logo_size/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2021 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "External Layout Standard Logo Size",
+    "version": "14.0.1.0.0",
+    "category": "Banking addons",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "web",
+    ],
+    "data": [
+        "report/external_layout_standard.xml",
+    ],
+    "installable": True,
+}
