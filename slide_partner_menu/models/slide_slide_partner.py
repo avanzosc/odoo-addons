023b1b3c06ba532e257f41439288cabeaa778bd5
Berezi <82805447+Berezi@users.noreply.github.com>
2021-06-23 15:07:11 +0200

    [ADD] slide_partner_menu: Put in the menu the table slide slide partner. (#1152)

---
+++ slide_partner_menu/models/slide_slide_partner.py
@@ -0,0 +1,16 @@
+# Copyright 2021 Berezi - Iker - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import models
+
+
+class SlideSlidePartner(models.Model):
+    _inherit = 'slide.slide.partner'
+
+    def name_get(self):
+        result = []
+        for slide_slide_partner in self:
+            display_name = u'{}: {}'.format(
+                slide_slide_partner.channel_id.name,
+                slide_slide_partner.slide_id.name)
+            result.append((slide_slide_partner.id, display_name))
+        return result
