ddb2d96a082becf0ff9ccdbfb52e33f902aa8043
leiremartinezdesantos <72156354+leiremartinezdesantos@users.noreply.github.com>
2021-06-23 11:48:23 +0200

    [ADD] event_registration_autocomplete: autocomplete register with child info (#1137)

---
+++ event_registration_autocomplete/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2021 Leire Martinez de Santos - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Event Registration Autocomplete Attendee",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "category": "website",
+    "version": "14.0.1.0.0",
+    "depends": [
+        "website",
+        "website_event",
+    ],
+    "data": [
+        "views/event_templates.xml",
+    ],
+}
