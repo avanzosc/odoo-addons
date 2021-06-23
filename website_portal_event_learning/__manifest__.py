7c4a1048621f939666b456ced2ad6a681322daf2
leiremartinezdesantos <72156354+leiremartinezdesantos@users.noreply.github.com>
2021-06-23 15:13:00 +0200

    [ADD] website_portal_event_learning: add portal entries for Events and Courses (#1158)

---
+++ website_portal_event_learning/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2021 Leire Martinez de Santos - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    'name': "Website Portal Events and Courses",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    'category': 'website',
+    'version': '14.0.1.0.0',
+    'depends': ['website_event', 'website_slides', 'portal'],
+    'data': [
+        'views/templates.xml',
+    ]
+}
