24fa0ff9a21e1812e930b39c1d0a0e27bf5422d4
leiremartinezdesantos <72156354+leiremartinezdesantos@users.noreply.github.com>
2021-06-29 14:23:42 +0200

    [FIX] event_registration_autocomplete: fix traceback in view and add custom fields (#1167)
    [ADD] New module event_registration_autocomplete_birthdate

--- event_registration_autocomplete/static/src/js/website.js
+++ event_registration_autocomplete/static/src/js/website.js
@@ -5,21 +5,26 @@ $(document).ready(function() {
     $(document).on('change', ".contact_select", function() {
 
         var sel_val = $(this).val();
+        var counter = $(this).attr('id');
+        if(sel_val != 'none'){
+            $('#partner_info > #'+sel_val).find('span').each(function(){
+                var target = $("input[name='"+ counter + "-" + $(this).attr("id")+"']");
+                target.val( $(this).text());
 
-        $('#partner_info > #'+sel_val).find('span').each(function(){
-
-            var target = $("input[name='"+ $(this).attr("id")+"']");
-            target.val( $(this).text());
-
-            if(!$(this).text()) {
-                target.attr('readonly', false);
-                console.log($(this).text());
-            }
-            else {
-                target.attr('readonly', true);
-            }
-        });
-
+                if(!$(this).text()) {
+                    target.attr('readonly', false);
+                }
+                else {
+                    target.attr('readonly', true);
+                }
+            });
+        }
+        else{
+            $(this).find('input').each(function(){
+                $(this).val('')
+                $(this).attr('readonly', false);
+            });
+        }
     });
 
 });
---
+++ event_registration_autocomplete_birthdate/README.rst
@@ -0,0 +1,27 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+    :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+    :alt: License: AGPL-3
+
+=========================================
+Event registration autocomplete birthdate
+=========================================
+
+Autocomplete selected partner birthdate information in event registration.
+
+Bug Tracker
+===========
+
+Bugs are tracked on `GitHub Issues
+<https://github.com/avanzosc/odoo-addons/issues>`_. In case of trouble,
+please check there if your issue has already been reported. If you spotted
+it first, help us smash it by providing detailed and welcomed feedback.
+
+Do not contact contributors directly about support or help with technical issues.
+
+Credits
+=======
+
+Contributors
+------------
+
+* Leire Martinez de Santos <leiremartinez@avanzosc.es>
