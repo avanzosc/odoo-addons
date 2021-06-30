60cece270547a0a9f9a5e36e0e75dccb6a625e55
Tu Nombre <you@example.com>
2021-06-30 13:04:47 +0200

    [ADD] meeting_timesheet_analytic: When the meeting is realized, inputs time sheet hours.

---
+++ calendar_timesheet/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2021 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Calendar Timesheet",
+    "version": "14.0.1.0.0",
+    "category": "Project",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "hr_timesheet",
+        "project_task_meeting",
+        "resource_time"
+    ],
+    "data": [
+        "views/calendar_event_views.xml",
+    ],
+    "installable": True,
+}
