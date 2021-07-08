069335443d29c4f42f7bac9ce255877048cfefcd
leiremartinezdesantos <72156354+leiremartinezdesantos@users.noreply.github.com>
2021-07-08 10:42:36 +0200

    [ADD] event_registration_sale: add sales info in Event registration (#1176)

---
+++ event_registration_sale/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2021 Leire Martinez de Santos - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Event Registration Sale",
+    'version': '14.0.1.0.0',
+    "author": "Avanzosc",
+    "category": "Sales/CRM",
+    "depends": [
+        "event_sale",
+    ],
+    "data": [
+        "views/event_registration_views.xml",
+    ],
+    "license": "AGPL-3",
+    'installable': True,
+}
---
+++ event_registration_sale/models/event_registration.py
@@ -0,0 +1,23 @@
+
+from odoo import models, fields, api
+
+
+class EventRegistration(models.Model):
+    _inherit = 'event.registration'
+
+    partner_bank_acc_id = fields.Many2one(
+        string='Partner bank account',
+        comodel_name='res.partner.bank',
+        stored=True,
+        compute='_compute_event_partner_bank_acc')
+    order_status = fields.Selection(
+        string='Order status',
+        related='sale_order_id.state')
+
+    @api.depends('partner_id',
+                 'partner_id.bank_ids')
+    def _compute_event_partner_bank_acc(self):
+        for record in self:
+            record.partner_bank_acc_id = self.env['res.partner.bank'].search(
+                [('id', 'in', record.partner_id.bank_ids.ids)],
+                order='id desc', limit=1)
