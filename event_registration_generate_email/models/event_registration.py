2fbe775aa81d9edb6eef09fe0553120897128e5a
leiremartinezdesantos <72156354+leiremartinezdesantos@users.noreply.github.com>
2021-07-08 10:18:12 +0200

    [FIX] event_registration_create_student, event_registration_generate_email: fix confirmation error (#1179)

--- event_registration_generate_email/models/event_registration.py
+++ event_registration_generate_email/models/event_registration.py
@@ -6,12 +6,14 @@ class EventRegistration(models.Model):
     _inherit = 'event.registration'
 
     def generate_user_email(self):
-        name = self.name
-        mail_name = name.replace(" ", ".")
-
         company_id = self.env.user.company_id
-        user_default_domain = (
-            company_id.portal_user_default_domain if company_id else '')
-
-        result_email = '%s@%s' % (mail_name.lower(), user_default_domain)
+        name = self.name
+        result_email = name
+        if company_id and name:
+            mail_name = name.replace(" ", "_")
+            result_email = mail_name.lower()
+            user_default_domain = (
+                company_id.portal_user_default_domain if company_id else '')
+            if user_default_domain:
+                result_email = result_email + '@' + user_default_domain
         return result_email
