b937c968f0fb87f11f324eb97feaa9bec9bea620
Thor <alfredodelafuente@avanzosc.es>
2021-07-09 10:40:44 +0200

    [ADD] event_registration_action: In event registration 3 new actions for confirm, cancel, and back to draft course participant registrations.

---
+++ event_registration_action/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2021 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Event Registration Action",
+    "version": '14.0.1.0.0',
+    "author": "Avanzosc",
+    "license": "AGPL-3",
+    "category": "Marketing/Events",
+    "depends": [
+        "event",
+    ],
+    "data": [
+        'security/ir.model.access.csv',
+        'wizard/wiz_event_registration_confirm_participant_views.xml',
+        'wizard/wiz_event_registration_cancel_participant_views.xml',
+        'wizard/wiz_event_registration_draft_participant_views.xml'
+    ],
+    
+    "installable": True,
+    "auto_install": True,
+}
