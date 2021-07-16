b7d5c1ad3987aedfa2e8c034f5f6d080e691978f
leiremartinezdesantos <72156354+leiremartinezdesantos@users.noreply.github.com>
2021-07-16 13:47:47 +0200

    Fases cursos (#1189)
    * [ADD] slide_channel_phase: add option to show course by phases

---
+++ slide_channel_phase/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2021 Leire Martinez de Santos - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    'name': "Slide Channel Phase",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    'category': 'website',
+    'version': '14.0.1.0.0',
+    'depends': ['website_slides', 'slide_partner_menu'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/templates.xml',
+        'views/views.xml',
+    ]
+}
