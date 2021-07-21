d9720760633d0ed58309ccd6e54a0a4ee321f184
Oihane Crucelaegui <oihanecruce@gmail.com>
2021-07-21 12:59:29 +0200

    [MIG] analytic_usability: Migration to 13.0

--- analytic_usability/tests/test_analytic_usability.py
+++ analytic_usability/tests/test_analytic_usability.py
@@ -1,11 +1,10 @@
 # Copyright 2019 Oihane Crucelaegui - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo.tests import common
+from odoo.tests import common, tagged
 
 
-@common.at_install(False)
-@common.post_install(True)
+@tagged("post_install", "-at_install")
 class TestAnalyticUsability(common.SavepointCase):
 
     @classmethod
@@ -22,22 +21,22 @@ class TestAnalyticUsability(common.SavepointCase):
         })
 
     def test_analytic_usability(self):
-        self.assertEquals(self.line.amount, 0)
-        self.assertEquals(self.line.amount_type, 'revenue')
+        self.assertEqual(self.line.amount, 0)
+        self.assertEqual(self.line.amount_type, 'revenue')
         self.line.write({
             'amount': -100,
         })
-        self.assertEquals(self.line.amount_type, 'cost')
+        self.assertEqual(self.line.amount_type, 'cost')
         self.line.write({
             'amount': 100,
         })
-        self.assertEquals(self.line.amount_type, 'revenue')
+        self.assertEqual(self.line.amount_type, 'revenue')
 
     def test_analytic_account(self):
         self.assertFalse(self.account.partner_id)
-        self.assertEquals(self.account.user_id, self.env.user)
+        self.assertEqual(self.account.user_id, self.env.user)
         self.account.write({
             "partner_id": self.partner.id,
         })
         self.account._onchange_partner_id()
-        self.assertEquals(self.account.user_id, self.partner.user_id)
+        self.assertEqual(self.account.user_id, self.partner.user_id)
