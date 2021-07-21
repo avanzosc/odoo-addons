0945f0d3d668e62b53547afaa41e2c8e8dd80fa7
Oihane Crucelaegui <oihanecruce@gmail.com>
2021-07-21 12:59:29 +0200

    [MIG] analytic_usability: Migration to 13.0

--- analytic_usability/models/account_analytic_line.py
+++ analytic_usability/models/account_analytic_line.py
@@ -6,29 +6,29 @@ from odoo import api, fields, models
 
 
 class AccountAnalyticLine(models.Model):
-    _inherit = 'account.analytic.line'
+    _inherit = "account.analytic.line"
 
     amount_type = fields.Selection(
-        selection=[('cost', 'Cost'),
-                   ('revenue', 'Revenue')],
-        compute='_compute_amount_type', string='Cost/Revenue', store=True)
-    invoice_id = fields.Many2one(
-        string='Invoice', comodel_name='account.invoice',
-        related='move_id.invoice_id', store=True)
-    invoice_number = fields.Char(
-        string='Invoice Number',
-        related='move_id.invoice_id.number', store=True)
+        selection=[("cost", "Cost"),
+                   ("revenue", "Revenue")],
+        compute="_compute_amount_type", string="Cost/Revenue", store=True)
+    account_move_id = fields.Many2one(
+        string="Invoice", comodel_name="account.move",
+        related="move_id.move_id", store=True)
+    invoice_name = fields.Char(
+        string="Invoice Number",
+        related="move_id.move_id.name", store=True)
     invoice_reference = fields.Char(
-        string='Invoice Vendor Reference',
-        related='move_id.invoice_id.reference', store=True)
+        string="Invoice Vendor Reference",
+        related="move_id.move_id.ref", store=True)
     invoice_partner_id = fields.Many2one(
-        string='Invoice Partner', comodel_name='res.partner',
-        related='move_id.invoice_id.partner_id', store=True)
+        string="Invoice Partner", comodel_name="res.partner",
+        related="move_id.move_id.partner_id", store=True)
     invoice_user_id = fields.Many2one(
-        string='Invoice Salesperson', comodel_name='res.users',
-        related='move_id.invoice_id.user_id', store=True)
+        string="Invoice Salesperson", comodel_name="res.users",
+        related="move_id.move_id.user_id", store=True)
 
-    @api.depends('amount')
+    @api.depends("amount")
     def _compute_amount_type(self):
         for line in self:
-            line.amount_type = 'cost' if line.amount < 0 else 'revenue'
+            line.amount_type = "cost" if line.amount < 0 else "revenue"
