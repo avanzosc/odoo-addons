fd4bc436f65bf0a4fde2bca9d65ee1a413274922
Ugaitz Olaizola <uolaizola@binovo.es>
2021-08-11 15:45:30 +0200

    Tarea #8486 hacer account_analytic_line_with_project NO autoinstalable

--- account_analytic_line_with_project/__manifest__.py
+++ account_analytic_line_with_project/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
     "name": "Account Analytic Line With Project",
-    "version": "12.0.1.0.0",
+    "version": "12.0.2.0.0",
     "license": "AGPL-3",
     "author": "AvanzOSC",
     "website": "http://www.avanzosc.es",
@@ -13,6 +13,6 @@
     "data": [
     ],
     "pre_init_hook": "pre_init_hook",
-    "installable": True,
+    "installable": False,
     "auto_install": True,
 }
