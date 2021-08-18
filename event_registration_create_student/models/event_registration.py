063b8586c953e930e934a7b8d344604418ea5059
Avanzosc <leiremartinez@avanzosc.es>
2021-08-18 10:07:54 +0200

    [IMP] event_registration_create_student: create student if not set

--- event_registration_create_student/models/event_registration.py
+++ event_registration_create_student/models/event_registration.py
@@ -12,45 +12,46 @@ class EventRegistration(models.Model):
         if not self:
             return result
         for registration in self:
-            user = self.env['res.users'].search([
-                ('email', '=', registration.email)], limit=1)
-            vals = {}
-            if not registration.email:
-                registration.write(
-                    {'email': registration.generate_user_email()})
-            if user:
-                partner = user.partner_id
-            else:
-                group_portal = self.env.ref('base.group_portal')
-                use_email = registration.email
-                user = registration.create_get_user({
-                    'name': registration.name,
-                    'email': use_email,
-                    'login': use_email,
-                    'groups_id': [(4, group_portal.id)]
-                })
-                partner = user.partner_id
-                vals.update({'parent_id': registration.partner_id.id})
-            if not partner:
-                # Create Portal User
-                partner = self.env['res.partner'].create({
-                    'name': registration.name,
-                    'parent_id': registration.partner_id.id
-                })
-                use_email = registration.email
-                user = registration.create_get_user({
-                    'name': registration.name,
-                    'email': use_email,
-                    'login': use_email,
-                    'partner_id': partner.id,
-                    'groups_id': [(4, group_portal.id)]
-                })
-            if not partner.email:
-                vals.update({'email': use_email})
-            if not partner.phone:
-                vals.update({'phone': registration.phone})
-            partner.write(vals)
-            registration.student_id = partner
+            if not registration.student_id:
+                user = self.env['res.users'].search([
+                    ('email', '=', registration.email)], limit=1)
+                vals = {}
+                if not registration.email:
+                    registration.write(
+                        {'email': registration.generate_user_email()})
+                if user:
+                    partner = user.partner_id
+                else:
+                    group_portal = self.env.ref('base.group_portal')
+                    use_email = registration.email
+                    user = registration.create_get_user({
+                        'name': registration.name,
+                        'email': use_email,
+                        'login': use_email,
+                        'groups_id': [(4, group_portal.id)]
+                    })
+                    partner = user.partner_id
+                    vals.update({'parent_id': registration.partner_id.id})
+                if not partner:
+                    # Create Portal User
+                    partner = self.env['res.partner'].create({
+                        'name': registration.name,
+                        'parent_id': registration.partner_id.id
+                    })
+                    use_email = registration.email
+                    user = registration.create_get_user({
+                        'name': registration.name,
+                        'email': use_email,
+                        'login': use_email,
+                        'partner_id': partner.id,
+                        'groups_id': [(4, group_portal.id)]
+                    })
+                if not partner.email:
+                    vals.update({'email': use_email})
+                if not partner.phone:
+                    vals.update({'phone': registration.phone})
+                partner.write(vals)
+                registration.student_id = partner
         return result
 
     def create_get_user(self, vals):
