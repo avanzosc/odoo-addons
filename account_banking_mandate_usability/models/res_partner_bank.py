afecdb5ba1a32a100464e3c380c7a6dc8d470d11
leiremartinezdesantos <72156354+leiremartinezdesantos@users.noreply.github.com>
2021-08-18 11:57:56 +0200

    [IMP] account_banking_mandate_usability: add function for mandate creation (#1180)

--- account_banking_mandate_usability/models/res_partner_bank.py
+++ account_banking_mandate_usability/models/res_partner_bank.py
@@ -1,11 +1,15 @@
 # Copyright 2019 Oihane Crucelaegui - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import models
+from odoo import api, fields, models
 
 
 class ResPartnerBank(models.Model):
     _inherit = "res.partner.bank"
+
+    error_bank_acc = fields.Boolean(
+        string='Bank account error',
+        compute="_compute_validate_bank_account")
 
     def _check_active_mandate(self):
         self.ensure_one()
@@ -26,3 +30,23 @@ class ResPartnerBank(models.Model):
                 self.company_id.id or
                 self.env.context.get("force_company") or self.env.company.id),
         }
+
+    @api.depends('acc_type')
+    def _compute_validate_bank_account(self):
+        for record in self:
+            record.error_bank_acc = False
+            if record.acc_type not in 'iban':
+                record.error_bank_acc = True
+
+    def create_validate_bank_account_mandate(self):
+        wiz_obj = self.sudo().env["res.partner.bank.mandate.generator"]
+        mandate_wiz = wiz_obj.create({
+            "bank_ids": [self.id],
+            "mandate_format": "sepa",
+            "mandate_type": "recurrent",
+            "mandate_scheme": "CORE",
+            "mandate_recurrent_sequence_type": "recurring",
+            "signed": True,
+            "validate": True,
+        })
+        mandate_wiz.button_generate_mandates()
