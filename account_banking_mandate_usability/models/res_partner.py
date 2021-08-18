9ebc6edd0182641d059540b191ac120cd56eed9a
leiremartinezdesantos <72156354+leiremartinezdesantos@users.noreply.github.com>
2021-08-18 11:57:56 +0200

    [IMP] account_banking_mandate_usability: add function for mandate creation (#1180)

--- account_banking_mandate_usability/models/res_partner.py
+++ account_banking_mandate_usability/models/res_partner.py
@@ -1,7 +1,7 @@
 # Copyright 2021 Daniel Campos - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ResPartner(models.Model):
@@ -10,6 +10,10 @@ class ResPartner(models.Model):
     bank_acc_count = fields.Integer(
         compute="_compute_bank_acc_count", string="Number of Bank accounts",
         readonly=True)
+
+    error_bank_acc = fields.Boolean(
+        string='Bank account error',
+        compute="_compute_partner_validate_bank_account")
 
     def _compute_bank_acc_count(self):
         bank_acc_data = self.env["res.partner.bank"].read_group(
@@ -21,3 +25,25 @@ class ResPartner(models.Model):
         }
         for partner in self:
             partner.bank_acc_count = mapped_data.get(partner.id, 0)
+
+    @api.depends('bank_ids')
+    def _compute_partner_validate_bank_account(self):
+        for record in self:
+            record.error_bank_acc = False
+            if not record.bank_ids or any(record.mapped(
+                    "bank_ids.error_bank_acc")):
+                record.error_bank_acc = True
+
+    def create_validate_bank_account_mandate(self):
+        if self.bank_ids:
+            bank_id = self.env['res.partner.bank'].search(
+                [('id', 'in', self.bank_ids.ids)], order='id desc', limit=1)
+            if bank_id:
+                if bank_id.error_bank_acc:
+                    raise ValidationError(
+                        _("The customer does not have a valid bank account."))
+                else:
+                    if not bank_id.mandate_ids:
+                        bank_id.create_validate_bank_account_mandate()
+        else:
+            raise ValidationError("The customer does not have a bank account.")
