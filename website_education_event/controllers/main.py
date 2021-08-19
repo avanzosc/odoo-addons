cae561384cbea3e5d0478bbea8aa8151f0f767b4
Avanzosc <leiremartinez@avanzosc.es>
2021-08-19 14:46:39 +0200

    [IMP] website_education_event: add domain calculation

--- website_education_event/controllers/main.py
+++ website_education_event/controllers/main.py
@@ -11,22 +11,7 @@ class WebsiteEventController(WebsiteEventController):
     def events(self, page=1, **searches):
         res = super(WebsiteEventController, self).events(page, **searches)
         values = {}
-        logged_user = request.env['res.users'].browse(request.uid)
-        logged_partner = logged_user.partner_id
-        domain = request.website.website_domain()
-        today = datetime.today()
-        domain += [
-            ('date_begin', '>=', today),
-            '|',
-            ('education_center_ids', '=', False),
-            ('education_center_ids', '=', logged_partner.current_center_id.id),
-            '|',
-            ('education_group_ids', '=', False),
-            ('education_group_ids', '=', logged_partner.current_group_id.id),
-            '|',
-            ('education_course_ids', '=', False),
-            ('education_course_ids', '=', logged_partner.current_course_id.id),
-        ]
+        domain = self.get_events_domain()
 
         event_obj = request.env[
             'event.event']
@@ -56,3 +41,32 @@ class WebsiteEventController(WebsiteEventController):
         })
         res.qcontext.update(values)
         return res
+
+    def get_events_domain(self):
+        logged_user = request.env['res.users'].browse(request.uid)
+        logged_partner = logged_user.partner_id
+        domain = request.website.website_domain()
+        today = datetime.today()
+        if logged_partner.educational_category != 'student':
+            center_ids = logged_partner.progenitor_child_ids.mapped('current_center_id').ids
+            group_ids = logged_partner.progenitor_child_ids.mapped('current_group_id').ids
+            course_ids = logged_partner.progenitor_child_ids.mapped('current_course_id').ids
+        else:
+            center_ids = logged_partner.current_center_id.id
+            group_ids = logged_partner.current_group_id.id
+            course_ids = logged_partner.current_course_id.id
+
+        domain += [
+            ('date_begin', '>=', today),
+            '|',
+            ('education_center_ids', '=', False),
+            ('education_center_ids', 'in', center_ids),
+            '|',
+            ('education_group_ids', '=', False),
+            ('education_group_ids', 'in', group_ids),
+            '|',
+            ('education_course_ids', '=', False),
+            ('education_course_ids', '=', course_ids),
+        ]
+
+        return domain
