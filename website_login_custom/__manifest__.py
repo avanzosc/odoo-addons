fb55fe2f116c37d7871b41aa0a333bdd87bf8dc7
leiremartinezdesantos <72156354+leiremartinezdesantos@users.noreply.github.com>
2021-08-23 13:10:08 +0200

    [ADD] New module website_login_custom (#1203)
    * [ADD] website_login_custom: add custom texts on login page
    
    * [IMP] website_login_custom: add translations

---
+++ website_login_custom/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2021 Leire Martinez de Santos - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    'name': "Website Custom Login",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    'category': 'website',
+    'version': '14.0.1.0.0',
+    'depends': ['web', 'auth_signup', 'website', 'portal'],
+    'data': [
+        'views/templates.xml',
+        'views/views.xml',
+    ]
+}
