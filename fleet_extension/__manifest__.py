75bf105c0d40e5ecafdb88fff391da725008f26c
Berezi <82805447+Berezi@users.noreply.github.com>
2021-08-31 12:46:20 +0200

    [IMP] fleet_extension: New three files of guarantee dates in product. (#1201)

--- fleet_extension/__manifest__.py
+++ fleet_extension/__manifest__.py
@@ -1,14 +1,16 @@
-# Copyright 2021 Berezi - Iker - AvanzOSC
+# Copyright 2021 Berezi - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Fleet Extension",
     'version': '14.0.1.0.0',
     "author": "Avanzosc",
+    "website": "http://www.avanzosc.es",
     "category": "Sales/CRM",
     "depends": [
         "fleet",
         "product",
-        "stock"
+        "stock",
+        "sale_management"
     ],
     "data": [
         "security/ir.model.access.csv",
@@ -16,7 +18,8 @@
         "views/fleet_vehicle_model_collection_views.xml",
         "views/fleet_vehicle_views.xml",
         "views/fleet_vehicle_model_type_views.xml",
-        "views/stock_production_lot_views.xml"
+        "views/stock_production_lot_views.xml",
+        "views/product_template_views.xml"
     ],
     "license": "AGPL-3",
     'installable': True,
