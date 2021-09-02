755452cfcc4134ad18844367c6da6b0b8d52dea9
Avanzosc <leiremartinez@avanzosc.es>
2021-09-02 08:53:03 +0200

    [IMP] website_event_member: change partner default not member

--- website_event_member/models/res_partner.py
+++ website_event_member/models/res_partner.py
@@ -5,4 +5,4 @@ from odoo import fields, models
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    is_member = fields.Boolean(string='Is member?', default=True)
+    is_member = fields.Boolean(string='Is member?', default=False)
