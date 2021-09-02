b40958dfef33be5e20d6380d63466c7357d9cdab
Avanzosc <leiremartinez@avanzosc.es>
2021-09-02 08:41:56 +0200

    [ADD] website_sale_zip_autocomplete: autocomplete address fields on ZIP select on website

---
+++ website_sale_zip_autocomplete/static/src/js/website.js
@@ -0,0 +1,25 @@
+odoo.define('website_sale_zip_autocomplete.web_zip_autocomplete', function (require) {
+    "use strict";
+
+    var ajax = require('web.ajax');
+$(document).ready(function() {
+    var update_json = $.Deferred();
+    update_json.resolve();
+    $("#zip_id").change(function(){
+        update_json = update_json.then(function(){
+            return ajax.jsonRpc("/shop/address/update_json", 'call', {
+                'zip_str': $("#zip_id").val(),
+                }).
+            then(function (data) {
+                if (!data) {
+                    return;
+                }
+                $("#country_id").val(data['country_id']);
+                $("select[name='state_id']").val(data['state_id']);
+                $("input[name='city']").val(data['city']);
+                $("input[name='zip']").val(data['zip']);
+            });
+        });
+    });
+});
+});
