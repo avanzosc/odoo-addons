f7ff48771ab92e6fb1cb97933e1515305696ddf2
Avanzosc <leiremartinez@avanzosc.es>
2021-09-03 12:48:31 +0200

    [IMP] website_event_member: add action to make members

--- website_event_member/models/res_partner.py
+++ website_event_member/models/res_partner.py
@@ -6,3 +6,7 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     is_member = fields.Boolean(string='Is member?', default=False)
+
+    def action_make_members(self):
+        for rec in self:
+            rec.is_member = True
