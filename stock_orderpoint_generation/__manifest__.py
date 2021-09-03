fb2094ba182b71f3dbc4635b6180edc4d6a65bfe
Tu Nombre <you@example.com>
2021-09-03 10:16:49 +0200

    [ADD] stock_orderpoint_generation

---
+++ stock_orderpoint_generation/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2021 Daniel Campos - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Stock Orderpoint Generation",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "stock",
+    ],
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "category": "Custom",
+    "data": [
+        "security/ir.model.access.csv",
+        "wizards/stock_orderpoint_generator_view.xml",
+    ],
+    "installable": True,
+}
