f6d704b80ab6e2950b5051e6a3129b629a4a5736
leiremartinezdesantos <72156354+leiremartinezdesantos@users.noreply.github.com>
2021-09-07 10:04:57 +0200

    Website events member ticket (#1231)
    * [ADD] website_event_member: show only member tickets to member partners

--- website_event_member/models/res_partner.py
+++ website_event_member/models/res_partner.py
@@ -5,4 +5,8 @@ from odoo import fields, models
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    is_member = fields.Boolean(string='Is member?', default=True)
+    is_member = fields.Boolean(string='Is member?', default=False)
+
+    def action_make_members(self):
+        for rec in self:
+            rec.is_member = True
