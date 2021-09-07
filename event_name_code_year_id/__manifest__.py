acc89c3d38a4718c812b323cabb6d8fb0e57b2e8
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2021-09-07 09:55:50 +0200

    [ADD] event_name_code_year_id: The format of the event name will be formed as follows: "code language"-"year start event"-"Event ID". (#1239)

---
+++ event_name_code_year_id/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2021 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Event Name Code Year Id",
+    'version': '14.0.1.0.0',
+    "category": "Marketing/Events",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "event_lang_level",
+    ],
+    "data": [
+        "views/event_event_views.xml",
+    ],
+    'installable': True,
+}
