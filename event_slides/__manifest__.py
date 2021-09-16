c4f6160f3cc71164774500031dbf3497674c9ff3
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2021-09-16 16:22:15 +0200

    [IMP] event_slides: Navegar a cursos desde sesiones de evento. (#1256)

--- event_slides/__manifest__.py
+++ event_slides/__manifest__.py
@@ -2,18 +2,20 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Event Slides",
-    'version': "14.0.1.0.0",
+    'version': "14.0.1.2.0",
     "author": "AvanzOSC",
     "category": "Website/eLearning",
     "depends": [
         "event_registration_student",
         "website_slides",
-        "event_attendee_birthdate"
+        "event_attendee_birthdate",
+        "website_event_track",
     ],
     "data": [
         "views/event_event_views.xml",
         "views/slide_channel_views.xml",
-        "views/slide_channel_partner_views.xml"
+        "views/slide_channel_partner_views.xml",
+        "views/event_track_views.xml"
     ],
     "license": "AGPL-3",
     "installable": True,
--- event_slides/models/__init__.py
+++ event_slides/models/__init__.py
@@ -2,3 +2,4 @@ from . import event_event
 from . import slide_channel
 from . import event_registration
 from . import slide_channel_partner
+from . import event_track
