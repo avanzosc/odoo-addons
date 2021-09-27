77c51261f88920ba4e67f59fb0ed2c13ba465a98
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2021-09-27 10:38:52 +0200

    [ADD] res_partner_headquarter, event_headquarter: Headquarter in partner, event, and analytic lines. (#1268)

---
+++ event_headquarter/models/event_event.py
@@ -0,0 +1,20 @@
+# Copyright 2021 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import models, fields
+
+
+class EventEvent(models.Model):
+    _inherit = 'event.event'
+
+    organizer_id = fields.Many2one(
+        default=lambda self: False,
+        domain="[('headquarter','=', True),'|',('company_id', '=', False),"
+        "('company_id', '=', company_id)]")
+
+    def write(self, values):
+        result = super(EventEvent, self).write(values)
+        if 'organizer_id' in values and values.get('organizer_id', False):
+            for event in self:
+                event.account_analytic_line_ids.write(
+                    {'headquarter_id': event.organizer_id.id})
+        return result
---
+++ res_partner_headquarter/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2021 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Res Partner Headquarter",
+    'version': '14.0.1.0.0',
+    "category": "Marketing/Events",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "views/res_partner_views.xml",
+    ],
+    'installable': True,
+}
