ace97ac29a0d2da65ef2882725f4f43b52c389c4
oihane <oihanecruce@gmail.com>
2021-09-29 16:40:07 +0200

    [IMP] website_education_blog: access blog post by education groups (#1270)

--- website_education_blog/models/blog_post.py
+++ website_education_blog/models/blog_post.py
@@ -15,86 +15,96 @@ class BlogPost(models.Model):
         related="blog_id.education_group_ids")
     blog_education_category_ids = fields.Char(
         related="blog_id.education_category_ids")
-
+    academic_year_id = fields.Many2one(
+        comodel_name="education.academic_year",
+        string="Academic Year")
     education_center_ids = fields.Many2many(
-        string='Education Centers',
+        string="Education Centers",
         comodel_name="res.partner",
-        relation='post_center_rel',
-        column1='post_id',
-        column2='center_id', store=True)
+        relation="post_center_rel",
+        column1="post_id",
+        column2="center_id")
     education_course_ids = fields.Many2many(
-        string='Education Courses', comodel_name="education.course", store=True)
+        string="Education Courses", comodel_name="education.course")
     education_group_ids = fields.Many2many(
-        string='Education Groups', comodel_name="education.group", store=True)
+        string="Education Groups", comodel_name="education.group")
     education_category_ids = fields.Char(
-        string='Educational Category')
+        string="Educational Category")
 
     invited_partner_ids = fields.Many2many(
-        string='Invited Partners', comodel_name="res.partner",
-        compute="_compute_invited_partners"
-    )
+        string="Invited Partners",
+        comodel_name="res.partner",
+        compute="_compute_invited_partners",
+        store=True)
     invited_count = fields.Integer(
-        'Count Invited People', compute="_compute_count_invited_partners")
+        string="Count Invited People",
+        compute="_compute_invited_partners",
+        store=True)
 
-    @api.model
-    def create(self, vals):
-        post_id = super(BlogPost, self).create(vals)
-        post_id.education_center_ids = post_id.blog_id.education_center_ids
-        post_id.education_course_ids = post_id.blog_id.education_course_ids
-        post_id.education_group_ids = post_id.blog_id.education_group_ids
-        return post_id
+    allowed_group_ids = fields.Many2many(
+        string="Allowed Groups",
+        comodel_name="education.group",
+        compute="_compute_allowed_group_ids",
+        relation="blog_post_group_rel",
+        column1="post_id",
+        column2="group_id",
+        store=True)
+
+    @api.depends("blog_id", "blog_id.allowed_group_ids", "academic_year_id",
+                 "education_center_ids", "education_course_ids",
+                 "education_group_ids")
+    def _compute_allowed_group_ids(self):
+        group_obj = self.env["education.group"]
+        for record in self:
+            groups = record.education_group_ids
+            if not record.education_group_ids:
+                domain = [("id", "in", record.blog_id.allowed_group_ids.ids)]
+                if record.education_center_ids:
+                    domain = expression.AND([
+                        domain, [("center_id", "in",
+                                  record.education_center_ids.ids)]])
+                if record.education_course_ids:
+                    domain = expression.AND([
+                        domain, [("course_id", "in",
+                                  record.education_course_ids.ids)]])
+                groups = group_obj.search(domain)
+            if record.academic_year_id:
+                groups = groups.filtered(
+                    lambda g: g.academic_year_id == record.academic_year_id)
+            record.allowed_group_ids = [(6, 0, groups.ids)]
 
     @api.multi
+    def _recompute_allowed_groups(self):
+        for record in self:
+            fields_list = ["allowed_group_ids"]
+            for field in fields_list:
+                self.env.add_todo(record._fields[field], record)
+            record.recompute()
+
+    @api.depends("allowed_group_ids")
     def _compute_invited_partners(self):
+        partner_obj = self.env["res.partner"]
         for record in self:
-            record._onchange_post_info()
-
-    @api.multi
-    @api.onchange(
-        'blog_id', 'blog_id.invited_partner_ids',
-        'education_center_ids', 'education_course_ids', 'education_group_ids')
-    def _onchange_post_info(self):
-        domain = [
-            ('id', 'in', self.blog_id.invited_partner_ids.ids)]
-        if self.education_center_ids:
-            domain += [
-                ('current_center_id', 'in', self.education_center_ids.ids)]
-        if self.education_course_ids:
-            domain += [
-                ('current_course_id', 'in', self.education_course_ids.ids)]
-        if self.education_group_ids:
-            domain += [
-                ('current_group_id', 'in', self.education_group_ids.ids)]
-        partners = self.env['res.partner'].search(domain)
-        self.invited_partner_ids = partners.ids
-
-    @api.multi
-    def _compute_count_invited_partners(self):
-        for record in self:
-            record._onchange_count_invited_partners()
-
-    @api.multi
-    @api.onchange('invited_partner_ids')
-    def _onchange_count_invited_partners(self):
-        self.invited_count = len(self.invited_partner_ids)
+            domain = [
+                "|", ("student_group_ids", "in", record.allowed_group_ids.ids),
+                ("progenitor_child_ids.student_group_ids", "in",
+                 record.allowed_group_ids.ids),
+            ]
+            partners = partner_obj.search(domain)
+            record.invited_partner_ids = [(6, 0, partners.ids)]
+            record.invited_count = len(partners)
 
     @api.multi
     def button_open_post_invitations(self):
         self.ensure_one()
         action = self.env.ref(
-            'website_education_blog.action_post_open_invitations')
+            "website_education_blog.action_post_open_invitations")
         action_dict = action.read()[0] if action else {}
         domain = expression.AND([
-            [('id', 'in', self.invited_partner_ids.ids)],
-            safe_eval(action.domain or '[]')])
-        action_dict['context'] = safe_eval(
-            action_dict.get('context', '{}'))
-        action_dict['context'].update({
-            'search_default_partner_id': self.id,
-            'default_partner_id': self.id,
-        })
+            [("id", "in", self.invited_partner_ids.ids)],
+            safe_eval(action.domain or "[]")])
         action_dict.update({
-            'domain': domain,
+            "domain": domain,
         })
         return action_dict
 
