0d1ef7e6e84947187bdc59a3f0fd880bc9eb7622
oihane <oihanecruce@gmail.com>
2021-10-05 15:37:54 +0200

    [ADD] new module pdf_previewer (#1286)

---
+++ pdf_previewer/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2021 Mikel Arregi Etxaniz - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "PDF Previewer",
+    "version": "13.0.1.0.0",
+    "category": "Extra Tools",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "wizards/binary_container_view.xml",
+    ],
+    "installable": True,
+}
