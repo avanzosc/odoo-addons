52fbb96b5a287dcddb54950b2c77fea229e5a5b3
Avanzosc <leiremartinez@avanzosc.es>
2021-10-08 11:07:55 +0200

    Add attachment

--- slide_channel_attachment/models/slide_slide_partner.py
+++ slide_channel_attachment/models/slide_slide_partner.py
@@ -9,3 +9,4 @@ class SlidePartnerRelation(models.Model):
     add_attachment = fields.Boolean("Ask for attachment",
                                     related="slide_id.add_attachment")
     slide_attachment = fields.Binary(string='Partner slide attachment')
+    slide_attachment_name = fields.Char(string='Partner slide attachment name')
