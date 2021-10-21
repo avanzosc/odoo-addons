83e11e0927afbb70349beab482c239fa22907195
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2021-10-21 15:40:37 +0200

    [ADD] storable_product_generate_task: With this module you can define a "Service tracking" to a product of type "Storable Product". (#1309)

---
+++ storable_product_generate_task/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2021 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Storable Product Generate Task",
+    "version": "12.0.1.0.0",
+    "category": "Banking addons",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "product",
+        "stock",
+        "sale_timesheet",
+    ],
+    "data": [
+        "views/product_template_views.xml",
+        "views/sale_order_views.xml",
+    ],
+    "installable": True,
+}
