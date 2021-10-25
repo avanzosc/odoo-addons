f76b41ad7b1ea26a5e35c4e7f42e6e12d83e70ee
Avanzosc <leiremartinez@avanzosc.es>
2021-10-25 18:39:58 +0200

    [ADD] product_template_variant_create: shortcut to variant creation widget

---
+++ product_template_variant_create/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2021 Leire Martinez de Santos - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Product Template Variant Create",
+    'version': '14.0.1.0.0',
+    "author": "Avanzosc",
+    "website": "http://www.avanzosc.es",
+    "category": "Sales",
+    "depends": [
+        "product",
+        "sale_product_configurator",
+    ],
+    "data": [
+        "views/product_views.xml",
+       # "views/templates.xml",
+    ],
+    "license": "AGPL-3",
+    'installable': True,
+}
