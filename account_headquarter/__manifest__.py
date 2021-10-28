df62d1bd35d7196c2d530c96025da6bd53934247
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2021-10-28 09:55:24 +0200

    [FIX] account_headquarter: Arreglar herencia funcion "def create" de "account.move.line". (#1318)

--- account_headquarter/__manifest__.py
+++ account_headquarter/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Account Headquarter",
-    'version': '14.0.1.2.0',
+    'version': '14.0.1.3.0',
     "category": "Invoices & Payments",
     "license": "AGPL-3",
     "author": "AvanzOSC",
