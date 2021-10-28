2fbf864f06b80ca6181bc7da0257299256b3c171
Tu Nombre <you@example.com>
2021-10-28 11:08:25 +0200

    [ADD] account_analytic_campaign: Campaign medium and source in account analytic line.

---
+++ account_analytic_campaign/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2021 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Account analytic Campaign",
+    'version': '14.0.1.0.0',
+    "author": "Avanzosc",
+    "category": "Sales/CRM",
+    "depends": [
+        "utm",
+        "sale_project",
+        "hr_timesheet"
+    ],
+    "data": [
+        "views/account_analytic_line_views.xml",
+    ],
+    "license": "AGPL-3",
+    'installable': True,
+}
