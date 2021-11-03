ce29b4a9880155eda3418984e7ffe1d14ba69f85
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2021-11-03 14:24:06 +0100

    [IMP] event_slides: Tener en cuenta participantes de curso, cuando se crea/borra relación entre evento-curso. (#1315)

--- event_slides/models/event_event.py
+++ event_slides/models/event_event.py
@@ -13,3 +13,23 @@ class EventEvent(models.Model):
         column1="event_id",
         column2="slides_id",
     )
+
+    def write(self, values):
+        result = super(EventEvent, self).write(values)
+        if 'slides_ids' in values:
+            for event in self:
+                event.recompute_courses_participants()
+        return result
+
+    def recompute_courses_participants(self):
+        channel_partner_obj = self.env['slide.channel.partner']
+        for registration in self.registration_ids.filtered(
+                lambda x: x.state == 'open'):
+            registration.create_student_in_courses()
+            cond = [('partner_id', '=', registration.student_id.id),
+                    ('event_registration_id', '=', registration.id),
+                    ('channel_id', 'not in', self.slides_ids.ids)]
+            channel_partners = channel_partner_obj.search(cond)
+            if channel_partners:
+                channel_partners.write(
+                    {'real_date_end': fields.Date.context_today(self)})
