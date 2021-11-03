1b495e97144aa374486c3b24099b909b7ccdeef0
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2021-11-03 14:24:06 +0100

    [IMP] event_slides: Tener en cuenta participantes de curso, cuando se crea/borra relación entre evento-curso. (#1315)

--- event_slides/models/event_registration.py
+++ event_slides/models/event_registration.py
@@ -12,9 +12,19 @@ class EventRegistration(models.Model):
             registration.create_student_in_courses()
 
     def create_student_in_courses(self):
+        channel_partner_obj = self.env['slide.channel.partner']
         for course in self.event_id.slides_ids:
-            vals = self._catch_values_for_slide_channel_partner(course)
-            self.env['slide.channel.partner'].create(vals)
+            cond = [('partner_id', '=', self.student_id.id),
+                    ('event_registration_id', '=', self.id),
+                    ('channel_id', '=', course.id)]
+            partner = channel_partner_obj.search(cond)
+            if not partner:
+                vals = self._catch_values_for_slide_channel_partner(course)
+                channel_partner_obj.create(vals)
+            else:
+                if partner.real_date_end:
+                    partner.write({'real_date_start': self.real_date_start,
+                                   'real_date_end': False})
 
     def _catch_values_for_slide_channel_partner(self, course):
         vals = {'partner_id': self.student_id.id,
@@ -28,11 +38,10 @@ class EventRegistration(models.Model):
         result = super(EventRegistration, self).write(vals)
         if (('real_date_start' in vals and
              vals.get('real_date_start', False)) or
-             ('real_date_end' in vals and
-                vals.get('real_date_end', False))):
+                ('real_date_end' in vals and
+                 vals.get('real_date_end', False))):
             self.update_course_participant_real_dates(vals)
         return result
-
 
     def update_course_participant_real_dates(self, vals):
         channel_partner_obj = self.env['slide.channel.partner']
