0c16229c100b687cce5605ef8410465bad05d77e
Thor <alfredodelafuente@avanzosc.es>
2021-11-04 12:15:59 +0100

    [ADD] account_invoice_event_report: Printing of invoice lines with event information.

---
+++ account_invoice_event_report/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2021 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Account Invoice Event Report",
+    "version": "14.0.1.0.0",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "category": "Custom",
+    "license": "AGPL-3",
+    "depends": [
+        "account",
+        "account_invoice_with_start_end_date_period",
+        "contract",
+        "sale_order_line_contract",
+        "event_sale",
+        "event_registration_student",
+        "event_registration_sale_line_contract"
+    ],
+    "data": [
+        "report/account_invoice_report.xml"
+    ],
+    "installable": True,
+}
