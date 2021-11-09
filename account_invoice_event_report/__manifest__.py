a2215dfe2f50444792ddd47f2fe6fced5eb0138f
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2021-11-09 14:40:18 +0100

    [IMP] account_invoice_event_report: Mostrar información de evento en facturas no recurrentes. (#1331)

--- account_invoice_event_report/__manifest__.py
+++ account_invoice_event_report/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
     "name": "Account Invoice Event Report",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.1.0",
     "author": "AvanzOSC",
     "website": "http://www.avanzosc.es",
     "category": "Custom",
