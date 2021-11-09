49d3c95a6c50df121f1e8f17117ce9d07dc50fbb
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2021-11-09 14:40:18 +0100

    [IMP] account_invoice_event_report: Mostrar información de evento en facturas no recurrentes. (#1331)

--- account_invoice_event_report/models/account_move_line.py
+++ account_invoice_event_report/models/account_move_line.py
@@ -1,6 +1,6 @@
 # Copyright 2021 Alfredo de la fuente - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class AccountMoveLline(models.Model):
@@ -8,10 +8,10 @@ class AccountMoveLline(models.Model):
 
     sale_order_line_id = fields.Many2one(
         string='Sale order line', store=True, comodel_name='sale.order.line',
-        related='contract_line_id.sale_order_line_id')
+        compute='_compute_sale_order_line_id')
     event_id = fields.Many2one(
         string='Event', store=True, comodel_name='event.event',
-        related='sale_order_line_id.event_id')
+        compute='_compute_event_id')
     student_name = fields.Char(
         string='Student name', compute='_compute_student_name')
 
@@ -23,3 +23,38 @@ class AccountMoveLline(models.Model):
             registration = self.env['event.registration'].search(cond, limit=1)
             if registration and registration.student_id:
                 line.student_name = registration.student_id.name
+        for line in self.filtered(
+                lambda x: x.sale_order_line_id and not x.contract_line_id):
+            sale_line = line.sale_order_line_id
+            cond = [('event_id', '=', sale_line.event_id.id),
+                    ('event_ticket_id', '=', sale_line.event_ticket_id.id),
+                    ('sale_order_line_id', '=', sale_line.id)]
+            registration = self.env['event.registration'].search(cond)
+            if len(registration) == 1 and registration.student_id:
+                line.student_name = registration.student_id.name
+
+    @api.depends('contract_line_id', 'contract_line_id.sale_order_line_id',
+                 'sale_line_ids')
+    def _compute_sale_order_line_id(self):
+        for line in self.filtered(
+            lambda x: x.contract_line_id and
+                x.contract_line_id.sale_order_line_id):
+            sale_line = line.contract_line_id.sale_order_line_id
+            line.sale_order_line_id = sale_line.id
+        for line in self.filtered(
+                lambda x: not x.contract_line_id and x.sale_line_ids):
+            if len(line.sale_line_ids) == 1:
+                line.sale_order_line_id = line.sale_line_ids[0].id
+
+    @api.depends('sale_order_line_id', 'sale_order_line_id.event_id',
+                 'sale_line_ids', 'sale_line_ids.event_id')
+    def _compute_event_id(self):
+        for line in self.filtered(
+            lambda x: x.contract_line_id and x.sale_order_line_id and
+                x.sale_order_line_id.event_id):
+            line.event_id = line.sale_order_line_id.event_id.id
+        for line in self.filtered(
+                lambda x: not x.contract_line_id and x.sale_line_ids):
+            if (len(line.sale_line_ids) == 1 and
+                    line.sale_line_ids[0].event_id):
+                line.event_id = line.sale_line_ids[0].event_id.id
