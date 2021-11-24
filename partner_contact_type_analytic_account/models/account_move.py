0d22ea31b8a647a2ea6c8f2e16bebea7aa4bc10c
Thor <alfredodelafuente@avanzosc.es>
2021-11-24 14:11:17 +0100

    [FIX] partner_contact_type_analytic_account: Estaba cogiendo mal la cta. analitica en líneas de factura.

--- partner_contact_type_analytic_account/models/account_move.py
+++ partner_contact_type_analytic_account/models/account_move.py
@@ -1,6 +1,6 @@
 # Copyright 2021 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class AccountMove(models.Model):
@@ -11,5 +11,15 @@ class AccountMove(models.Model):
         related='partner_id.contact_type_id', store=True)
     analytic_account_id = fields.Many2one(
         string='Analytic Account', comodel_name='account.analytic.account',
-        related='contact_type_id.analytic_account_id', company_dependent=True,
-        store=True)
+        compute='_compute_analytic_account_id', store=True, related=False,
+        company_dependent=False)
+
+    @api.depends('contact_type_id', 'contact_type_id.analytic_account_id')
+    def _compute_analytic_account_id(self):
+        for move in self.filtered(
+            lambda x: x.contact_type_id and
+                x.move_type in ('out_invoice', 'out_refund', 'out_receipt')):
+            if (move.contact_type_id.with_company(
+                    move.company_id.id).analytic_account_id):
+                move.analytic_account_id = move.contact_type_id.with_company(
+                    move.company_id.id).analytic_account_id.id
