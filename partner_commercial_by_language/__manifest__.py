e733b9034263089937355982c75af1d95d3b4dd6
Tu Nombre <you@example.com>
2021-12-01 12:25:41 +0100

    [ADD] partner_commercial_by_language

---
+++ partner_commercial_by_language/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2021 Daniel Campos - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Partner Commercial By Language",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://www.avanzosc.es",
+    'depends': [
+        "base",
+        "sales_team",
+        ],
+    "data": [
+        "views/res_lang_view.xml",
+    ],
+    "installable": True,
+}
