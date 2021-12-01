14081ec1c1af2834c48e89630060d87e4ceffacd
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2021-12-01 14:34:41 +0100

    [IMP] event_registration_student: Al marcar como asistido un participante, quitar fechas proximas del contrato, y fecha ultima factura = fecha fin. (#1330)

--- event_registration_student/models/contract_line.py
+++ event_registration_student/models/contract_line.py
@@ -1,7 +1,6 @@
 # Copyright 2021 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from odoo import models, fields
-from dateutil.relativedelta import relativedelta
 
 
 class ContractLine(models.Model):
@@ -12,27 +11,26 @@ class ContractLine(models.Model):
         inverse_name='contract_line_id')
 
     def write(self, vals):
-        if len(self) == 1 and vals.get('date_end', False):
-            try:
-                date_end = vals.get('date_end') + relativedelta(days=-1)
-            except Exception:
-                date_end = (fields.Date.from_string(vals.get('date_end')) +
-                            relativedelta(days=-1))
+        vals2 = vals
+        if (len(self) == 1 and
+            'finish_from_registration' in self.env.context and
+                vals.get('date_end', False)):
             if self.last_date_invoiced:
-                vals['last_date_invoiced'] = date_end
-            if ('recurring_next_date' in vals and not
-                    vals.get('reccurring_next_date')):
-                vals['recurring_next_date'] = vals.get('date_end')
+                vals['last_date_invoiced'] = vals.get('date_end')
+            vals.update({
+                'next_period_date_start': False,
+                'next_period_date_end': False,
+                'recurring_next_date': False})
         result = super(ContractLine, self).write(vals)
         if 'no_update_event_reg_dates' not in self.env.context:
-            if (('date_start' in vals and vals.get('date_start', False)) or
-                    ('date_end' in vals and vals.get('date_end', False))):
+            if (('date_start' in vals2 and vals2.get('date_start', False)) or
+                    ('date_end' in vals2 and vals2.get('date_end', False))):
                 lines = self.filtered(
                     lambda x: x.sale_order_id and x.sale_order_line_id and
                     x.sale_order_line_id.event_id and
                     x.sale_order_line_id.event_ticket_id)
                 if lines:
-                    lines.update_dates_in_event_registration(vals)
+                    lines.update_dates_in_event_registration(vals2)
         return result
 
     def update_dates_in_event_registration(self, vals):
