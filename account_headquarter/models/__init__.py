f56329e0d7cc9f181a3bbd383482e587374134e7
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2021-12-01 15:06:49 +0100

    [IMP] account_headquarter: Al crear linea factura desde pedido de venta, llevar el headquarter. (#1349)

--- account_headquarter/models/__init__.py
+++ account_headquarter/models/__init__.py
@@ -1,6 +1,7 @@
 from . import account_move
 from . import account_move_line
 from . import sale_order
+from . import sale_order_line
 from . import purchase_order
 from . import account_analytic_line
 from . import account_group
