8c5f4e29ceab1be2fcb940b83bdd161017c9b051
Avanzosc <leiremartinez@avanzosc.es>
2021-12-03 11:30:26 +0100

    [IMP] contacts_school_permission_age: only sign student if able

--- contacts_school_permission_age/models/res_partner_permission_type.py
+++ contacts_school_permission_age/models/res_partner_permission_type.py
@@ -2,7 +2,7 @@
 from odoo import api, fields, models
 
 
-class ResPartnerPermission(models.Model):
+class ResPartnerPermissionType(models.Model):
     _inherit = 'res.partner.permission.type'
 
     min_age = fields.Integer('Minimum signer age')
