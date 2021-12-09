fe133e215a84e1fe72ae95880746fdb680279c64
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2021-12-09 12:50:23 +0100

    [ADD] event_responsible_force_employee Domain para responsables de eventos, para que sean profesores. (#1352)

---
+++ event_responsible_force_employee/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2021 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Event Responsible Force Employee",
+    'version': '14.0.1.0.0',
+    "author": "Avanzosc",
+    "category": "Marketing/Events",
+    "depends": [
+        "hr",
+        "event_schedule"
+    ],
+    "data": [
+        "views/event_views.xml",
+    ],
+    "license": "AGPL-3",
+    'installable': True,
+}
