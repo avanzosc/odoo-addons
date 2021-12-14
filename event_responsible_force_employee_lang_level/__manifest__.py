1b7a0612b3e77a7bca6f5be596b43265bf8a050a
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2021-12-14 15:05:04 +0100

    [ADD] event_responsible_force_employee_lang_level: Responsables de eventos obligados a ser profes, y tener lenguage del evento. (#1364)

---
+++ event_responsible_force_employee_lang_level/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2021 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Event Responsible Force Employee Lang Level",
+    "version": "14.0.1.0.0",
+    "author": "Avanzosc",
+    "category": "Marketing/Events",
+    "depends": [
+        "event_responsible_force_employee",
+        "event_lang_level"
+    ],
+    "data": [
+        "views/event_event_views.xml",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": True,
+}
