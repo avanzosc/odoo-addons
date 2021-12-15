d16ba85faf0f213e5da5ddd0eca259feb9e76f90
Tu Nombre <you@example.com>
2021-12-15 09:30:47 +0100

    [MIG] partner_zone: Migration to 14.0

--- partner_zone/__manifest__.py
+++ partner_zone/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Zone for partner",
-    "version": "11.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Generic Modules",
     "license": "AGPL-3",
     "author": "AvanzOSC",
