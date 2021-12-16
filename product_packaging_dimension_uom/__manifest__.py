25316c6591412f0a8d7d055e36d203e90b4cdf53
Tu Nombre <you@example.com>
2021-12-16 15:36:13 +0100

    [ADD] product_packaging_dimension: Set the package dimension fields as floating and set the unit as eligible.

---
+++ product_packaging_dimension_uom/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2021 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Product Packaging Dimension UOM",
+    'version': '14.0.1.0.0',
+    "author": "Avanzosc",
+    "website": "http://www.avanzosc.es",
+    "category": "Product",
+    "depends": [
+        "delivery",
+        "product_packaging_dimension"
+    ],
+    "data": [
+        "views/product_packaging_views.xml"
+    ],
+    "license": "AGPL-3",
+    'installable': True,
+}
