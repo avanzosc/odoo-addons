983b5442f2d43c21b4d41c36d35f5bf7944e9b43
Thor <alfredodelafuente@avanzosc.es>
2021-12-20 09:19:37 +0100

    [ADD] event_price_shared_fix: Fix price sharing events.

---
+++ event_price_shared_fix/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2021 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Event Price Shared Fix",
+    'version': '14.0.1.0.0',
+    "category": "Marketing/Events",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "event_price_shared",
+    ],
+    "data": [
+        "views/event_event_views.xml",
+    ],
+    'installable': True,
+    'auto_install': False,
+}
