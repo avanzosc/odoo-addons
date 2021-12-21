a8683f79b612f3b3ace81c62d310e2d6cae7079a
Thor <alfredodelafuente@avanzosc.es>
2021-12-21 10:10:19 +0100

    [IMP] slide_channel_event_responsible: Al cancelar, o finalizar un evento, tener en cuenta los responsables del evento para poner fecha fin en sus cursos.

--- slide_channel_event_responsible/models/event_event.py
+++ slide_channel_event_responsible/models/event_event.py
@@ -1,6 +1,6 @@
 # Copyright 2021 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import models
+from odoo import models, fields
 
 
 class EventEvent(models.Model):
@@ -8,21 +8,31 @@ class EventEvent(models.Model):
 
     def write(self, values):
         confirm_stage = self.env.ref("event.event_stage_announced")
+        cancel_stage = self.env.ref("event.event_stage_cancelled")
+        done_stage = self.env.ref("event.event_stage_done")
         result = super(EventEvent, self).write(values)
         if ('stage_id' in values and
                 values.get('stage_id', 99) == confirm_stage.id):
-            self.event_reponsibles_to_slide_channel()
+            self.update_responsibles_in_slide_channel()
+        if ('stage_id' in values and
+                values.get('stage_id', 99) == cancel_stage.id):
+            self.update_responsibles_in_slide_channel(
+                fields.Date.context_today(self))
+        if ('stage_id' in values and
+                values.get('stage_id', 99) == done_stage.id):
+            self.update_responsibles_in_slide_channel(
+                self.date_end.date())
         return result
 
-    def event_reponsibles_to_slide_channel(self):
+    def update_responsibles_in_slide_channel(self, date=None):
         for event in self:
             for slide_channel in event.slides_ids:
                 if event.user_id:
-                    slide_channel.insert_event_reponsible_in_slide_channel(
-                        event, event.user_id)
+                    slide_channel.update_event_reponsible_in_slide_channel(
+                        event, event.user_id, date)
                 if event.main_responsible_id:
-                    slide_channel.insert_event_reponsible_in_slide_channel(
-                        event, event.main_responsible_id)
+                    slide_channel.update_event_reponsible_in_slide_channel(
+                        event, event.main_responsible_id, date)
                 if event.second_responsible_id:
-                    slide_channel.insert_event_reponsible_in_slide_channel(
-                        event, event.second_responsible_id)
+                    slide_channel.update_event_reponsible_in_slide_channel(
+                        event, event.second_responsible_id, date)
--- slide_channel_event_responsible/models/slide_channel.py
+++ slide_channel_event_responsible/models/slide_channel.py
@@ -6,14 +6,31 @@ from odoo import models
 class SlideChannel(models.Model):
     _inherit = 'slide.channel'
 
-    def insert_event_reponsible_in_slide_channel(self, event, responsible):
+    def update_event_reponsible_in_slide_channel(self, event, responsible,
+                                                 date):
         slide_channel_partner_obj = self.env['slide.channel.partner']
         cond = [('channel_id', '=', self.id),
                 ('partner_id', '=', responsible.partner_id.id)]
         slide_channel = slide_channel_partner_obj.search(cond, limit=1)
-        if not slide_channel:
-            self.create_responsible_in_slide_channel(
-                event, responsible.partner_id)
+        if not date:
+            if not slide_channel:
+                self.create_responsible_in_slide_channel(
+                    event, responsible.partner_id)
+            else:
+                if slide_channel.real_date_end:
+                    slide_channel.write({
+                        'real_date_start': event.date_begin.date(),
+                        'real_date_end': False})
+        if date and slide_channel:
+            events = self.event_ids.filtered(
+                lambda x: x.id != event.id and not x.stage_id.pipe_end and
+                (x.user_id and x.user_id == responsible) or
+                (x.main_responsible_id and
+                 x.main_responsible_id == responsible) or
+                (x.second_responsible_id == responsible))
+            if (not events or
+                    (events and len(events) == 1) and event == event):
+                slide_channel.write({'real_date_end': date})
 
     def create_responsible_in_slide_channel(self, event, partner):
         slide_channel_partner_obj = self.env['slide.channel.partner']
@@ -22,6 +39,6 @@ class SlideChannel(models.Model):
             'partner_id': partner.id,
             'partner_email': partner.email,
             'real_date_start': event.date_begin.date(),
-            'real_date_end': event.date_end.date()
+            'real_date_end': False
             }
         slide_channel_partner_obj.create(vals)
