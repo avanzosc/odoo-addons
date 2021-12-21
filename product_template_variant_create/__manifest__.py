062b44bc872d40b845ef2e0ea00a666917b49803
Oihane Crucelaegui <oihanecruce@gmail.com>
2021-12-21 14:42:46 +0100

    [FIX] product_template_variant_create: installation

--- product_template_variant_create/__manifest__.py
+++ product_template_variant_create/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Product Template Variant Create",
-    'version': '14.0.1.0.0',
+    "version": "14.0.1.0.0",
     "author": "Avanzosc",
     "website": "http://www.avanzosc.es",
     "category": "Sales",
@@ -11,10 +11,10 @@
         "sale_product_configurator",
     ],
     "data": [
-        "views/product_views.xml",
-        "wizards/create_product_variant_view.xml",
         "security/ir.model.access.csv",
+        "wizards/create_product_variant_view.xml",
+        "views/product_views.xml",
     ],
     "license": "AGPL-3",
-    'installable': True,
+    "installable": True,
 }
