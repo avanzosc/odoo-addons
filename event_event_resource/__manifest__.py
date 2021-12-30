e3dcdf71377e653a13979654293336af2db11eab
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2021-12-30 10:38:29 +0100

    [ADD] event_event_resource: In event new field "Resource". (#1379)

---
+++ event_event_resource/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2021 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Event Event Resource",
+    "version": "14.0.1.3.0",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "category": "Marketing/Events",
+    "license": "AGPL-3",
+    "depends": [
+        "event",
+        "website_event_track",
+        "resource"
+    ],
+    "data": [
+        "views/event_event_views.xml",
+        "views/event_track_views.xml"
+    ],
+    "installable": True,
+}
