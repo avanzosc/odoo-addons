b7743addced5072a5cf502770afb6c3d0d61ea5a
Tu Nombre <you@example.com>
2022-01-05 13:47:35 +0100

    [ADD][14.0] portal_wizard_extension

---
+++ portal_wizard_extension/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2022 Daniel Campos - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Portal Wizard Extension",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+        "portal",
+    ],
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "category": "Custom",
+    "data": [
+        "views/res_users_view.xml",
+        "views/portal_wizard_view.xml",
+    ],
+    "installable": True,
+}
