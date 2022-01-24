d3364feda4dd2f5c363ef6088b87b4599d71d373
leiremartinezdesantos <72156354+leiremartinezdesantos@users.noreply.github.com>
2022-01-24 11:48:13 +0100

    [ADD] slide_channel_survey: menu for own surveys (#1401)
    * [ADD] slide_channel_survey: menu for own surveys
    
    * [FIX] slide_channel_survey: fix menu order error
    
    * [IMP] slide_channel_survey: add restrictions for survey inputs

---
+++ slide_channel_survey/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2022 Leire Martinez de Santos - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    'name': "Slide Channel Survey",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    'category': 'website',
+    'version': '14.0.1.0.0',
+    'depends': ['survey', 'website_slides', 'slide_partner_menu'],
+    'data': [
+        #'security/ir.model.access.csv',
+        'security/slide_channel_survey_security.xml',
+        'views/views.xml',
+        'views/menus.xml',
+    ]
+}
