79847a286e9c97a3e007da8e12aab52a2f760604
Thor <alfredodelafuente@avanzosc.es>
2022-02-02 08:38:24 +0100

    [IMP] event_event_resource: In "resource" object new fields "email", and "password".

--- event_event_resource/models/event_event.py
+++ event_event_resource/models/event_event.py
@@ -1,6 +1,6 @@
 # Copyright 2021 Alfredo de la fuente - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class EventEvent(models.Model):
@@ -8,3 +8,11 @@ class EventEvent(models.Model):
 
     resource_id = fields.Many2one(
         string="Resource", comodel_name="resource.resource")
+
+    @api.onchange('resource_id')
+    def onchange_resource_id(self):
+        for event in self.filtered(lambda x: x.resource_id):
+            if event.resource_id.email:
+                event.teacher_zoom_email = event.resource_id.email
+            if event.resource_id.password:
+                event.teacher_zoom_pwd = event.resource_id.password
