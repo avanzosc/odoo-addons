3da98d021e6779caa39333b5b3491d53cddc8de2
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2022-02-02 09:45:53 +0100

    [ADD] calendar_event_resource: In "Calendar Event" object new field "Resource". (#1423)

---
+++ calendar_event_resource/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Calendar Event Resource",
+    "version": "14.0.1.0.0",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "category": "Marketing/Events",
+    "license": "AGPL-3",
+    "depends": [
+        "calendar",
+        "resource"
+    ],
+    "data": [
+        "views/calendar_event_views.xml",
+    ],
+    "installable": True,
+}
