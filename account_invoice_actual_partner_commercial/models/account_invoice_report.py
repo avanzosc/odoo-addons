d4ea7cfd3cb42c07c070f7ba6b3b208aa1f9064d
Avanzosc <leiremartinez@avanzosc.es>
2022-02-02 17:52:05 +0100

    [IMP] account_invoice_actual_partner_commercial: add field to report

--- account_invoice_actual_partner_commercial/models/account_invoice_report.py
+++ account_invoice_actual_partner_commercial/models/account_invoice_report.py
@@ -1,10 +1,30 @@
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class AccountInvoiceReport(models.Model):
     _inherit = 'account.invoice.report'
 
     current_user_id = fields.Many2one(
-        comodel_name='res.users', related='partner_id.user_id',
-        string="Current customer salesperson", store=True)
+        comodel_name='res.users',
+        string='Current customer salesperson',
+        #compute='_compute_invoice_current_user',
+        readonly=True)
+
+    @api.depends('partner_id', 'partner_id.user_id')
+    def _compute_invoice_current_user(self):
+        for record in self:
+            record.current_user_id = record.partner_id.user_id.id
+
+    def _select(self):
+        return super(AccountInvoiceReport, self)._select() + \
+            ", sub.current_user_id as current_user_id"
+
+    def _sub_select(self):
+        return super(AccountInvoiceReport, self)._sub_select() + \
+            ", ai.current_user_id as current_user_id"
+
+    def _group_by(self):
+        return super(AccountInvoiceReport, self)._group_by() + \
+            ", ai.current_user_id"
+
