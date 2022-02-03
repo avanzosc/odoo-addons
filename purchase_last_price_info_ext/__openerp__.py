07c28da78510c6b21991f16f8fe308ad180a400c
Daniel <7643575+Daniel-CA@users.noreply.github.com>
2022-02-03 13:25:58 +0100

    [ADD][8.0] purchase_last_price_info_ext (#1426)
    Co-authored-by: Daniel-CA <{ID}+{username}@users.noreply.github.com>

---
+++ purchase_last_price_info_ext/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# © 2022 Daniel Campos - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Purchase Last Price Info Extension",
+    "version": "8.0.1.0.0",
+    "category": "Custom Module",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "contributors": [
+        "Daniel Campos <danielcampos@avanzosc.es>",
+    ],
+    "depends": [
+        "purchase_last_price_info",
+        "purchase_discount",
+    ],
+    "data": [
+        "views/product_view.xml",
+    ],
+    "installable": True,
+}
