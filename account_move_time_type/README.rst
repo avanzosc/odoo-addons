9e364af5fee7bfda2f4d7baa4bb29fa452398e47
Thor <alfredodelafuente@avanzosc.es>
2022-02-28 15:19:40 +0100

    [FIX] account_move_time_type: Si en lineas factura no se calcula quantity2, no move a quantity.

--- account_move_time_type/README.rst
+++ account_move_time_type/README.rst
@@ -6,7 +6,9 @@
 Account Move Time Type
 ======================
 
-* Estimate hours, limit hours and time types in account move line. If limit hour is greater than the billable hours, the quantity to be billed will be the limit hour.
+* Estimate hours, limit hours and time types in account move line. If limit
+  hour is greater than the billable hours, the quantity to be billed will be
+  the limit hour.
 
 Bug Tracker
 ===========
--- account_move_time_type/wizard/sale_advance_payment_inv.py
+++ account_move_time_type/wizard/sale_advance_payment_inv.py
@@ -9,8 +9,11 @@ class SaleAdvancePaymentInv(models.TransientModel):
     def _create_invoice(self, order, so_line, amount):
         invoice = super(SaleAdvancePaymentInv, self)._create_invoice(
             order, so_line, amount)
+        print ('99999999999999999999999999999999999999')
         for line in invoice.invoice_line_ids.filtered(
-                lambda x: x.quantity != x.quantity2):
+                lambda x: x.quantity != x.quantity2 and
+                    x.calculated_quantity2):
+            print ('9999 entro en if')
             invoice.invoice_line_ids = [
                 (1, line.id, {'quantity': line.quantity2})]
         return invoice
