3b65d2de77b2e9d822a7d8a2da06ff18b92fe6d9
Tu Nombre <you@example.com>
2022-03-04 10:03:00 +0100

    [IMP] custom_breeding

--- custom_breeding/models/res_partner.py
+++ custom_breeding/models/res_partner.py
@@ -1,6 +1,6 @@
 # Copyright 2022 Berezi Amubieta - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class ResPartner(models.Model):
@@ -13,3 +13,25 @@ class ResPartner(models.Model):
     farm_type = fields.Selection(
         [('integrated', 'Integrated'),
          ('own', 'Own')], string="Farm Type", copy=False)
+    leaving_date = fields.Date(string='Leaving Date')
+    other_activity = fields.Char(string='Other Activity')
+    eurowin_account = fields.Char(string='Eurowin Account')
+    account_journal_id = fields.Many2one(
+        string='Journal',
+        comodel_name='account.journal',
+        domain=[('type', '=', 'purchase')])
+
+    @api.depends('is_company', 'name', 'parent_id.display_name', 'type', 'company_name', 'ref')
+    def _compute_display_name(self):
+        super(ResPartner, self)._compute_display_name()
+
+    def name_get(self):
+        result = []
+        for partner in self:
+            name = partner.name
+            if partner.parent_id:
+                name = u'{}, {}'.format(partner.parent_id.name, name)
+            if partner.ref:
+                name = u'{} {}'.format(partner.ref, name)
+            result.append((partner.id, name))
+        return result
