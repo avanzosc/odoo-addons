57522bb74289ae76d3235b1ad966d04145688963
Tu Nombre <you@example.com>
2022-03-08 09:52:45 +0100

    [ADD] res_partner_extra_email: Extra email in contacts.

---
+++ res_partner_extra_email/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2022 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Res Partner Extra Email",
+    'version': '14.0.1.0.0',
+    "author": "Avanzosc",
+    "website": "https://www.avanzosc.es",
+    "category": "Contacts",
+    "depends": [
+        "contacts",
+    ],
+    "data": [
+        "views/res_partner_views.xml",
+    ],
+    "license": "AGPL-3",
+    'installable': True,
+}
