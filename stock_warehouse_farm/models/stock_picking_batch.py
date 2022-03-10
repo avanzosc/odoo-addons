010de055c24fee40727b34f3f41494442188cbd4
Tu Nombre <you@example.com>
2022-03-10 14:30:00 +0100

    [IMP] stock_warehouse_farm: Farmer and tax entity in tree view.

--- stock_warehouse_farm/models/stock_picking_batch.py
+++ stock_warehouse_farm/models/stock_picking_batch.py
@@ -1,7 +1,7 @@
 # Copyright 2022 Berezi Amubieta - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models, _
+from odoo import fields, models
 
 
 class StockPickinBatch(models.Model):
