26517646e5f9e95c3072006a3d7408681e370a0a
Thor <alfredodelafuente@avanzosc.es>
2022-03-10 08:26:17 +0100

    [IMP] event_price_shared: Mostrar error si se intenta poner tarea a evento de precio compartido.

--- event_price_shared/models/event_event.py
+++ event_price_shared/models/event_event.py
@@ -1,6 +1,7 @@
 # Copyright 2021 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import models, fields
+from odoo import models, fields, _
+from odoo.exceptions import UserError
 
 
 class EventEvent(models.Model):
@@ -15,7 +16,10 @@ class EventEvent(models.Model):
         return super(EventEvent, self)._create_event_task(project, ticket)
 
     def write(self, values):
-        if (self.shared_price_event and len(self) == 1 and len(values) == 1 and
-                values.get('task_id', False)):
-            values = {}
+        if 'task_id' in values and values.get('task_id', False):
+            for event in self:
+                if event.shared_price_event:
+                    raise UserError(
+                        _("You can't put task to price sharing event: "
+                          "{}.").format(event.name))
         return super(EventEvent, self).write(values)
