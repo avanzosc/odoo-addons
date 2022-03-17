60b6a4412be7d365de5bb257d523c286ac1a6916
Berezi <82805447+Berezi@users.noreply.github.com>
2022-03-17 16:36:12 +0100

    [ADD] partner_contact:birthdate_visibility: Personal information notebook visible for all contact. (#1412)

---
+++ partner_contact_birthdate_visibility/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Res Partner Birthdate Visibiliy",
+    'version': '14.0.1.0.0',
+    "author": "Avanzosc",
+    "category": "Contact",
+    "depends": [
+        "partner_contact_birthdate",
+        "partner_contact_personal_information_page"
+        
+    ],
+    "data": [
+        "views/res_partner_views.xml",
+    ],
+    "license": "AGPL-3",
+    'installable': True,
+}
