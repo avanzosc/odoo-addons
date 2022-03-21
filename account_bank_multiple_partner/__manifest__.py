4735af8e9b4defd6e444244bb3af8bfbcd302e17
Oihane Crucelaegui <oihanecruce@gmail.com>
2022-03-21 11:18:41 +0100

    [MIG] account_bank_multiple_partner: Migration to 14.0

--- account_bank_multiple_partner/__manifest__.py
+++ account_bank_multiple_partner/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Account Bank Multiple Partner",
-    "version": "12.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "AvanzOSC",
     "summary": """Account Bank Multiple Partner""",
