905fa47bd13377628d8d7227dc00f0b012e6bc4e
Tu Nombre <you@example.com>
2022-03-22 18:07:21 +0100

    [IMP] stock_warehouse_farm

--- stock_warehouse_farm/models/stock_warehouse.py
+++ stock_warehouse_farm/models/stock_warehouse.py
@@ -30,3 +30,11 @@ class StockWarehouse(models.Model):
     tax_entity_id = fields.Many2one(
         string='Tax Entity',
         comodel_name='res.partner')
+    activity = fields.Selection(
+        [('fattening', 'Fattening'),
+         ('incubation', 'Incubation'),
+         ('reproduction', 'Reproduction')], string="Activity", copy=False)
+    other_activity = fields.Char(string='Other Activity')
+    farm_type = fields.Selection(
+        [('integrated', 'Integrated'),
+         ('own', 'Own')], string="Farm Type", copy=False)
