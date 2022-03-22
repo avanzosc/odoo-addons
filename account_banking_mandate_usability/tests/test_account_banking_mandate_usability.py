934dc32e3917aef484753a9350b70b7006128783
oihane <oihanecruce@gmail.com>
2022-03-22 17:05:22 +0100

    [FIX] account_banking_mandate_usability: missing import (#1485)

--- account_banking_mandate_usability/tests/test_account_banking_mandate_usability.py
+++ account_banking_mandate_usability/tests/test_account_banking_mandate_usability.py
@@ -1,47 +1,45 @@
 # Copyright 2019 Oihane Crucelaegui - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from .common import AccountBankingMandateUsabilityCommon
 from odoo import fields
 from odoo.tests import tagged
+
+from .common import AccountBankingMandateUsabilityCommon
 
 
 @tagged("post_install", "-at_install")
 class TestAccountBankingMandateUsability(AccountBankingMandateUsabilityCommon):
-
     def test_generate_banking_mandate_wizard_defaults(self):
         mandate_field_list = self.mandate_model.fields_get_keys()
         mandate_dict = self.mandate_model.default_get(mandate_field_list)
+        self.assertEqual(self.wizard_dict.get("bank_ids"), [(6, 0, self.bank.ids)])
         self.assertEqual(
-            self.wizard_dict.get("bank_ids"), [(6, 0, self.bank.ids)])
+            self.wizard_dict.get("mandate_format"), mandate_dict.get("format", False)
+        )
         self.assertEqual(
-            self.wizard_dict.get("mandate_format"),
-            mandate_dict.get("format", False))
+            self.wizard_dict.get("mandate_type"), mandate_dict.get("type", False)
+        )
         self.assertEqual(
-            self.wizard_dict.get("mandate_type"),
-            mandate_dict.get("type", False))
-        self.assertEqual(
-            self.wizard_dict.get("mandate_scheme"),
-            mandate_dict.get("scheme", False))
+            self.wizard_dict.get("mandate_scheme"), mandate_dict.get("scheme", False)
+        )
         self.assertEqual(
             self.wizard_dict.get("mandate_recurrent_sequence_type"),
-            mandate_dict.get("recurrent_sequence_type", False))
-        mandate_selection = self.mandate_model.fields_get(
-            allfields=mandate_field_list)
+            mandate_dict.get("recurrent_sequence_type", False),
+        )
+        mandate_selection = self.mandate_model.fields_get(allfields=mandate_field_list)
         mandate_format_sel = self.mandate_wiz_model._get_format_selection()
-        self.assertEqual(
-            mandate_selection["format"]["selection"], mandate_format_sel)
+        self.assertEqual(mandate_selection["format"]["selection"], mandate_format_sel)
         mandate_type_sel = self.mandate_wiz_model._get_type_selection()
-        self.assertEqual(
-            mandate_selection["type"]["selection"], mandate_type_sel)
+        self.assertEqual(mandate_selection["type"]["selection"], mandate_type_sel)
         mandate_scheme_sel = self.mandate_wiz_model._get_scheme_selection()
-        self.assertEqual(
-            mandate_selection["scheme"]["selection"], mandate_scheme_sel)
+        self.assertEqual(mandate_selection["scheme"]["selection"], mandate_scheme_sel)
         mandate_recurrent_sequence_type_sel = (
-            self.mandate_wiz_model._get_recurrent_sequence_type_selection())
+            self.mandate_wiz_model._get_recurrent_sequence_type_selection()
+        )
         self.assertEqual(
             mandate_selection["recurrent_sequence_type"]["selection"],
-            mandate_recurrent_sequence_type_sel)
+            mandate_recurrent_sequence_type_sel,
+        )
 
     def test_generate_banking_mandate_wizard_onchanges(self):
         self.assertFalse(self.wizard.signed)
@@ -59,15 +57,17 @@ class TestAccountBankingMandateUsability(AccountBankingMandateUsabilityCommon):
         today = fields.Date.context_today(self.mandate_model)
         self.assertFalse(self.bank._check_active_mandate())
         self.assertIn(self.bank, self.wizard.bank_ids)
-        self.wizard.write({
-            'signed': True,
-            'validate': True,
-        })
+        self.wizard.write(
+            {
+                "signed": True,
+                "validate": True,
+            }
+        )
         self.wizard.button_generate_mandates()
         self.assertTrue(self.bank._check_active_mandate())
         mandate = self.bank.mandate_ids[:1]
         self.assertEqual(mandate.signature_date, today)
-        self.assertTrue(mandate.state, 'valid')
+        self.assertTrue(mandate.state, "valid")
 
     def test_generate_banking_mandate_wizard_force_company(self):
         bank = self.bank.with_company(self.env.user.company_id.id)
@@ -75,12 +75,14 @@ class TestAccountBankingMandateUsability(AccountBankingMandateUsabilityCommon):
         today = fields.Date.context_today(self.mandate_model)
         self.assertFalse(bank._check_active_mandate())
         self.assertIn(bank, wizard.bank_ids)
-        wizard.write({
-            'signed': True,
-            'validate': True,
-        })
+        wizard.write(
+            {
+                "signed": True,
+                "validate": True,
+            }
+        )
         wizard.button_generate_mandates()
         self.assertTrue(bank._check_active_mandate())
         mandate = bank.mandate_ids[:1]
         self.assertEqual(mandate.signature_date, today)
-        self.assertTrue(mandate.state, 'valid')
+        self.assertTrue(mandate.state, "valid")
