14257e68a8f8a1ded5e4957e14eb965178f33e64
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2022-03-31 08:55:53 +0200

    [IMP] slide_partner_menu: Desde eventos y sesiones, atajo a "slide.slide.partner". (#1487)

--- slide_partner_menu/__manifest__.py
+++ slide_partner_menu/__manifest__.py
@@ -2,21 +2,24 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Slide partner menu",
-    'version': '14.0.1.0.0',
+    'version': '14.0.1.1.0',
     "author": "Avanzosc",
     "category": "Sales/CRM",
     "depends": [
         "website_slides",
         "event_slides",
         "website_slides_survey",
-        "contacts"
+        "contacts",
+        "website_event_track_claim"
     ],
     "data": [
         "views/slide_slide_partner_views.xml",
         "views/slide_channel_partner_views.xml",
         "views/slide_channel_views.xml",
         "views/res_partner_views.xml",
-        "views/slide_slide_views.xml"
+        "views/slide_slide_views.xml",
+        "views/event_event_views.xml",
+        "views/event_track_views.xml",
     ],
     "license": "AGPL-3",
     'installable': True,
--- slide_partner_menu/models/__init__.py
+++ slide_partner_menu/models/__init__.py
@@ -3,3 +3,5 @@ from . import slide_channel
 from . import slide_slide
 from . import res_partner
 from . import slide_channel_partner
+from . import event_event
+from . import event_track
