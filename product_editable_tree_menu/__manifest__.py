9bfe4f3417b89ab4ea0ac46078cfeacabed43366
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2022-04-19 09:52:18 +0200

    [ADD] product_editable_tree_menu: In Sales - Products, new menu "Products editable tree". (#1508)

---
+++ product_editable_tree_menu/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2022 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Product Editable Tree Menu",
+    'version': '14.0.1.0.0',
+    "category": "Sales/Sales",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "product",
+        "sale",
+        "sales_team",
+        "stock_picking_batch_breeding"
+    ],
+    "data": [
+        "views/product_template_views.xml",
+    ],
+    'installable': True,
+    'auto_install': False,
+}
