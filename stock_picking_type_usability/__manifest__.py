9aea052130c9be63a7711de2c2f9bf8b2f0f7b18
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2022-04-20 11:41:15 +0200

    [ADD] stock_picking_type_usability: In "Picking Type" object, show "Default Source Location", and "Default Destination Location" fields in tree view. (#1516)

---
+++ stock_picking_type_usability/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Stock Picking Type Usability",
+    "version": "14.0.1.0.0",
+    "category": "Hidden",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "stock",
+    ],
+    "excludes": [],
+    "data": [
+        "views/stock_picking_type_view.xml",
+    ],
+    "installable": True,
+}
