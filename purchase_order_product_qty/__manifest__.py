3ac55169ab4d461744984e63f5ebbd4e4555d3f9
Berezi <82805447+Berezi@users.noreply.github.com>
2022-04-20 14:21:16 +0200

    [ADD] purchase_order_product_qty: Calculated field, quantity of products from purchase order. (#1506)
    * [ADD] purchase_order_product_qty: Calculated field, quantity of products from purchase order.
    * [IMP] purchase_order_product_qty: take all lines
    
    Co-authored-by: Oihane Crucelaegui <oihanecruce@gmail.com>

---
+++ purchase_order_product_qty/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Purchase Order Product Qty",
+    "version": "14.0.1.0.0",
+    "category": "Purchase",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "purchase",
+    ],
+    "data": [
+        "views/purchase_order_view.xml",
+    ],
+    "installable": True,
+}
