c8260bb99df457864600305b654c949e4ae5de86
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2022-04-22 10:55:36 +0200

    [IMP] event_commute: Planificador para arreglar tareas de producto desplazamiento en líneas de venta. (#1515)

--- event_commute/__manifest__.py
+++ event_commute/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     'name': "Event Commute",
-    'version': '14.0.1.0.0',
+    'version': '14.0.1.1.0',
     'author': 'AvanzOSC',
     'website': 'http://www.avanzosc.es',
     'category': 'Marketing/Events',
