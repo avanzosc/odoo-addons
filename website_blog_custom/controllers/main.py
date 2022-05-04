55d1f0044e5743238182c309554cd45677d4663f
Avanzosc <leiremartinez@avanzosc.es>
2022-05-04 17:32:09 +0200

    [IMP] website_blog_custom: redirect to post if only one

--- website_blog_custom/controllers/main.py
+++ website_blog_custom/controllers/main.py
@@ -1,6 +1,8 @@
 
 from odoo import http
 from odoo.http import request
+from werkzeug.utils import redirect
+from odoo.addons.http_routing.models.ir_http import slug
 from odoo.addons.website_blog.controllers.main import WebsiteBlog
 
 
@@ -15,3 +17,13 @@ class WebsiteBlog(WebsiteBlog):
         res.qcontext.update({'blogs': blogs})
         return request.render("website_blog_custom.latest_blogs_custom",
                               res.qcontext)
+
+    @http.route()
+    def blog(self, blog=None, tag=None, page=1, **opt):
+        res = super(WebsiteBlog, self).blog(blog, tag, page, **opt)
+        posts = request.env['blog.post'].sudo().search([
+            ('blog_id', '=', blog.id)
+        ])
+        if posts and len(posts) == 1:
+            return redirect('/blog/%s/post/%s' % (slug(blog), slug(posts)))
+        return res
