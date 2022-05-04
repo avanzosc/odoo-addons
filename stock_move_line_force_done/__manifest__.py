588d0b4a97915780c7af2bc2a4da28a80083c538
Thor <alfredodelafuente@avanzosc.es>
2022-05-04 15:36:06 +0200

    [MIG] stock_move_line_force_done: Migration to 14.0

--- stock_move_line_force_done/__manifest__.py
+++ stock_move_line_force_done/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
     "name": "Stock Move Line Force Done",
-    "version": "12.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "AvanzOSC",
     "website": "http://www.avanzosc.es",
--- stock_move_line_force_done/models/stock_picking.py
+++ stock_move_line_force_done/models/stock_picking.py
@@ -1,12 +1,11 @@
 # Copyright 2022 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from odoo import models, api
+from odoo import models
 
 
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
-    @api.multi
     def button_force_done_detailed_operations(self):
         for picking in self:
             for move in picking.move_ids_without_package:
