cf4057a86d55dcfb45b71d2b94057e12eb33f62d
Avanzosc <leiremartinez@avanzosc.es>
2022-05-05 12:37:51 +0200

    [ADD] website_education_blog_crm: blogs for future students

---
+++ website_education_blog_crm/README.rst
@@ -0,0 +1,30 @@
+===========================
+Website Education Blogs CRM
+===========================
+
+.. |badge1| image:: https://img.shields.io/badge/licence-AGPL--3-blue.png
+    :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+    :alt: License: AGPL-3
+
+|badge1|
+
+This module add CRM to website blogs.
+
+
+**Table of contents**
+
+.. contents::
+   :local:
+
+Credits
+=======
+
+Authors
+~~~~~~~
+
+* Avanzosc
+
+Contributors
+~~~~~~~~~~~~
+
+* Leire Martinez de Santos <leiremartinez@avanzosc.es>
---
+++ website_education_blog_crm/models/blog_blog.py
@@ -0,0 +1,48 @@
+
+from odoo import api, fields, models
+from odoo.osv import expression
+from odoo.tools.safe_eval import safe_eval
+
+
+class BlogBlog(models.Model):
+    _inherit = "blog.blog"
+
+    future_students = fields.Boolean(
+        string="Allow future students")
+    invited_future_lead_ids = fields.Many2many(
+        string="Invited Partners",
+        comodel_name="crm.lead.future.student",
+        compute="_compute_future_invited_partners")
+    invited_future_partner_ids = fields.Many2many(
+        string="Invited Partners",
+        comodel_name="res.partner",
+        compute="_compute_future_invited_partners",
+        store=True)
+    invited_future_count = fields.Integer(
+        string="Count Invited People",
+        compute="_compute_future_invited_partners")
+
+    @api.depends("future_students", "education_course_ids")
+    def _compute_future_invited_partners(self):
+        future_student_obj = self.env["crm.lead.future.student"]
+        for record in self:
+            lead_students = future_student_obj.search( [
+                ("course_id", "in", record.education_course_ids.ids),
+            ])
+            record.invited_future_lead_ids = [(6, 0, lead_students.ids)]
+            record.invited_future_partner_ids = [(6, 0, lead_students.mapped('child_id').ids)]
+            record.invited_future_count = len(lead_students)
+
+    @api.multi
+    def button_open_blog_future_invitations(self):
+        self.ensure_one()
+        action = self.env.ref(
+            "website_education_blog_crm.action_blog_open_future_invitations")
+        action_dict = action.read()[0] if action else {}
+        domain = expression.AND([
+            [("id", "in", self.invited_future_lead_ids.ids)],
+            safe_eval(action.domain or "[]")])
+        action_dict.update({
+            "domain": domain,
+        })
+        return action_dict
