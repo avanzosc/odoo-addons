1a0da4dad310395f9253107821328ef6a829b7ef
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2022-05-10 16:18:44 +0200

    14.0 mig stock move line force done (#1526)
    * [ADD] stock_move_line_force_done: New button in pickings "Force done detailed operations". (#1479)
    
    * [MIG] stock_move_line_force_done: Migration to 14.0

---
+++ stock_move_line_force_done/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2022 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Stock Move Line Force Done",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "category": "Warehouse",
+    "depends": [
+        "stock"
+    ],
+    "data": [
+        "views/stock_picking_view.xml",
+    ],
+    "installable": True,
+}
