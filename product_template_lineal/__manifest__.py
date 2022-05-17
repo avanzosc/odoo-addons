47b65dd349c1f9524f7b2e76ae3f38531b0a7a45
Thor <alfredodelafuente@avanzosc.es>
2022-05-17 08:59:49 +0200

    [ADD] product_template_lineal: Lineal field in product template and variant.

---
+++ product_template_lineal/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2022 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Product Template Lineal",
+    'version': '12.0.1.0.0',
+    "author": "Avanzosc",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "category": "Inventory",
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/product_lineal_views.xml",
+        "views/product_template_views.xml",
+        "views/stock_picking_views.xml",
+        "views/stock_move_line_views.xml",
+        "views/stock_move_views.xml",
+    ],
+    "license": "AGPL-3",
+    'installable': True,
+}
---
+++ product_template_lineal/models/product_template.py
@@ -0,0 +1,11 @@
+# Copyright 2022 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import models, fields
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    lineal_id = fields.Many2one(
+        string='Lineal',
+        comodel_name='product.lineal')
