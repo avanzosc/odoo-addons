46177a8658ad2de57663a42c92a012ddc99af31c
oihane <oihanecruce@gmail.com>
2022-05-17 09:49:12 +0200

    [FIX] custom_breeding: add missing dependency (#1554)

--- custom_breeding/__manifest__.py
+++ custom_breeding/__manifest__.py
@@ -2,15 +2,16 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Custom Breeding",
-    'version': '14.0.1.0.0',
+    "version": "14.0.1.0.0",
     "author": "Avanzosc",
     "category": "Contacts",
     "depends": [
         "contacts",
+        "account",
     ],
     "data": [
         "views/res_partner_views.xml",
     ],
     "license": "AGPL-3",
-    'installable': True,
+    "installable": True,
 }
