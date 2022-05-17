54fafe518cf13f963e9cc803a9db8d89403d1c1b
Thor <alfredodelafuente@avanzosc.es>
2022-05-17 07:49:01 +0200

    [IMP] product_vehicle_model: Put dependency with "stock_production_lot_fleet_vehicle".

--- product_vehicle_model/__manifest__.py
+++ product_vehicle_model/__manifest__.py
@@ -2,11 +2,12 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Product Vehicle Model",
-    'version': '14.0.1.0.0',
+    'version': '14.0.1.1.0',
     "author": "Avanzosc",
     "category": "Sales/CRM",
     "depends": [
-        "fleet_extension"
+        "fleet_extension",
+        "stock_production_lot_fleet_vehicle"
     ],
     "data": [
         "views/product_template_views.xml",
