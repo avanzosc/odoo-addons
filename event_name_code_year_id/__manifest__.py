e9fcd14c14bee8158b734100457c10d5545c5fa1
Oihane Crucelaegui <oihanecruce@gmail.com>
2022-05-26 12:19:58 +0200

    [IMP] event_name_code_year_id: black, isort, prettier

--- event_name_code_year_id/__manifest__.py
+++ event_name_code_year_id/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Event Name Code Year Id",
-    'version': '14.0.1.1.0',
+    "version": "14.0.1.1.0",
     "category": "Marketing/Events",
     "license": "AGPL-3",
     "author": "AvanzOSC",
@@ -13,5 +13,5 @@
     "data": [
         "views/event_event_views.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
