666ca3975ae90e3b64d9b707b7e5ea0675514bed
Oihane Crucelaegui <oihanecruce@gmail.com>
2022-05-26 11:33:16 +0200

    [ADD] New module stock_account_valuation

---
+++ stock_account_valuation/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Stock Account Valuation",
+    "version": "14.0.1.0.0",
+    "category": "Hidden",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": [
+        "stock_account",
+    ],
+    "data": [
+        "views/stock_valuation_layer_views.xml",
+    ],
+    "installable": True,
+}
