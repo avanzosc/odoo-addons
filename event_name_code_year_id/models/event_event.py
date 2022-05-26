b642053a650b344daf006351ae390842a78b8213
Oihane Crucelaegui <oihanecruce@gmail.com>
2022-05-26 12:19:58 +0200

    [IMP] event_name_code_year_id: black, isort, prettier

--- event_name_code_year_id/models/event_event.py
+++ event_name_code_year_id/models/event_event.py
@@ -1,58 +1,71 @@
 # Copyright 2021 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class EventEvent(models.Model):
-    _inherit = 'event.event'
+    _inherit = "event.event"
 
     name = fields.Char(default="/")
 
     @api.model
     def create(self, values):
         event = super(EventEvent, self).create(values)
-        name = ''
+        name = ""
         if event.lang_id and event.lang_id.code:
             name = event.lang_id.code
-        name = event.id if not name else '{}-{}'.format(name, event.id)
+        name = event.id if not name else "{}-{}".format(name, event.id)
         if event.date_begin:
-            name = (event.date_begin.year if not name else
-                    '{}-{}'.format(name, event.date_begin.year))
+            name = (
+                event.date_begin.year
+                if not name
+                else "{}-{}".format(name, event.date_begin.year)
+            )
         if event.level_id:
-            name = (event.level_id.name if not name else
-                    '{}-{}'.format(name, event.level_id.name))
+            name = (
+                event.level_id.name
+                if not name
+                else "{}-{}".format(name, event.level_id.name)
+            )
         event.name = name
         return event
 
     def write(self, values):
-        print ()
-        if (('lang_id' in values and values.get('lang_id', False)) or
-            ('level_id' in values and values.get('level_id', False)) or
-                ('date_begin' in values and values.get('date_begin', False))):
-            name = ''
-            if 'lang_id' in values and values.get('lang_id', False):
-                lang = self.env['hr.skill'].browse(values.get('lang_id'))
+        if (
+            ("lang_id" in values and values.get("lang_id", False))
+            or ("level_id" in values and values.get("level_id", False))
+            or ("date_begin" in values and values.get("date_begin", False))
+        ):
+            name = ""
+            if "lang_id" in values and values.get("lang_id", False):
+                lang = self.env["hr.skill"].browse(values.get("lang_id"))
                 name = lang.code
             else:
                 name = self.lang_id.code
-            name = self.id if not name else '{}-{}'.format(name, self.id)
-            if 'date_begin' in values and values.get('date_begin', False):
-                date_begin = fields.Datetime.from_string(
-                    values.get('date_begin'))
-                name = (date_begin.year if not name else
-                        '{}-{}'.format(name, date_begin.year))
+            name = self.id if not name else "{}-{}".format(name, self.id)
+            if "date_begin" in values and values.get("date_begin", False):
+                date_begin = fields.Datetime.from_string(values.get("date_begin"))
+                name = (
+                    date_begin.year
+                    if not name
+                    else "{}-{}".format(name, date_begin.year)
+                )
             else:
-                name = (self.date_begin.year if not name else
-                        '{}-{}'.format(name, self.date_begin.year))
-            if 'level_id' in values and values.get('level_id', False):
-                level = self.env['hr.skill.level'].browse(
-                    values.get('level_id'))
-                name = level.name if not name else '{}-{}'.format(
-                    name, level.name)
+                name = (
+                    self.date_begin.year
+                    if not name
+                    else "{}-{}".format(name, self.date_begin.year)
+                )
+            if "level_id" in values and values.get("level_id", False):
+                level = self.env["hr.skill.level"].browse(values.get("level_id"))
+                name = level.name if not name else "{}-{}".format(name, level.name)
             else:
                 if self.level_id:
-                    name = self.level_id.name if not name else '{}-{}'.format(
-                        name, self.level_id)
-            values['name'] = name
+                    name = (
+                        self.level_id.name
+                        if not name
+                        else "{}-{}".format(name, self.level_id)
+                    )
+            values["name"] = name
         result = super(EventEvent, self).write(values)
         return result
--- event_name_code_year_id/tests/test_event_name_code_year_id.py
+++ event_name_code_year_id/tests/test_event_name_code_year_id.py
@@ -1,7 +1,6 @@
 # Copyright 2021 Alfredo de la Fuente - Avanzosc S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo.tests import common
-from odoo.tests import tagged
+from odoo.tests import common, tagged
 
 
 @tagged("post_install", "-at_install")
@@ -9,24 +8,25 @@ class TestNameCodeYearId(common.SavepointCase):
     @classmethod
     def setUpClass(cls):
         super(TestNameCodeYearId, cls).setUpClass()
-        cls.event_obj = cls.env['event.event']
-        cls.skill_type_lang = cls.env.ref('hr_skills.hr_skill_type_lang')
-        cls.skill_spanish = cls.env.ref('hr_skills.hr_skill_spanish')
-        cls.skill_filipino = cls.env.ref('hr_skills.hr_skill_filipino')
+        cls.event_obj = cls.env["event.event"]
+        cls.skill_type_lang = cls.env.ref("hr_skills.hr_skill_type_lang")
+        cls.skill_spanish = cls.env.ref("hr_skills.hr_skill_spanish")
+        cls.skill_filipino = cls.env.ref("hr_skills.hr_skill_filipino")
         cls.skill_type_lang.skill_language = True
-        cls.skill_spanish.code = 'SP'
-        cls.skill_filipino.code = 'FI'
+        cls.skill_spanish.code = "SP"
+        cls.skill_filipino.code = "FI"
 
     def test_event_name_code_year_id(self):
-        vals = {'name': 'User for event lang level',
-                'date_begin': '2025-01-06 08:00:00',
-                'date_end': '2025-01-15 10:00:00',
-                'lang_id': self.skill_spanish.id}
+        vals = {
+            "name": "User for event lang level",
+            "date_begin": "2025-01-06 08:00:00",
+            "date_end": "2025-01-15 10:00:00",
+            "lang_id": self.skill_spanish.id,
+        }
         event = self.event_obj.create(vals)
-        name = 'SP-{}-2025'.format(event.id)
+        name = "SP-{}-2025".format(event.id)
         self.assertEqual(event.name, name)
-        vals = {'date_begin': '2024-01-06 08:00:00',
-                'lang_id': self.skill_filipino.id}
+        vals = {"date_begin": "2024-01-06 08:00:00", "lang_id": self.skill_filipino.id}
         event.write(vals)
-        name = 'FI-{}-2024'.format(event.id)
+        name = "FI-{}-2024".format(event.id)
         self.assertEqual(event.name, name)
