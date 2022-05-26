0004bcc3b510b0421791dcaaf510a98bc9c9780c
Oihane Crucelaegui <oihanecruce@gmail.com>
2022-05-26 12:26:07 +0200

    [IMP] partner_commercial_by_language: black, isort, prettier

--- partner_commercial_by_language/models/res_lang.py
+++ partner_commercial_by_language/models/res_lang.py
@@ -7,8 +7,9 @@ from odoo import fields, models
 class ResLang(models.Model):
     _inherit = "res.lang"
 
-    last_comercial_id = fields.Many2one(string="Last Commercial",
-                                        comodel_name="res.users")
+    last_comercial_id = fields.Many2one(
+        string="Last Commercial", comodel_name="res.users"
+    )
 
     def _get_commercial_from_team(self):
         users_obj = self.env["res.users"]
@@ -18,20 +19,21 @@ class ResLang(models.Model):
                 team_id = user._get_crm_team()
                 if team_id:
                     users_dict.update(
-                        {user.id: {
-                            "user_name": user.name,
-                            "user_team": team_id
-                        }})
+                        {user.id: {"user_name": user.name, "user_team": team_id}}
+                    )
         new_commercial = False
         if users_dict:
             for user_data in users_dict.items():
-                if not self.last_comercial_id or \
-                        self.last_comercial_id.name < user_data[1]["user_name"]:
+                if (
+                    not self.last_comercial_id
+                    or self.last_comercial_id.name < user_data[1]["user_name"]
+                ):
                     new_commercial = users_obj.browse(user_data[0])
                     break
             if not new_commercial:
-                sorted_dict = sorted(users_dict.items(),
-                                     key=lambda item: item[1]["user_name"])
+                sorted_dict = sorted(
+                    users_dict.items(), key=lambda item: item[1]["user_name"]
+                )
                 new_commercial = users_obj.browse(sorted_dict[0][0])
             self.last_comercial_id = new_commercial
         return new_commercial
--- partner_commercial_by_language/tests/test_partner_commercial.py
+++ partner_commercial_by_language/tests/test_partner_commercial.py
@@ -12,30 +12,40 @@ class TestCommercialLanguage(common.SavepointCase):
         cls.ResUsers = cls.env["res.users"]
         cls.ResLang = cls.env["res.lang"]
         cls.CrmTeam = cls.env["crm.team"]
-        languages = cls.env["res.lang"].search([
-            ["code", "in", ["en_US", "fr_FR"]],
-            "|", ["active", "=", True], ["active", "=", False]
-        ])
+        languages = cls.env["res.lang"].search(
+            [
+                ["code", "in", ["en_US", "fr_FR"]],
+                "|",
+                ["active", "=", True],
+                ["active", "=", False],
+            ]
+        )
         languages.write({"active": True})
         cls.lang_us = cls.ResLang.search([("code", "=", "en_US")])
         cls.lang_fr = cls.ResLang.search([("code", "=", "fr_FR")])
-        cls.com_user_1 = cls.ResUsers.sudo().create({
-            "name": "Commercial User 1",
-            "login": "com_user1",
-            "email": "user1@example.com",
-            "commercial_lang_ids": [(4, cls.lang_us.id), (4, cls.lang_fr.id)],
-        })
-        cls.com_user_2 = cls.ResUsers.sudo().create({
-            "name": "Commercial User 2",
-            "login": "com_user2",
-            "email": "user2@example.com",
-            "commercial_lang_ids": [(4, cls.lang_us.id), (4, cls.lang_fr.id)],
-        })
-        cls.sales_team = cls.CrmTeam.sudo().create({
-            "name": "Test Sales Team",
-            "sequence": 5,
-            "member_ids": [(4, cls.com_user_1.id), (4, cls.com_user_2.id)],
-        })
+        cls.com_user_1 = cls.ResUsers.sudo().create(
+            {
+                "name": "Commercial User 1",
+                "login": "com_user1",
+                "email": "user1@example.com",
+                "commercial_lang_ids": [(4, cls.lang_us.id), (4, cls.lang_fr.id)],
+            }
+        )
+        cls.com_user_2 = cls.ResUsers.sudo().create(
+            {
+                "name": "Commercial User 2",
+                "login": "com_user2",
+                "email": "user2@example.com",
+                "commercial_lang_ids": [(4, cls.lang_us.id), (4, cls.lang_fr.id)],
+            }
+        )
+        cls.sales_team = cls.CrmTeam.sudo().create(
+            {
+                "name": "Test Sales Team",
+                "sequence": 5,
+                "member_ids": [(4, cls.com_user_1.id), (4, cls.com_user_2.id)],
+            }
+        )
 
     def test_comercial_language(self):
         partner_1 = self.ResPartner.create({"name": "Partner 1 test", "lang": "en_US"})
