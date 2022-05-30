71fc1f7bae9237eac075b7ea77811d84a2cac71f
Thor <alfredodelafuente@avanzosc.es>
2022-05-30 12:59:45 +0200

    [IMP] fleet_extension: Quitar de aqui la creación del módelo "fleet.vehicle.model.type".

--- fleet_extension/models/__init__.py
+++ fleet_extension/models/__init__.py
@@ -1,7 +1,6 @@
 from . import fleet_vehicle_model
 from . import fleet_vehicle_model_collection
 from . import fleet_vehicle
-from . import fleet_vehicle_model_type
 from . import stock_production_lot
 from . import product_template
 from . import fleet_vehicle_range
