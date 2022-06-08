af0ff2e36c899c17b75fd724ebf06482eb67a5d0
Thor <alfredodelafuente@avanzosc.es>
2022-06-08 14:15:51 +0200

    [ADD] account_fleet_trailer: In invoice lines show the vehicle.

---
+++ account_fleet_trailer/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2022 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Account Fleet Trailer",
+    'version': '14.0.1.0.0',
+    "author": "Avanzosc",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "category": "Accounting/Accounting",
+    "depends": [
+        "account_fleet",
+    ],
+    "data": [
+        "views/account_move_views.xml",
+    ],
+    "license": "AGPL-3",
+    'installable': True,
+}
