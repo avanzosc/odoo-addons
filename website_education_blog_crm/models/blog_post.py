2c4c6a76af845454ace6e11867712c2f474fb461
Avanzosc <leiremartinez@avanzosc.es>
2022-06-09 11:38:11 +0200

    [FIX] website_education_blog_crm: fix future students selection

--- website_education_blog_crm/models/blog_post.py
+++ website_education_blog_crm/models/blog_post.py
@@ -8,33 +8,42 @@ class BlogPost(models.Model):
     _inherit = "blog.post"
 
     future_students = fields.Boolean(
-        default="blog_id.future_students")
+        related="blog_id.future_students")
     invited_future_lead_ids = fields.Many2many(
         string="Invited Leads",
         comodel_name="crm.lead.future.student",
-        compute="_compute_future_invited_leads",)
+        compute="_compute_future_invited_leads", store=True)
     invited_future_count = fields.Integer(
         string="Count Invited Future People",)
 
-    @api.depends("education_course_ids", "education_center_ids")
+    @api.depends("allowed_group_ids")
+    def _compute_invited_partners(self):
+        for record in self:
+            if not record.future_students:
+                super(BlogPost, self)._compute_invited_partners()
+            else:
+                record._compute_future_invited_leads()
+
+    @api.depends("future_students", "education_course_ids", "education_center_ids")
     def _compute_future_invited_leads(self):
         future_student_obj = self.env["crm.lead.future.student"]
         for record in self:
             if record.future_students:
-                domain = [('id', 'in', record.blog_id.invited_future_lead_ids.ids)]
+                domain = [
+                    ('id', 'in', record.blog_id.invited_future_lead_ids.ids)]
                 if record.education_center_ids:
-                    domain += [("enrollment_center_id", "in", record.education_center_ids.ids),]
+                    domain += [
+                        ("school_id", "in", record.education_center_ids.ids),]
                 if record.education_course_ids:
-                    domain += [("enrollment_course_id", "in", record.education_course_ids.ids),]
+                    domain += [
+                        ("course_id", "in", record.education_course_ids.ids),]
                 lead_students = future_student_obj.search(domain)
                 record.invited_future_lead_ids = [(6, 0, lead_students.ids)]
-
-    @api.depends("invited_future_lead_ids")
-    def _compute_future_invited_partners(self):
-        for record in self:
-            if record.future_students and record.invited_future_lead_ids:
-                record.invited_partner_ids = record.invited_future_lead_ids.mapped('child_id').ids
-            record.invited_future_count = len(record.invited_future_lead_ids)
+                record.invited_partner_ids = [
+                    (6, 0, record.invited_future_lead_ids.mapped('child_id').ids)]
+                record.invited_future_count = len(
+                    record.invited_future_lead_ids)
+                record.invited_count = len(record.invited_partner_ids)
 
     @api.multi
     def button_open_post_future_invitations(self):
