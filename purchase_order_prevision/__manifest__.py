9482f346c4a89ef0fd230a1c1152ee45d3d7b0f4
Thor <alfredodelafuente@avanzosc.es>
2022-06-13 10:14:08 +0200

    [ADD] purchase_order_prevision: Prevision de pagos en analisis de compras.

---
+++ purchase_order_prevision/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2022 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Purchase Order Prevision",
+    "version": "12.0.1.0.0",
+    "category": "Purchases",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "contributors": [
+        "Ana Juaristi <anajuaristi@avanzosc.es>",
+        "Alfredo de la Fuente<alfredodelafuente@avanzosc.es>",
+    ],
+    "depends": [
+        "purchase",
+        "sale",
+        "account_payment_term_extension",
+    ],
+    "data": [
+        "views/purchase_order_views.xml",
+        "views/purchase_report_views.xml",
+    ],
+    "installable": True,
+}
