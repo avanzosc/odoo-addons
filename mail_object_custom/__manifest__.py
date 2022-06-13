392fdabcf79a4d1303af1238c0cea3fd29f02823
Avanzosc <leiremartinez@avanzosc.es>
2022-06-13 15:04:12 +0200

    [ADD] mail_object_custom: custom object mailing

---
+++ mail_object_custom/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 Leire Martinez de Santos - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Mail Object Custom",
+    "version": "12.0.1.0.0",
+    "category": "Website",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "mail",
+    ],
+    "data": [
+        "views/templates.xml",
+    ],
+    "installable": True,
+}
