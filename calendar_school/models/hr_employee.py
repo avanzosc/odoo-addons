75726c9f09b45e9c48fbeafc5b382a1181669c24
Oihane Crucelaegui <oihanecruce@gmail.com>
2022-06-14 17:23:10 +0200

    [IMP] calendar_school: new filters by substitute

--- calendar_school/models/hr_employee.py
+++ calendar_school/models/hr_employee.py
@@ -6,35 +6,54 @@ from odoo.tools.safe_eval import safe_eval
 
 
 class HrEmployee(models.Model):
-    _inherit = 'hr.employee'
+    _inherit = "hr.employee"
 
-    meeting_ids = fields.One2many(
-        comodel_name='calendar.event',
-        inverse_name='teacher_id', string='Tutoring meetings')
     count_meetings = fields.Integer(
-        string='# Tutoring meetings', compute='_compute_count_meetings')
+        string="# Tutoring meetings", compute="_compute_count_meetings")
+
+    @api.multi
+    def _get_meetings_domain(self):
+        self.ensure_one()
+        return [
+            "|", ("teacher_id", "=", self.id),
+            ("substitute_teacher_id", "=", self.id),
+        ]
 
     @api.multi
     def _compute_count_meetings(self):
-        meeting_obj = self.env['calendar.event']
+        meeting_obj = self.env["calendar.event"]
         for employee in self:
-            employee.count_meetings = meeting_obj.search_count([
-                ('supervised_year_id', 'in', employee.tutored_student_ids.ids),
-            ])
+            employee.count_meetings = meeting_obj.search_count(
+                employee._get_meetings_domain())
 
     @api.multi
     def button_show_meetings(self):
         self.ensure_one()
-        action = self.env.ref('calendar.action_calendar_event')
+        action = self.env.ref("calendar.action_calendar_event")
         action_dict = action.read()[0] if action else {}
-        action_dict['context'] = safe_eval(
-            action_dict.get('context', '{}'))
-        action_dict['context'].update({
-            'search_default_teacher_id': self.id,
-            'default_teacher_id': self.id,
+        action_dict["context"] = safe_eval(
+            action_dict.get("context", "{}"))
+        action_dict["context"].update({
+            "search_default_teacher_id": self.id,
+            "default_teacher_id": self.id,
         })
         domain = expression.AND([
-            [('teacher_id', 'in', self.ids)],
-            safe_eval(action.domain or '[]')])
-        action_dict.update({'domain': domain})
+            self._get_meetings_domain(),
+            safe_eval(action.domain or "[]")])
+        action_dict.update({"domain": domain})
+        return action_dict
+
+    @api.multi
+    def button_show_tutored_students(self):
+        self.ensure_one()
+        action_dict = super(HrEmployee, self).button_show_tutored_students()
+        action = self.env.ref(
+            "hr_school.action_hr_employee_supervised_year_form")
+        domain = expression.OR([
+            ["|", ("teacher_id", "=", self.id),
+             ("current_substitute_id", "=", self.id)],
+            safe_eval(action.domain or "[]")])
+        action_dict.update({
+            "domain": domain,
+        })
         return action_dict
--- calendar_school/tests/test_calendar_school.py
+++ calendar_school/tests/test_calendar_school.py
@@ -21,33 +21,33 @@ class TestCalendarSchool(TestCalendarSchoolCommon):
         self.assertEquals(self.student.student_count_meetings, 4)
         action_dict = self.student.button_show_meetings()
         self.assertIn(
-            ('student_id', 'in', self.student.ids),
-            action_dict.get('domain'))
+            ("student_id", "in", self.student.ids),
+            action_dict.get("domain"))
         self.assertEquals(self.family.family_count_meetings, 2)
         action_dict = self.family.button_show_meetings()
         self.assertIn(
-            ('family_id', 'in', self.family.ids),
-            action_dict.get('domain'))
+            ("family_id", "in", self.family.ids),
+            action_dict.get("domain"))
         self.assertEquals(self.teacher.count_meetings, 4)
         action_dict = self.teacher.button_show_meetings()
         self.assertIn(
-            ('teacher_id', 'in', self.teacher.ids),
-            action_dict.get('domain'))
+            ("teacher_id", "=", self.teacher.id),
+            action_dict.get("domain"))
         self.assertEquals(self.tutor.count_meetings, 4)
         action_dict = self.tutor.button_show_meetings()
         self.assertIn(
-            ('supervised_year_id', 'in', self.tutor.ids),
-            action_dict.get('domain'))
+            ("supervised_year_id", "in", self.tutor.ids),
+            action_dict.get("domain"))
         event = self.tutor.meeting_ids[:1]
-        self.assertEquals(event.state, 'draft')
+        self.assertEquals(event.state, "draft")
         event.action_open()
-        self.assertEquals(event.state, 'open')
+        self.assertEquals(event.state, "open")
         event.action_done()
-        self.assertEquals(event.state, 'done')
+        self.assertEquals(event.state, "done")
         event.action_cancel()
-        self.assertEquals(event.state, 'cancel')
+        self.assertEquals(event.state, "cancel")
         event.action_draft()
-        self.assertEquals(event.state, 'draft')
+        self.assertEquals(event.state, "draft")
 
     def test_calendar_school_wizard(self):
         self.assertEquals(self.tutor.count_meetings, 0)
@@ -56,21 +56,25 @@ class TestCalendarSchool(TestCalendarSchoolCommon):
         wiz.meetings_confirm()
         self.tutor.invalidate_cache()
         self.assertEquals(self.tutor.count_meetings, 4)
-        cond = [('supervised_year_id', '=', self.tutor[0].id),
-                ('teacher_id', '=',  self.tutor[0].teacher_id.id),
-                ('student_id', '=', self.tutor[0].student_id.id)]
+        cond = [
+            ("supervised_year_id", "=", self.tutor[0].id),
+            ("teacher_id", "=",  self.tutor[0].teacher_id.id),
+            ("student_id", "=", self.tutor[0].student_id.id),
+        ]
         calendars = self.calendar_model.search(cond)
         from_date = self.calendar_model.browse(
             min(calendars, key=lambda x: x.start)).start
         to_date = self.calendar_model.browse(
             max(calendars, key=lambda x: x.stop)).stop
-        wiz_vals = {'substitute_teacher_id': self.teacher2.id,
-                    'from_date': from_date.date(),
-                    'to_date': to_date.date()}
+        wiz_vals = {
+            "substitute_teacher_id": self.teacher2.id,
+            "from_date": from_date.date(),
+            "to_date": to_date.date(),
+        }
         wizard = self.wizard2_model.with_context(
-            active_ids=self.tutor.ids).create(wiz_vals)
-        wizard.onchange_dates()
-        self.assertEquals(len(wizard.lines_ids), 1)
+            active_ids=self.tutor.ids,
+            active_model=self.tutor._name,
+        ).create(wiz_vals)
         wizard.with_context(
             active_ids=self.tutor.ids).change_teacher()
         self.assertEquals(len(self.tutor.substitution_ids), 1)
@@ -78,11 +82,13 @@ class TestCalendarSchool(TestCalendarSchoolCommon):
             self.tutor.substitution_ids[0].from_date, wizard.from_date)
         self.assertEquals(
             self.tutor.substitution_ids[0].to_date, wizard.to_date)
-        cond = [('supervised_year_id', '=', self.tutor[0].id),
-                ('teacher_id', '=',  self.tutor[0].teacher_id.id),
-                ('start', '>=', from_date),
-                ('start', '<=', to_date),
-                ('student_id', '=', self.tutor[0].student_id.id)]
+        cond = [
+            ("supervised_year_id", "=", self.tutor[0].id),
+            ("teacher_id", "=",  self.tutor[0].teacher_id.id),
+            ("start", ">=", from_date),
+            ("start", "<=", to_date),
+            ("student_id", "=", self.tutor[0].student_id.id),
+        ]
         calendars = self.calendar_model.search(cond)
         for calendar in calendars:
             self.assertIn(
