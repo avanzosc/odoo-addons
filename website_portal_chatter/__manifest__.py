b3d42ef9f952de8a65e7ea4cdd1005a8284d7db5
leiremartinezdesantos <72156354+leiremartinezdesantos@users.noreply.github.com>
2022-06-14 13:22:12 +0200

    [ADD] website_portal_chatter: hide chatters from portal objects (#1604)

---
+++ website_portal_chatter/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2022 Leire Martinez de Santos - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Website Portal Chatter",
+    "version": "12.0.1.0.0",
+    "category": "website",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "portal",
+        "sale",
+    ],
+    "data": [
+        "views/templates.xml",
+    ],
+    "installable": True,
+}
