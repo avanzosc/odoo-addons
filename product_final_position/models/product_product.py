34cd03ce4bc5e5b2e4770d79f415d52e8baf0fcb
Oihane Crucelaegui <oihanecruce@gmail.com>
2022-06-22 12:05:25 +0200

    [IMP] product_final_position: black, isort, prettier

--- product_final_position/models/product_product.py
+++ product_final_position/models/product_product.py
@@ -1,9 +1,14 @@
+# Copyright 2022 AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
 from odoo import fields, models
+
 
 class ProductProduct(models.Model):
     _inherit = "product.product"
-    
-    quartering_location_ids = fields.One2many(comodel_name="product.quartering.location",
-                                            inverse_name="product_id",
-                                            string="Quartering Location")
-                                            
+
+    quartering_location_ids = fields.One2many(
+        comodel_name="product.quartering.location",
+        inverse_name="product_id",
+        string="Quartering Location",
+    )
--- product_final_position/models/product_quartering_location.py
+++ product_final_position/models/product_quartering_location.py
@@ -1,11 +1,20 @@
+# Copyright 2022 AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
 from odoo import fields, models
+
 
 class ProductQuarteringLocation(models.Model):
     _name = "product.quartering.location"
     _description = "Product Quartering Locations"
-    _order = "product_final_id,position"   
-    
-    product_final_id = fields.Many2one(comodel_name="product.final", 
-                                       string="Final Product")
-    product_id = fields.Many2one(comodel_name="product.product")
+    _order = "product_final_id,position"
+
+    product_final_id = fields.Many2one(
+        comodel_name="product.final",
+        string="Final Product",
+    )
+    product_id = fields.Many2one(
+        comodel_name="product.product",
+        string="Product",
+    )
     position = fields.Char(string="Position")
