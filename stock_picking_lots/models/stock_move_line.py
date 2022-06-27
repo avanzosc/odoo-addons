ed0701d64f475adc205934a8f4fe07b7ea67d56b
oihane <oihanecruce@gmail.com>
2022-06-27 14:28:30 +0200

    [IMP] stock_picking_lots: add new column to excel as lot internal reference (#1617)

--- stock_picking_lots/models/stock_move_line.py
+++ stock_picking_lots/models/stock_move_line.py
@@ -1,12 +1,25 @@
 # Copyright 2019 Mikel Arregi Etxaniz - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class StockMoveLine(models.Model):
     _inherit = "stock.move.line"
 
-    imei = fields.Char(string="imei")
+    imei = fields.Char(
+        string="imei",
+    )
+    lot_ref = fields.Char(
+        string="Lot Internal Reference",
+    )
+
+    @api.onchange("lot_name", "lot_id")
+    def onchange_serial_number(self):
+        result = super(StockMoveLine, self).onchange_serial_number()
+        if self.lot_id:
+            self.imei = self.lot_id.imei if self.lot_id.imei else self.imei
+            self.lot_ref = self.lot_id.ref if self.lot_id.ref else self.lot_ref
+        return result
 
 
 class StockMove(models.Model):
@@ -14,9 +27,12 @@ class StockMove(models.Model):
 
     def _action_done(self):
         res = super()._action_done()
-        for line in res.mapped('move_line_ids').filtered(
-                lambda l: l.imei and l.lot_id):
-            line.lot_id.imei = line.imei
+        for line in res.mapped("move_line_ids").filtered(
+                lambda l: (l.imei or l.lot_ref) and l.lot_id):
+            if line.imei:
+                line.lot_id.imei = line.imei
+            if line.lot_ref:
+                line.lot_id.ref = line.lot_ref
         return res
 
     def _prepare_move_line_vals(self, quantity=None, reserved_quant=None):
@@ -26,5 +42,6 @@ class StockMove(models.Model):
             vals = dict(
                 vals,
                 imei=reserved_quant.lot_id.imei,
+                lot_ref=reserved_quant.lot_id.ref,
             )
         return vals
