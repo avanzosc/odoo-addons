8c172162cad480946ec346e27063978f84804e03
oihane <oihanecruce@gmail.com>
2022-07-04 18:23:43 +0200

    [IMP] product_vehicle_model: remove tags from product.template (#1643)

--- product_vehicle_model/__manifest__.py
+++ product_vehicle_model/__manifest__.py
@@ -2,16 +2,14 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Product Vehicle Model",
-    'version': '14.0.1.0.0',
+    "version": "14.0.1.0.0",
     "author": "Avanzosc",
     "category": "Sales/CRM",
-    "depends": [
-        "fleet_extension"
-    ],
+    "depends": ["fleet_extension"],
     "data": [
         "views/product_template_views.xml",
         "views/fleet_vehicle_views.xml",
     ],
     "license": "AGPL-3",
-    'installable': True,
+    "installable": True,
 }
