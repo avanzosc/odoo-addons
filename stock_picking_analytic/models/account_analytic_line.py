e755a960c4d4f6859efbadb4deb185514c0be885
Oihane Crucelaegui <oihanecruce@gmail.com>
2022-07-05 09:51:06 +0200

    [FIX] stock_picking_analytic: missing keyword

--- stock_picking_analytic/models/account_analytic_line.py
+++ stock_picking_analytic/models/account_analytic_line.py
@@ -5,10 +5,12 @@ from odoo import fields, models
 
 
 class AccountAnalyticLine(models.Model):
-    _inherit = 'account.analytic.line'
+    _inherit = "account.analytic.line"
 
-    stock_move_id = fields.Many2one(
-        comodel_name='stock.move', string='Stock Move')
+    stock_move_id = fields.Many2one(comodel_name="stock.move", string="Stock Move")
     picking_id = fields.Many2one(
-        comodel_name='stock.picking', string='Picking',
-        related='stock_move_id.picking_id', store=True)
+        comodel_name="stock.picking",
+        string="Picking",
+        related="stock_move_id.picking_id",
+        store=True,
+    )
