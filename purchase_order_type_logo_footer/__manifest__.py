91b431855f31e76c663d2d40efdcd953c4c50c0c
Thor <alfredodelafuente@avanzosc.es>
2022-07-06 13:25:48 +0200

    [ADD] purchase_order_type_logo_footer: In "Purchase order types" new fields: "Logo for reports", and "Footer for reports".

---
+++ purchase_order_type_logo_footer/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Purchase Order Type Logo Footer",
+    "version": "14.0.1.0.0",
+    "category": "Purchases",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": [
+        "purchase_order_type",
+    ],
+    "excludes": [],
+    "data": [
+        "views/purchase_order_type_view.xml",
+    ],
+    "installable": True,
+}
