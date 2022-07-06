39b83f5435f8ee6d7ec20f7c3da4b4a47db3f02a
Tu Nombre <you@example.com>
2022-07-06 08:10:08 +0200

    [14.0][IMP] custom_warehouse_farm:

--- stock_warehouse_farm/models/stock_location.py
+++ stock_warehouse_farm/models/stock_location.py
@@ -1,10 +1,23 @@
 # Copyright 2022 Berezi Amubieta - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import models, api
+from odoo import api, fields, models
 
 
 class StockLocation(models.Model):
     _inherit = 'stock.location'
+
+    type_id = fields.Many2one(
+        string='Section',
+        comodel_name='category.type')
+    activity = fields.Selection(
+        [('fattening', 'Fattening'),
+         ('incubation', 'Incubation'),
+         ('reproduction', 'Reproduction'),
+         ('recry', 'Recry'),
+         ('birth', 'Birth')], string="Activity", copy=False,
+        related="warehouse_id.activity",
+        store=True)
+    is_hatchery = fields.Boolean(string="Is Hatchery", default=False)
 
     @api.model
     def name_search(self, name='', args=None, operator='ilike', limit=100):
@@ -13,7 +26,7 @@ class StockLocation(models.Model):
         if not name:
             return result
         my_name = '%{}%'.format(name)
-        cond = [('warehouse_id', 'ilike', my_name)]
+        cond = [('warehouse_id', 'ilike', my_name), ("usage", "=", "internal")]
         warehouses = self.search(cond)
         for warehouse in warehouses:
             found = False
