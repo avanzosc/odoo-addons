8c3df175c30d22fc5ad416fec992091efae43f7b
Tu Nombre <you@example.com>
2022-07-07 14:16:00 +0200

    [14.0][IMP] stock_picking_batch_mother:

--- stock_picking_batch_mother/models/laying_rate.py
+++ stock_picking_batch_mother/models/laying_rate.py
@@ -1,6 +1,6 @@
 # Copyright 2022 Berezi Amubieta - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import models, fields
+from odoo import api, fields, models
 
 
 class LayingRate(models.Model):
@@ -15,3 +15,17 @@ class LayingRate(models.Model):
         comodel_name='stock.picking.batch')
     week = fields.Integer(string='Week')
     percentage_laying = fields.Float(string='% of Laying')
+    laying_start_date = fields.Date(string="Laying Start Date")
+    estimate_laying = fields.Integer(
+        string="Estimate Laying",
+        compute="_compute_estimate_laying",
+        store=True)
+    real_laying = fields.Float(
+        string="Real Laying")
+
+    @api.depends("mother_id", "mother_id.hen_unit",
+                 "percentage_laying")
+    def _compute_estimate_laying(self):
+        for line in self:
+            line.estimate_laying = 7 * line.mother_id.hen_unit * (
+                line.percentage_laying) / 100
