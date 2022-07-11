47aa31142f552ccae32f0eb4fb86cbd415621c4a
Tu Nombre <you@example.com>
2022-07-11 13:16:15 +0200

    [14.0][IMP] stock_picking_date_donte:

--- stock_picking_date_done/models/stock_picking.py
+++ stock_picking_date_done/models/stock_picking.py
@@ -1,9 +1,17 @@
 # Copyright 2022 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import models, fields
+from odoo import _, fields, models
+from odoo.exceptions import ValidationError
 
 
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
     custom_date_done = fields.Datetime(string='Date realized')
+
+    def button_validate(self):
+        result = super(StockPicking, self).button_validate()
+        if not self.custom_date_done:
+            raise ValidationError(
+                _("You must introduce the done date."))
+        return result
