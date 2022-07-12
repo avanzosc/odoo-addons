9583f97482f15f3bc8b800271bf362b3322dab41
Oihane Crucelaegui <oihanecruce@gmail.com>
2022-07-12 11:43:05 +0200

    [IMP] product_type_allow_change: black, isort, prettier

--- product_type_allow_change/models/product_template.py
+++ product_type_allow_change/models/product_template.py
@@ -14,26 +14,33 @@ class ProductTemplate(models.Model):
     @api.onchange("type")
     def _onchange_type(self):
         res = super(ProductTemplate, self)._onchange_type() or {}
-        if ("warning" in res and
-                self.env.user.has_group("product_type_allow_change."
-                                        "group_product_allow_type_change")):
+        if "warning" in res and self.env.user.has_group(
+            "product_type_allow_change." "group_product_allow_type_change"
+        ):
             warning = res.get("warning").get("message", _("Not launched warning!"))
             _logger.warning(warning)
             del res["warning"]
         return res
 
     def write(self, vals):
-        if (any("type" in vals and vals["type"] != prod_tmpl.type for prod_tmpl in self)
-                and self.env.user.has_group("product_type_allow_change."
-                                            "group_product_allow_type_change")):
-            existing_move_lines = self.env["stock.move.line"].search([
-                ("product_id", "in", self.mapped("product_variant_ids").ids),
-                ("state", "in", ["partially_available", "assigned"]),
-            ])
+        if any(
+            "type" in vals and vals["type"] != prod_tmpl.type for prod_tmpl in self
+        ) and self.env.user.has_group(
+            "product_type_allow_change." "group_product_allow_type_change"
+        ):
+            existing_move_lines = self.env["stock.move.line"].search(
+                [
+                    ("product_id", "in", self.mapped("product_variant_ids").ids),
+                    ("state", "in", ["partially_available", "assigned"]),
+                ]
+            )
             if existing_move_lines:
                 _logger.warning(
-                    _("You should not change the type of a product that is currently "
-                      "reserved on a stock move. If you need to change the type, you "
-                      "should first unreserve the stock move."))
+                    _(
+                        "You should not change the type of a product that is currently "
+                        "reserved on a stock move. If you need to change the type, you "
+                        "should first unreserve the stock move."
+                    )
+                )
                 existing_move_lines.mapped("move_id")._do_unreserve()
         return super(ProductTemplate, self).write(vals)
