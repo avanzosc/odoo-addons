55de55ea35d19559a5889a0f8da58eb3f5d3b90c
Tu Nombre <you@example.com>
2022-07-14 16:49:26 +0200

    [14.0][IMP] custom_descarga

---
+++ custom_descarga/models/stock_move_line.py
@@ -0,0 +1,12 @@
+# Copyright 2022 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import fields, models
+
+
+class StockMoveLine(models.Model):
+    _inherit = "stock.move.line"
+
+    download_unit = fields.Integer(
+        string="Download Unit",
+        related="saca_line_id.download_unit",
+        store=True)
