24d7d7461dcc73f0d251d20135dfc8607192009b
Oihane Crucelaegui <oihanecruce@gmail.com>
2022-07-20 14:24:37 +0200

    [IMP] stock_picking_lots: add error log

--- stock_picking_lots/__manifest__.py
+++ stock_picking_lots/__manifest__.py
@@ -4,24 +4,23 @@
 
 {
     "name": "Stock Picking Lots and Locations",
-    "version": "12.0.1.0.0",
+    "version": "12.0.2.0.0",
     "license": "AGPL-3",
-    "category": "Others",
+    "category": "Tools",
     "author": "cubells",
     "website": "http://obertix.net",
     "depends": [
-        'stock',
+        "stock",
     ],
     "data": [
-        # 'wizard/serial_and_locations_view.xml',
-        'wizard/import_lots_view.xml',
-        'views/serial_lots_views.xml',
-        'views/stock_production_lot_view.xml',
+        # "wizard/serial_and_locations_view.xml",
+        "wizard/import_lots_view.xml",
+        "views/serial_lots_views.xml",
+        "views/stock_production_lot_view.xml",
     ],
-    'external_dependencies': {
-        'python': [
-            # 'xlsxwriter',
-            'xlrd',
+    "external_dependencies": {
+        "python": [
+            "xlrd",
         ],
     },
     "installable": True
