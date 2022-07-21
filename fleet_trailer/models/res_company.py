874980d233a2c37338b1f476bf6f0243bab10d24
Thor <alfredodelafuente@avanzosc.es>
2022-07-21 12:18:42 +0200

    [IMP] fleet_trailer: New report "Tecnical sheet".

---
+++ fleet_trailer/models/res_company.py
@@ -0,0 +1,16 @@
+# Copyright 2022 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import models, fields
+
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    lang = fields.Selection(
+        string="Language", related="partner_id.lang")
+    company_city = fields.Char(
+        string="City", related="partner_id.city")
+    company_zip = fields.Char(
+        string="Zip", related="partner_id.zip")
+    company_street = fields.Char(
+        string="Street", related="partner_id.street")
