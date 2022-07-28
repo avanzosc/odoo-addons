ed7df00e9b0351498c9332330a5e184cb2d5c2ea
Tu Nombre <you@example.com>
2022-07-28 09:56:55 +0200

    [14.0][IMP] stock_picking_package_usability:

--- stock_picking_package_usability/models/product_packaging.py
+++ stock_picking_package_usability/models/product_packaging.py
@@ -1,6 +1,6 @@
 # Copyright 2021 Berezi Amubieta - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import models, fields, api
+from odoo import fields, models
 
 
 class ProductPackaging(models.Model):
--- stock_picking_package_usability/models/stock_picking.py
+++ stock_picking_package_usability/models/stock_picking.py
@@ -1,27 +1,35 @@
 # Copyright 2021 Berezi Amubieta - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import models, fields, _
+from odoo import _, api, fields, models
 
 
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
+    @api.depends("quant_package_ids", "quant_package_ids.shipping_weight")
     def _compute_packages_weight(self):
         for picking in self:
-            picking.packages_weight = sum(picking.quant_package_ids.mapped('shipping_weight'))
+            picking.packages_weight = (
+                sum(picking.quant_package_ids.mapped('shipping_weight')))
 
+    @api.depends("quant_package_ids", "quant_package_ids.volume")
     def _compute_packages_volume(self):
         for picking in self:
-            picking.packages_volume = sum(picking.quant_package_ids.mapped('volume'))
+            picking.packages_volume = (
+                sum(picking.quant_package_ids.mapped("volume")))
 
+    @api.depends("quant_package_ids")
     def _compute_packages_qty(self):
         for picking in self:
             picking.packages_qty = len(picking.quant_package_ids)
 
     def _compute_volume_uom_name(self):
         for package in self:
-            package.volume_uom_name = self.env['product.template']._get_volume_uom_name_from_ir_config_parameter()
+            if package.quant_package_ids:
+                package.volume_uom_name = (
+                    package.quant_package_ids[0].volume_uom_name)
 
+    @api.depends("packages_qty", "packages_weight", "weight_uom_name")
     def _compute_packages_qty_weight(self):
         for picking in self:
             picking.packages_qty_weight = u'{} {} {} {}'.format(
@@ -30,17 +38,30 @@ class StockPicking(models.Model):
                 picking.weight_uom_name)
 
     quant_package_ids = fields.One2many(
-        string='Packages', comodel_name='stock.quant.package',
+        string='Packages',
+        comodel_name='stock.quant.package',
         inverse_name='picking_id')
     packages_qty_weight = fields.Char(
-        string='# Packages', compute='_compute_packages_qty_weight')
+        string='# Packages',
+        compute='_compute_packages_qty_weight',
+        store=True)
     qty_packages = fields.Integer(string='Number of Packages')
     packages_weight = fields.Float(
-        string='Packages Weight', compute='_compute_packages_weight')
+        string='Packages Weight',
+        compute='_compute_packages_weight',
+        store=True)
     packages_volume = fields.Float(
-        string='Packages Volume', compute='_compute_packages_volume')
-    volume_uom_name = fields.Char(string='Volume UOM', compute='_compute_volume_uom_name')
-    packages_qty = fields.Integer(string='Packages Quantity', compute='_compute_packages_qty')
+        string='Packages Volume',
+        compute='_compute_packages_volume',
+        store=True)
+    volume_uom_name = fields.Char(
+        string='Volume UOM',
+        compute='_compute_volume_uom_name',
+        store=True)
+    packages_qty = fields.Integer(
+        string='Packages Quantity',
+        compute='_compute_packages_qty',
+        store=True)
 
     def action_view_package(self):
         context = self.env.context.copy()
