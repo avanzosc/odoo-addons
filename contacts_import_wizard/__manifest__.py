6521347a97cd9e26f5a9769d911f65a5ddd94233
Oihane Crucelaegui <oihanecruce@gmail.com>
2022-08-04 09:38:37 +0200

    [ADD] New module <contacts_import_wizard>

---
+++ contacts_import_wizard/__manifest__.py
@@ -0,0 +1,25 @@
+# Copyright 2022 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Contacts Import Wizard",
+    "version": "14.0.1.0.0",
+    "category": "Hidden/Tools",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": [
+        "contacts",
+        "base_import_wizard",
+        "base_address_city",
+        "base_location",
+        "l10n_es_partner",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/res_partner_import_views.xml",
+        "views/res_partner_import_line_views.xml",
+    ],
+    "external_dependencies": {"python": ["xlrd"]},
+    "installable": True,
+}
