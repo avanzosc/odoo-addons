eaeabcae38c4a6f2baa7ecf22aa081dfc8571e46
Tu Nombre <you@example.com>
2022-08-05 13:19:32 +0200

    [14.0][IMP] custom_saca:

--- custom_saca/models/saca_line.py
+++ custom_saca/models/saca_line.py
@@ -1,16 +1,18 @@
 # Copyright 2022 Berezi Amubieta - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import api, fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class SacaLine(models.Model):
     _name = "saca.line"
     _description = "Saca Line"
     _order = "sequence"
+    _inherit = ["mail.thread", "mail.activity.mixin"]
 
     def _get_default_weight_uom(self):
         try:
-            return self.env.ref("uom.product_uom_gram").id
+            return self.env.ref("uom.product_uom_kgm").id
         except Exception:
             return False
 
@@ -61,24 +63,42 @@ class SacaLine(models.Model):
     existence = fields.Integer(string="Existence")
     age = fields.Integer(string="Age")
     vehicle_id = fields.Many2one(
-        string="Vehicle", comodel_name="fleet.vehicle")
+        string="Vehicle", comodel_name="fleet.vehicle", copy=False)
     remolque_id = fields.Many2one(
-        string="Remolque", comodel_name="fleet.vehicle")
+        string="Remolque", comodel_name="fleet.vehicle", copy=False)
     ates = fields.Char(
-        string="Ates", related="remolque_id.ates", store=True)
+        string="Ates", related="remolque_id.ates", store=True ,copy=False)
     cages_num = fields.Integer(
         string="Number of Cages", related="remolque_id.cages_num", store=True)
     driver_id = fields.Many2one(
         string="Driver",
         comodel_name="res.partner",
+        copy=False,
         domain="['|', ('category_id', '=', 'Driver'), ('category_id', '=', 'Conductor')]")
-    unit_burden = fields.Float(string="Unit Burden")
-    estimate_weight = fields.Float(string="Estimate Weight")
+    unit_burden = fields.Float(
+        string="Units per Cages",
+        digits="Weight Decimal Precision",
+        copy=False)
+    estimate_weight = fields.Float(
+        string="Chicken Average Weight",
+        digits="Weight Decimal Precision",
+        copy=False)
     box_weight = fields.Float(
-        string="Box weight", compute="_compute_box_weight")
+        string="Box weight",
+        compute="_compute_box_weight",
+        digits="Weight Decimal Precision",
+        store=True)
     max_weight = fields.Float(
-        string="Max Burden", compute="_compute_max_weight")
-    coya_id = fields.Many2one(string="Coya", comodel_name="coya")
+        string="Chicken Max Burden",
+        help="Chicken unit",
+        digits="Weight Decimal Precision",
+        compute="_compute_max_weight",
+        store=True)
+    estimate_burden = fields.Integer(
+        string="Chicken Estimate Burden",
+        help = "Chicken unit",
+        copy=False)
+    coya_id = fields.Many2one(string="Coya", comodel_name="coya", copy=False)
     burden_type = fields.Selection(
         string="Burden Type", selection=[
             ("two_farms", "Two farms"),
@@ -86,22 +106,25 @@ class SacaLine(models.Model):
             ("triplet", "Triplet"),
             ("outside", "Outside"),
             ("quadruple", "Quadruple"),
-            ("quintulete", "Quintuplete")])
-    cleaned_date = fields.Date(string="Remolque Disinfection Date")
-    saca_time = fields.Float(string="Saca Time")
-    cleaned_time = fields.Float(string="Cleaned Time")
-    fasting = fields.Float(string="Fasting")
-    cleaning_seal_number = fields.Char(string="Cleaning Seal Number")
+            ("quintulete", "Quintuplete")],
+        copy=False)
+    cleaned_date = fields.Date(string="Remolque Disinfection Date", copy=False)
+    saca_time = fields.Float(string="Saca Time", copy=False)
+    cleaned_time = fields.Float(string="Cleaned Time", copy=False)
+    fasting = fields.Float(string="Fasting", copy=False)
+    cleaning_seal_number = fields.Char(
+        string="Cleaning Seal Number", copy=False)
     main_scale = fields.Many2one(
-        string="Main Scale", comodel_name="main.scale")
-    note = fields.Text(string="Note")
+        string="Scale", comodel_name="main.scale", copy=False)
+    note = fields.Text(string="Note", copy=False)
     weight_uom_id = fields.Many2one(
         string="Weight UOM",
         comodel_name="uom.uom",
         default=_get_default_weight_uom,
         domain=lambda self: [
             ("category_id", "=",
-             self.env.ref("uom.product_uom_categ_kgm").id)])
+             self.env.ref("uom.product_uom_categ_kgm").id)],
+        copy=False)
     currency_id = fields.Many2one(
         string="Currency",
         comodel_name="res.currency",
@@ -115,7 +138,16 @@ class SacaLine(models.Model):
     disinfectant_id = fields.Many2one(
         string="Disinfectant",
         comodel_name="product.product",
-        default=lambda self: self.env.company.disinfectant_id.id)
+        default=lambda self: self.env.company.disinfectant_id.id, copy=False)
+    date = fields.Date(
+        string="Date",
+        related="saca_id.date",
+        store=True)
+    quality_responsible_id = fields.Many2one(
+        string="Quality Responsible",
+        comodel_name="res.partner",
+        copy=False,
+        domain="[('category_id', '=', 'Quality Responsible')]")
 
     @api.depends("unit_burden", "estimate_weight")
     def _compute_box_weight(self):
@@ -127,12 +159,31 @@ class SacaLine(models.Model):
         for line in self:
             line.max_weight = line.unit_burden * line.cages_num
 
+    @api.onchange("seq")
+    def onchange_seq(self):
+        if self.seq:
+            line = self.env["saca.line"].search([("lot", "=", self.lot), ("seq", "=", self.seq)], limit=1)
+            if line:
+                self.write({
+                    "external_supplier": line.external_supplier,
+                    "supplier_id": line.supplier_id.id,
+                    "farmer_id": line.farmer_id.id,
+                    "farm_id": line.farm_id.id,
+                    "breeding_id": line.breeding_id.id,
+                    "existence": line.existence,
+                    "age": line.age})
+
     @api.onchange("supplier_id", "saca_id")
     def onchange_name(self):
         if self.supplier_id:
             today = fields.Date.today()
             self.name = u"{}, {}".format(
                 today, self.supplier_id.name)
+
+    @api.onchange("farmer_id", "breeding_id")
+    def onchange_farmer(self):
+        if self.farm_id and self.farmer_id.main_scale:
+            self.main_scale = self.farmer_id.main_scale
 
     @api.onchange("breeding_id", "external_supplier")
     def onchange_breeding_id(self):
@@ -152,3 +203,14 @@ class SacaLine(models.Model):
             cond = [("driver_id", "=", self.driver_id.id)]
             self.vehicle_id = self.env["fleet.vehicle"].search(
                 cond, limit=1).id
+
+    @api.constrains("lot", "seq", "farmer_id", "farm_id")
+    def _check_lots(self):
+        for line in self:
+            same_lot = self.env["saca.line"].search(
+                [("lot", "=", line.lot), ("seq", "=", line.seq)])
+            if same_lot and same_lot.farm_id and same_lot.farmer_id and (
+                same_lot.farmer_id != line.farmer_id or (
+                    same_lot.farm_id != line.farm_id)):
+                raise ValidationError(
+                        _("It can't be that farm or farmer for that lot."))
