831981bea29e8b468146a338313324394eba9b50
Thor <alfredodelafuente@avanzosc.es>
2022-08-22 13:46:17 +0200

    [ADD] bbc_custom: bbc custom.

---
+++ bbc_custom/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2022 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "BBC Custom",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "account",
+    ],
+    "author": "AvanzOSC",
+    "website": "https://www.avanzosc.es",
+    "category": "Custom",
+    "data": [
+        "views/account_move_views.xml",
+    ],
+    "installable": True,
+}
