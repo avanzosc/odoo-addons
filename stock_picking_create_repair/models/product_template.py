4c0cc49f8d1bebaf95b04c69bc79e264056cd7ce
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2022-08-29 12:06:09 +0200

    [IMP] stock_picking_create_repair: En informe de pedidos de venta, y en informe de facturas, mostrar RMAs. (#1690)

--- stock_picking_create_repair/models/product_template.py
+++ stock_picking_create_repair/models/product_template.py
@@ -8,6 +8,9 @@ class ProductTemplate(models.Model):
 
     is_repair = fields.Boolean(
         string="Is repair", default=False, copy=False)
+    default_product_manufacturing_operations = fields.Boolean(
+        string="Default product for manufacturing operations", default=False,
+        copy=False)
 
     @api.onchange('type')
     def _onchange_type(self):
@@ -15,4 +18,23 @@ class ProductTemplate(models.Model):
         for template in self:
             if template.type != "service":
                 template.is_repair = False
+        return result
+
+    @api.model
+    def create(self, vals):
+        product = super(ProductTemplate, self).create(vals)
+        return product
+
+    def write(self, vals):
+        result = super(ProductTemplate, self).write(vals)
+        if ("no_update_product" not in self.env.context and
+                "default_product_manufacturing_operations" in vals):
+            for template in self:
+                if template.product_variant_count == 1:
+                    variant = template.product_variant_ids[0]
+                    variant_vals = {
+                        "default_product_manufacturing_operations":
+                        template.default_product_manufacturing_operations}
+                    variant.with_context(
+                        no_update_template=True).write(variant_vals)
         return result
