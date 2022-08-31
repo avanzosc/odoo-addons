020c02a626563fe72b1011e41d981dd5bed5a868
Thor <alfredodelafuente@avanzosc.es>
2022-08-31 16:16:07 +0200

    [IMP] product_final: Hacer obligatorios campos de 'product.location.exploded'.

--- product_final/models/product_location_exploded.py
+++ product_final/models/product_location_exploded.py
@@ -8,8 +8,8 @@ class ProductLocationExploded(models.Model):
     _order="product_id, product_final_id, position asc"
     _rec_name = 'product_final_id'
 
-    product_final_id = fields.Many2one(comodel_name="product.final",
-                                       string="Final Product")
+    product_final_id = fields.Many2one(
+        comodel_name="product.final", string="Final Product", required=True)
     position = fields.Char(string="Position")
-    product_id = fields.Many2one(comodel_name="product.product",
-                                 string="Product")
+    product_id = fields.Many2one(
+        comodel_name="product.product", string="Product", required=True)
