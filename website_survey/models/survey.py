7e1e0db7557baa6acc530f4d23b86685a1b2ad76
leiremartinezdesantos <72156354+leiremartinezdesantos@users.noreply.github.com>
2022-09-01 14:22:49 +0200

    [14.0][ADD] website_survey: survey portal (#1677)
    * [ADD] website_survey: survey portal
    
    * [IMP] website_survey: change layout
    
    * [IMP] website_survey: add print certification on web
    
    * [IMP] website_survey: fix button url

---
+++ website_survey/models/survey.py
@@ -0,0 +1,29 @@
+
+from odoo import api, fields, models
+import werkzeug
+
+
+class SurveyUserInput(models.Model):
+    _inherit = 'survey.user_input'
+
+    def get_portal_url(self, suffix=None, report_type=None, download=None, query_string=None, anchor=None):
+        self.ensure_one()
+        url = '/my/survey/'+str(self.id)
+        return url
+
+    def action_print_certification(self, suffix=None, report_type=None, download=None, query_string=None, anchor=None):
+        """ Open the website page with the survey form """
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_url',
+            'name': "View Answers",
+            'target': 'self',
+            'url': '/survey/certification/print/%s?answer_token=%s' % (
+                self.survey_id.access_token, self.access_token)
+        }
+
+    def _get_report_base_filename(self):
+        self.ensure_one()
+        return '%s - %s'% (
+            self.survey_id.display_name,
+            self.student_id.display_name if self.student_id else '')
