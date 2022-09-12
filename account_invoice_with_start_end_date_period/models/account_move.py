aa6af98b1a7255415792d64f0c1687b81a5e8018
Oihane Crucelaegui <oihanecruce@gmail.com>
2022-09-12 09:56:24 +0200

    [FIX] account_invoice_with_start_end_date_period: TypeError: _create_invoices() got an unexpected keyword argument 'start_date'

--- account_invoice_with_start_end_date_period/models/account_move.py
+++ account_invoice_with_start_end_date_period/models/account_move.py
@@ -1,10 +1,10 @@
 # Copyright 2021 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import models, fields
+from odoo import fields, models
 
 
 class AccountMove(models.Model):
-    _inherit = 'account.move'
+    _inherit = "account.move"
 
-    start_date_period = fields.Date(string='Start date period')
-    end_date_period = fields.Date(string='End date period')
+    start_date_period = fields.Date(string="Start date period")
+    end_date_period = fields.Date(string="End date period")
