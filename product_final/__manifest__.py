b7cf62bfcde3a43efa6f5d975f5d94c4123d6055
Thor <alfredodelafuente@avanzosc.es>
2022-09-12 14:10:40 +0200

    [IMP] product_final: Todos los campos obligatorios para modelo "product.final". y "@api.constrains" para que su código sea único.

--- product_final/__manifest__.py
+++ product_final/__manifest__.py
@@ -6,7 +6,7 @@
     "website": "https://github.com/avanzosc/mrp-addons",
     "category": "Sales",
     "license": "AGPL-3",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.1.0",
     "depends": ["stock",
                 "product",
                 ],
