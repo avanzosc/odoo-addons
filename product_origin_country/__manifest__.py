a1eeaea214ec8b79acf065a7c6d1c403aecfe2e1
Thor <alfredodelafuente@avanzosc.es>
2022-09-14 15:50:55 +0200

    [ADD] product_origin_country: In products new field "Origin" against the table of countries.

---
+++ product_origin_country/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Product Origin Country",
+    "version": "12.0.1.0.0",
+    "author": "Avanzosc",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "license": "AGPL-3",
+    "category": "Sales",
+    "depends": [
+        "product",
+        "stock"
+    ],
+    "data": [
+        "views/product_product_views.xml",
+    ],
+    'installable': True,
+}
