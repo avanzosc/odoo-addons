18dc1d4535d306429b769b680ba2ccd5e4d5646e
Thor <alfredodelafuente@avanzosc.es>
2022-09-15 12:20:40 +0200

    [IMP] account_move_time_type: Change 0.85 by 1 in limit_hour.

--- account_move_time_type/__manifest__.py
+++ account_move_time_type/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Account Move Time Type",
-    'version': '14.0.1.0.0',
+    'version': '14.0.1.1.0',
     "author": "Avanzosc",
     "website": "https://www.avanzosc.es",
     "category": "Sales/CRM",
