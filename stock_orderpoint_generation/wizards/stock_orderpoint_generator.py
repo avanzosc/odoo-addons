9b4b291682f5939fa0d8b59f378747ac405f5eec
Oihane Crucelaegui <oihanecruce@gmail.com>
2022-09-15 15:10:36 +0200

    [IMP] stock_orderpoint_generation: multiple destinations

--- stock_orderpoint_generation/wizards/stock_orderpoint_generator.py
+++ stock_orderpoint_generation/wizards/stock_orderpoint_generator.py
@@ -5,60 +5,74 @@ from odoo import fields, models
 
 
 class StockOrderpointGenerator(models.TransientModel):
-    _name = 'stock.orderpoint.generator'
+    _name = "stock.orderpoint.generator"
     _description = "Wizard to generate stock.warehouse.orderpoints"
 
     generation_type = fields.Selection(
-        [('from_location', 'Generate from Location'),
-         ('all_0', 'Generate 0 Min/Max')],
-        string="Generate Orderpoints")
+        [("from_location", "Generate from Location"), ("all_0", "Generate 0 Min/Max")],
+        string="Generate Orderpoints",
+    )
     location_from = fields.Many2one(
-        comodel_name="stock.location", string="From Location",
-        domain="[('usage','=', 'internal')]")
-    location_to = fields.Many2one(
-        comodel_name="stock.location", string="To Location",
-        domain="[('usage','=', 'internal')]")
+        comodel_name="stock.location",
+        string="From Location",
+        domain="[('usage','=', 'internal')]",
+    )
+    location_to = fields.Many2many(
+        comodel_name="stock.location",
+        string="To Location",
+        domain="[('usage','=', 'internal')]",
+    )
     update = fields.Boolean(
         string="Update",
-        help="Check this if you want just update existing orderpoint rules.")
+        help="Check this if you want just update existing orderpoint rules.",
+    )
 
     def _update_orderpoint_rule(self, rule, location):
-        """ Create/Update location_id with the rule_id provided"""
-        orderpoin_obj = self.env['stock.warehouse.orderpoint']
+        """Create/Update location_id with the rule_id provided"""
+        orderpoin_obj = self.env["stock.warehouse.orderpoint"]
         rule2update = orderpoin_obj.search(
-            [('location_id', '=', location.id),
-             ('product_id', '=', rule.product_id.id)])
+            [("location_id", "=", location.id), ("product_id", "=", rule.product_id.id)]
+        )
         if rule2update:
             rule2update.product_min_qty = rule.product_min_qty
             rule2update.product_max_qty = rule.product_max_qty
             rule2update.qty_multiple = rule.qty_multiple
         else:
-            rule.copy({'warehouse_id': location.get_warehouse().id,
-                       'location_id': location.id})
+            rule.copy(
+                {
+                    "warehouse_id": location.get_warehouse().id,
+                    "location_id": location.id,
+                }
+            )
 
     def button_generate(self):
-        orderpoin_obj = self.env['stock.warehouse.orderpoint']
-        product_obj = self.env['product.product']
-        if self.generation_type == 'from_location':
+        orderpoin_obj = self.env["stock.warehouse.orderpoint"]
+        product_obj = self.env["product.product"]
+        if self.generation_type == "from_location":
             orderpoint_rules = orderpoin_obj.search(
-                [('location_id', '=', self.location_from.id)])
+                [("location_id", "=", self.location_from.id)]
+            )
             for rule in orderpoint_rules:
-                self._update_orderpoint_rule(rule, self.location_to)
-        elif self.generation_type == 'all_0':
-            products = product_obj.search([('type', '=', 'product')])
-            for product in products:
-                rule_exist = orderpoin_obj.search(
-                    [('location_id', '=', self.location_to.id),
-                     ('product_id', '=', product.id)])
-                if rule_exist:
-                    continue
-                rule_data = {
-                    'warehouse_id': self.location_to.get_warehouse().id,
-                    'location_id': self.location_to.id,
-                    'product_id': product.id,
+                for location in self.location_to:
+                    self._update_orderpoint_rule(rule, location)
+        elif self.generation_type == "all_0":
+            products = product_obj.search([("type", "=", "product")])
+            for location in self.location_to:
+                for product in products:
+                    rule_exist = orderpoin_obj.search(
+                        [
+                            ("location_id", "=", location.id),
+                            ("product_id", "=", product.id),
+                        ]
+                    )
+                    if rule_exist:
+                        continue
+                    rule_data = {
+                        "warehouse_id": self.location_to.get_warehouse().id,
+                        "location_id": location.id,
+                        "product_id": product.id,
                     }
-                print('Producto: {}'.format(product.id))
-                try:
-                    orderpoin_obj.create(rule_data)
-                except Exception:
-                    continue
+                    try:
+                        orderpoin_obj.create(rule_data)
+                    except Exception:
+                        continue
