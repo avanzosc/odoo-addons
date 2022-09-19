4c419b40a945f53a58bfa41927370e839780eb9c
Oihane Crucelaegui <oihanecruce@gmail.com>
2022-09-19 10:02:47 +0200

    [IMP] account_move_time_type: add date paremeter

--- account_move_time_type/wizard/sale_advance_payment_inv.py
+++ account_move_time_type/wizard/sale_advance_payment_inv.py
@@ -8,9 +8,10 @@ class SaleAdvancePaymentInv(models.TransientModel):
 
     def _create_invoice(self, order, so_line, amount):
         invoice = super(SaleAdvancePaymentInv, self)._create_invoice(
-            order, so_line, amount)
+            order, so_line, amount
+        )
         for line in invoice.invoice_line_ids.filtered(
-                lambda x: x.quantity != x.quantity2):
-            invoice.invoice_line_ids = [
-                (1, line.id, {'quantity': line.quantity2})]
+            lambda x: x.quantity != x.quantity2
+        ):
+            invoice.invoice_line_ids = [(1, line.id, {"quantity": line.quantity2})]
         return invoice
