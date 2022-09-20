4253c54a9a8558cbaafeddf975b16c0b26b41562
Tu Nombre <you@example.com>
2022-09-20 12:00:27 +0200

    [14.0][ADD] contact_extra_phone: New object to insert the extra phones of the contacts.

---
+++ contact_extra_phone/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Contact Extra Phones",
+    'version': '14.0.1.0.0',
+    "category": "Contact",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": [
+        "contacts",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/res_partner_views.xml",
+    ],
+    'installable': True,
+}
---
+++ contact_extra_phone/models/res_partner.py
@@ -0,0 +1,12 @@
+# Copyright 2021 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import models, fields
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    extra_phone_ids = fields.One2many(
+        string="Extra Phones",
+        comodel_name="extra.phone",
+        inverse_name="partner_id")
