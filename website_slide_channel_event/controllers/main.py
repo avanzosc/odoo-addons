b8943af23c79c0a8b18d29c2ed0d035e52f5dfaa
Avanzosc <leiremartinez@avanzosc.es>
2022-09-27 12:59:19 +0200

    [IMP] website_slide_channel_event: do not show past slide.channels

--- website_slide_channel_event/controllers/main.py
+++ website_slide_channel_event/controllers/main.py
@@ -2,6 +2,7 @@
 from odoo.http import request
 from odoo import http
 from odoo.addons.website_slides.controllers.main import WebsiteSlides
+from datetime import datetime
 
 
 class WebsiteSlides(WebsiteSlides):
@@ -11,13 +12,16 @@ class WebsiteSlides(WebsiteSlides):
         res = super(WebsiteSlides, self).slides_channel_all(slide_type, my, **post)
         user = request.env['res.users'].sudo().browse(request.uid)
         partner = user.partner_id
+        today = datetime.today()
         registrations = request.env['event.registration'].sudo().search([
             '|',
             ('partner_id', '=', partner.id),
             ('student_id', '=', partner.id)
         ])
         events = request.env['event.event'].sudo().search([
-            ('registration_ids', 'in', registrations.ids)
+            ('registration_ids', 'in', registrations.ids),
+            ('date_begin', '<=', today),
+            ('date_end', '>', today)
         ])
         res.qcontext.update({'events': events})
         return res
