06216c62e8cf5675409540210135895bea7fb695
Thor <alfredodelafuente@avanzosc.es>
2022-09-28 15:28:21 +0200

    [ADD] product_supplierinfo_usability: New fields in "product.supplierinfo" by supplier.

---
+++ product_supplierinfo_usability/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2022 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Product Supplierinfo Usability",
+    'version': '14.0.1.0.0',
+    "category": "Sales/Sales",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": [
+        "product",
+        "purchase",
+        "purchase_discount",
+        "product_usability"
+    ],
+    "data": [
+        "views/product_supplierinfo_views.xml",
+    ],
+    'installable': True,
+}
