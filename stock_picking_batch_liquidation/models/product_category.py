eb78f03b3e3a5f5acfc6b9b05ada1ade14cef247
Tu Nombre <you@example.com>
2022-09-29 09:40:39 +0200

    [14.0][IMP] stock_picking_batch_liquidation

---
+++ stock_picking_batch_liquidation/models/product_category.py
@@ -0,0 +1,11 @@
+# Copyright 2022 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import fields, models
+
+
+class ProductCategory(models.Model):
+    _inherit = "product.category"
+
+    move_type_id = fields.Many2one(
+        string="Move Type",
+        comodel_name="move.type")
