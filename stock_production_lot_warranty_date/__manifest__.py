a973dda24117f4c84ea0f914baf9ec3398484114
Thor <alfredodelafuente@avanzosc.es>
2022-10-13 07:53:02 +0200

    [ADD] stock_production_lot_warranty_date: When validating an out picking: update the guarantee date of all lots of the out picking.

---
+++ stock_production_lot_warranty_date/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Stock Production Lot Warranty Date",
+    'version': '14.0.1.0.0',
+    "category": "Inventory/Inventory",
+    "license": "AGPL-3",
+    "author": "Avanzosc",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": [
+        "stock",
+        "product_expiry"
+    ],
+    "data": [
+        "views/stock_production_lot_views.xml",
+    ],
+    "installable": True,
+}
