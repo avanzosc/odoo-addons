b12b4493c60f2aba26c5d3bb191a0befed3209c1
Thor <alfredodelafuente@avanzosc.es>
2022-10-14 09:05:45 +0200

    [ADD] purchase_last_price_info_ext: When modifying the unit price of a purchase line that is not in draft status, update the information of the last purchase of the product.

---
+++ purchase_last_price_info_ext/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Purchase Last Price Info Ext",
+    "version": "14.0.1.0.0",
+    "category": "Purchase Management",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": [
+        "purchase_last_price_info",
+    ],
+    "excludes": [],
+    "data": [
+    ],
+    "installable": True,
+    "auto_install": False,
+}
