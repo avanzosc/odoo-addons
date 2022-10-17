7b484aa78fc12c288baeb9334ee3d7b29edc741c
Tu Nombre <you@example.com>
2022-10-17 15:39:18 +0200

    [14.0][IMP] stock_picking_emptying_expired

--- stock_picking_emptying_expired/models/stock_picking.py
+++ stock_picking_emptying_expired/models/stock_picking.py
@@ -2,6 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from odoo import _, fields, models
 from odoo.exceptions import ValidationError
+from datetime import timedelta, datetime
 
 
 class StockPicking(models.Model):
@@ -19,37 +20,31 @@ class StockPicking(models.Model):
         default=_default_get_custom_date_done)
 
     def action_emptying_expired(self):
+        expiration_products = self.env["product.product"].search(
+            [("use_expiration_date", "=", True)])
         for picking in self:
-            in_ml = self.env["stock.move.line"].search([
-                ("location_dest_id", "=", picking.location_id.id),
-                ("state", "=", "done")])
-            out_ml = self.env["stock.move.line"].search([
-                ("location_id", "=", picking.location_id.id),
-                ("state", "=", "done")])
             if not picking.custom_date_done:
                 raise ValidationError(_("You must enter the date done."))
-            products = []
-            for line in in_ml:
-                if line.product_id not in products:
-                    products.append(line.product_id)
-                    expire_lines = in_ml.filtered(
-                        lambda c: c.product_id == line.product_id and (
-                            c.expiration_date) and (
-                                c.expiration_date.date()) <= (
-                                    picking.custom_date_done.date()))
-                    in_qty = sum(expire_lines.mapped("qty_done"))
-                    out_qty = sum(out_ml.filtered(
-                        lambda c: c.product_id == line.product_id).mapped(
-                            "qty_done"))
-                    dif = in_qty - out_qty
-                    if dif > 0 and not (
-                        picking.move_ids_without_package.filtered(
-                            lambda c: (c.product_id == line.product_id))):
-                        self.env["stock.move.line"].create(
-                            {"product_id": line.product_id.id,
-                             "location_id": picking.location_id.id,
-                             "location_dest_id": picking.location_dest_id.id,
-                             "product_uom_id": line.product_id.uom_id.id,
-                             "qty_done": dif,
-                             "picking_id": picking.id})
+            for product in expiration_products:
+                time = product.expiration_time
+                date = picking.custom_date_done - timedelta(days=time)
+                date_stock = product.with_context(
+                    to_date=date,
+                    location=picking.location_id.id).qty_available
+                out_ml = self.env["stock.move.line"].search([
+                    ("product_id", "=", product.id),
+                    ("location_id", "=", picking.location_id.id),
+                    ("state", "=", "done"),
+                    ("date", ">", date),
+                    ("date", "<=", picking.custom_date_done)])
+                out_qty = sum(out_ml.mapped("qty_done"))
+                dif = date_stock - out_qty
+                if dif > 0:
+                    self.env["stock.move.line"].create(
+                        {"product_id": product.id,
+                         "location_id": picking.location_id.id,
+                         "location_dest_id": picking.location_dest_id.id,
+                         "product_uom_id": product.uom_id.id,
+                         "qty_done": dif,
+                         "picking_id": picking.id})
             picking.action_confirm()
