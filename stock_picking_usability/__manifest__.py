28a80df202bc46bffefb91ec6360460b559c75d7
Tu Nombre <you@example.com>
2022-10-18 16:51:37 +0200

    [14.0][ADD] stock_picking_usability

---
+++ stock_picking_usability/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2022 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Stock Picking Usability",
+    'version': '14.0.1.0.0',
+    "author": "Avanzosc",
+    "category": "Inventory",
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        "views/stock_picking_views.xml",
+    ],
+    "license": "AGPL-3",
+    'installable': True,
+}
