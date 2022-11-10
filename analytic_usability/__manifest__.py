e7359560a8f1c5a78600eeed48e81606d3237564
Oihane Crucelaegui <oihanecruce@gmail.com>
2022-11-10 16:13:01 +0100

    [MIG] analytic_usability: Migration to 16.0

--- analytic_usability/__manifest__.py
+++ analytic_usability/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Analytic Usability",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "AvanzOSC",
     "website": "https://github.com/avanzosc/odoo-addons",
