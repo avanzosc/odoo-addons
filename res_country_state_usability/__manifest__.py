be06d92cb1148509114b77755dd09d522253a4dc
Oihane Crucelaegui <oihanecruce@gmail.com>
2022-11-14 10:48:41 +0100

    [MIG] res_country_state_usability: Migration to 16.0

--- res_country_state_usability/__manifest__.py
+++ res_country_state_usability/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Countries and States Usability",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Hidden",
     "license": "AGPL-3",
     "author": "AvanzOSC",
