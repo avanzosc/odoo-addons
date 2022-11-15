08774f9f97981860691293af8e5c2996bf4c9b49
Tu Nombre <you@example.com>
2022-11-15 09:26:51 +0100

    [14.0][IMP] stock_picking_date_done:

--- stock_picking_date_done/models/stock_picking.py
+++ stock_picking_date_done/models/stock_picking.py
@@ -15,3 +15,13 @@ class StockPicking(models.Model):
             raise ValidationError(
                 _("You must introduce the done date."))
         return result
+
+    def write(self, vals):
+        result = super(StockPicking, self).write(vals)
+        if "custom_date_done" in vals:
+            for line in self:
+                for move in line.move_ids_without_package:
+                    move.date = line.custom_date_done
+                for move_line in line.move_line_ids_without_package:
+                    move_line.date = line.custom_date_done
+        return result
