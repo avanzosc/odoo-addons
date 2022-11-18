6a34f7f43723a0c54960e23f37e6ef809c7d0795
Oihane Crucelaegui <oihanecruce@gmail.com>
2022-11-18 09:50:20 +0100

    [IMP] contract_line_menu: there exists already access to lines

--- contract_line_menu/__manifest__.py
+++ contract_line_menu/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Contract Line Menu",
-    'version': '14.0.1.0.0',
+    "version": "14.0.1.0.0",
     "author": "Avanzosc",
     "category": "Sales/CRM",
     "depends": [
@@ -12,5 +12,5 @@
         "views/contract_line_views.xml",
     ],
     "license": "AGPL-3",
-    'installable': True,
+    "installable": True,
 }
--- contract_line_menu/models/contract_line.py
+++ contract_line_menu/models/contract_line.py
@@ -1,20 +1,20 @@
 # Copyright 2021 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ContractLine(models.Model):
-    _inherit = 'contract.line'
-    _order = "contract_type, partner_id, sequence,id"
+    _inherit = "contract.line"
 
-    contract_type = fields.Selection(
-        index=True, related='contract_id.contract_type', store=True)
-    partner_id = fields.Many2one(
-        string='Partner', comodel_name='res.partner',
-        related='contract_id.partner_id', store=True)
     journal_id = fields.Many2one(
-        string="Journal", comodel_name="account.journal",
-        related='contract_id.journal_id', store=True)
+        string="Journal",
+        comodel_name="account.journal",
+        related="contract_id.journal_id",
+        store=True,
+    )
     pricelist_id = fields.Many2one(
-        string="Pricelist", comodel_name="product.pricelist",
-        related='contract_id.pricelist_id', store=True)
+        string="Pricelist",
+        comodel_name="product.pricelist",
+        related="contract_id.pricelist_id",
+        store=True,
+    )
