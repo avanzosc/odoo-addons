bad4fba05d564e5e84663f6fbeb7fe3bf002b886
Tu Nombre <you@example.com>
2022-12-01 15:18:50 +0100

    [14.0][ADD] product_multi_company_usability:

---
+++ product_multi_company_usability/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Product Multi Company Usability",
+    'version': '14.0.1.0.0',
+    "category": "Inventory",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        "security/product_company_security.xml",
+        "views/product_template_views.xml",
+    ],
+    'installable': True,
+}
