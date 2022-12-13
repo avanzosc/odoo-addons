154a56dab9bfbf042e51b7e0b76415dc87186ad8
Thor <alfredodelafuente@avanzosc.es>
2022-12-13 14:00:37 +0100

    [ADD] stock_picking_batch_shortcut: In form view of object "Batch Transfer", two new shorcusts to: "Delivery" and "Sale Orders".

---
+++ stock_picking_batch_shortcut/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2022 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Stock Picking Batch Shorcut",
+    "version": "14.0.1.0.0",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "category": "Inventory",
+    "license": "AGPL-3",
+    "depends": [
+        "sale_stock",
+        "stock_picking_batch",
+    ],
+    "data": [
+        "views/stock_picking_batch_views.xml"
+    ],
+    "installable": True,
+    "auto_install": True,
+}
