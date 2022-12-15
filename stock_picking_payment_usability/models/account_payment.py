05566b2c1e834b195e9bc855dcf8c033d6b5bf3e
Tu Nombre <you@example.com>
2022-12-15 13:48:09 +0100

    [14.0][ADD] stock_picking_payment_usability: Shortcut from pickings to account payments.

---
+++ stock_picking_payment_usability/models/account_payment.py
@@ -0,0 +1,11 @@
+# Copyright 2022 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import fields, models
+
+
+class AccountPayment(models.Model):
+    _inherit = "account.payment"
+
+    picking_id = fields.Many2one(
+        string="Picking",
+        comodel_name="stock.picking")
