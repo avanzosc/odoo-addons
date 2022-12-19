b1dfe2845eed813be6a13b13a46456514d0b1af1
Tu Nombre <you@example.com>
2022-12-19 13:02:49 +0100

    [14.0][ADD] contact_supplierinfo_usability: In contacts a One2many field to product supplierinfo.

---
+++ contact_supplierinfo_usability/models/purchase_order.py
@@ -0,0 +1,16 @@
+# Copyright 2022 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import api, models
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = "purchase.order.line"
+
+    @api.onchange("order_id")
+    def onchange_move_id(self):
+        domain = self.product_id.search_contact_products(
+            "=", self.order_id.partner_id.id)
+        if domain and len(domain[0][2]) == 1:
+            product = self.env["product.product"].search([
+                ("product_tmpl_id", "=", domain[0][2][0])])
+            self.product_id = product.id
---
+++ contact_supplierinfo_usability/models/res_partner.py
@@ -0,0 +1,15 @@
+# Copyright 2022 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    product_ids = fields.One2many(
+        string="Products",
+        comodel_name="product.supplierinfo",
+        inverse_name="name")
+    limit_product = fields.Boolean(
+        string="Limit the products it provides",
+        default=False)
