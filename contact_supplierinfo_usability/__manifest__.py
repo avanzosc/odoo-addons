3f9c0112741b42840f6be24061b7efa8f7ee5137
Tu Nombre <you@example.com>
2022-12-19 13:02:49 +0100

    [14.0][ADD] contact_supplierinfo_usability: In contacts a One2many field to product supplierinfo.

---
+++ contact_supplierinfo_usability/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2022 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Contacts Supplierinfo Usability",
+    'version': '14.0.1.0.0',
+    "author": "Avanzosc",
+    "category": "Contacts",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "product",
+        "contacts",
+        "purchase",
+        "account",
+    ],
+    "data": [
+        "views/res_partner_view.xml",
+        "views/purchase_order_view.xml",
+        "views/account_move_view.xml",
+    ],
+    "license": "AGPL-3",
+    'installable': True,
+}
