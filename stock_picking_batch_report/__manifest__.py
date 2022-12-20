9b2c0a1bcac71bb6787a30e5e3211ca9bb1f8f01
Thor <alfredodelafuente@avanzosc.es>
2022-12-20 14:48:43 +0100

    [ADD] stock_picking_batch_report: In "Batch Transfer" new report "Grouped picking".

---
+++ stock_picking_batch_report/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Stock Picking Batch Report",
+    "version": "14.0.1.0.0",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "category": "Inventory/Inventory",
+    "license": "AGPL-3",
+    "depends": [
+        "sale",
+        "stock_picking_batch",
+    ],
+    "data": [
+        "report/stock_picking_batch_report.xml",
+    ],
+    "installable": True,
+}
