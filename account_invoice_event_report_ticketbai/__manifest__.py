c9271e4ccfedd76e3d0631a46bf08711bb775c59
Thor <alfredodelafuente@avanzosc.es>
2022-12-21 11:06:37 +0100

    [ADD] account_invoice_event_report_ticketbai: Printing of invoice lines with event information, and with ticketbai.

---
+++ account_invoice_event_report_ticketbai/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2022 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Account Invoice Event Report Ticketbai",
+    "version": "14.0.1.0.0",
+    "author": "AvanzOSC",
+    "website": "https://github.com/alfredoavanzosc/odoo-addons.git",
+    "category": "Custom",
+    "license": "AGPL-3",
+    "depends": [
+        "account_invoice_event_report",
+        "l10n_es_ticketbai",
+    ],
+    "data": [
+        "report/account_invoice_report.xml"
+    ],
+    "installable": True,
+    "auto_install": True,
+}
