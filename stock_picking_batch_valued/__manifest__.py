5aa39eed0943f7ef90229ac83596c01ae9207c2d
Thor <alfredodelafuente@avanzosc.es>
2022-12-28 09:46:22 +0100

    [ADD] stock_picking_batch_valued: In "Batch Transfer" new report "Grouped picking valued".

---
+++ stock_picking_batch_valued/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2022 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Stock Picking Batch Valued",
+    "version": "14.0.1.0.0",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "category": "Inventory/Inventory",
+    "license": "AGPL-3",
+    "depends": [
+        "sale",
+        "stock",
+        "stock_picking_batch",
+    ],
+    "data": [
+        "report/stock_picking_batch_valued_report.xml",
+        "views/stock_move_views.xml",
+        "views/stock_picking_views.xml",
+    ],
+    "installable": True,
+}
---
+++ stock_picking_batch_valued/models/stock_move_line.py
@@ -0,0 +1,11 @@
+# Copyright 2022 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import api, fields, models
+
+
+class StockMoveLine(models.Model):
+    _inherit = "stock.move.line"
+
+    sale_price_unit = fields.Float(
+        string="Sale Unit Price", digits="Product Price",
+        related="move_id.sale_price_unit", store=True, copy=False)
