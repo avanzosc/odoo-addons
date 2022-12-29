aeb2cee7b11197a2a7caacf2ca9275e25a918c0b
Tu Nombre <you@example.com>
2022-12-29 15:26:38 +0100

    [MIG] MODULE: Migration to 16.0

---
+++ lot_name_with_stock_qty/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2022 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Lot Name With Product Qty",
+    'version': '16.0.1.0.0',
+    "author": "Avanzosc",
+    "category": "Inventory",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "stock"
+    ],
+    "data": [
+    ],
+    "license": "AGPL-3",
+    'installable': True,
+}
