b86296015df7da6861a397e799cae8feebe19bba
Tu Nombre <you@example.com>
2022-12-29 15:26:38 +0100

    [MIG] MODULE: Migration to 16.0

---
+++ lot_name_with_stock_qty/models/stock_lot.py
@@ -0,0 +1,18 @@
+# Copyright 2022 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import models
+import math
+
+
+class StockLot(models.Model):
+    _inherit = "stock.lot"
+
+    def name_get(self):
+        result = []
+        for lot in self:
+            qty = round(lot.product_qty, 2)
+            qty = "{:,}".format(qty)
+            qty = qty.replace(',','~').replace('.',',').replace('~','.')
+            name = u'{} ({})'.format(lot.name, qty)
+            result.append((lot.id, name))
+        return result
