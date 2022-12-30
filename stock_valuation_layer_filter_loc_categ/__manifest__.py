73842c4f1c77f9fe709b81d69827824f62fe57c6
Thor <alfredodelafuente@avanzosc.es>
2022-12-30 10:46:09 +0100

    [ADD] stock_valuation_layer_filter_loc_categ: In Inventory - Reporting - Inventory Valuation, new filters: "Remove locations", and Remove categories.

---
+++ stock_valuation_layer_filter_loc_categ/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2022 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Stock Valuation Layer Filter Loc Categ",
+    'version': '14.0.1.0.0',
+    "author": "Avanzosc",
+    "category": "Inventory/Inventory",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "license": "AGPL-3",
+    "depends": [
+        "stock_account",
+        "report_stock_quantity_filter_loc_categ"
+    ],
+    "data": [
+        "views/stock_valuation_layer_views.xml",
+    ],
+    "installable": True,
+    "auto_install": True,
+    "post_init_hook": "load_data_into_stock_valuation_layer",
+}
