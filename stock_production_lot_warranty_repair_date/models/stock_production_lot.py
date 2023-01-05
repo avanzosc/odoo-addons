a3c614a90ee184a0f9e8f084184e5e240418c878
Oihane Crucelaegui <oihanecruce@gmail.com>
2023-01-05 10:15:24 +0100

    [FIX] stock_production_lot_warranty_repair_date: missing dependency

--- stock_production_lot_warranty_repair_date/models/stock_production_lot.py
+++ stock_production_lot_warranty_repair_date/models/stock_production_lot.py
@@ -6,13 +6,13 @@ from odoo import fields, models
 class StockProductionLot(models.Model):
     _inherit = "stock.production.lot"
 
-    warranty_repair_date = fields.Date(
-        string="warranty repair date")
+    warranty_repair_date = fields.Date(string="warranty repair date")
     repair_order_ids = fields.One2many(
-        string="Repair orders", comodel_name="repair.order",
-        inverse_name="lot_id")
+        string="Repair orders", comodel_name="repair.order", inverse_name="lot_id"
+    )
     count_repair_orders = fields.Integer(
-        string="Num. Repairs", compute="_compute_count_repair_orders")
+        string="Num. Repairs", compute="_compute_count_repair_orders"
+    )
 
     def _compute_count_repair_orders(self):
         for lot in self:
@@ -22,9 +22,10 @@ class StockProductionLot(models.Model):
         self.ensure_one()
         if self.count_repair_orders > 0:
             action = self.env["ir.actions.actions"]._for_xml_id(
-                "repair.action_repair_order_tree")
-            action['domain'] = [('id', 'in', self.repair_order_ids.ids)]
-            action['context'] = {
+                "repair.action_repair_order_tree"
+            )
+            action["domain"] = [("id", "in", self.repair_order_ids.ids)]
+            action["context"] = {
                 "default_lot_id": self.id,
                 "search_default_lot_id": self.id,
                 "default_company_id": (self.company_id or self.env.company).id,
