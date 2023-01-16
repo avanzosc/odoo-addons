df727c63624bf0128637fe73e83c150d5999798d
Tu Nombre <you@example.com>
2023-01-16 14:57:16 +0100

    [14.0][ADD] account_analytic_distribution: Returns an action that will open a form view (in a popup) allowing to work on all the analytic lines of a particular account move line.

---
+++ account_analytic_distribution/models/account_account.py
@@ -0,0 +1,12 @@
+# Copyright 2023 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import fields, models
+
+
+class AccountAccount(models.Model):
+    _inherit = "account.account"
+
+    analytic_template_ids = fields.One2many(
+        string="Analytic Distribution",
+        comodel_name="account.analytic.template",
+        inverse_name="account_id")
