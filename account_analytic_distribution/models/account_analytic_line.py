4718b2d3854d827cbf8c37c94d4bcbcdfd5e6bd8
Tu Nombre <you@example.com>
2023-01-16 14:57:16 +0100

    [14.0][ADD] account_analytic_distribution: Returns an action that will open a form view (in a popup) allowing to work on all the analytic lines of a particular account move line.

---
+++ account_analytic_distribution/models/account_analytic_line.py
@@ -0,0 +1,22 @@
+# Copyright 2023 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import api, fields, models
+
+
+class AccountAnalyticLine(models.Model):
+    _inherit = "account.analytic.line"
+
+    pre_amount = fields.Float(
+        string="Amount")
+
+    @api.onchange("pre_amount")
+    def onchange_pre_amount(self):
+        self.amount = abs(self.pre_amount)
+        if self.move_id.debit != 0:
+            self.amount = (-1) * abs(self.pre_amount)
+
+    @api.model
+    def create(self, values):
+        if "amount" in values:
+            values["pre_amount"] = values["amount"]
+        return super(AccountAnalyticLine, self).create(values)
