67416a42eb1ff373cf1167df7c40e0bf2220deda
Thor <alfredodelafuente@avanzosc.es>
2023-01-17 10:52:04 +0100

    [IMP] stock_lot_origin_global_gap: In products new field "Show Origin/Glogal Gap in documents".

--- stock_lot_origin_global_gap/models/stock_move_line.py
+++ stock_lot_origin_global_gap/models/stock_move_line.py
@@ -16,6 +16,47 @@ class StockMoveLine(models.Model):
     lot_global_gap = fields.Char(
         string="Lot Global Gap", related="lot_id.ref", copy=False,
         store=True)
+    lot_country_to_print = fields.Char(
+        string="Lot Origin to print",
+        compute="_compute_country_global_gap_to_print")
+    lot_global_gap_to_print = fields.Char(
+        string="Lot Global Gap to print",
+        compute="_compute_country_global_gap_to_print")
+
+    def _compute_country_global_gap_to_print(self):
+        for line in self:
+            lot_country_to_print = ""
+            lot_global_gap_to_print = ""
+            if not line.move_id.created_production_id:
+                if line.lot_country_id:
+                    lot_country_to_print = line.lot_country_id.name
+                if line.lot_global_gap:
+                    lot_global_gap_to_print = line.lot_global_gap
+            else:
+                move_lines = line.move_id.created_production_id.move_raw_ids
+                move_lines = move_lines.filtered(
+                    lambda x: x.state != "cancel" and
+                    x.product_id.show_origin_global_gap_in_documents)
+                for ml in move_lines:
+                    for move_line in ml.move_line_ids.filtered(
+                            lambda z: z.lot_id):
+                        if move_line.lot_id.country_id:
+                            if not lot_country_to_print:
+                                lot_country_to_print = (
+                                    move_line.lot_id.country_id.name)
+                            else:
+                                lot_country_to_print = u"{}, {}".format(
+                                    lot_country_to_print,
+                                    move_line.lot_id.country_id.name)
+                        if move_line.lot_id.ref:
+                            if not lot_global_gap_to_print:
+                                lot_global_gap_to_print = move_line.lot_id.ref
+                            else:
+                                lot_global_gap_to_print = u"{}, {}".format(
+                                    lot_global_gap_to_print,
+                                    move_line.lot_id.ref)
+            line.lot_country_to_print = lot_country_to_print
+            line.lot_global_gap_to_print = lot_global_gap_to_print
 
     def _create_and_assign_production_lot(self):
         result = super(StockMoveLine, self)._create_and_assign_production_lot()
