f3c7ad87564b22ef7561ea69323ce02a2e2bf7b3
Thor <alfredodelafuente@avanzosc.es>
2023-01-18 09:42:25 +0100

    [ADD] product_cost_purchase_last_price_info: In product new field "LPP in company currency".

---
+++ product_cost_purchase_last_price_info/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Product Cost Purchase Last Price Info",
+    'version': '14.0.1.0.0',
+    "category": "Purchase Management",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": [
+        "stock_account",
+        "purchase_last_price_info"
+    ],
+    "data": [
+        "views/product_product_views.xml",
+        "views/stock_quant_views.xml",
+    ],
+    'installable': True,
+}
