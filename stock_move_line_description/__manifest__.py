c35b2bc1ac46ed09d8c4379a63d6db982b29f4ee
Lei <lei@lei.com>
2023-01-19 11:31:59 +0100

    [IMP] stock_move_line_description: show move line

---
+++ stock_move_line_description/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2023 Leire Martinez de Santos - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Stock Move Line Description",
+    'version': '14.0.1.0.0',
+    "author": "Avanzosc",
+    "category": "Sale",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "sale_stock"
+    ],
+    "data": [
+        "report/report_deliveryslip.xml",
+       # "views/stock_move_line_view.xml",
+        "views/stock_picking_view.xml",
+    ],
+    "license": "AGPL-3",
+    'installable': True,
+}
