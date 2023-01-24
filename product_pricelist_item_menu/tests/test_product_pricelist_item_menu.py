1342c8b78dd43db483ba5eceab3a81f911d6dbe6
Oihane Crucelaegui <oihanecruce@gmail.com>
2023-01-24 10:41:58 +0100

    [IMP] product_pricelist_item_menu: black, isort, prettier

--- product_pricelist_item_menu/tests/test_product_pricelist_item_menu.py
+++ product_pricelist_item_menu/tests/test_product_pricelist_item_menu.py
@@ -6,24 +6,24 @@ from odoo.tests import common
 @common.at_install(False)
 @common.post_install(True)
 class TestProductPricelistItemMenu(common.SavepointCase):
-
     @classmethod
     def setUpClass(cls):
         super(TestProductPricelistItemMenu, cls).setUpClass()
-        cls.partner_model = cls.env['res.partner']
+        cls.partner_model = cls.env["res.partner"]
 
     def test_product_pricelist_item_menu1(self):
-        cond = [('customer', '=', True)]
+        cond = [("customer", "=", True)]
         partners = self.partner_model.search(cond)
         for partner in partners:
             self.assertEqual(
                 partner.count_pricelists_item,
-                len(partner.property_product_pricelist.item_ids))
+                len(partner.property_product_pricelist.item_ids),
+            )
 
     def test_product_pricelist_item_menu2(self):
-        cond = [('customer', '=', True)]
+        cond = [("customer", "=", True)]
         partner = self.partner_model.search(cond, limit=1)
         res = partner.button_show_partner_pricelist_items()
-        self.assertEqual(res.get('name'), 'Pricelists item')
-        cond = [('pricelist_id', '=', partner.property_product_pricelist.id)]
-        self.assertEqual(res.get('domain'), cond)
+        self.assertEqual(res.get("name"), "Pricelists item")
+        cond = [("pricelist_id", "=", partner.property_product_pricelist.id)]
+        self.assertEqual(res.get("domain"), cond)
