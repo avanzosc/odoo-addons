b37e3643ef49cbf17c04656d755378ee7eb5c04d
Oihane Crucelaegui <oihanecruce@gmail.com>
2023-01-24 11:20:23 +0100

    [MIG] product_pricelist_item_menu: Migration to 14.0

--- product_pricelist_item_menu/__manifest__.py
+++ product_pricelist_item_menu/__manifest__.py
@@ -2,10 +2,12 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
     "name": "Product Pricelist Item Menu",
-    "version": "12.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "depends": [
         "sale",
+        "sales_team",
+        "product",
     ],
     "author": "AvanzOSC",
     "website": "https://github.com/avanzosc/odoo-addons",
