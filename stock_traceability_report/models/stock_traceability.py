321fbd9d9f6477582a6a416f7334d504b2536f3e
Avanzosc <leiremartinez@avanzosc.es>
2023-01-26 13:28:37 +0100

    [14.0][ADD]stock_traceability_report: add partner info on Traceability Report

---
+++ stock_traceability_report/models/stock_traceability.py
@@ -0,0 +1,20 @@
+from odoo import api, models, _
+
+
+class MrpStockReport(models.TransientModel):
+    _inherit = 'stock.traceability.report'
+
+    @api.model
+    def _final_vals_to_lines(self, final_vals, level):
+        lines = super()._final_vals_to_lines(final_vals, level)
+        for key, data in enumerate(final_vals):
+            cols = lines[key].get('columns').append(data.get('partner_id', ''))
+        return lines
+
+    def _make_dict_move(self, level, parent_id, move_line, unfoldable=False):
+        res = super()._make_dict_move(level, parent_id, move_line, unfoldable)
+        if move_line.move_id.partner_id:
+            res[0].update({
+                'partner_id': move_line.move_id.partner_id.display_name,
+            })
+        return res
