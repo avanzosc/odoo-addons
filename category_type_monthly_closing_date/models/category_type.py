7dd443fe84849f1b543a4f5cd00b184e8ae5ae83
Tu Nombre <you@example.com>
2023-02-06 16:18:19 +0100

    [14.0][ADD] category_type_monthly_closing_date:

---
+++ category_type_monthly_closing_date/models/category_type.py
@@ -0,0 +1,10 @@
+# Copyright 2023 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import fields, models
+
+
+class CategoryType(models.Model):
+    _inherit = "category.type"
+
+    monthly_closing_date = fields.Date(
+        string="Monthly Closing Date")
---
+++ category_type_monthly_closing_date/models/stock_picking.py
@@ -0,0 +1,26 @@
+# Copyright 2023 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import _, models
+from odoo.exceptions import ValidationError
+
+
+class StockPicking(models.Model):
+    _inherit = "stock.picking"
+
+    def button_validate(self):
+        for picking in self:
+            if picking.custom_date_done and picking.category_type_id and (
+                picking.category_type_id.monthly_closing_date) and (
+                    picking.custom_date_done.date()) < (
+                        picking.category_type_id.monthly_closing_date):
+                raise ValidationError(
+                        _("The date of the picking cannot be earlier " +
+                          "than the monthly closing date of the sections."))
+            if picking.custom_date_done and picking.dest_category_type_id and (
+                picking.dest_category_type_id.monthly_closing_date) and (
+                    picking.custom_date_done.date()) < (
+                        picking.dest_category_type_id.monthly_closing_date):
+                raise ValidationError(
+                        _("The date of the picking cannot be earlier " +
+                          "than the monthly closing date of the sections."))
+        return super(StockPicking, self).button_validate()
