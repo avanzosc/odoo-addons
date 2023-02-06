138791ffe32185f8f2f15cf3cd2276fcef35884f
Tu Nombre <you@example.com>
2023-02-06 16:18:19 +0100

    [14.0][ADD] category_type_monthly_closing_date:

---
+++ category_type_monthly_closing_date/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2023 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Category Type Monthly Closing Date",
+    'version': '14.0.1.0.0',
+    "author": "Avanzosc",
+    "category": "Inventory",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "stock",
+        "stock_picking_date_done",
+        "stock_warehouse_farm",
+    ],
+    "data": [
+        "views/category_type_view.xml",
+    ],
+    "license": "AGPL-3",
+    'installable': True,
+}
