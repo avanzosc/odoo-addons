022f7c18e41bfc0626caed64b1d571c28c486665
Tu Nombre <you@example.com>
2023-02-09 15:55:15 +0100

    [14.0][ADD] stock_picking_type_category: New object to categorize the picking types.

---
+++ stock_picking_type_category/models/stock_picking_type.py
@@ -0,0 +1,11 @@
+# Copyright 2023 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import fields, models
+
+
+class StockPickingType(models.Model):
+    _inherit = "stock.picking.type"
+
+    category_id = fields.Many2one(
+        string="Category",
+        comodel_name="stock.picking.type.category")
