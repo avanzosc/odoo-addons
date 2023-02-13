61d77c9649c305391df1e7676cf3e4670f03a56f
leiremartinez <leiremartinez@avanzosc.es>
2023-02-13 15:23:23 +0100

    [ADD] product_attribute_show: add condition to show attribute values on product display name

---
+++ product_attribute_show/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2023 Leire Martinez de Santos - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Product attribute show",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "product",
+    ],
+    "author": "AvanzOSC",
+    "category": "Product Management",
+    "website": "http://www.avanzosc.es",
+    "data": [
+        "views/product_view.xml",
+    ],
+    "installable": True,
+}
---
+++ product_attribute_show/models/product.py
@@ -0,0 +1,34 @@
+
+from odoo import api, fields, models
+
+
+class ProductAttribute(models.Model):
+    _inherit = "product.attribute"
+
+    attr_display = fields.Boolean('Display attribute', default=True)
+
+
+class ProductAttributeValue(models.Model):
+    _inherit = "product.attribute.value"
+
+    attr_display = fields.Boolean('Display attribute value', default=True)
+
+
+class ProductProduct(models.Model):
+    _inherit = "product.product"
+
+    def _compute_display_name(self):
+        super()._compute_display_name()
+        for record in self.filtered(lambda s: s.attribute_value_ids):
+            display_name = record.display_name.split('(')[0]
+            show_attibutes = record.attribute_value_ids.filtered(lambda a: a.attr_display)
+            if show_attibutes:
+                display_name += ' ('
+                fst = True
+                for attribute in show_attibutes:
+                    attr_val = '' if fst > 0 else ', '
+                    attr_val += attribute.name
+                    display_name += attr_val
+                    fst = False
+                display_name += ')'
+            record.display_name = display_name
