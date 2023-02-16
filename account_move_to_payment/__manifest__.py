39c066746a91b685362fd98a2478ea8d61617de1
Tu Nombre <you@example.com>
2023-02-16 15:36:41 +0100

    [14.0][ADD] account_move_to_payment:

---
+++ account_move_to_payment/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2023 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Account Move To Payment",
+    'version': '14.0.1.0.0',
+    "category": "Analytic",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": [
+        "account"
+    ],
+    "data": [
+        "views/account_move_view.xml",
+    ],
+    'installable': True,
+}
