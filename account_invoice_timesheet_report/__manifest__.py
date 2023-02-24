08deb85932a1a50b67507967b496ce660f3b41b3
Alfredo <alfredodelafuente@avanzosc.es>
2023-02-24 10:45:01 +0100

    [ADD] account_invoice_timesheet_report: Report invoice with "Timesheets".

---
+++ account_invoice_timesheet_report/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Account Invoice Timesheet Report",
+    'version': '14.0.1.0.0',
+    "author": "Avanzosc",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "category": "Accounting/Accounting",
+    "depends": [
+        "account",
+        "sale_timesheet",
+    ],
+    "data": [
+        "report/account_invoice_report.xml",
+    ],
+    "license": "AGPL-3",
+    'installable': True,
+}
