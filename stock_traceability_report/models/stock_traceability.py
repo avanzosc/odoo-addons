125009095a24edca8a862260702a9bec399edb2e
Avanzosc <leiremartinez@avanzosc.es>
2023-02-24 13:33:00 +0100

    [FIX][16.0]stock_traceability_report: fix partner show

--- stock_traceability_report/models/stock_traceability.py
+++ stock_traceability_report/models/stock_traceability.py
@@ -15,6 +15,6 @@ class MrpStockReport(models.TransientModel):
         res = super()._make_dict_move(level, parent_id, move_line, unfoldable)
         if move_line.move_id.partner_id:
             res[0].update({
-                'partner_id': move_line.move_id.partner_id.display_name,
+                'partner_id': move_line.move_id.picking_id.partner_id.display_name,
             })
         return res
