ab2712cd7fc61d4a71f919dfa81573417131325d
Avanzosc <leiremartinez@avanzosc.es>
2023-02-24 13:46:37 +0100

    [16.0][ADD]stock_traceability_report: add fields to Traceability Report

---
+++ stock_traceability_report/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2023 Leire Martinez de Santos - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Stock Traceability Report",
+    "version": "16.0.1.0.0",
+    "category": "Stock",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        "reports/stock_views.xml",
+    ],
+    "installable": True,
+}
