3fc365b0096eed1d8260eddf86f9e07e243bce8a
Tu Nombre <you@example.com>
2023-02-27 16:47:57 +0100

    [14.0][ADD] res_company_stamp: stamp image in company.

---
+++ res_company_stamp/models/res_company.py
@@ -0,0 +1,9 @@
+# Copyright 2023 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import fields, models
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    stamp = fields.Binary(
+        string="Stamp")
