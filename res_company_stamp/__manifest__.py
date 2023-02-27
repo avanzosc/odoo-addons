945026627a9d41d60a5cf8e76a70e198a44db9ee
Tu Nombre <you@example.com>
2023-02-27 16:47:57 +0100

    [14.0][ADD] res_company_stamp: stamp image in company.

---
+++ res_company_stamp/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2023 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Res Company Stamp",
+    "version": "14.0.1.0.0",
+    "category": "Generic Modules",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "views/res_company_view.xml",
+    ],
+    "installable": True,
+}
