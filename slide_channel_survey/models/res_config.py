1ba607c4f1eb6866fb1fdd4b4865a6898665af79
leiremartinezdesantos <72156354+leiremartinezdesantos@users.noreply.github.com>
2023-02-27 10:07:44 +0000

    [IMP] slide_channel_survey: change report (#1844)
    * [IMP] change report layout·
    * [FIX] fix evaluation doc
    * [IMP] add custom header to evaluations report
    * [IMP] add header on survey.survey configuration
    * [IMP] change evaluations document
    * [IMP] change header size

---
+++ slide_channel_survey/models/res_config.py
@@ -0,0 +1,18 @@
+
+from odoo import api, fields, models
+
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    certification_header_image = fields.Binary(
+        "Certifications Header Image")
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = "res.config.settings"
+
+    certification_header_image = fields.Binary(
+        "Certifications Header Image",
+        related="company_id.certification_header_image",
+        readonly=False,)
