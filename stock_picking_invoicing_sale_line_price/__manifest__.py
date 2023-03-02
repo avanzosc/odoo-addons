354dd5f63e3baf185903fef6f2302e090ee24d13
Alfredo <alfredodelafuente@avanzosc.es>
2023-03-02 12:21:03 +0100

    [ADD] stock_picking_invoicing_sale_line_price: hen the invoice is generated from the picking, take the unit price from the sales order line, when generating the invoice line.

---
+++ stock_picking_invoicing_sale_line_price/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Stock Picking Invoicing Sale Line Price",
+    "version": "14.0.1.0.0",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "category": "Warehouse Management",
+    "license": "AGPL-3",
+    "depends": [
+        "stock_picking_invoicing"
+    ],
+    "data": [
+    ],
+    "installable": True,
+}
---
+++ stock_picking_invoicing_sale_line_price/wizards/stock_invoice_onshipping.py
@@ -0,0 +1,14 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import models
+
+
+class StockInvoiceOnshipping(models.TransientModel):
+    _inherit = "stock.invoice.onshipping"
+
+    def _get_invoice_line_values(self, moves, invoice_values, invoice):
+        result = super(StockInvoiceOnshipping, self)._get_invoice_line_values(
+            moves, invoice_values, invoice)
+        if len(moves) == 1 and moves.sale_line_id:
+            result["price_unit"] = moves.sale_line_id.price_unit
+        return result
