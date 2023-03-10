378b616b532272acf16a9c57144e027b3a745992
Tu Nombre <you@example.com>
2023-03-10 09:13:24 +0100

    [14.0][ADD] stock_quant_create_fix:

---
+++ stock_quant_create_fix/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2023 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Stock Quant Create Fix",
+    "version": "14.0.1.0.0",
+    "category": "Stock",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        "views/stock_production_lot_view.xml"
+    ],
+    "installable": True,
+}
---
+++ stock_quant_create_fix/models/__init__.py
@@ -0,0 +1,2 @@
+from . import stock_quant
+from . import stock_production_lot
