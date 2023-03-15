d8c133ce223a483ff1c9a00d0c7f9d49c1efff3b
Alfredo <alfredodelafuente@avanzosc.es>
2023-03-15 14:04:36 +0100

    [IMP] stock_lot_origin_global_gap: In lot, and stock move line, new field "Lot Origin/Global Gap OF".

--- stock_lot_origin_global_gap/models/stock_lot.py
+++ stock_lot_origin_global_gap/models/stock_lot.py
@@ -10,3 +10,36 @@ class StockLot(models.Model):
         string="Origin", comodel_name="res.country")
     ref = fields.Char(
         string="Global Gap")
+    lot_country_of = fields.Text(
+        string="Lot Origin OF",
+        compute="_compute_country_global_gap_of")
+    lot_global_gap_of = fields.Text(
+        string="Lot Global Gap OF",
+        compute="_compute_country_global_gap_of")
+    lot_country_gloval_gap_of = fields.Text(
+        string="Lot Origin/Global Gap OF",
+        compute="_compute_country_global_gap_of")
+
+    def _compute_country_global_gap_of(self):
+        for lot in self:
+            country_of = ""
+            global_gap_of = ""
+            country_gloval_gap_of = ""
+            cond = [("lot_producing_id", "=", lot.id)]
+            production = self.env["mrp.production"].search(cond, limit=1)
+            if production:
+                move_lines = production.move_raw_ids
+                move_lines = move_lines.filtered(
+                    lambda x: x.state != "cancel" and
+                    x.product_id.show_origin_global_gap_in_documents)
+                for ml in move_lines:
+                    for move_line in ml.move_line_ids.filtered(
+                            lambda z: z.lot_id and (z.lot_id.country_id or
+                                                    z.lot_id.ref)):
+                        country_of, global_gap_of, country_gloval_gap_of = (
+                            move_line._get_name_country_global_gap_of(
+                                country_of, global_gap_of,
+                                country_gloval_gap_of))
+            lot.lot_country_of = country_of
+            lot.lot_global_gap_of = global_gap_of
+            lot.lot_country_gloval_gap_of = country_gloval_gap_of
