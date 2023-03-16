0f8cd1a1b125c38bc6fdb7cc472a7607708321b2
Tu Nombre <you@example.com>
2023-03-16 13:13:42 +0100

    [14.0][ADD] purchase_import_wizard:

--- purchase_import_wizard/__manifest__.py
+++ purchase_import_wizard/__manifest__.py
@@ -1,21 +1,24 @@
-# Copyright 2022 Oihane Crucelaegui - AvanzOSC
+# Copyright 2022 Berezi Amubieta - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    "name": "Product Import Wizard",
+    "name": "Purchase Import Wizard",
     "version": "14.0.1.0.0",
     "category": "Hidden/Tools",
     "license": "AGPL-3",
     "author": "AvanzOSC",
     "website": "https://github.com/avanzosc/odoo-addons",
     "depends": [
+        "purchase",
         "base_import_wizard",
-        "product",
+        "purchase_discount",
+        "product_trim_name"
     ],
     "data": [
         "security/ir.model.access.csv",
-        "views/product_import_views.xml",
-        "views/product_import_line_views.xml",
+        "security/purchase_import_wizard_security.xml",
+        "views/purchase_order_import_line_views.xml",
+        "views/purchase_order_import_views.xml",
     ],
     "external_dependencies": {"python": ["xlrd"]},
     "installable": True,
