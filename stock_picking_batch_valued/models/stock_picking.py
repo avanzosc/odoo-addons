16766a546ac0b31d946121b9cde3f251eb7f84ea
Alfredo <alfredodelafuente@avanzosc.es>
2023-03-20 09:24:26 +0100

    [IMP] stock_picking_batch_valued: In picking new field "Sale Subtotal".

---
+++ stock_picking_batch_valued/models/stock_picking.py
@@ -0,0 +1,22 @@
+# Copyright 2022 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import api, fields, models
+
+
+class StockPicking(models.Model):
+    _inherit = "stock.picking"
+
+    sale_price_subtotal = fields.Float(
+        string="Sale Subtotal", digits="Product Price", store=True, copy=False,
+        compute="_compute_sale_price_subtotal")
+
+    @api.depends("move_ids_without_package",
+                 "move_ids_without_package.sale_price_subtotal")
+    def _compute_sale_price_subtotal(self):
+        for picking in self:
+            sale_price_subtotal = 0
+            if picking.move_ids_without_package:
+                sale_price_subtotal = sum(
+                    picking.move_ids_without_package.mapped(
+                        "sale_price_subtotal"))
+            picking.sale_price_subtotal = sale_price_subtotal
