1f59ae3a291f7436d4ee7fc4f319f5de63eab2b9
leiremartinezdesantos <72156354+leiremartinezdesantos@users.noreply.github.com>
2023-03-21 08:59:57 +0000

    [ADD] stock_move_line_description (#1852)
    * show description in done pickings
    * change stock move view
    * fix create function decorator
    * fix missing tag

---
+++ stock_move_line_description/models/account_move.py
@@ -0,0 +1,21 @@
+from odoo import api, models
+
+
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
+
+    @api.onchange('sale_line_ids')
+    def compute_account_move_line_description(self):
+        for record in self:
+            str_name = ''
+            for line in record.sale_line_ids:
+                if len(record.sale_line_ids)>1:
+                    str_name += ' - '
+                str_name += line.name
+            record.name = str_name
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        res = super(AccountMoveLine, self).create(vals_list)
+        res.compute_account_move_line_description()
+        return res
