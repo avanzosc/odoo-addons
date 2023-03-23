4e1ab63e824d3a35f0d0ff1e9e2fa8d8e584867a
Tu Nombre <you@example.com>
2023-03-23 13:37:41 +0100

    [14.0][ADD] product_supplierinfo_import: Supplier rate importer.

---
+++ product_supplierinfo_import/__manifest__.py
@@ -0,0 +1,26 @@
+# Copyright 2022 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Product Supplierinfo Import",
+    "version": "14.0.1.0.0",
+    "category": "Hidden/Tools",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": [
+        "product",
+        "purchase",
+        "purchase_discount",
+        "product_trim_name",
+        "base_import_wizard",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "security/product_supplierinfo_import_security.xml",
+        "views/product_supplierinfo_import_line_views.xml",
+        "views/product_supplierinfo_import_views.xml",
+    ],
+    "external_dependencies": {"python": ["xlrd"]},
+    "installable": True,
+}
