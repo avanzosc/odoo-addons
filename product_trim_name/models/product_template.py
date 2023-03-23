8fd1f27889e6b5e542e5c221da19fc55713343ca
Tu Nombre <you@example.com>
2023-03-23 09:35:49 +0100

    [14.0][ADD] product_trim_name:

---
+++ product_trim_name/models/product_template.py
@@ -0,0 +1,21 @@
+# Copyright 2023 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import api, fields, models
+import unicodedata
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    trim_name = fields.Char(
+        string="Trim Name",
+        compute="_compute_trim_name",
+        store=True)
+
+    @api.depends("name")
+    def _compute_trim_name(self):
+        for product in self:
+            trim_name = product.name.replace(" ", "")
+            trim_name = ''.join(c for c in unicodedata.normalize(
+                'NFD', trim_name) if unicodedata.category(c) != 'Mn')
+            product.trim_name = trim_name
