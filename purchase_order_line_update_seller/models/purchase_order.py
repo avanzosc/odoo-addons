84b45a56ff540e5fef8fa499e58e9da18d19913d
Alfredo <alfredodelafuente@avanzosc.es>
2023-03-29 09:23:58 +0200

    [ADD] purchase_order_line_update_seller: In purchase order line new field "Update price on tab".

---
+++ purchase_order_line_update_seller/models/purchase_order.py
@@ -0,0 +1,16 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import api, fields, models
+
+
+class PurchaseOrder(models.Model):
+    _inherit = "purchase.order"
+
+    update_price_on_tab = fields.Boolean(
+        string="Update price on tab", default=False, copy=False,
+        )
+
+    @api.onchange("update_price_on_tab")
+    def onchange_update_price_on_tab(self):
+        for line in self.order_line:
+            line.update_price_on_tab = self.update_price_on_tab
