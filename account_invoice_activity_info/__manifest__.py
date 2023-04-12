d0e15cbcf42d97e882ff5c416b74983c5909de9a
Alfredo <alfredodelafuente@avanzosc.es>
2023-04-12 10:04:36 +0200

    [ADD] account_invoice_activity_info: Activity info in invoces.

---
+++ account_invoice_activity_info/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Account Invoice Activity Info",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "category": "Accounting & Finance",
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "views/account_invoice_views.xml",
+    ],
+    "installable": True,
+}
