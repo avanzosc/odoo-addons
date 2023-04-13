09ecb11fd5cfb4d9911e415fafe80b817f960468
Alfredo <alfredodelafuente@avanzosc.es>
2023-04-13 10:05:04 +0200

    [IMP] product_packaging_palet: Tener en cuetna el cambio de cantidad de palets en líneas de venta.

--- product_packaging_palet/__manifest__.py
+++ product_packaging_palet/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
     "name": "Product Packaging Palet",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.1.0",
     "category": "Sales/Sales",
     "license": "AGPL-3",
     "author": "https://github.com/avanzosc/odoo-addons",
