af25d910fb9f068d6600dcf4dadd5c0958be969d
Tu Nombre <you@example.com>
2023-04-13 10:30:18 +0200

    [16.0][ADD] stock_move_line_qty_domain:

---
+++ stock_move_line_qty_domain/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2023 Berezi Amubieta - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Stock Move Line Quantity Domain",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "category": "Warehouse",
+    "depends": [
+        "stock"
+    ],
+    "data": [
+    ],
+    "installable": True,
+}
