8bda74ac46f4158b2d5064e5fbdddee867f0523b
Alfredo <alfredodelafuente@avanzosc.es>
2023-04-13 10:05:04 +0200

    [IMP] product_packaging_palet: Tener en cuetna el cambio de cantidad de palets en líneas de venta.

--- product_packaging_palet/models/sale_order_line.py
+++ product_packaging_palet/models/sale_order_line.py
@@ -11,10 +11,12 @@ class SaleOrderLine(models.Model):
     palet_qty = fields.Float(
         string="Contained Palet Quantity", default=1,
         digits="Product Unit of Measure", copy=False)
-
+    no_update_palet_qty = fields.Boolean(
+        string="No update palet_qty", default=False)
 
     @api.onchange("product_packaging_id")
     def _onchange_product_packaging_id(self):
+        self.no_update_palet_qty = True
         result = super(SaleOrderLine, self)._onchange_product_packaging_id()
         if self.product_packaging_id and self.product_packaging_id.palet_id.id:
             self.palet_id = self.product_packaging_id.palet_id.id
@@ -27,6 +29,7 @@ class SaleOrderLine(models.Model):
 
     @api.onchange("product_packaging_qty")
     def _onchange_product_packaging_qty(self):
+        self.no_update_palet_qty = True
         result = super(SaleOrderLine, self)._onchange_product_packaging_qty()
         if self.palet_id and self.product_uom_qty:
             self.palet_qty = self._get_palet_qty()
@@ -34,6 +37,7 @@ class SaleOrderLine(models.Model):
 
     @api.onchange("product_uom_qty")
     def _onchange_product_uom_qty(self):
+        self.no_update_palet_qty = True
         result = super(SaleOrderLine, self)._onchange_product_uom_qty()
         if self.palet_id and self.product_uom_qty:
             self.palet_qty = self._get_palet_qty()
@@ -43,3 +47,18 @@ class SaleOrderLine(models.Model):
         return (self.product_uom_qty /
                 (self.product_packaging_id.qty *
                  self.product_packaging_id.palet_qty))
+
+    @api.onchange("palet_qty")
+    def _onchange_palet_qty(self):
+        if (not self.no_update_palet_qty and self.product_id and
+            self.product_packaging_id and self.palet_id and
+                self.product_id.packaging_ids and self.palet_qty):
+            line = self.product_id.packaging_ids.filtered(
+                lambda x: x.name and x.palet_id and
+                x.name == self.product_packaging_id.name and
+                x.palet_id == self.palet_id)
+            if line and len(line) == 1:
+                self.product_packaging_qty = (
+                    self.palet_qty * line.palet_qty)
+        if self.no_update_palet_qty:
+            self.no_update_palet_qty = False
