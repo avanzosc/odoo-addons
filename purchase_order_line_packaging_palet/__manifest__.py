229f2f5a78539d51e0607f1ba5e88a3808b40532
Alfredo <alfredodelafuente@avanzosc.es>
2023-04-18 08:30:57 +0200

    [IMP] purchase_order_line_packaging_palet: Tener en cuenta la cantidad definida en el palet.

--- purchase_order_line_packaging_palet/__manifest__.py
+++ purchase_order_line_packaging_palet/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
     "name": "Purchase Order Line Packaging Palet",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.1.0",
     "category": "Inventory/Purchase",
     "license": "AGPL-3",
     "author": "https://github.com/avanzosc/odoo-addons",
