39cf5787e9f4ec376ad56c58dfe4cfe20e0e6ae2
Tu Nombre <you@example.com>
2023-04-25 15:16:41 +0200

    [14.0][ADD] stock_picking_emptying_location: In pickings, button to empty the source location of the picking.

---
+++ stock_picking_emptying_location/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2023 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Stock Picking Emptying Location",
+    "version": "14.0.1.0.0",
+    "category": "Hidden/Tools",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        "views/stock_picking_view.xml",
+        "views/stock_picking_type_view.xml",
+    ],
+    "installable": True,
+}
---
+++ stock_picking_emptying_location/models/stock_picking.py
@@ -0,0 +1,31 @@
+# Copyright 2023 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class StockPicking(models.Model):
+    _inherit = "stock.picking"
+
+    show_empty_location = fields.Boolean(
+        string="Show Emptying Location",
+        related="picking_type_id.show_empty_location",
+        store=True)
+
+    def button_empty_location(self):
+        self.ensure_one()
+        if self.move_line_ids:
+            self.move_line_ids.unlink()
+        if self.location_id:
+            stock = self.env["stock.quant"].search([
+                ("location_id", "=", self.location_id.id)])
+            for line in stock:
+                if line.available_quantity > 0:
+                    self.env["stock.move.line"].create({
+                        "product_id": line.product_id.id,
+                        "location_id": self.location_id.id,
+                        "location_dest_id": self.location_dest_id.id,
+                        "lot_id": line.lot_id.id,
+                        "qty_done": line.available_quantity,
+                        "product_uom_id": line.product_uom_id.id,
+                        "picking_id": self.id})
