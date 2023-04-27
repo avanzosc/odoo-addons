adcaee6f4a60e7ca847c1806b86216de3df6ace6
Alfredo <alfredodelafuente@avanzosc.es>
2023-04-27 13:19:02 +0200

    [ADD] purchase_order_report_fix: Fix purchase order report.

---
+++ purchase_order_report_fix/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Purchase Order Report Fix",
+    'version': '14.0.1.0.0',
+    "author": "Avanzosc",
+    "category": "Inventory",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "purchase",
+        "purchase_stock"
+    ],
+    "data": [
+        "reports/purchase_order_templates.xml",
+    ],
+    "license": "AGPL-3",
+    'installable': True,
+}
