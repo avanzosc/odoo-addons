c1fa0997d10f8028480e628fc30325b41c30399e
Oihane Crucelaegui <oihanecruce@gmail.com>
2023-05-05 11:53:18 +0200

    [IMP] purchase_order_line_input: made compatible with purchase_open_qty

--- purchase_order_line_input/__manifest__.py
+++ purchase_order_line_input/__manifest__.py
@@ -10,6 +10,8 @@
     "license": "AGPL-3",
     "depends": [
         "purchase",
+        "purchase_order_line_menu",
+        "purchase_open_qty",
     ],
     "data": [
         "views/purchase_order_line_view.xml",
