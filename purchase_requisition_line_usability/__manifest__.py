37154ea4801b1858e9f4633dd39a5e5958c16e7b
Tu Nombre <you@example.com>
2023-05-09 11:04:47 +0200

    [14.0][ADD] purchase_requisition_line_usability:

---
+++ purchase_requisition_line_usability/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2023 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Purchase Requisition Line Usability",
+    'version': '14.0.1.0.0',
+    "category": "Purchase Management",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": [
+        "stock",
+        "purchase",
+        "purchase_requisition",
+    ],
+    "data": [
+        "data/price_unit_decimal_precision.xml",
+        "views/purchase_requisition_views.xml",
+        "views/purchase_requisition_line_view.xml",
+    ],
+    'installable': True,
+}
