7f2cfc0fe08579b478e0d406b380a1abe4f5f548
Tu Nombre <you@example.com>
2023-05-10 10:42:01 +0200

    [14.0][ADD] purchase_requisition_stock_usability:

---
+++ purchase_requisition_stock_usability/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2023 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Purchase Requisition Stock Usability",
+    'version': '14.0.1.0.0',
+    "author": "Avanzosc",
+    "category": "Inventory",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": [
+        "purchase_requisition_stock",
+    ],
+    "data": [
+        "views/purchase_requisition_view.xml",
+    ],
+    "license": "AGPL-3",
+    'installable': True,
+}
