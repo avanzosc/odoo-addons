bd48515c64d1933d73e66222d22e41ee268e78a1
Tu Nombre <you@example.com>
2023-05-10 09:35:42 +0200

    [14.0][ADD] contact_to_move_line: Shorcut from contacts to move lines.

---
+++ contact_to_move_line/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2023 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Contact To Move Line",
+    'version': '14.0.1.0.0',
+    "author": "Avanzosc",
+    "category": "Inventory",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "stock",
+        "contacts",
+        "sale_picking_return_containers",
+        "stock_move_in_out_qty",
+    ],
+    "data": [
+        "views/res_partner_view.xml",
+    ],
+    "license": "AGPL-3",
+    'installable': True,
+}
