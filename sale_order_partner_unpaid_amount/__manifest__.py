7d5314bfb4bb0883c990e9f3991008742776bb3b
Alfredo <alfredodelafuente@avanzosc.es>
2023-05-12 09:27:22 +0200

    [ADD] sale_order_partner_import_unpaid: In sale order new field "Import unpaid invoices", for sale order client.

---
+++ sale_order_partner_unpaid_amount/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Sale Order Partner Unpaid Amount",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "category": "Tools",
+    "depends": [
+        "account",
+        "sale",
+    ],
+    "data": [
+        "views/sale_order_views.xml",
+    ],
+    "installable": True,
+}
