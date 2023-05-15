981d00dee5bb908ebe5b917e2e33c7d3bdd49475
Alfredo <alfredodelafuente@avanzosc.es>
2023-05-15 14:35:27 +0200

    [ADD] account_invoice_report_grouped_by_picking_ext: Print invoice lines that are not associated with any stock picking line.

---
+++ account_invoice_report_grouped_by_picking_ext/models/account_move.py
@@ -0,0 +1,32 @@
+# Copyright 2023 Alfredo de la fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+from odoo import models
+from collections import OrderedDict
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    def lines_grouped_by_picking(self):
+        self.ensure_one()
+        result = super(AccountMove, self).lines_grouped_by_picking()
+        picking_dict = OrderedDict()
+        so_dict = {x.sale_id: x for x in self.picking_ids if x.sale_id}
+        for line in self.invoice_line_ids.filtered(lambda x: not x.display_type):
+            remaining_qty = line.quantity
+            if not line.move_line_ids and line.sale_line_ids:
+                for so_line in line.sale_line_ids:
+                    if not so_dict.get(so_line.order_id):
+                        key = (self.env["stock.picking"], line)
+                        picking_dict.setdefault(key, 0)
+                        qty = line.quantity
+                        picking_dict[key] += qty
+                        remaining_qty -= qty
+        with_picking = [
+            {"picking": key[0], "line": key[1], "quantity": value}
+            for key, value in picking_dict.items()
+        ]
+        if picking_dict:
+            result = result + with_picking
+            result = self._sort_grouped_lines(result)
+        return result
