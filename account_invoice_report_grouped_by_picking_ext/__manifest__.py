07029d9608bd51cee278e28e498b39486476af99
Alfredo <alfredodelafuente@avanzosc.es>
2023-05-15 14:35:27 +0200

    [ADD] account_invoice_report_grouped_by_picking_ext: Print invoice lines that are not associated with any stock picking line.

---
+++ account_invoice_report_grouped_by_picking_ext/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Account Invoice Report Grouped By Picking Ext",
+    "version": "14.0.1.4.0",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "category": "Accounting & Finance",
+    "license": "AGPL-3",
+    "depends": [
+        "account_invoice_report_grouped_by_picking",
+    ],
+    "data": [
+#        "views/event_event_views.xml",
+    ],
+    "installable": True,
+}
