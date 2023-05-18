0e109657d3c08c8cdb4ad4e38266c881bf474e86
Tu Nombre <you@example.com>
2023-05-18 14:44:32 +0200

    [14.0][ADD] stock_picking_batch_license_plate:

---
+++ stock_picking_batch_license_plate/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2022 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Stock Picking Batch License Plate",
+    'version': '14.0.1.0.0',
+    "author": "Avanzosc",
+    "category": "Inventory",
+    "depends": [
+        "stock_picking_batch",
+        "purchase_order_shipping_method",
+    ],
+    "data": [
+        "views/stock_picking_batch_views.xml",
+    ],
+    "license": "AGPL-3",
+    'installable': True,
+}
