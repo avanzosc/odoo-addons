7688dfc27703acaabbbff19d824b471e717013f0
Alfredo <alfredodelafuente@avanzosc.es>
2023-05-22 08:58:26 +0200

    [IMP] stock_production_lot_warranty_repair_date: Nuevo calculo para "Vencimiento de la garantia", y control al iniciar la reparación.

--- stock_production_lot_warranty_repair_date/models/repair_order.py
+++ stock_production_lot_warranty_repair_date/models/repair_order.py
@@ -1,7 +1,8 @@
 # Copyright 2022 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from dateutil.relativedelta import relativedelta
-from odoo import api, fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
 
 
 class RepairOrder(models.Model):
@@ -19,10 +20,20 @@ class RepairOrder(models.Model):
         store=True,
         related="lot_id.warranty_repair_date",
     )
+    tag_ids = fields.Many2many(required=True)
 
     @api.onchange("lot_expiration_date", "lot_warranty_repair_date")
     def onchange_expiration_warranty_repair_date(self):
+        self._calculate_guarantee_limit()
         self._check_if_it_is_under_warranty()
+
+    @api.onchange("guarantee_limit")
+    def onchange_guarantee_limit(self):
+        if self.guarantee_limit:
+            if self.guarantee_limit < fields.Date.context_today(self):
+                self.invoice_method = "b4repair"
+            else:
+                self.invoice_method = "none"
 
     def action_repair_end(self):
         result = super(RepairOrder, self).action_repair_end()
@@ -35,6 +46,7 @@ class RepairOrder(models.Model):
     @api.model
     def create(self, vals):
         repair = super(RepairOrder, self).create(vals)
+        repair._calculate_guarantee_limit()
         repair._check_if_it_is_under_warranty()
         return repair
 
@@ -53,3 +65,28 @@ class RepairOrder(models.Model):
             in_warranty = True
         if in_warranty and self.invoice_method != "none":
             self.invoice_method = "none"
+
+    def _calculate_guarantee_limit(self):
+        guarantee_limit = False
+        if self.lot_expiration_date:
+            guarantee_limit = self.lot_expiration_date
+        if (self.lot_warranty_repair_date and
+            (not guarantee_limit or
+             (self.lot_warranty_repair_date > self.lot_expiration_date))):
+            guarantee_limit = self.lot_warranty_repair_date
+        self.guarantee_limit = guarantee_limit
+        if guarantee_limit:
+            if guarantee_limit < fields.Date.context_today(self):
+                self.invoice_method = "b4repair"
+            else:
+                self.invoice_method = "none"
+
+    def action_validate(self):
+        self.ensure_one()
+        if self.sale_order_id and self.sale_order_id.state == "draft":
+            raise UserError(
+                _(
+                    "You must confirm the sales order before starting the "
+                    "repair."
+                ))
+        return super(RepairOrder, self).action_validate()
