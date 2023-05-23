018ae7f39c93c7d1d9b30864213e637784e9be7e
Tu Nombre <you@example.com>
2023-05-23 10:42:33 +0200

    [14.0][IMP] custom_saca_intercompany:

--- custom_saca_intercompany/models/purchase_order.py
+++ custom_saca_intercompany/models/purchase_order.py
@@ -1,10 +1,23 @@
 # Copyright 2022 Berezi Amubieta - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import models
+from odoo import api, fields, models
 
 
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
+
+    total_qty = fields.Float(
+        string="Total Qty",
+        compute="_compute_total_qty",
+        store=True)
+
+    @api.depends("order_line", "order_line.qty_received")
+    def _compute_total_qty(self):
+        for purchase in self:
+            total_qty = 0
+            if purchase.order_line:
+                total_qty = sum(purchase.order_line.mapped("qty_received"))
+            purchase.total_qty = total_qty
 
     def _prepare_sale_order_data(
             self, name, partner, dest_company, direct_delivery_address):
