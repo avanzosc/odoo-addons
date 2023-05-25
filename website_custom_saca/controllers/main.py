829aa858c0ce292a02df9a9e6792a9f2f2d4e8d6
Leire <leiremartinez@avanzosc.es>
2023-05-25 11:40:54 +0200

    [IMP]website_custom_saca

--- website_custom_saca/controllers/main.py
+++ website_custom_saca/controllers/main.py
@@ -141,6 +141,9 @@ class CustomerPortal(CustomerPortal):
                     new_val = 0
 
             if ttype.type in ['char', 'text', 'selection']:
+                if value == 'floor':
+                    if update_vals.get(value) == 0:
+                        break
                 new_val = update_vals.get(value)
 
            # if ttype.type == 'binary':
