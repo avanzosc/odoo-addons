982d17abd7025312f5d791589ec0859ed399bd17
Alfredo <alfredodelafuente@avanzosc.es>
2023-05-26 08:17:53 +0200

    [ADD] stock_picking_batch_invoice_rel: Shortcuts from batch transfer to invoices, and from invoice to batch transfers.

---
+++ stock_picking_batch_invoice_rel/models/account_move.py
@@ -0,0 +1,40 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import fields, models, _
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    stock_picking_batch_ids = fields.Many2many(
+        string="Batch Transfers", comodel_name="stock.picking.batch",
+        relation="rel_picking_batch_invoice", column1="invoice_id",
+        column2="picking_batch_id"
+        )
+    stock_picking_batch_count = fields.Integer(
+        string="Batch Transfers Counter",
+        compute="_compute_stock_picking_batch_count"
+        )
+
+    def _compute_stock_picking_batch_count(self):
+        for invoice in self:
+            invoice.stock_picking_batch_count = len(
+                invoice.stock_picking_batch_ids)
+
+    def action_view_picking_batch(self):
+        self.ensure_one()
+        action = {
+            "name": _("Batch Transfers"),
+            "type": "ir.actions.act_window",
+            "view_type": "form",
+            "view_mode": "form",
+            "res_model": "stock.picking.batch",
+            }
+        action.update(
+            {
+                "name": _("Batch Transfers"),
+                "view_mode": "tree,kanban,form",
+                "domain": [("id", "in", self.stock_picking_batch_ids.ids)],
+            }
+        )
+        return action
---
+++ stock_picking_batch_invoice_rel/models/stock_picking_batch.py
@@ -0,0 +1,42 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import api, fields, models
+from odoo.models import expression
+from odoo.tools.safe_eval import safe_eval
+
+
+class StockPickingBatch(models.Model):
+    _inherit = "stock.picking.batch"
+
+    invoice_ids = fields.Many2many(
+        string="Invoices", comodel_name="account.move",
+        compute="_compute_invoice_ids", relation="rel_picking_batch_invoice",
+        column1="picking_batch_id", column2="invoice_id", store=True,
+        copy=False
+        )
+    invoice_count = fields.Integer(
+        string="Invoices counter", compute="_compute_invoice_count"
+        )
+
+    @api.depends("picking_ids", "picking_ids.invoice_ids")
+    def _compute_invoice_ids(self):
+        for batch in self:
+            account_move = self.env["account.move"]
+            for picking in batch.picking_ids:
+                for invoice in picking.invoice_ids:
+                    if invoice not in account_move:
+                        account_move += invoice
+            batch.invoice_ids = [(6, 0, account_move.ids)]
+
+    def _compute_invoice_count(self):
+        for batch in self:
+            batch.invoice_count = len(batch.invoice_ids)
+
+    def action_view_invoice(self):
+        action = self.env.ref("account.action_move_out_invoice_type")
+        action_dict = action.read()[0] if action else {}
+        domain = expression.AND(
+            [[("id", "in", self.invoice_ids.ids)],
+             safe_eval(action.domain or "[]")])
+        action_dict.update({"domain": domain})
+        return action_dict
