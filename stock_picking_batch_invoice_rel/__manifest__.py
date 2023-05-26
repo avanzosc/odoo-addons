7a244d8d6a1c83a0e23a165f08b6f1f8f8fbf305
Alfredo <alfredodelafuente@avanzosc.es>
2023-05-26 08:17:53 +0200

    [ADD] stock_picking_batch_invoice_rel: Shortcuts from batch transfer to invoices, and from invoice to batch transfers.

---
+++ stock_picking_batch_invoice_rel/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Stock Picking Batch Invoice Rel",
+    "version": "14.0.1.0.0",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "category": "Inventory/Inventory",
+    "license": "AGPL-3",
+    "depends": [
+        "account",
+        "stock_picking_batch",
+        "stock_picking_invoice_link"
+    ],
+    "data": [
+        "views/account_move_views.xml",
+        "views/stock_picking_batch_views.xml",
+    ],
+    "installable": True,
+}
