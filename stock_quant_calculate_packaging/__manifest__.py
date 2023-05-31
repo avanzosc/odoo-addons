73922a42d8276066b6196e75c1d5c78f452b602e
Alfredo <alfredodelafuente@avanzosc.es>
2023-05-31 09:48:21 +0200

    [ADD] stock_quant_calculate_packaging:On inventory adjustments calculate "Packaging" and "Packaging Quantity".

---
+++ stock_quant_calculate_packaging/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Stock Quant Calculate Packaging",
+    "version": "16.0.1.0.0",
+    "category": "Sales/Sales",
+    "license": "AGPL-3",
+    "author": "https://github.com/avanzosc/odoo-addons",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "product",
+        "stock"
+    ],
+    "data": [
+        "views/stock_quant_views.xml",
+    ],
+    "installable": True,
+}
