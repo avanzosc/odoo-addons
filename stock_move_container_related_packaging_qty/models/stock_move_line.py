0c364ec4cd0b1cd788e823c092354fbf8b2869a7
Alfredo <alfredodelafuente@avanzosc.es>
2023-06-07 10:09:24 +0200

    [ADD] stock_move_container_related_packaging_qty: In sale order line, and stock move objects, the field "container" related to packaging qty.

---
+++ stock_move_container_related_packaging_qty/models/stock_move_line.py
@@ -0,0 +1,11 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import fields, models
+
+
+class StockMoveLine(models.Model):
+    _inherit = "stock.move.line"
+
+    container = fields.Integer(
+        string="Quantity contained per package"
+        )
