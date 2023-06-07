83687dd626535a2928173f8ce280ab5bcf72725e
Alfredo <alfredodelafuente@avanzosc.es>
2023-06-07 10:14:40 +0200

    [ADD] stock_move_line_automatic_result_package: Automatically create a package for each line of detailed operations

---
+++ stock_move_line_automatic_result_package/models/stock_move.py
@@ -0,0 +1,17 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import models
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    def _update_reserved_quantity(self, need, available_quantity, location_id,
+                                  lot_id=None, package_id=None, owner_id=None,
+                                  strict=True):
+        taken_quantity = super(StockMove, self)._update_reserved_quantity(
+            need, available_quantity, location_id, lot_id=lot_id,
+            package_id=package_id, owner_id=owner_id, strict=strict)
+        for move in self:
+            move.picking_id.create_automatic_packages()
+        return taken_quantity
