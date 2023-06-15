eac34fb5a4ff134b729cf3793faac534bef198a5
Tu Nombre <you@example.com>
2023-06-15 13:14:31 +0200

    [MIG] stock_move_availability: Migration to 16.0

--- stock_move_availability/__manifest__.py
+++ stock_move_availability/__manifest__.py
@@ -2,9 +2,10 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Stock Move Availability",
-    'version': '14.0.1.0.0',
-    "author": "Avanzosc",
-    "category": "Inventory",
+    "version": "16.0.1.0.0",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "category": "Inventory/Inventory",
     "depends": [
         "stock",
     ],
@@ -12,5 +13,5 @@
         "views/stock_move_views.xml",
     ],
     "license": "AGPL-3",
-    'installable': True,
+    "installable": True,
 }
