42d32b6f56cf23b75a9c11fe57e215bc1822759c
Berezi <82805447+Berezi@users.noreply.github.com>
2023-06-27 09:38:08 +0200

    [ADD] product_import_with_packaging: (#1995)

---
+++ product_import_with_packaging/models/product_packaging_import.py
@@ -0,0 +1,10 @@
+# Copyright 2023 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import fields, models
+
+
+class ProductPackagingImport(models.Model):
+    _inherit = "product.packaging.import"
+
+    data = fields.Binary(
+        required=False)
