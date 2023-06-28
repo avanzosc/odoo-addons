19fe4a5795ec9bdd53b3540bdca41cb1baedb850
Alfredo <alfredodelafuente@avanzosc.es>
2023-06-28 11:09:21 +0200

    [IMP] account_invoice_with_start_end_date_period: Quitar dependencia con "contract".

--- account_invoice_with_start_end_date_period/__manifest__.py
+++ account_invoice_with_start_end_date_period/__manifest__.py
@@ -2,12 +2,12 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Account Invoice With Start End Date Period",
-    "version": "14.0.1.3.0",
+    "version": "14.0.1.4.0",
     "category": "Invoices & Payments",
     "license": "AGPL-3",
     "author": "AvanzOSC",
     "website": "http://www.avanzosc.es",
-    "depends": ["sale", "sale_timesheet", "account", "contract"],
+    "depends": ["sale", "sale_timesheet", "account"],
     "data": [
         "views/account_move_views.xml",
     ],
