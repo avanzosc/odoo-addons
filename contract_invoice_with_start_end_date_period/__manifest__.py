fc76372bc7d55de09f4c42da278a2989c421d80e
Alfredo <alfredodelafuente@avanzosc.es>
2023-06-28 11:18:18 +0200

    [ADD] contract_invoice_with_start_end_date_period: Generar facturas desde contractos con fechas desde, y hasta.

---
+++ contract_invoice_with_start_end_date_period/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Contract Invoice With Start End Date Period",
+    "version": "14.0.1.0.0",
+    "category": "Invoices & Payments",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": ["contract", "account_invoice_with_start_end_date_period"],
+    "data": [
+    ],
+    "installable": True,
+    "auto_install": True,
+}
