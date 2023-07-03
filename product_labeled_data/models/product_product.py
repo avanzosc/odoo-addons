3f58ad3d9dd9a9b46c8b74ff3eaf3f5a98a89dfa
Alfredo <alfredodelafuente@avanzosc.es>
2023-07-03 12:55:46 +0200

    [ADD] product_labeled_data: In product from view new tab "Labeled", for customized fields to print.

---
+++ product_labeled_data/models/product_product.py
@@ -0,0 +1,24 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import fields, models
+
+
+class ProductProduct(models.Model):
+    _inherit = "product.product"
+
+    labeled_length = fields.Char(
+        string="Length", copy=False)
+    description_label_es = fields.Char(
+        string="Description label (es)", copy=False)
+    description_label_fr = fields.Char(
+        string="Description label (fr)", copy=False)
+    description_label_en = fields.Char(
+        string="Description label (en)", copy=False)
+    labeled_finished_code = fields.Char(
+        string="Finished code", copy=False)
+    labeled_color_es = fields.Char(
+        string="Color (es)", copy=False)
+    labeled_color_fr = fields.Char(
+        string="Color (fr)", copy=False)
+    labeled_color_en = fields.Char(
+        string="Color (en)", copy=False)
