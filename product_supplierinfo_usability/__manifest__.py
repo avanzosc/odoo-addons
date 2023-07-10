5f9b1748b71edb46a484b9e05aa052756fac5b37
Alfredo <alfredodelafuente@avanzosc.es>
2023-07-10 14:43:54 +0200

    [IMP] product_supplierinfo_usability: Cambio calculo campo "Pending receipt from supplier".

--- product_supplierinfo_usability/__manifest__.py
+++ product_supplierinfo_usability/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Product Supplierinfo Usability",
-    'version': '14.0.1.0.0',
+    'version': '14.0.1.2.0',
     "category": "Sales/Sales",
     "license": "AGPL-3",
     "author": "AvanzOSC",
