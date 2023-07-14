5affcddeb18468d224aa6faf19c43d535319254c
Tu Nombre <you@example.com>
2023-07-14 12:41:59 +0200

    [14.0][ADD] res_partner_title_hide: Hide the title field in contacts.

---
+++ res_partner_title_hide/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2023 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Res Partner Title Hide",
+    "version": "14.0.1.0.0",
+    "category": "MRP",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "contacts",
+    ],
+    "data": [
+        "views/res_partner_view.xml",
+    ],
+    "installable": True,
+}
