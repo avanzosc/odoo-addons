c6b2080c86b6e9f9a16a419e6c187f7eb7fe9cd7
Tu Nombre <you@example.com>
2023-07-17 16:15:26 +0200

    [14.0][ADD] account_journal_rebu:

---
+++ account_journal_rebu/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2023 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Account Journal Rebu",
+    'version': '14.0.1.0.0',
+    "category": "Accounting/Accounting",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": [
+        "account"
+    ],
+    "data": [
+        "views/account_journal_view.xml",
+        "views/account_move_view.xml",
+        "report/account_move_report.xml",
+    ],
+    'installable': True,
+}
