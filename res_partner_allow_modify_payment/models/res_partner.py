41a173aa4fd4237264389d4c2d116b818f96f648
Alfredo <alfredodelafuente@avanzosc.es>
2023-07-24 09:04:26 +0200

    [ADD] res_partner_allow_modify_payment: New group "Allow modifying mode and form of payment".

---
+++ res_partner_allow_modify_payment/models/res_partner.py
@@ -0,0 +1,19 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    allow_modify_payment = fields.Boolean(
+        string="Allow modify payment",
+        compute="_compute_allow_modify_payment"
+        )
+
+    def _compute_allow_modify_payment(self):
+        group = self.env.ref(
+            "res_partner_allow_modify_payment.group_allow_mod_partner_payment")
+        allow_modify_payment = True if self.env.user in group.users else False
+        for partner in self:
+            partner.allow_modify_payment = allow_modify_payment
