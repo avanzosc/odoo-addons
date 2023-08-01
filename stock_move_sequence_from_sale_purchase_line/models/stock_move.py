a594cd3072010eb34503abd0722b3138f8cfb701
Alfredo <alfredodelafuente@avanzosc.es>
2023-08-01 14:03:49 +0200

    [ADD] stock_move_sequence_from_sale_purchase_line: This module redefines the "sequence" field of the "stock.move" object, taking the value of the "sequence" field of the sales order line, or from the purchase order line.

---
+++ stock_move_sequence_from_sale_purchase_line/models/stock_move.py
@@ -0,0 +1,18 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import api, models
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    @api.depends("sale_line_id", "sale_line_id.sequence", "purchase_line_id",
+                 "purchase_line_id.sequence")
+    def _compute_sequence(self):
+        for move in self:
+            sequence = 0
+            if move.sale_line_id:
+                sequence = move.sale_line_id.sequence
+            if move.purchase_line_id:
+                sequence = move.purchase_line_id.sequence
+            move.sequence = sequence
