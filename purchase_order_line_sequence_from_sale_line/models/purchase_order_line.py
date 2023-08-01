fe2c92e7da52e535c8df8130c67a4e5bfd2eaada
Alfredo <alfredodelafuente@avanzosc.es>
2023-08-01 13:58:48 +0200

    [ADD] purchase_order_line_sequence_from_sale_line: This module redefines the "sequence" field of the "purchase.order.line" object, taking the value of the "sequence" field of the sales order line.

---
+++ purchase_order_line_sequence_from_sale_line/models/purchase_order_line.py
@@ -0,0 +1,36 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import api, fields, models
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = "purchase.order.line"
+
+    sequence = fields.Integer(
+        compute="_compute_sequence", store=True, copy=False, readonly=True)
+
+    @api.depends("sale_line_id", "sale_line_id.sequence")
+    def _compute_sequence(self):
+        for purchase in self:
+            sequence = 0
+            if purchase.sale_line_id:
+                sequence = purchase.sale_line_id.sequence
+            purchase.sequence = sequence
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        lines = super(PurchaseOrderLine, self).create(vals_list)
+        for line in lines:
+            if not line.sale_line_id and line.order_id.origin:
+                line.put_sale_order_line()
+        return lines
+
+    def put_sale_order_line(self):
+        cond = [("name", "=", self.order_id.origin)]
+        sale = self.env["sale.order"].search(cond, limit=1)
+        if sale:
+            line = sale.order_line.filtered(
+                lambda x: x.product_id == self.product_id and
+                x.product_uom_qty == self.product_qty)
+            if len(line) == 1:
+                self.sale_line_id = line.id
