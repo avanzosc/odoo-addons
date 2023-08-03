2c275013fa6c39c8fb8facf3861eb41230a32b11
Tu Nombre <you@example.com>
2023-08-03 12:49:44 +0200

    [14.0][ADD] product_computer_management:

---
+++ product_computer_management/models/storage_type.py
@@ -0,0 +1,14 @@
+# Copyright 2023 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class StorageType(models.Model):
+    _name = "storage.type"
+    _description = "Storage Type"
+
+    name = fields.Char(
+        string="Name",
+        required=True,
+        copy=False)
