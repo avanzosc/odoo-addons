bb77bad39c61809760c228fc6c0e052cf0161dfb
Alfredo <alfredodelafuente@avanzosc.es>
2023-08-04 09:59:33 +0200

    [ADD] stock_production_lot_product_class: In lots new field "Pump type", related to product.

---
+++ stock_production_lot_product_class/models/__init__.py
@@ -0,0 +1,3 @@
+from . import product_class
+from . import product_template
+from . import stock_production_lot
---
+++ stock_production_lot_product_class/models/product_class.py
@@ -0,0 +1,11 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import fields, models
+
+
+class ProductClass(models.Model):
+    _name = "product.class"
+    _description = "Product class"
+
+    name = fields.Char(
+        string="Product class", required=True, copy=False)
