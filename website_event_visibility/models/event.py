88a4e39fc4fdd1c8724c1cf4ea7665ccfd8f1d5d
leiremartinezdesantos <72156354+leiremartinezdesantos@users.noreply.github.com>
2023-08-08 14:11:11 +0200

    [IMP] website_event_visibility: compute fields on event outdated (#2046)
    * [IMP] website_event_visibility: compute fields on event outdated
    
    * [IMP] website_event_visibility: compute fields on event outdated
    
    * [IMP] website_event_visibility: compute fields on event outdated
    
    * [IMP] website_event_visibility: compute fields on event outdated
    
    * [IMP] website_event_visibility: change stage id
    
    * [IMP] website_event_visibility: change stage id
    
    * FIX unnecessary added files

--- website_event_visibility/models/event.py
+++ website_event_visibility/models/event.py
@@ -1,6 +1,6 @@
 
 from odoo import api, fields, models
-from datetime import datetime
+from datetime import datetime, date
 
 
 class EventEvent(models.Model):
@@ -25,12 +25,50 @@ class EventEvent(models.Model):
                 record.website_published = True
             else:
                 record.website_published = False
+                record.update_data_unpublish()
 
     def cron_compute_unpublish_website(self):
         events = self.env['event.event'].search([
-            ('website_published', '=', True)
+          #  ('website_published', '=', True)
         ])
         events.compute_unpublish_website()
+
+    def update_data_unpublish(self):
+        for event in self:
+            event.stage_id = self.env.ref('website_event_track.event_track_stage3').id
+            event.make_participants_done()
+            event.finish_responsible_courses()
+
+    def make_participants_done(self):
+        registration_ids = self.env['event.registration'].search([
+            ('event_id', '=', self.id),
+            ('state', 'in', ["draft", "open"])
+        ])
+        for registration in registration_ids:
+            registration.action_set_done()
+
+    def finish_responsible_courses(self):
+        for event in self:
+            partner_ids = self.env['res.partner'].search([
+                ('id', 'in', [
+                    event.user_id.partner_id.id,
+                    event.main_responsible_id.partner_id.id,
+                    event.second_responsible_id.partner_id.id])
+            ])
+            event.finish_event_participant_course(partner_ids)
+
+    def finish_event_participant_course(self, partner_ids):
+        self.ensure_one()
+        if partner_ids:
+            partner_courses = self.env['slide.channel.partner'].search([
+                ('event_id', '=', False),
+                ('partner_id', 'in', partner_ids.ids),
+                ('channel_id', 'in', self.slides_ids.ids),
+            ])
+            for course in partner_courses:
+                course.update({
+                    'real_date_end': self.date_end.date()
+                })
 
 
 class EventEventTicket(models.Model):
