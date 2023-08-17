49e4d998fa18ce7401c58971c47e98f4fd487824
Oihane Crucelaegui <oihanecruce@gmail.com>
2023-08-17 13:54:26 +0200

    [IMP] stock_picking_package_usability: numbered 3 digits

--- stock_picking_package_usability/models/stock_picking.py
+++ stock_picking_package_usability/models/stock_picking.py
@@ -4,19 +4,19 @@ from odoo import _, api, fields, models
 
 
 class StockPicking(models.Model):
-    _inherit = 'stock.picking'
+    _inherit = "stock.picking"
 
     @api.depends("quant_package_ids", "quant_package_ids.shipping_weight")
     def _compute_packages_weight(self):
         for picking in self:
-            picking.packages_weight = (
-                sum(picking.quant_package_ids.mapped('shipping_weight')))
+            picking.packages_weight = sum(
+                picking.quant_package_ids.mapped("shipping_weight")
+            )
 
     @api.depends("quant_package_ids", "quant_package_ids.volume")
     def _compute_packages_volume(self):
         for picking in self:
-            picking.packages_volume = (
-                sum(picking.quant_package_ids.mapped("volume")))
+            picking.packages_volume = sum(picking.quant_package_ids.mapped("volume"))
 
     @api.depends("quant_package_ids")
     def _compute_packages_qty(self):
@@ -26,73 +26,66 @@ class StockPicking(models.Model):
     def _compute_volume_uom_name(self):
         for package in self:
             if package.quant_package_ids:
-                package.volume_uom_name = (
-                    package.quant_package_ids[0].volume_uom_name)
+                package.volume_uom_name = package.quant_package_ids[0].volume_uom_name
 
     @api.depends("packages_qty", "packages_weight", "weight_uom_name")
     def _compute_packages_qty_weight(self):
         for picking in self:
-            picking.packages_qty_weight = u'{} {} {} {}'.format(
-                picking.packages_qty, '-',
+            picking.packages_qty_weight = "{} {} {} {}".format(
+                picking.packages_qty,
+                "-",
                 picking.packages_weight,
-                picking.weight_uom_name)
+                picking.weight_uom_name,
+            )
 
     quant_package_ids = fields.One2many(
-        string='Packages',
-        comodel_name='stock.quant.package',
-        inverse_name='picking_id')
+        string="Packages", comodel_name="stock.quant.package", inverse_name="picking_id"
+    )
     packages_qty_weight = fields.Char(
-        string='# Packages',
-        compute='_compute_packages_qty_weight',
-        store=True)
-    qty_packages = fields.Integer(string='Number of Packages')
+        string="# Packages", compute="_compute_packages_qty_weight", store=True
+    )
+    qty_packages = fields.Integer(string="Number of Packages")
     packages_weight = fields.Float(
-        string='Packages Weight',
-        compute='_compute_packages_weight',
-        store=True)
+        string="Packages Weight", compute="_compute_packages_weight", store=True
+    )
     packages_volume = fields.Float(
-        string='Packages Volume',
-        compute='_compute_packages_volume',
-        store=True)
+        string="Packages Volume", compute="_compute_packages_volume", store=True
+    )
     volume_uom_name = fields.Char(
-        string='Volume UOM',
-        compute='_compute_volume_uom_name',
-        store=True)
+        string="Volume UOM", compute="_compute_volume_uom_name", store=True
+    )
     packages_qty = fields.Integer(
-        string='Packages Quantity',
-        compute='_compute_packages_qty',
-        store=True)
+        string="Packages Quantity", compute="_compute_packages_qty", store=True
+    )
 
     def action_view_package(self):
         context = self.env.context.copy()
-        context.update({'default_picking_id': self.id})
+        context.update({"default_picking_id": self.id})
         return {
-            'name': _("Packages"),
-            'view_mode': 'tree',
-            'res_model': 'stock.quant.package',
-            'domain': [('id', 'in', self.quant_package_ids.ids)],
-            'type': 'ir.actions.act_window',
-            'view_id': self.env.ref(
-                'stock_picking_package_usability.stock_quant_package_view_tree'
-                ).id,
-            'context': context
+            "name": _("Packages"),
+            "view_mode": "tree",
+            "res_model": "stock.quant.package",
+            "domain": [("id", "in", self.quant_package_ids.ids)],
+            "type": "ir.actions.act_window",
+            "view_id": self.env.ref(
+                "stock_picking_package_usability.stock_quant_package_view_tree"
+            ).id,
+            "context": context,
         }
 
     def action_create_package(self):
         self.ensure_one()
-        pack_vals = {
-            'picking_id': self.id}
+        pack_vals = {"picking_id": self.id}
         for i in range(1, self.qty_packages + 1):
-            name = u'{} {} {}{}'.format(self.name, '-', '00', i)
-            pack_vals.update({'name': name})
-            self.env['stock.quant.package'].create(pack_vals)
+            name = "{} {} {:0>3}".format(self.name, "-", i)
+            pack_vals.update({"name": name})
+            self.env["stock.quant.package"].create(pack_vals)
 
     def _put_in_pack(self, move_line_ids, create_package_level=True):
-        move_line_ids = move_line_ids.filtered(
-                    lambda x: not x.result_package_id)
+        move_line_ids = move_line_ids.filtered(lambda x: not x.result_package_id)
         if move_line_ids:
-            result = super(StockPicking, self)._put_in_pack(
-                move_line_ids, create_package_level=True)
-            result.name = u'{} {} {}{}'.format(self.name, '-', '00', len(
-                self.quant_package_ids))
+            result = super()._put_in_pack(move_line_ids, create_package_level=True)
+            result.name = "{} {} {:0>3}".format(
+                self.name, "-", len(self.quant_package_ids)
+            )
             return result
