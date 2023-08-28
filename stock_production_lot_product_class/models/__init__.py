7f7ea72ff07466eba955839ef454a2df9ea88278
Alfredo <alfredodelafuente@avanzosc.es>
2023-08-28 12:45:40 +0200

    [IMP] stock_production_lot_product_class: New object "Type of application" for lots.

--- stock_production_lot_product_class/models/__init__.py
+++ stock_production_lot_product_class/models/__init__.py
@@ -1,3 +1,4 @@
 from . import product_class
 from . import product_template
 from . import stock_production_lot
+from . import stock_production_lot_type_application
---
+++ stock_production_lot_product_class/models/stock_production_lot_type_application.py
@@ -0,0 +1,11 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import fields, models
+
+
+class StockProductionLotTypeApplication(models.Model):
+    _name = "stock.production.lot.type.application"
+    _description = "Type of application"
+
+    name = fields.Char(
+        string="Description", copy=False)
