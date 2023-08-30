49805e1731db3ae3473eb07b25ff54ce9580fa10
Alfredo <alfredodelafuente@avanzosc.es>
2023-08-30 07:26:06 +0200

    [ADD] stock_move_line_weight: In stock move line, and stock move, new field "Weight".

---
+++ stock_move_line_weight/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Stock Move Line Weight",
+    "version": "16.0.1.0.0",
+    "category": "Inventory/Inventory",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": [
+        "product",
+        "stock",
+    ],
+    "excludes": [],
+    "data": [
+        "views/stock_move_line_views.xml",
+        "views/stock_picking_views.xml",
+    ],
+    "installable": True,
+}
