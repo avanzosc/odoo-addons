3f8b4c75a2484466bff50320cd0db4b63cf862fc
Alfredo <alfredodelafuente@avanzosc.es>
2023-08-30 14:59:27 +0200

    [ADD] crm_lead_create_repair: Create repair orders from Initiatives/Opportunities.

---
+++ crm_lead_create_repair/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Crm Lead Create Repair",
+    "version": "14.0.1.1.0",
+    "category": "Inventory/Inventory",
+    "license": "AGPL-3",
+    "author": "Avanzosc",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": [
+        "crm",
+        "repair",
+        "stock"
+    ],
+    "data": [
+        "views/crm_lead_views.xml",
+        "views/repair_order_views.xml",
+    ],
+    "installable": True,
+}
