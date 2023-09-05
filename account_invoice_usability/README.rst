3037e6147b1ec2846bb85b2688f9844769cbbbfa
Alfredo <alfredodelafuente@avanzosc.es>
2023-09-05 12:07:32 +0200

    [ADD] account_invoice_usability: In Journal Item" new field "Product Internal Reference".

--- account_invoice_usability/README.rst
+++ account_invoice_usability/README.rst
@@ -7,7 +7,11 @@ Account Invoice Usability
 =========================
 
 * In Journal Item" new field "Product Internal Reference".
+
 * Search invoices by product, or line description.
+
+* Search invoices by product.
+
 
 Bug Tracker
 ===========
--- account_invoice_usability/models/account_move_line.py
+++ account_invoice_usability/models/account_move_line.py
@@ -13,4 +13,6 @@ class AccountMoveLine(models.Model):
         readonly=True,
         index=True,
         copy=False,
+        string="Product Internal Reference", related="product_id.default_code",
+        store=True, readonly=True, index=True, copy=False
     )
