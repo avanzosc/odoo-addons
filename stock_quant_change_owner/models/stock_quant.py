8c7e124861b021762bbb6eb8e6d4d8dce66d3eb4
Tu Nombre <you@example.com>
2023-09-06 11:45:44 +0200

    [14.0][ADD] stock_quant_change_owner:

---
+++ stock_quant_change_owner/models/stock_quant.py
@@ -0,0 +1,13 @@
+# Copyright 2023 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import _, fields, models
+
+
+class StockQuant(models.Model):
+    _inherit = "stock.quant"
+
+    def action_change_owner(self, owner, quants):
+        for quant in quants:
+            quant = self.env["stock.quant"].search(
+                [("id", "=", quant)], limit=1)
+            quant.sudo().owner_id = owner.id
---
+++ stock_quant_change_owner/wizard/stock_quant_change_owner_wizard.py
@@ -0,0 +1,23 @@
+# Copyright 2023 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import _, models, fields
+from odoo.exceptions import ValidationError
+
+
+class StockQuantChangeOwnerWizard(models.TransientModel):
+    _name = "stock.quant.change.owner.wizard"
+    _description = "Wizard to change the owner"
+
+    owner_id = fields.Many2one(
+        string="New Owner",
+        comodel_name="res.partner",
+        )
+
+    def button_change_owner(self):
+        self.ensure_one()
+        owner = self.owner_id
+        quants = self.env.context["active_ids"]
+        result = self.env["stock.quant"].action_change_owner(
+            owner=owner,
+            quants=quants)
+        return result
