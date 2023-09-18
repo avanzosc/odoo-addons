3a6b430e78743d1399d09e0238d3fe39b3714e9e
Alfredo <alfredodelafuente@avanzosc.es>
2023-09-18 15:17:04 +0200

    [ADD] purchase_penalty_warning: In purchase order new field "Penalty warning".

---
+++ purchase_penalty_warning/models/purchase_order.py
@@ -0,0 +1,14 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import fields, models
+
+
+class PurchaseOrder(models.Model):
+    _inherit = "purchase.order"
+
+    penalty_warning_id = fields.Many2one(
+        string="Penalty warning", comodel_name="penalty.warning",
+        copy=False)
+    penalty_warning_description = fields.Char(
+        string="Penalty warning description",
+        related="penalty_warning_id.description")
