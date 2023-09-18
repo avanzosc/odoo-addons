05463dd1073a79f1ffdd79d833fbac334e3c9830
Alfredo <alfredodelafuente@avanzosc.es>
2023-09-18 12:22:14 +0200

    [ADD] product_old_reference: In products new field "Old reference".

---
+++ product_old_reference/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Product Old Reference",
+    "version": "14.0.1.0.0",
+    "category": "Sales/Sales",
+    "author": "https://avanzosc.es/",
+    "license": "AGPL-3",
+    "depends": [
+        "product",
+    ],
+    "data": [
+        "views/product_template_view.xml",
+        "views/product_product_view.xml",
+    ],
+}
