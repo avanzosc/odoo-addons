9593e67ae63d9173a5c4c5ecf48782642598747b
Berezi <82805447+Berezi@users.noreply.github.com>
2023-09-25 15:42:01 +0200

    [IMP] lot_name_with_stock_qty: force precision rounding(#2112)

--- lot_name_with_stock_qty/models/stock_production_lot.py
+++ lot_name_with_stock_qty/models/stock_production_lot.py
@@ -1,7 +1,7 @@
 # Copyright 2022 Berezi Amubieta - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from odoo import api, models
-from odoo.tools import float_round
+from odoo.tools import float_repr, float_round
 
 
 class StockProductionLot(models.Model):
@@ -14,7 +14,10 @@ class StockProductionLot(models.Model):
         )
         for lot in self:
             name = "{} ({})".format(
-                lot.name, float_round(lot.product_qty, precision_digits=rounding)
+                lot.name,
+                float_repr(
+                    float_round(lot.product_qty, precision_digits=rounding), rounding
+                ),
             )
             result.append((lot.id, name))
         return result
