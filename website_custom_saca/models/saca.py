2865e19803fe18bfaad43659192e7ad2b583fdd3
oihane <oihanecruce@gmail.com>
2023-09-25 11:29:15 +0200

    [FIX] website_custom_saca: max_width and max_heigth only for Image fields (#2131)

--- website_custom_saca/models/saca.py
+++ website_custom_saca/models/saca.py
@@ -1,15 +1,29 @@
-
-from odoo import _, api, fields, models
 from datetime import datetime
+
+from odoo import fields, models
 
 
 class SacaLine(models.Model):
     _inherit = "saca.line"
 
-    signature_driver = fields.Binary('Signature Driver', help='Signature received through the portal.', copy=False, attachment=True, max_width=1024, max_height=1024)
-    date_signature_driver = fields.Date('Date signature driver')
-    signature_farm = fields.Binary('Signature Farm', help='Signature received through the portal.', copy=False, attachment=True, max_width=1024, max_height=1024)
-    date_signature_farm = fields.Date('Date signature farm')
+    signature_driver = fields.Image(
+        string="Driver's Signature",
+        help="Signature received through the portal.",
+        copy=False,
+        attachment=True,
+        max_width=1024,
+        max_height=1024,
+    )
+    date_signature_driver = fields.Date("Date signature driver")
+    signature_farm = fields.Image(
+        string="Farm's Signature Farm",
+        help="Signature received through the portal.",
+        copy=False,
+        attachment=True,
+        max_width=1024,
+        max_height=1024,
+    )
+    date_signature_farm = fields.Date("Date signature farm")
 
     ticket_farm_attachment_id = fields.Many2one(
         string="Farm ticket attachment",
@@ -26,9 +40,9 @@ class SacaLine(models.Model):
         return fname
 
     def has_to_be_signed(self, sign_by=None):
-        if sign_by == 'farm':
+        if sign_by == "farm":
             return not self.signature_farm
-        if sign_by == 'driver':
+        if sign_by == "driver":
             return not self.signature_driver
         return not self.signature_driver and not self.signature_farm
 
@@ -43,20 +57,20 @@ class SacaLine(models.Model):
         self.date_signature_driver = None
 
     def action_send_saca_mail(self):
-        mail_template = self.env.ref('website_custom_saca.saca_pdf_send')
+        mail_template = self.env.ref("website_custom_saca.saca_pdf_send")
         mail_template.send_mail(self.id, force_send=True)
 
     def set_timesheet_start_stop(self, stamp_type, timesheet_id):
         self.ensure_one()
         timesheet = self.timesheet_ids.filtered(lambda t: t.id == timesheet_id)
         now = datetime.today().strftime("%H:%M:%S")
-        if stamp_type == 'btn_start':
+        if stamp_type == "btn_start":
             timesheet.time_start = self.conv_time_float(now)
         else:
             timesheet.time_stop = self.conv_time_float(now)
 
     def conv_time_float(self, value):
-        vals = value.split(':')
+        vals = value.split(":")
         t, hours = divmod(float(vals[0]), 24)
         t, minutes = divmod(float(vals[1]), 60)
         minutes = minutes / 60.0
