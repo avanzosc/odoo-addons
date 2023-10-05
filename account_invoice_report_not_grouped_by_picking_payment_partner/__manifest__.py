a137ef66d6be592031e2e2b9bf3289bca1fae12e
Alfredo <alfredodelafuente@avanzosc.es>
2023-10-05 14:55:38 +0200

    [ADD] account_invoice_report_not_grouped_by_picking_payment_partner: Report invoice not grouped by picking.

---
+++ account_invoice_report_not_grouped_by_picking_payment_partner/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Account Invoice Report Not Grouped By Picking Payment Partner",
+    "version": "14.0.1.0.0",
+    "author": "Avanzosc",
+    "category": "Invoices & Payments",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "account_invoice_report_not_grouped_by_picking",
+        "account_payment_partner",
+    ],
+    "data": [
+        "reports/account_invoice_report.xml",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": True,
+}
