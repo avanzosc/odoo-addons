a9ba71ee91de6d28560b72253bc39cf621b17820
Alfredo <alfredodelafuente@avanzosc.es>
2023-10-05 14:59:07 +0200

    [ADD] account_invoice_report_not_grouped_by_picking_ticketbai: Report invoice not grouped by picking.

---
+++ account_invoice_report_not_grouped_by_picking_ticketbai/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Account Invoice Report Not Grouped By Picking Ticketbai",
+    "version": "14.0.1.0.0",
+    "author": "Avanzosc",
+    "category": "Invoices & Payments",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "account_invoice_report_not_grouped_by_picking",
+        "l10n_es_ticketbai",
+    ],
+    "data": [
+        "reports/account_invoice_report.xml",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": True,
+}
