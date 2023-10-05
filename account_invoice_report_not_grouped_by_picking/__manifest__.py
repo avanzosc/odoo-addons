df869612dc41f334563c70341423706ac22b8dcc
Alfredo <alfredodelafuente@avanzosc.es>
2023-10-05 14:46:50 +0200

    [ADD] account_invoice_report_not_grouped_by_picking: Report invoice not grouped by picking.

---
+++ account_invoice_report_not_grouped_by_picking/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Account Invoice Report Not Grouped By Picking",
+    "version": "14.0.1.0.0",
+    "author": "Avanzosc",
+    "category": "Invoices & Payments",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "web",
+        "account",
+        "sale",
+        "stock",
+        "sale_stock",
+        "stock_picking_invoice_link"
+    ],
+    "data": [
+        "reports/account_invoice_report.xml",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+}
