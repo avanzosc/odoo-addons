1f5be9f9252794a068bdeeaa74fbdd450d78c168
Berezi <82805447+Berezi@users.noreply.github.com>
2023-10-19 15:12:02 +0200

    [IMP] custom_purchase_import_wizard: (#2059)

---
+++ custom_purchase_import_wizard/models/purchase_requisition.py
@@ -0,0 +1,18 @@
+# Copyright 2022 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import models
+
+
+class PurchaseRequisition(models.Model):
+    _inherit = "purchase.requisition"
+
+    def name_get(self):
+        super(PurchaseRequisition, self).name_get()
+        result = []
+        for req in self:
+            name = req.name
+            if req.origin:
+                name = u'{} - {}'.format(name, req.origin)
+            result.append((req.id, name))
+        return result
