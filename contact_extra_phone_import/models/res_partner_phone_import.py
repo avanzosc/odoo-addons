62008a85fe728915f13f856141c5c8663f9d41b4
oihane <oihanecruce@gmail.com>
2023-10-19 13:13:20 +0200

    [IMP] contact_extra_phone_import: allow to validate / process lines individually (#2157)

--- contact_extra_phone_import/models/res_partner_phone_import.py
+++ contact_extra_phone_import/models/res_partner_phone_import.py
@@ -1,10 +1,11 @@
 # Copyright 2022 Berezi Amubieta - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import _, api, fields, models
-from odoo.addons.base_import_wizard.models.base_import import convert2str
+from odoo import _, fields, models
 from odoo.models import expression
 from odoo.tools.safe_eval import safe_eval
+
+from odoo.addons.base_import_wizard.models.base_import import convert2str
 
 
 class ResPartnerPhoneImport(models.Model):
@@ -22,7 +23,7 @@ class ResPartnerPhoneImport(models.Model):
     company_id = fields.Many2one(
         string="Company",
         comodel_name="res.company",
-        required=True)
+    )
 
     def _get_line_values(self, row_values=False):
         self.ensure_one()
@@ -34,24 +35,22 @@ class ResPartnerPhoneImport(models.Model):
             contact_phone = row_values.get("Phone", "")
             contact_email = row_values.get("Email", "")
             log_info = ""
-            if not contact_name and not contact_code:
-                return {}
-            values.update(
-                {
-                    "contact_name": contact_name.title(),
-                    "contact_code": convert2str(contact_code),
-                    "contact_description": convert2str(contact_description),
-                    "contact_phone": convert2str(contact_phone),
-                    "contact_email": convert2str(contact_email),
-                    "log_info": log_info,
-                }
-            )
+            if contact_name or contact_code:
+                values.update(
+                    {
+                        "contact_name": contact_name.title(),
+                        "contact_code": convert2str(contact_code),
+                        "contact_description": convert2str(contact_description),
+                        "contact_phone": convert2str(contact_phone),
+                        "contact_email": convert2str(contact_email),
+                        "log_info": log_info,
+                    }
+                )
         return values
 
     def _compute_res_partner_count(self):
         for record in self:
-            record.res_partner_count = len(
-                record.mapped("import_line_ids.contact_id"))
+            record.res_partner_count = len(record.mapped("import_line_ids.contact_id"))
 
     def button_open_res_partner(self):
         self.ensure_one()
@@ -59,7 +58,8 @@ class ResPartnerPhoneImport(models.Model):
         action = self.env.ref("contacts.action_contacts")
         action_dict = action.read()[0] if action else {}
         domain = expression.AND(
-            [[("id", "in", contacts.ids)], safe_eval(action.domain or "[]")])
+            [[("id", "in", contacts.ids)], safe_eval(action.domain or "[]")]
+        )
         action_dict.update({"domain": domain})
         return action_dict
 
@@ -69,39 +69,15 @@ class ResPartnerPhoneImportLine(models.Model):
     _inherit = "base.import.line"
     _description = "Lines to import contacts extra phone"
 
-    @api.model
-    def _get_selection_contact_type(self):
-        return self.env["res.partner"].fields_get(
-            allfields=["type"])["type"]["selection"]
-
-    def default_contact_type(self):
-        default_dict = self.env["res.partner"].default_get(["type"])
-        return default_dict.get("type")
-
     import_id = fields.Many2one(
         comodel_name="res.partner.phone.import",
     )
-    action = fields.Selection(
-        string="Action",
-        selection=[
-            ("update", "Update"),
-            ("nothing", "Nothing"),
-        ],
-        default="nothing",
-        states={"done": [("readonly", True)]},
-        copy=False,
-        required=True,
-    )
-    contact_id = fields.Many2one(
-        string="Contact",
-        comodel_name="res.partner")
+    contact_id = fields.Many2one(string="Contact", comodel_name="res.partner")
     contact_name = fields.Char(
-        string="Contact Name",
         states={"done": [("readonly", True)]},
         copy=False,
     )
     contact_code = fields.Char(
-        string="Contact Code",
         states={"done": [("readonly", True)]},
         copy=False,
     )
@@ -121,65 +97,29 @@ class ResPartnerPhoneImportLine(models.Model):
         copy=False,
     )
 
-    def action_validate(self):
-        super().action_validate()
-        line_values = []
-        for line in self.filtered(lambda l: l.state != "done"):
-            log_info = ""
-            contact = False
-            if line.contact_name or line.contact_code:
-                contact, log_info_contact = line._check_contact()
-                if log_info_contact:
-                    log_info += log_info_contact
-            state = "error" if log_info else "pass"
-            action = "nothing"
-            if state != "error":
-                action = "update"
-            update_values = {
+    def _action_validate(self):
+        update_values = super()._action_validate()
+        if self.contact_name or self.contact_code:
+            contact, log_info = self._check_contact()
+        state = "error" if log_info else "pass"
+        update_values.update(
+            {
                 "contact_id": contact and contact.id,
                 "log_info": log_info,
                 "state": state,
-                "action": action,
-                }
-            line_values.append(
-                (
-                    1,
-                    line.id,
-                    update_values,
-                )
-            )
-        return line_values
+            }
+        )
+        return update_values
 
-    def action_process(self):
-        super().action_validate()
-        line_values = []
-        for line in self.filtered(lambda l: l.state not in ("error", "done")):
-            if line.action == "update":
-                contact, log_info = line._check_contact()
-                if contact:
-                    contact.extra_phone_ids = [(0, 0, {
-                        "description": line.contact_description,
-                        "phone": line.contact_phone,
-                        "email": line.contact_email})]
-            else:
-                continue
-            state = "error" if log_info else "done"
-            line.write({
-                "contact_id": contact.id,
-                "log_info": log_info,
-                "state": state})
-            line_values.append(
-                (
-                    1,
-                    line.id,
-                    {
-                        "contact_id": contact.id,
-                        "log_info": log_info,
-                        "state": state,
-                    },
-                )
-            )
-        return line_values
+    def _action_process(self):
+        update_values = super()._action_process()
+        self._create_extra_phone()
+        update_values.update(
+            {
+                "state": "done",
+            }
+        )
+        return update_values
 
     def _check_contact(self):
         self.ensure_one()
@@ -187,19 +127,47 @@ class ResPartnerPhoneImportLine(models.Model):
         if self.contact_id:
             return self.contact_id, log_info
         contact_obj = self.env["res.partner"]
-        search_domain = [("company_id", "=", self.import_id.company_id.id)]
+        search_domain = [
+            "|",
+            ("company_id", "=", self.import_id.company_id.id),
+            ("company_id", "=", False),
+        ]
         if self.contact_code:
-            search_domain = [
-                ("ref", "like", self.contact_code)]
+            search_domain = expression.AND(
+                [
+                    [
+                        ("ref", "like", self.contact_code),
+                    ],
+                    search_domain,
+                ]
+            )
         else:
-            search_domain = [
-                ("company_id", "=", self.import_id.company_id.id),
-                ("name", "=", self.contact_name)]
+            search_domain = expression.AND(
+                [
+                    [
+                        ("name", "=", self.contact_name),
+                    ],
+                    search_domain,
+                ]
+            )
         contacts = contact_obj.search(search_domain)
         if not contacts:
-            contacts = False
-            log_info = _("Error: No contact found.")
+            log_info = _("No contact found.")
         elif len(contacts) > 1:
             contacts = False
-            log_info = _("Error: More than one contact found.")
+            log_info = _("More than one contact found.")
         return contacts and contacts[:1], log_info
+
+    def _create_extra_phone(self):
+        self.ensure_one()
+        self.sudo().env["extra.phone"].create(self._extra_phone_values())
+        return True
+
+    def _extra_phone_values(self):
+        self.ensure_one()
+        return {
+            "partner_id": self.contact_id.id,
+            "description": self.contact_description,
+            "phone": self.contact_phone,
+            "email": self.contact_email,
+        }
