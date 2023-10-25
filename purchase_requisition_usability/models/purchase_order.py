60bb2d7541ab27ad4d023f203f80781120d74f1a
Tu Nombre <you@example.com>
2023-10-25 12:56:49 +0200

    [14.0][ADD] purchase_requisition_usability: Requisition and requisition origin fields in purchase order tree view.

---
+++ purchase_requisition_usability/models/purchase_order.py
@@ -0,0 +1,13 @@
+# Copyright 2023 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import fields, models
+
+
+class PurchaseOrder(models.Model):
+    _inherit = "purchase.order"
+
+    requisition_origin = fields.Char(
+        string="Requisition Origin",
+        related="requisition_id.origin",
+        store=True
+    )
