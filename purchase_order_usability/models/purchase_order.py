012b7b1a5f3ebd808becfc6eda35997834fb7350
Tu Nombre <you@example.com>
2023-10-25 12:52:23 +0200

    [14.0][ADD] purchase_order_usability: Approve date, planned date and effective date in purchase order tree view.

---
+++ purchase_order_usability/models/purchase_order.py
@@ -0,0 +1,14 @@
+# Copyright 2023 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import fields, models
+
+
+class PurchaseOrder(models.Model):
+    _inherit = "purchase.order"
+
+    warehouse_id = fields.Many2one(
+        string="Warehouse",
+        comodel_name="stock.warehouse",
+        related="picking_type_id.warehouse_id",
+        store=True
+    )
