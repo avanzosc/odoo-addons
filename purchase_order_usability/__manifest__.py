2c535a91cf2692f790f064980ebe7d30d95a9339
Tu Nombre <you@example.com>
2023-10-25 12:52:23 +0200

    [14.0][ADD] purchase_order_usability: Approve date, planned date and effective date in purchase order tree view.

---
+++ purchase_order_usability/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2023 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Purchase Order Usability",
+    'version': '14.0.1.0.0',
+    "author": "Avanzosc",
+    "category": "Inventory",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "purchase_stock",
+    ],
+    "data": [
+        "views/purchase_order_view.xml",
+    ],
+    "license": "AGPL-3",
+    'installable': True,
+}
