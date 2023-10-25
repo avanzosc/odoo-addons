fdfbfc1948bf9e8f75c1b9b2e5bf9e8a2cbf0815
Alfredo <alfredodelafuente@avanzosc.es>
2023-10-25 07:27:47 +0200

    [IMP] product_packaging_palet: En lineas de venta cambiar "product_packaging_id" por "product_packaging".

--- product_packaging_palet/models/sale_order_line.py
+++ product_packaging_palet/models/sale_order_line.py
@@ -17,12 +17,11 @@ class SaleOrderLine(models.Model):
     no_update_palet_qty = fields.Boolean(
         string="No update palet_qty", default=False)
 
-    @api.onchange("product_packaging_id")
-    def _onchange_product_packaging_id(self):
-        self.no_update_palet_qty = True
-        result = super(SaleOrderLine, self)._onchange_product_packaging_id()
-        if self.product_packaging_id and self.product_packaging_id.palet_id.id:
-            self.palet_id = self.product_packaging_id.palet_id.id
+    @api.onchange("product_packaging")
+    def _onchange_product_packaging(self):
+        result = super(SaleOrderLine, self)._onchange_product_packaging()
+        if self.product_packaging and self.product_packaging.palet_id.id:
+            self.palet_id = self.product_packaging.palet_id.id
         else:
             self.palet_id = False
             self.palet_qty = 0
@@ -51,17 +50,17 @@ class SaleOrderLine(models.Model):
 
     def _get_palet_qty(self):
         return (self.product_uom_qty /
-                (self.product_packaging_id.qty *
-                 self.product_packaging_id.palet_qty))
+                (self.product_packaging.qty *
+                 self.product_packaging.palet_qty))
 
     @api.onchange("palet_qty")
     def _onchange_palet_qty(self):
         if (not self.no_update_palet_qty and self.product_id and
-            self.product_packaging_id and self.palet_id and
+            self.product_packaging and self.palet_id and
                 self.product_id.packaging_ids and self.palet_qty):
             line = self.product_id.packaging_ids.filtered(
                 lambda x: x.name and x.palet_id and
-                x.name == self.product_packaging_id.name and
+                x.name == self.product_packaging.name and
                 x.palet_id == self.palet_id)
             if line and len(line) == 1:
                 self.product_packaging_qty = (
