3e31532e2ee7cac5fdcf92e9640c37cb3f8d7c90
Alfredo <alfredodelafuente@avanzosc.es>
2023-10-30 09:28:59 +0100

    [ADD] account_invoice_report_without_tax_breakdown: In invoice report, do not show tax breakdown in invoice totals.

---
+++ account_invoice_report_without_tax_breakdown/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Account Invoice Report Without Tax Breakdown",
+    'version': '14.0.1.0.0',
+    "category": "Accounting/Accounting",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "reports/account_invoice_report.xml",
+    ],
+    "installable": True,
+}
