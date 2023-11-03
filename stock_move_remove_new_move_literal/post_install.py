514201b860b4dc031b1af1b19b27a8a19bd75bdd
Alfredo <alfredodelafuente@avanzosc.es>
2023-11-03 08:07:29 +0100

    [ADD] stock_move_remove_new_move_literal: Remove "New move:" literal in stock move name.

---
+++ stock_move_remove_new_move_literal/post_install.py
@@ -0,0 +1,20 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+from odoo import SUPERUSER_ID, api
+
+
+def stock_move_remove_literal(cr, registry):
+    with api.Environment.manage():
+        env = api.Environment(cr, SUPERUSER_ID, {})
+        cond = ["|", ("name", "ilike", "Nuevo movimiento:%"),
+                ("name", "ilike", "New Move:%")]
+        moves = env["stock.move"].search(cond)
+        for move in moves:
+            name = False
+            if "Nuevo movimiento:" in move.name:
+                name = move.name.replace("Nuevo movimiento:", "")
+            if "New Move:" in move.name:
+                name = move.name.replace("New Move:", "")
+            if name:
+                move.name = name
+    return
