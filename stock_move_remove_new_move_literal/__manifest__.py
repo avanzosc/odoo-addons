a5e5f9e3712bf426ba5b9d6611f38cb1e8b3326c
Alfredo <alfredodelafuente@avanzosc.es>
2023-11-03 08:07:29 +0100

    [ADD] stock_move_remove_new_move_literal: Remove "New move:" literal in stock move name.

---
+++ stock_move_remove_new_move_literal/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Stock Move Remove New Move Literal",
+    "version": "14.0.1.0.0",
+    "category": "Inventory/Inventory",
+    "author": "AvanzOSC",
+    "license": "AGPL-3",
+    "website": "https://www.avanzosc.es",
+    "depends": [
+        "stock",
+    ],
+    "data": [
+    ],
+    "post_init_hook": "stock_move_remove_literal",
+    "installable": True,
+}
