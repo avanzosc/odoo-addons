229b85204f26df3672cb6ef4d7685b17c8c43c0c
Alfredo <alfredodelafuente@avanzosc.es>
2023-11-09 10:55:47 +0100

    [ADD] warehouse_analysis_report_group_visible: Group for view "Warehouse Analysis" menu option.

---
+++ warehouse_analysis_report_group_visible/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Warehouse Analysis Report Group Visible",
+    "version": "14.0.1.0.0",
+    "category": "Hidden",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://www.avanzosc.es",
+    "depends": [
+        "stock_enterprise",
+        "product_cost_visible"
+    ],
+    "data": [
+        "views/stock_report_views.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
