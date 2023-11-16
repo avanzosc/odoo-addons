a3852ac4f19f6ab6da5db7abbdf8332904499907
Alfredo <alfredodelafuente@avanzosc.es>
2023-11-16 08:56:44 +0100

    [ADD] invoice_mail_optional_follower_notification: In invoices, in the send email wizard, new field to send email to followers.

---
+++ invoice_mail_optional_follower_notification/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Invoice Mail Optional Follower Notification",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "category": "Invoicing Management",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "account",
+        "mail",
+    ],
+    "data": [
+        "wizard/account_invoice_send_view.xml",
+    ],
+}
