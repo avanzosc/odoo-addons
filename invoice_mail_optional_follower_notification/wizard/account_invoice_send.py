1a69579299cc380f7e2e522bddb3c7bc04cfc3fb
Alfredo <alfredodelafuente@avanzosc.es>
2023-11-16 08:56:44 +0100

    [ADD] invoice_mail_optional_follower_notification: In invoices, in the send email wizard, new field to send email to followers.

---
+++ invoice_mail_optional_follower_notification/wizard/account_invoice_send.py
@@ -0,0 +1,27 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+from odoo import models, fields, api
+
+
+class AccountInvoiceSend(models.TransientModel):
+    _inherit = "account.invoice.send"
+
+    notify_followers = fields.Boolean(default=True)
+
+    @api.multi
+    def send_mail(self, auto_commit=False):
+        ctx = self.env.context.copy()
+        for wizard in self:
+            ctx['notify_followers'] = wizard.notify_followers
+            wizard = wizard.with_context(ctx)
+            super(AccountInvoiceSend, wizard).send_mail(
+                auto_commit=auto_commit)
+        return {'type': 'ir.actions.act_window_close'}
+
+    @api.multi
+    def send_and_print_action(self):
+        self.ensure_one()
+        ctx = self.env.context.copy()
+        ctx['notify_followers'] = self.notify_followers
+        wizard = self.with_context(ctx)
+        return super(AccountInvoiceSend, wizard).send_and_print_action()
