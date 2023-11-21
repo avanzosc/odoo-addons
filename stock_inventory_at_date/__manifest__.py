b1a07a8fbcf117c89e023706923b0da2a0fb8c7a
Tu Nombre <you@example.com>
2023-11-21 11:56:34 +0100

    [14.0][ADD] stock_inventory_at_date:

---
+++ stock_inventory_at_date/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2023 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Stock Inventory At Date",
+    "version": "14.0.1.0.0",
+    "category": "Hidden/Tools",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": [
+        "stock_account",
+    ],
+    "data": [
+        "views/stock_inventory_line_view.xml",
+        ],
+    "installable": True,
+}
