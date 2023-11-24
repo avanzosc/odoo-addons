7e7b5c0e90f6a5dbf61ec6063fda190d91fec061
Tu Nombre <you@example.com>
2023-11-24 12:16:21 +0100

    [14.0][FIX] stock_picking_return_usability:

--- stock_picking_return_usability/wizard/stock_picking_return.py
+++ stock_picking_return_usability/wizard/stock_picking_return.py
@@ -8,21 +8,38 @@ class ReturnPicking(models.TransientModel):
     _inherit = 'stock.return.picking'
 
     def _prepare_move_default_values(self, return_line, new_picking):
-        result = super(ReturnPicking, self)._prepare_move_default_values(return_line, new_picking)
-        if self.picking_id and self.picking_id.picking_type_id and self.picking_id.picking_type_id.return_picking_type_id:
+        result = super(ReturnPicking, self)._prepare_move_default_values(
+            return_line, new_picking)
+        if self.picking_id and (
+            self.picking_id.picking_type_id) and (
+                self.picking_id.picking_type_id.return_picking_type_id):
             result.update({
-                "location_id": self.picking_id.picking_type_id.return_picking_type_id.default_location_src_id.id,
-                "location_dest_id": self.picking_id.picking_type_id.return_picking_type_id.default_location_dest_id.id})
+                "location_id": self.picking_id.picking_type_id
+                .return_picking_type_id.default_location_src_id.id or (
+                    return_line.move_id.location_dest_id.id),
+                "location_dest_id": self.picking_id.picking_type_id
+                .return_picking_type_id.default_location_dest_id.id or (
+                    self.location_id.id or return_line.move_id.location_id.id)
+                })
         return result
 
     def _create_returns(self):
-        new_picking, picking_type_id = super(ReturnPicking, self)._create_returns()
+        new_picking, picking_type_id = super(
+            ReturnPicking, self)._create_returns()
         return_picking = self.env["stock.picking"].browse(new_picking)
-        if self.picking_id and self.picking_id.picking_type_id and self.picking_id.picking_type_id.return_picking_type_id:
+        if self.picking_id and (
+            self.picking_id.picking_type_id) and (
+                self.picking_id.picking_type_id.return_picking_type_id):
             return_picking.write({
-                "location_id": self.picking_id.picking_type_id.return_picking_type_id.default_location_src_id.id,
-                "location_dest_id": self.picking_id.picking_type_id.return_picking_type_id.default_location_dest_id.id})
-        if self.picking_id and self.picking_id.picking_type_id and self.picking_id.picking_type_id.retun_picking_draft:
+                "location_id": self.picking_id.picking_type_id
+                .return_picking_type_id.default_location_src_id.id or self
+                .picking_id.location_dest_id.id,
+                "location_dest_id": self.picking_id.picking_type_id
+                .return_picking_type_id.default_location_dest_id.id or self
+                .location_id.id})
+        if self.picking_id and (
+            self.picking_id.picking_type_id) and (
+                self.picking_id.picking_type_id.retun_picking_draft):
             for move in return_picking.move_ids_without_package:
                 move.state = "draft"
         return new_picking, picking_type_id
