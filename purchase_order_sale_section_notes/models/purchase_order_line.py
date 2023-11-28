20af24362df2fef95e7dc306eec854e0a856549b
Alfredo <alfredodelafuente@avanzosc.es>
2023-11-28 09:32:04 +0100

    [ADD] purchase_order_sale_section_notes: Drag notes and sales sections to purchases.

---
+++ purchase_order_sale_section_notes/models/purchase_order_line.py
@@ -0,0 +1,19 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+from odoo import api, models
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = "purchase.order.line"
+
+    @api.model
+    def _prepare_purchase_order_line_from_procurement(self, product_id,
+                                                      product_qty, product_uom,
+                                                      company_id, values, po):
+        vals = super(PurchaseOrderLine,
+                     self)._prepare_purchase_order_line_from_procurement(
+                         product_id, product_qty, product_uom, company_id,
+                         values, po)
+        if "sequence" in values:
+            vals["sequence"] = values.get("sequence")
+        return vals
