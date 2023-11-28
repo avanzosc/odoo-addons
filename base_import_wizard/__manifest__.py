0ef3b23478ccb159992b451494a74798d50ea439
oihane <oihanecruce@gmail.com>
2023-11-28 12:41:41 +0100

    [IMP] base_import_wizard: backport new methods from 14.0 (#2248)

--- base_import_wizard/__manifest__.py
+++ base_import_wizard/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Base Import Wizard",
-    "version": "12.0.1.0.0",
+    "version": "12.0.2.0.0",
     "category": "Hidden/Dependency",
     "license": "AGPL-3",
     "author": "AvanzOSC",
