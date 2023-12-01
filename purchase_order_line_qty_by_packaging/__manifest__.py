b4fb6c060774f85c3243c25cbfd6fd75702ec220
Tu Nombre <you@example.com>
2023-12-01 12:42:30 +0100

    [14.0][ADD] purchase_order_line_qty_by_packaging:

---
+++ purchase_order_line_qty_by_packaging/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2023 Berezi Amubieta - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Purchase Order Line Qty By Packaging",
+    "version": "14.0.1.1.0",
+    "category": "Warehouse Management",
+    "license": "AGPL-3",
+    "author": "Avanzosc",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": [
+        "purchase_order_line_packaging_qty",
+        "stock_move_qty_by_packaging",
+    ],
+    "data": [
+    ],
+    "installable": True,
+}
