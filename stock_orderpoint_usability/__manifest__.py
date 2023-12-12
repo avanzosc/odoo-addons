d401ca6ec99dc4a805dc46224d3cadec7a0006a4
Unai Beristain <unaiberistain@avanzosc.es>
2023-12-12 16:46:51 +0100

    [MIG] stock_orderpoint_usability: Migration to 16.0

--- stock_orderpoint_usability/__manifest__.py
+++ stock_orderpoint_usability/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
     "name": "Stock Orderpoint Usability",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "depends": [
         "stock",
