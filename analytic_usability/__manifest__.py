e0d4ed5c78a45716e68e910ff4e0c114b9011a5f
Unai Beristain <unaiberistain@avanzosc.es>
2023-12-13 17:14:23 +0100

    [MIG] analytic_usability: Migration to 16.0

--- analytic_usability/__manifest__.py
+++ analytic_usability/__manifest__.py
@@ -3,11 +3,10 @@
 
 {
     "name": "Analytic Usability",
-<<<<<<< HEAD
     "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "AvanzOSC",
-    "website": "https://github.com/avanzosc/odoo-addons",
+    "website": "http://www.avanzosc.es",
     "category": "Tools",
     "depends": [
         "analytic",
@@ -18,18 +17,6 @@
         "views/account_analytic_account_view.xml",
         "views/account_analytic_line_view.xml",
         "views/res_config_view.xml",
-=======
-    "version": "12.0.1.0.0",
-    "license": "AGPL-3",
-    "author": "AvanzOSC",
-    "website": "http://www.avanzosc.es",
-    "category": "Tools",
-    "depends": [
-        "analytic",
-    ],
-    "data": [
-        "views/account_analytic_line_view.xml",
->>>>>>> [ADD] new module analytic_usability
     ],
     "installable": True,
 }
