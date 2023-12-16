810974292cec82f986b43a74ecb23c0b42ecf61d
Unai Beristain <unaiberis@gmail.com>
2023-12-16 15:43:20 +0100

    [IMP] purchase_partner_company: pre-commit execution

--- stock_lot_purchase_info/__manifest__.py
+++ stock_lot_purchase_info/__manifest__.py
@@ -6,7 +6,7 @@
     "category": "Inventory/Inventory",
     "license": "AGPL-3",
     "author": "https://github.com/avanzosc/odoo-addons",
-    "website": "http://www.avanzosc.es",
+    "website": "https://github.com/avanzosc/odoo-addons",
     "depends": [
         "purchase",
         "stock",
