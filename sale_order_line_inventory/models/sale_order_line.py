1636ef9caf0d695f65e07f8ae3d12d3b74d49db3
Unai Beristain <unaiberistain@avanzosc.es>
2023-12-19 15:55:56 +0100

    [IMP] sale_order_line_inventory: pre-commit stuff

--- sale_order_line_inventory/models/sale_order_line.py
+++ sale_order_line_inventory/models/sale_order_line.py
@@ -1,7 +1,8 @@
 # Copyright 2020 Alfredo de la  Fuente - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import models, fields, api
 import json
+
+from odoo import api, fields, models
 
 
 class SaleOrderLine(models.Model):
@@ -9,14 +10,13 @@ class SaleOrderLine(models.Model):
 
     @api.multi
     def show_product_inventory(self):
-        history_obj = self.env['stock.quantity.history']
-        vals = {'compute_at_date': 0,
-                'date': fields.Datetime.now()}
+        history_obj = self.env["stock.quantity.history"]
+        vals = {"compute_at_date": 0, "date": fields.Datetime.now()}
         wiz = history_obj.create(vals)
         result = wiz.open_table()
-        context = result.get('context')
-        json_acceptable_string = context.replace("'", "\"")
+        context = result.get("context")
+        json_acceptable_string = context.replace("'", '"')
         context = json.loads(json_acceptable_string)
-        context['search_default_product_id'] = self.product_id.id
-        result['context'] = str(context)
+        context["search_default_product_id"] = self.product_id.id
+        result["context"] = str(context)
         return result
