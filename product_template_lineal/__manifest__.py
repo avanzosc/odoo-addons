e9c17368db61860160601bec3972f0c08cc686c8
Unai Beristain <unaiberistain@avanzosc.es>
2023-12-20 17:15:58 +0100

    [IMP] product_template_lineal: pre-commit stuff

--- product_template_lineal/__manifest__.py
+++ product_template_lineal/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Product Template Lineal",
-    'version': '14.0.1.0.0',
+    "version": "14.0.1.0.0",
     "author": "Avanzosc",
     "website": "http://www.avanzosc.es",
     "category": "Inventory",
@@ -18,5 +18,5 @@
         "views/stock_move_views.xml",
     ],
     "license": "AGPL-3",
-    'installable': True,
+    "installable": True,
 }
