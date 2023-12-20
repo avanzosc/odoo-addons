827fc18740909057ab6bad0f77b68cf62b5b7164
Unai Beristain <unaiberistain@avanzosc.es>
2023-12-20 17:16:11 +0100

    [MIG] product_template_lineal: Migration to 16.0

--- product_template_lineal/__manifest__.py
+++ product_template_lineal/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Product Template Lineal",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Avanzosc",
     "website": "http://www.avanzosc.es",
     "category": "Inventory",
