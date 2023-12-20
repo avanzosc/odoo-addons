0efb8f3fa2429601c4bcc401560b84feb1714499
Unai Beristain <unaiberistain@avanzosc.es>
2023-12-20 17:13:53 +0100

    [IMP] product_storable_generate_task_estimated_hours: pre-commit stuff

--- product_storable_generate_task_estimated_hours/models/sale_order.py
+++ product_storable_generate_task_estimated_hours/models/sale_order.py
@@ -7,19 +7,22 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     def _action_confirm(self):
-        result = super(SaleOrder, self)._action_confirm()
+        result = super()._action_confirm()
         self.treatment_storable_product_generate_task()
         return result
 
     def treatment_storable_product_generate_task(self):
-        result = super(
-            SaleOrder, self).treatment_storable_product_generate_task()
+        result = super().treatment_storable_product_generate_task()
         for order in self:
             lines = order.order_line.filtered(
-                lambda x: x.product_id.service_tracking != 'no' and
-                x.state == 'sale' and not x.is_expense and x.task_id)
+                lambda x: x.product_id.service_tracking != "no"
+                and x.state == "sale"
+                and not x.is_expense
+                and x.task_id
+            )
             for line in lines:
                 if line.product_id.service_hour:
                     line.task_id.planned_hours = (
-                        line.product_id.service_hour * line.product_uom_qty)
+                        line.product_id.service_hour * line.product_uom_qty
+                    )
         return result
