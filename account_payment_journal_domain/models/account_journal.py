1a04245303f32b6c87ee6054fe3874f93365cb0c
Tu Nombre <you@example.com>
2023-12-28 14:27:00 +0100

    [14.0][ADD] account_payment_journal_domain:

---
+++ account_payment_journal_domain/models/account_journal.py
@@ -0,0 +1,12 @@
+# Copyright 2023 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import _, api, fields, models
+
+
+class AccountJournal(models.Model):
+    _inherit = "account.journal"
+
+    payment_journal = fields.Boolean(
+        string="Payment Journal",
+        default=False
+    )
