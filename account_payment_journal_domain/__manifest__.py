48d01326e25c7300ab5de08051871d9f9058487c
Tu Nombre <you@example.com>
2023-12-28 14:27:00 +0100

    [14.0][ADD] account_payment_journal_domain:

---
+++ account_payment_journal_domain/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2023 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Account Payment Journal Domain",
+    'version': '14.0.1.0.0',
+    "category": "Analytic",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "views/account_payment_view.xml",
+        "views/account_journal_view.xml",
+    ],
+    'installable': True,
+}
