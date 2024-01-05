8247d2e36590e095183fd1971b81e8dde5270bfe
Berezi <82805447+Berezi@users.noreply.github.com>
2024-01-05 10:09:57 +0100

    [ADD] custom_breeding_report: Breeding reports. (#2050)

---
+++ custom_breeding_report/models/lineage_percentage.py
@@ -0,0 +1,286 @@
+# Copyright 2023 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import api, fields, models
+
+
+class LineagePercentage(models.Model):
+    _inherit = "lineage.percentage"
+
+    entry_date = fields.Date(
+        string="Entry Date",
+        related="batch_id.entry_date",
+        store=True)
+    cleaned_date = fields.Date(
+        string="Cleaned Date",
+        related="batch_id.cleaned_date",
+        store=True)
+    warehouse_id = fields.Many2one(
+        string="Farm",
+        comodel_name="stock.warehouse",
+        related="batch_id.warehouse_id",
+        store=True)
+    city = fields.Char(
+        string="City",
+        related="batch_id.city",
+        store=True)
+    chick_entry_qty = fields.Float(
+        string="Chick Entry Qty",
+        compute="_compute_chick_entry_qty",
+        store=True)
+    output_units = fields.Float(
+        string="Output Units",
+        compute="_compute_output_units",
+        store=True)
+    cancellation_percentage = fields.Float(
+        string="Cancellation Percentage",
+        related="batch_id.cancellation_percentage",
+        store=True)
+    density = fields.Float(
+        string="Density",
+        related="batch_id.density",
+        store=True)
+    growth_speed = fields.Float(
+        string="Growth Speed",
+        related="batch_id.growth_speed",
+        store=True)
+    feed = fields.Integer(
+        string="Feep",
+        related="batch_id.feed",
+        store=True)
+    meat_kilos = fields.Float(
+        string="Meat Kilos",
+        compute="_compute_meat_kilos",
+        store=True)
+    consume_feed = fields.Float(
+        string="Consume Feed",
+        compute="_compute_consume_feed",
+        store=True)
+    feed_family = fields.Many2one(
+        string="Feed Family",
+        comodel_name="breeding.feed",
+        related="batch_id.feed_family",
+        store=True)
+    average_age = fields.Float(
+        string="Average Age",
+        related="batch_id.average_age",
+        store=True)
+    farm_day = fields.Integer(
+        string="Farm Day",
+        related="batch_id.farm_day",
+        store=True)
+    average_weight = fields.Float(
+        string="Average Weight",
+        related="batch_id.average_weight",
+        store=True)
+    conversion = fields.Float(
+        string="Conversion",
+        related="batch_id.conversion",
+        store=True)
+    dif_weight = fields.Float(
+        string="Dif.",
+        related="batch_id.dif_weight",
+        store=True)
+    liquidation_amount = fields.Float(
+        string="Liquidation Amount",
+        compute="_compute_liquidation_amount",
+        store=True)
+    chick_liquidation = fields.Float(
+        string="Chick Liquidation",
+        related="batch_id.chick_liquidation",
+        store=True)
+    liquidation_area = fields.Float(
+        string="Liquidation Area",
+        related="batch_id.liquidation_area",
+        store=True)
+    cost_kilo = fields.Float(
+        string="Cost Kilo",
+        related="batch_id.cost_kilo",
+        store=True)
+    liquidated = fields.Boolean(
+        string="Liquidated",
+        related="batch_id.liquidated",
+        store=True)
+    billed = fields.Boolean(
+        string="Billed",
+        related="batch_id.billed",
+        store=True)
+    closed = fields.Boolean(
+        string="Closed",
+        related="batch_id.closed",
+        store=True)
+    output_amount_days = fields.Float(
+        string="Output Amount Days",
+        compute="_compute_output_amount_days",
+        store=True)
+    output_feed_amount = fields.Float(
+        string="Output Feed Amount",
+        compute="_compute_output_feed_amount",
+        store=True)
+    medicine_amount = fields.Float(
+        string="Medicine Amount",
+        compute="_compute_medicine_amount",
+        store=True)
+    warehouse_area = fields.Float(
+        string="Warehouse Area",
+        compute="_compute_warehouse_area",
+        store=True)
+    age_output = fields.Float(
+        string="Age Output",
+        compute="_compute_age_output",
+        store=True)
+
+    @api.depends("batch_id", "batch_id.medicine_qty", "percentage")
+    def _compute_medicine_amount(self):
+        for line in self:
+            medicine = 0
+            if line.batch_id:
+                medicine = (
+                    line.batch_id.output_medicine_amount * line.percentage
+                    ) / 100
+            line.medicine_amount = medicine
+
+    @api.depends("batch_id", "batch_id.output_feed_amount", "percentage")
+    def _compute_output_feed_amount(self):
+        for line in self:
+            qty = 0
+            if line.batch_id:
+                qty = (
+                    line.batch_id.output_feed_amount * line.percentage
+                    ) / 100
+            line.output_feed_amount = qty
+
+    @api.depends("batch_id", "batch_id.output_amount_days", "percentage")
+    def _compute_output_amount_days(self):
+        for line in self:
+            amount = 0
+            if line.batch_id:
+                amount = (
+                    line.batch_id.output_amount_days * line.percentage
+                    ) / 100
+            line.output_amount_days = amount
+
+    @api.depends("batch_id", "batch_id.age_output", "percentage")
+    def _compute_age_output(self):
+        for line in self:
+            age_output = 0
+            if line.batch_id:
+                age_output = (
+                    line.batch_id.age_output * line.percentage) / 100
+            line.age_output = age_output
+
+    @api.depends("batch_id", "batch_id.warehouse_area", "percentage")
+    def _compute_warehouse_area(self):
+        for line in self:
+            warehouse_area = 0
+            if line.batch_id:
+                warehouse_area = (
+                    line.batch_id.warehouse_area * line.percentage) / 100
+            line.warehouse_area = warehouse_area
+
+    @api.depends("batch_id", "batch_id.chick_entry_qty", "percentage")
+    def _compute_chick_entry_qty(self):
+        for line in self:
+            qty = 0
+            if line.batch_id:
+                qty = line.batch_id.chick_entry_qty * line.percentage / 100
+            line.chick_entry_qty = qty
+
+    @api.depends("batch_id", "batch_id.output_units", "percentage")
+    def _compute_output_units(self):
+        for line in self:
+            unit = 0
+            if line.batch_id:
+                unit = line.batch_id.output_units * line.percentage / 100
+            line.output_units = unit
+
+    @api.depends("batch_id", "batch_id.meat_kilos", "percentage")
+    def _compute_meat_kilos(self):
+        for line in self:
+            meat = 0
+            if line.batch_id:
+                meat = (
+                    line.batch_id.meat_kilos * line.percentage) / 100
+            line.meat_kilos = meat
+
+    @api.depends("batch_id", "batch_id.consume_feed", "percentage")
+    def _compute_consume_feed(self):
+        for line in self:
+            feed = 0
+            if line.batch_id:
+                feed = (
+                    line.batch_id.consume_feed * line.percentage) / 100
+            line.consume_feed = feed
+
+    @api.depends("batch_id", "batch_id.liquidation_amount", "percentage")
+    def _compute_liquidation_amount(self):
+        for line in self:
+            amount = 0
+            if line.batch_id:
+                amount = (
+                    line.batch_id.liquidation_amount * line.percentage) / 100
+            line.liquidation_amount = amount
+
+    def read_group(
+            self, domain, fields, groupby, offset=0, limit=None, orderby=False,
+            lazy=True):
+        result = super(LineagePercentage, self).read_group(
+            domain, fields, groupby, offset=0, limit=None, orderby=False,
+            lazy=True)
+        for line in result:
+            if '__domain' in line:
+                cancellation = 0
+                density = 0
+                growth_spped = 0
+                feed = 0
+                age = 0
+                day = 0
+                avg_wight = 0
+                conversion = 0
+                dif = 0
+                chick_liq = 0
+                liq_area = 0
+                cost_kilo = 0
+                aux = 0
+                lines = self.search(line['__domain'])
+                for x in lines:
+                    cancellation += x.cancellation_percentage
+                    density += x.density
+                    growth_spped += x.growth_speed
+                    feed += x.feed
+                    age += x.average_age
+                    day += x.farm_day
+                    avg_wight += x.average_weight
+                    conversion += x.conversion
+                    dif += x.dif_weight
+                    chick_liq += x.chick_liquidation
+                    liq_area += x.liquidation_area
+                    cost_kilo += x.cost_kilo
+                    aux += 1
+                total_cancellation = cancellation/aux
+                total_density = density/aux
+                total_growth_speed = growth_spped/aux
+                total_feed = feed/aux
+                total_age = age/aux
+                total_day = day/aux
+                total_avg_weight = avg_wight/aux
+                total_conversion = conversion/aux
+                total_dif = dif/aux
+                total_liq_area = liq_area/aux
+                total_chick_liq = chick_liq/aux
+                total_cost_kilo = cost_kilo/aux
+                line['cancellation_percentage'] = total_cancellation
+                line['density'] = total_density
+                line['growth_speed'] = total_growth_speed
+                line['feed'] = total_feed
+                line['average_age'] = total_age
+                line['farm_day'] = total_day
+                line['average_weight'] = total_avg_weight
+                line['conversion'] = total_conversion
+                line['dif_weight'] = total_dif
+                line['chick_liquidation'] = total_chick_liq
+                line['liquidation_area'] = total_liq_area
+                line['cost_kilo'] = total_cost_kilo
+            else:
+                fields.remove('cancellation_percentage')
+                fields.remove('density')
+        return result
---
+++ custom_breeding_report/report/breeding_general_summary_xlsx.py
@@ -0,0 +1,411 @@
+# Copyright 2023 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import _, models
+
+
+class ReportBreedingGeneralSummaryXlsx(models.AbstractModel):
+    _name = "report.breeding_general_summary_xlsx"
+    _inherit = "report.report_xlsx.abstract"
+    _description = "Breeding General Summary Report"
+
+    def generate_xlsx_report(self, workbook, data, objects):
+        table_header = workbook.add_format({
+            'bold': True,
+            'align': 'center',
+            'valign': 'vcenter',
+            'fg_color': '#D7E4BC',
+        })
+        summary = workbook.add_format({
+            'bold': True,
+            'num_format': '#,##0.00;(#,##0.00)',
+        })
+        int_format = workbook.add_format({
+            'num_format': '#,##0;(#,##0)',
+        })
+        two_decimal_format = workbook.add_format({
+            'num_format': '#,##0.00;(#,##0.00)',
+        })
+        three_decimal_format = workbook.add_format({
+            'num_format': '#,##0.000;(#,##0.000)',
+        })
+        eight_decimal_format = workbook.add_format({
+            'num_format': '#,##0.00000000;(#,##0.00000000)',
+        })
+        result_two_decimal = workbook.add_format({
+            'fg_color': '#afd095',
+            'num_format': '#,##0.00;(#,##0.00)',
+        })
+        result_three_decimal = workbook.add_format({
+            'fg_color': '#afd095',
+            'num_format': '#,##0.000;(#,##0.000)',
+        })
+        result_summary = workbook.add_format({
+            'bold': True,
+            'fg_color': '#afd095',
+            'num_format': '#,##0.000;(#,##0.000)',
+        })
+        table_header.set_text_wrap()
+        summary.set_text_wrap()
+        int_format.set_text_wrap()
+        two_decimal_format.set_text_wrap()
+        three_decimal_format.set_text_wrap()
+        eight_decimal_format.set_text_wrap()
+        result_three_decimal.set_text_wrap()
+        result_two_decimal.set_text_wrap()
+        result_summary.set_text_wrap()
+        table_detail_right_num = workbook.add_format({
+            'border': 1,
+            'align': 'right',
+            'valign': 'vcenter',
+        })
+        table_detail_right_num.set_num_format('#,##0.00')
+        worksheet = workbook.add_worksheet("Breeding General Summary")
+        for i in range(0, 9):
+            worksheet.set_column(0, i, 20)
+        n = 0
+        m = 0
+        worksheet.write(n, m, _("Concept"), table_header)
+        m += 1
+        worksheet.write(n, m, _("Value"), table_header)
+        m += 1
+        worksheet.write(n, m, _("Units"), table_header)
+        m += 1
+        worksheet.write(n, m, _("%"), table_header)
+        m += 1
+        worksheet.write(n, m, _("Price"), table_header)
+        m += 1
+        worksheet.write(n, m, _("Kg"), table_header)
+        m += 1
+        worksheet.write(n, m, _("Amount"), table_header)
+        m += 1
+        worksheet.write(n, m, _("Per Kilo"), table_header)
+        m += 1
+        worksheet.write(n, m, _("Per Chick"), table_header)
+        n += 2
+        m = 0
+        worksheet.write(n, m, _("Movement Summary"), table_header)
+        for m in range(m + 1, 9):
+            worksheet.write(n, m, "", table_header)
+        n += 1
+        m = 0
+        worksheet.write(n, m, _("Chick Input"))
+        m += 2
+        chick_qty = sum(objects.mapped("chick_entry_qty"))
+        worksheet.write(n, m, chick_qty, int_format)
+        m += 1
+        worksheet.write(n, m, 100, two_decimal_format)
+        m += 1
+        entry_chicken_amount = sum(objects.mapped("entry_chicken_amount"))
+        worksheet.write(n, m, round(
+            entry_chicken_amount / chick_qty, 8), eight_decimal_format)
+        m += 2
+        worksheet.write(n, m, round(
+            entry_chicken_amount, 2), two_decimal_format)
+        m += 1
+        meat_kilos = sum(objects.mapped("meat_kilos"))
+        worksheet.write(n, m, round(
+            entry_chicken_amount / meat_kilos, 3), three_decimal_format)
+        m += 1
+        output_unit = sum(objects.mapped("output_units"))
+        worksheet.write(n, m, round(
+            entry_chicken_amount / output_unit, 3), three_decimal_format)
+        n += 1
+        m = 0
+        worksheet.write(n, m, _("Chicken Output"))
+        m += 2
+        worksheet.write(n, m, output_unit, int_format)
+        m += 1
+        worksheet.write(n, m, round(
+            output_unit * 100 / chick_qty, 2), two_decimal_format)
+        m += 1
+        output_amount = sum(objects.mapped("output_amount"))
+        worksheet.write(n, m, round(
+            output_amount / meat_kilos, 8), eight_decimal_format)
+        m += 1
+        worksheet.write(n, m, meat_kilos, int_format)
+        m += 1
+        worksheet.write(n, m, round(output_amount, 2), two_decimal_format)
+        m += 1
+        worksheet.write(n, m, round(
+            output_amount / meat_kilos, 3), three_decimal_format)
+        m += 1
+        worksheet.write(n, m, round(
+            output_amount / output_unit, 3), three_decimal_format)
+        n += 1
+        m = 0
+        worksheet.write(n, m, _("Total Cancellations"))
+        m += 2
+        worksheet.write(n, m, chick_qty - output_unit, int_format)
+        m += 1
+        cancellation_percentage = 100 - output_unit * 100 / chick_qty
+        worksheet.write(n, m, round(
+            cancellation_percentage, 2), two_decimal_format)
+        n += 1
+        m = 0
+        worksheet.write(n, m, _("Consumed Feed"))
+        m += 4
+        feed_qty = sum(objects.mapped("consume_feed"))
+        feed_amount = sum(objects.mapped("output_feed_amount"))
+        worksheet.write(n, m, round(
+            feed_amount / feed_qty, 8), eight_decimal_format)
+        m += 1
+        worksheet.write(n, m, feed_qty, int_format)
+        m += 1
+        worksheet.write(n, m, round(feed_amount, 2), two_decimal_format)
+        n += 1
+        m = 0
+        worksheet.write(n, m, _("Medicine"))
+        m += 2
+        medicine_qty = sum(objects.mapped("medicine_qty"))
+        output_medicine_amount = sum(objects.mapped("output_medicine_amount"))
+        worksheet.write(n, m, medicine_qty, int_format)
+        m += 2
+        worksheet.write(n, m, round(
+            output_medicine_amount / medicine_qty, 8), eight_decimal_format)
+        m += 2
+        worksheet.write(n, m, round(
+            output_medicine_amount, 2), two_decimal_format)
+        m += 1
+        worksheet.write(n, m, round(
+            output_medicine_amount / meat_kilos, 3), three_decimal_format)
+        m += 1
+        worksheet.write(n, m, round(
+            output_medicine_amount / output_unit, 3), three_decimal_format)
+        n += 1
+        m = 0
+        worksheet.write(n, m, _("Liquidation"))
+        m += 2
+        worksheet.write(n, m, output_unit, int_format)
+        m += 3
+        worksheet.write(n, m, meat_kilos, int_format)
+        m += 1
+        liquidation_amount = sum(objects.mapped("liquidation_amount"))
+        worksheet.write(n, m, round(
+            liquidation_amount, 2), two_decimal_format)
+        m += 1
+        worksheet.write(n, m, round(
+            liquidation_amount / meat_kilos, 3), three_decimal_format)
+        m += 1
+        worksheet.write(n, m, round(
+            liquidation_amount / output_unit, 3), three_decimal_format)
+        n += 2
+        m = 0
+        worksheet.write(n, m, _("Costs and Benefits"), table_header)
+        for m in range(m + 1, 9):
+            worksheet.write(n, m, "", table_header)
+        n += 1
+        m = 0
+        chick_amount = 0
+        feed_amount = 0
+        medicine_amount = 0
+        liquidation_amount = 0
+        chicken_load = 0
+        overheads = 0
+        sales = 0
+        total_costs = 0
+        for line in objects:
+            chick = (-1) * sum(line.analytic_line_ids.filtered(
+                lambda c: c.name == "Pollito").mapped("amount"))
+            chick_amount += chick
+            feed = (-1) * sum(line.analytic_line_ids.filtered(
+                lambda c: c.name == "Pienso").mapped("amount"))
+            feed_amount += feed
+            medicine = (-1) * sum(line.analytic_line_ids.filtered(
+                lambda c: c.name == "Medicamento").mapped("amount"))
+            medicine_amount += medicine
+            liquidation = (-1) * sum(line.analytic_line_ids.filtered(
+                lambda c: c.name == "Liquidación").mapped("amount"))
+            liquidation_amount += liquidation
+            load = (-1) * sum(line.analytic_line_ids.filtered(
+                lambda c: c.name == "Carga Pollos").mapped("amount"))
+            chicken_load += load
+            expenses = (-1) * sum(line.analytic_line_ids.filtered(
+                lambda c: c.name == "Gtos. Generales").mapped("amount"))
+            overheads += expenses
+            sales += sum(line.analytic_line_ids.filtered(
+                lambda c: c.name == "Ventas").mapped("amount"))
+            total_costs += (
+                chick + feed + medicine + liquidation + load + expenses)
+        worksheet.write(n, m, _("Chicks"))
+        m += 3
+        worksheet.write(n, m, round(
+            chick_amount * 100 / total_costs, 2), result_two_decimal)
+        m += 2
+        worksheet.write(n, m, chick_amount / total_costs)
+        m += 1
+        worksheet.write(n, m, round(chick_amount, 2), two_decimal_format)
+        m += 1
+        worksheet.write(n, m, round(
+            chick_amount / meat_kilos, 3), result_three_decimal)
+        m += 1
+        worksheet.write(n, m, round(
+            chick_amount / output_unit, 3), result_three_decimal)
+        n += 1
+        m = 0
+        worksheet.write(n, m, _("Feed"))
+        m += 3
+        worksheet.write(n, m, round(
+            feed_amount * 100 / total_costs, 2), result_two_decimal)
+        m += 3
+        worksheet.write(n, m, round(feed_amount, 2), two_decimal_format)
+        m += 1
+        worksheet.write(n, m, round(
+            feed_amount / meat_kilos, 3), result_three_decimal)
+        m += 1
+        worksheet.write(n, m, round(
+            feed_amount / output_unit, 3), result_three_decimal)
+        n += 1
+        m = 0
+        worksheet.write(n, m, _("Medicine"))
+        m += 3
+        worksheet.write(n, m, round(
+            medicine_amount * 100 / total_costs, 2), result_two_decimal)
+        m += 3
+        worksheet.write(n, m, round(medicine_amount, 2), two_decimal_format)
+        m += 1
+        worksheet.write(n, m, round(
+            medicine_amount / meat_kilos, 3), result_three_decimal)
+        m += 1
+        worksheet.write(n, m, round(
+            medicine_amount / output_unit, 3), result_three_decimal)
+        n += 1
+        m = 0
+        worksheet.write(n, m, _("Liquidation"))
+        m += 3
+        worksheet.write(n, m, round(
+            liquidation_amount * 100 / total_costs, 2), result_two_decimal)
+        m += 3
+        worksheet.write(n, m, round(liquidation_amount, 2), two_decimal_format)
+        m += 1
+        worksheet.write(n, m, round(
+            liquidation_amount / meat_kilos, 3), result_three_decimal)
+        m += 1
+        worksheet.write(n, m, round(
+            liquidation_amount / output_unit, 3), result_three_decimal)
+        n += 1
+        m = 0
+        worksheet.write(n, m, _("Chicken Load"))
+        m += 3
+        worksheet.write(n, m, round(
+            chicken_load * 100 / total_costs, 2), result_two_decimal)
+        m += 3
+        worksheet.write(n, m, round(chicken_load, 2), two_decimal_format)
+        m += 1
+        worksheet.write(n, m, round(
+            chicken_load / meat_kilos, 3), result_three_decimal)
+        m += 1
+        worksheet.write(n, m, round(
+            chicken_load / output_unit, 3), result_three_decimal)
+        n += 1
+        m = 0
+        worksheet.write(n, m, _("Overheads"))
+        m += 3
+        worksheet.write(n, m, round(
+            overheads * 100 / total_costs, 2), result_two_decimal)
+        m += 3
+        worksheet.write(n, m, round(overheads, 2), two_decimal_format)
+        m += 1
+        worksheet.write(n, m, round(
+            overheads / meat_kilos, 3), result_three_decimal)
+        m += 1
+        worksheet.write(n, m, round(
+            overheads / output_unit, 3), result_three_decimal)
+        n += 1
+        m = 0
+        worksheet.write(n, m, _("Total Costs"), summary)
+        m += 6
+        worksheet.write(n, m, round(total_costs, 2), summary)
+        m += 1
+        worksheet.write(n, m, round(
+            total_costs / meat_kilos, 3), result_summary)
+        m += 1
+        worksheet.write(n, m, round(
+            total_costs / output_unit, 3), result_summary)
+        n += 1
+        m = 0
+        worksheet.write(n, m, _("Total Sales"), summary)
+        m += 6
+        worksheet.write(n, m, round(sales, 2), summary)
+        m += 1
+        worksheet.write(n, m, round(sales / meat_kilos, 3), result_summary)
+        m += 1
+        worksheet.write(n, m, round(sales / output_unit, 3), result_summary)
+        n += 1
+        m = 0
+        worksheet.write(n, m, _("Loss/Benefits"), summary)
+        m += 6
+        benefits = sales - total_costs
+        worksheet.write(n, m, round(benefits, 2), summary)
+        m += 1
+        worksheet.write(n, m, round(benefits / meat_kilos, 3), result_summary)
+        m += 1
+        worksheet.write(n, m, round(benefits / output_unit, 3), result_summary)
+        n += 2
+        m = 0
+        worksheet.write(n, m, _("Results"), table_header)
+        for m in range(m + 1, 9):
+            worksheet.write(n, m, "", table_header)
+        n += 1
+        m = 0
+        worksheet.write(n, m, _("Cancellation %"))
+        m += 1
+        worksheet.write(n, m, round(
+            cancellation_percentage, 2), two_decimal_format)
+        n += 1
+        m = 0
+        worksheet.write(n, m, _("Total M2"))
+        m += 1
+        area = sum(objects.mapped("warehouse_area"))
+        worksheet.write(n, m, area, int_format)
+        n += 1
+        m = 0
+        worksheet.write(n, m, _("Liquidation / M2"))
+        m += 1
+        worksheet.write(n, m, round(
+            liquidation_amount / area, 3), three_decimal_format)
+        n += 1
+        m = 0
+        worksheet.write(n, m, _("Weight Average"))
+        m += 1
+        average_weight = meat_kilos / output_unit
+        worksheet.write(n, m, round(average_weight, 3), three_decimal_format)
+        n += 1
+        m = 0
+        worksheet.write(n, m, _("Conversion"))
+        m += 1
+        conversion = feed_qty / meat_kilos
+        worksheet.write(n, m, round(conversion, 3), three_decimal_format)
+        n += 1
+        m = 0
+        worksheet.write(n, m, _("Difference"))
+        m += 1
+        worksheet.write(n, m, round(
+            average_weight - conversion, 3), three_decimal_format)
+        n += 1
+        m = 0
+        worksheet.write(n, m, _("Age Average"))
+        m += 1
+        average_age = sum(objects.mapped("age_output")) / output_unit
+        worksheet.write(n, m, round(average_age, 2), two_decimal_format)
+        n += 1
+        m = 0
+        worksheet.write(n, m, _("Growth Speed"))
+        m += 1
+        growth_speed = meat_kilos / output_unit / average_age * 1000
+        worksheet.write(n, m, round(growth_speed, 2), two_decimal_format)
+        n += 1
+        m = 0
+        worksheet.write(n, m, _("FEEP"))
+        m += 1
+        worksheet.write(n, m, int(growth_speed * (
+            100 - cancellation_percentage) / (10 * conversion)))
+        n += 2
+        m = 0
+        worksheet.write(n, m, _("Totals"), table_header)
+        for m in range(m + 1, 9):
+            worksheet.write(n, m, "", table_header)
+        n += 1
+        m = 0
+        worksheet.write(n, m, _("Breeding Nº"))
+        m += 1
+        worksheet.write(n, m, len(objects), int_format)
