5f7069d67e5d6fc0eff013e427a02fc16affacb9
Unai Beristain <unaiberistain@avanzosc.es>
2024-01-12 13:19:25 +0100

    [MIG] stock_move_line_force_done: Migration to 16.0

--- stock_move_line_force_done/__manifest__.py
+++ stock_move_line_force_done/__manifest__.py
@@ -5,7 +5,7 @@
     "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "AvanzOSC",
-    "website": "https://github.com/avanzosc/odoo-addons",
+    "website": "http://www.avanzosc.es",
     "category": "Warehouse",
     "depends": ["stock"],
     "data": [
