66258ce9e1988bbed6d439ddb587ffb4ffcc5163
Unai Beristain <unaiberistain@avanzosc.es>
2024-01-12 13:23:19 +0100

    [MIG] account_move_import_wizard: Migration to 16.0

--- account_move_import_wizard/__manifest__.py
+++ account_move_import_wizard/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Account Move Import Wizard",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Hidden/Tools",
     "license": "AGPL-3",
     "author": "AvanzOSC",
