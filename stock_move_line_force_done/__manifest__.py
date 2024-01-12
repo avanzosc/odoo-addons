98001306bac4b130151ef230478d19bf4ac32ffd
Unai Beristain <unaiberistain@avanzosc.es>
2024-01-12 13:19:06 +0100

    [IMP] stock_move_line_force_done: pre-commit stuff

--- stock_move_line_force_done/__manifest__.py
+++ stock_move_line_force_done/__manifest__.py
@@ -5,11 +5,9 @@
     "version": "12.0.1.0.0",
     "license": "AGPL-3",
     "author": "AvanzOSC",
-    "website": "http://www.avanzosc.es",
+    "website": "https://github.com/avanzosc/odoo-addons",
     "category": "Warehouse",
-    "depends": [
-        "stock"
-    ],
+    "depends": ["stock"],
     "data": [
         "views/stock_picking_view.xml",
     ],
