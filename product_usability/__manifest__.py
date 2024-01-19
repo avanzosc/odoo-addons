7712198fb4e19cacb33eb4e78757412f2c15e793
Tu Nombre <you@example.com>
2024-01-19 13:22:04 +0100

    [14.0][IMP] product_usability:

--- product_usability/__manifest__.py
+++ product_usability/__manifest__.py
@@ -7,12 +7,14 @@
     "category": "Sales/Sales",
     "license": "AGPL-3",
     "author": "AvanzOSC",
-    "website": "http://www.avanzosc.es",
+    "website": "https://github.com/avanzosc/odoo-addons",
     "depends": ["product", "stock"],
     "data": [
         "views/res_partner_view.xml",
         "views/product_product_view.xml",
         "views/product_template_view.xml",
+        "views/product_category_view.xml",
+        "views/stock_quant_view.xml",
     ],
     "installable": True,
 }
