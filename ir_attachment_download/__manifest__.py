ee751c2e2f9f89448bb1060bb3b483382e6e4973
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-01-24 09:53:18 +0100

    [MIG] ir_attachment_download: Migration to 14.0

--- ir_attachment_download/__manifest__.py
+++ ir_attachment_download/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
     "name": "Attachments download",
-    "version": "12.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "depends": [
         "base",
@@ -12,6 +12,7 @@
     "website": "http://www.avanzosc.es",
     "category": "Tools",
     "data": [
+        "security/ir.model.access.csv",
         "wizard/attach_download_action_view.xml",
         "views/ir_model_view.xml",
     ],
