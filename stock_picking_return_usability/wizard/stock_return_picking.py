ca992db2e18c736ca3947b9ed1273d2655b8df50
Tu Nombre <you@example.com>
2024-01-26 13:11:21 +0100

    [14.0][IMP] stock_picking_return_usability: All lots and quantities are taken to the return picking.

--- stock_picking_return_usability/wizard/stock_return_picking.py
+++ stock_picking_return_usability/wizard/stock_return_picking.py
@@ -4,11 +4,11 @@
 from odoo import models
 
 
-class ReturnPicking(models.TransientModel):
+class StockReturnPicking(models.TransientModel):
     _inherit = "stock.return.picking"
 
     def _prepare_move_default_values(self, return_line, new_picking):
-        result = super(ReturnPicking, self)._prepare_move_default_values(
+        result = super(StockReturnPicking, self)._prepare_move_default_values(
             return_line, new_picking)
         if self.picking_id and (
             self.picking_id.picking_type_id) and (
@@ -22,7 +22,7 @@ class ReturnPicking(models.TransientModel):
 
     def _create_returns(self):
         new_picking, picking_type_id = super(
-            ReturnPicking, self)._create_returns()
+            StockReturnPicking, self)._create_returns()
         return_picking = self.env["stock.picking"].browse(new_picking)
         if self.picking_id and (
             self.picking_id.picking_type_id) and (
@@ -37,4 +37,33 @@ class ReturnPicking(models.TransientModel):
                 self.picking_id.picking_type_id.retun_picking_draft):
             for move in return_picking.move_ids_without_package:
                 move.state = "draft"
+        else:
+            return_picking.button_force_done_detailed_operations()
+            for line in self.picking_id.move_line_ids_without_package:
+                if line.lot_id:
+                    return_movelines = (
+                        return_picking.move_line_ids_without_package
+                    )
+                    return_line = (
+                        return_movelines.filtered(
+                            lambda c: c.product_id == line.product_id and not (
+                                c.lot_id
+                            )
+                        )
+                    )
+                    if return_line:
+                        return_line[:1].write({
+                            "lot_id": line.lot_id.id,
+                            "qty_done": line.qty_done,
+                        })
+                    else:
+                        return_line = return_movelines.filtered(
+                            lambda c: c.product_id == line.product_id
+                        )
+                        if return_line:
+                            new_return_line = return_line[:1].copy()
+                            new_return_line.write({
+                                "lot_id": line.lot_id.id,
+                                "qty_done": line.qty_done,
+                            })
         return new_picking, picking_type_id
