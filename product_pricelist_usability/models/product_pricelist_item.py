c6b548c5144cd5110872e2e7ce9e2fd34b5da3bd
Tu Nombre <you@example.com>
2024-01-29 14:58:15 +0100

    [ADD][14.0] product_pricelist_usability: Shorcut to pricelist items.

---
+++ product_pricelist_usability/models/product_pricelist_item.py
@@ -0,0 +1,13 @@
+# Copyright 2024 Berezi Amubieta - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+from odoo import _, fields, models
+
+
+class ProductPricelistItem(models.Model):
+    _inherit = "product.pricelist.item"
+
+    product_code = fields.Char(
+        string="Product Code",
+        related="product_tmpl_id.default_code",
+        store=True,
+    )
