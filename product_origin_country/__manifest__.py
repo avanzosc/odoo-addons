c2b249bf28604e1a36fb6221471f36c7e53b43bc
Unai Beristain <unaiberistain@avanzosc.es>
2024-01-29 11:17:54 +0100

    [MIG] product_origin_country: Migration to 16.0

--- product_origin_country/__manifest__.py
+++ product_origin_country/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Product Origin Country",
-    "version": "12.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Avanzosc",
     "website": "https://github.com/avanzosc/odoo-addons",
     "license": "AGPL-3",
