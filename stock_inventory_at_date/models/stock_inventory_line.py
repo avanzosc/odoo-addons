52e8e6b8506852f18c233ab771f8bae12216b9a8
Tu Nombre <you@example.com>
2024-01-30 12:13:14 +0100

    [14.0][FIX] stock_inventory_at_date: Correct the calculation of quantities to date.

--- stock_inventory_at_date/models/stock_inventory_line.py
+++ stock_inventory_at_date/models/stock_inventory_line.py
@@ -27,14 +27,65 @@ class StockInventoryLine(models.Model):
                         "partner_id"] if "partner_id" in group else False
                     package = group[
                         "package_id"] if "package_id" in group else False
-                    qty = product.with_context(
-                        location=location,
-                        to_date=inventory.accounting_date,
-                        lot_id=lot,
-                        owner_id=owner,
-                        package_id=package).qty_available
+                    domain_move_in = [
+                        ('product_id', '=', product.id),
+                        ("location_dest_id", "=", location),
+                        ("date", "<=", inventory.accounting_date),
+                        ("state", "=", "done")
+                    ]
+                    domain_move_out = [
+                        ('product_id', '=', product.id),
+                        ("location_id", "=", location),
+                        ("date", "<=", inventory.accounting_date),
+                        ("state", "=", "done")
+                    ]
+                    if lot:
+                        domain_move_in += [('lot_id', '=', lot)]
+                        domain_move_out += [('lot_id', '=', lot)]
+                    if owner:
+                        domain_move_in += [('owner_id', '=', owner)]
+                        domain_move_out += [('owner_id', '=', owner)]
+                    if package:
+                        domain_move_in += [('package_id', '=', package)]
+                        domain_move_out += [('package_id', '=', package)]
+                    MoveLine = self.env['stock.move.line'].with_context(
+                        active_test=False
+                    )
+                    moves_in = dict(
+                        (item['product_id'][0], item['qty_done']) for item in (
+                            MoveLine.read_group(
+                                domain_move_in,
+                                ['product_id', 'qty_done'],
+                                ['product_id'], orderby='id'
+                            )
+                        )
+                    )
+                    if moves_in and moves_in[product.id]:
+                        qty_in = moves_in[product.id]
+                    else:
+                        qty_in = 0
+                    moves_out = dict(
+                        (item['product_id'][0], item['qty_done']) for item in (
+                            MoveLine.read_group(
+                                domain_move_out,
+                                ['product_id', 'qty_done'],
+                                ['product_id'], orderby='id'
+                            )
+                        )
+                    )
+                    if moves_out and moves_out[product.id]:
+                        qty_out = moves_out[product.id]
+                    else:
+                        qty_out = 0
+                    qty = qty_in - qty_out
                     group["theoretical_qty"] = qty
                     group["inventory_date"] = inventory.accounting_date
+                    if "product_qty" in group and group["product_qty"] != (
+                        group["theoretical_qty"]
+                    ) and (
+                        inventory.prefill_counted_quantity == "counted"
+                    ):
+                        group["product_qty"] = group["theoretical_qty"]
         res = super(StockInventoryLine, self).create(vals_list)
         return res
 
