97c8403eb17ad4861d3d37d62b548278d9fc6afa
Tu Nombre <you@example.com>
2024-01-30 13:41:50 +0100

    [14.0][FIX] product_multi_company_usability: Resolve traceback when duplicating a product.

--- product_multi_company_usability/models/product_product.py
+++ product_multi_company_usability/models/product_product.py
@@ -10,14 +10,14 @@ class ProductProduct(models.Model):
     @api.model
     def create(self, values):
         line = super(ProductProduct, self).create(values)
-        if "company_id" in values:
+        if "company_id" in values and values.get("company_id", False):
             company = self.env["res.company"].browse(values.get("company_id"))
             line.company_ids = [(4, company.id)]
         return line
 
     def write(self, values):
         result = super(ProductProduct, self).write(values)
-        if "company_id" in values:
+        if "company_id" in values and values.get("company_id", False):
             for line in self:
                 if line.company_id:
                     line.company_ids = [(4, line.company_id.id)]
--- product_multi_company_usability/models/product_template.py
+++ product_multi_company_usability/models/product_template.py
@@ -25,14 +25,14 @@ class ProductTemplate(models.Model):
     @api.model
     def create(self, values):
         line = super(ProductTemplate, self).create(values)
-        if "company_id" in values:
+        if "company_id" in values and values.get("company_id", False):
             company = self.env["res.company"].browse(values.get("company_id"))
             line.company_ids = [(4, company.id)]
         return line
 
     def write(self, values):
         result = super(ProductTemplate, self).write(values)
-        if "company_id" in values:
+        if "company_id" in values and values.get("company_id", False):
             for line in self:
                 if line.company_id:
                     line.company_ids = [(4, line.company_id.id)]
