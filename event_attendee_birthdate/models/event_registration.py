39addd29f9633e9038c56fad98df93f1cd89a018
Unai Beristain <unaiberistain@avanzosc.es>
2024-02-15 12:09:47 +0100

    [IMP] event_attendee_birthdate: pre-commit execution

--- event_attendee_birthdate/models/event_registration.py
+++ event_attendee_birthdate/models/event_registration.py
@@ -2,16 +2,17 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from dateutil.relativedelta import relativedelta
+
 from odoo import api, fields, models
 
 
 class EventRegistration(models.Model):
-    _inherit = 'event.registration'
+    _inherit = "event.registration"
 
-    birthdate = fields.Date('Attendee birthdate')
-    age = fields.Integer('Attendee age', compute="_compute_attendee_age")
+    birthdate = fields.Date("Attendee birthdate")
+    age = fields.Integer("Attendee age", compute="_compute_attendee_age")
 
-    @api.depends('birthdate')
+    @api.depends("birthdate")
     def _compute_attendee_age(self):
         for res in self:
             age = 0
@@ -20,17 +21,16 @@ class EventRegistration(models.Model):
             res.age = age
 
     def _get_website_registration_allowed_fields(self):
-        res = super(EventRegistration,
-                    self)._get_website_registration_allowed_fields()
-        res.update({'birthdate'})
+        res = super()._get_website_registration_allowed_fields()
+        res.update({"birthdate"})
         return res
-    
+
     @api.onchange("student_id", "partner_id")
     def _onchange_student_id(self):
-        super(EventRegistration, self)._onchange_student_id()
+        super()._onchange_student_id()
         self.update_student_birthdate()
 
-    @api.depends('student_id', 'student_id.birthdate_date')
+    @api.depends("student_id", "student_id.birthdate_date")
     def update_student_birthdate(self):
         self.ensure_one()
         if self.student_id and self.student_id.birthdate_date:
