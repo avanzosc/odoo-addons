6108cc5f1a565efe08f676e0de101211d2d4fd55
Unai Beristain <unaiberistain@avanzosc.es>
2024-02-22 09:34:23 +0100

    [16.0][ADD] okatek_survey_report_template: Add survey templates

---
+++ okatek_survey_report_template/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2024 Unai Beristain - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Report qweb auto generation",
+    "version": "16.0.1.0.0",
+    "depends": [
+        "base",
+    ],
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/",
+    "license": "AGPL-3",
+    "category": "Tools",
+    "data": [
+        "views/report.xml",
+    ],
+    "installable": True,
+}
