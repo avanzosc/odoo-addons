ee4ea742cbd8f87d2f9baf2aec2325a028eb3592
oihane <oihanecruce@gmail.com>
2024-03-05 09:35:00 +0100

    [FIX] contacts_import_wizard: avoid errors when migrating databases (#2420)

--- contacts_import_wizard/migrations/16.0.1.1.0/pre-migration.py
+++ contacts_import_wizard/migrations/16.0.1.1.0/pre-migration.py
@@ -6,11 +6,12 @@ from openupgradelib import openupgrade
 
 @openupgrade.migrate()
 def migrate(env, version):
-    openupgrade.logged_query(
-        env.cr,
-        """
-        UPDATE res_partner
-        SET comercial = trade_name
-        WHERE comercial is null;
-        """,
-    )
+    if openupgrade.column_exists(env.cr, "res_partner", "trade_name"):
+        openupgrade.logged_query(
+            env.cr,
+            """
+            UPDATE res_partner
+            SET comercial = trade_name
+            WHERE comercial is null;
+            """,
+        )
