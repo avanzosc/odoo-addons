aebede7b80beabfacf5a80d9b6f993389f19f92d
Unai Beristain <unaiberistain@avanzosc.es>
2024-03-11 16:27:27 +0100

    [IMP] Add fields uso and texto_reglamento_certificacion

--- res_partner_building_type_section/models/building_type.py
+++ res_partner_building_type_section/models/building_type.py
@@ -2,11 +2,19 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from odoo import fields, models
 
-
 class BuildingType(models.Model):
     _name = "building.type"
     _description = "Building type"
 
     name = fields.Char(
         string="Description", required=True, copy=False
+    )
+
+    uso = fields.Selection(
+        [('industrial', 'Industrial'), ('no_industrial', 'No Industrial')],
+        string="Uso",
+    )
+
+    texto_reglamento_certificacion = fields.Text(
+        string="Texto Reglamento Certificación",
     )
