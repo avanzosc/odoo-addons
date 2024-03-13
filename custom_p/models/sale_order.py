de0f58a4f268ad12104fcc4b7cc85d996b35177b
Tu Nombre <you@example.com>
2024-03-13 15:19:21 +0100

    [14.0][IMP] custom_p: add new functionality to the module.

---
+++ custom_p/models/sale_order.py
@@ -0,0 +1,48 @@
+# Copyright 2024 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+from datetime import timedelta
+from odoo.exceptions import ValidationError
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    update_line_qty = fields.Boolean(
+        string="Update Order Lines Qty",
+        default=False,
+        copy=False,
+    )
+
+    def button_return_picking(self):
+        self.update_line_qty = True
+        return super(SaleOrder, self).button_return_picking()
+
+    def action_last_month_partner_sales(self):
+        self.ensure_one()
+        if not self.partner_id:
+            raise ValidationError(
+                _("This sale has not partner.")
+            )
+        else:
+            now = fields.Datetime.now()
+            last_month = now - timedelta(days=31)
+            domain = [
+                ("order_partner_id", "=", self.partner_id.id),
+                ("state", "=", "sale"),
+                ("date_order", ">=", last_month)
+            ]
+            sale_lines = self.env["sale.order.line"].search(domain)
+            products = []
+            for line in sale_lines:
+                if line.product_id not in products:
+                    products.append(line.product_id)
+            for product in products:
+                if product not in self.order_line.mapped("product_id"):
+                    self.env["sale.order.line"].create({
+                        "product_id": product.id,
+                        "product_uom": product.uom_id.id,
+                        "product_uom_qty": 0,
+                        "order_id": self.id
+                    })
--- custom_p/models/sale_order_line.py
+++ custom_p/models/sale_order_line.py
@@ -1,18 +1,28 @@
 # Copyright 2024 Berezi Amubieta - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models
+from odoo import api, models
 
 
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    @api.model
-    def read_group(self, domain, fields, groupby, offset=0, limit=None, orderby=False, lazy=True):
-        result = super(SaleOrderLine, self).read_group(domain, fields, groupby, offset=offset, limit=limit, orderby=orderby, lazy=lazy)
-        for line in result:
-            if '__domain' in line:
-                lines = self.search(line["__domain"])
-                price_unit = sum(lines.mapped("price_unit"))/len(lines)
-                line["price_unit"] = price_unit
-        return result
+    @api.depends("move_ids.state", "move_ids.scrapped",
+                 "move_ids.product_uom_qty", "move_ids.product_uom")
+    def _compute_qty_delivered(self):
+        super(SaleOrderLine, self)._compute_qty_delivered()
+        for line in self:
+            if line.qty_delivered_method == "stock_move" and (
+                line.order_id.update_line_qty
+            ) and line.return_qty:
+                line.product_uom_qty = line.qty_delivered
+            if len(
+                line.order_id.picking_ids
+            ) > 1 and line.order_id.update_line_qty and all(
+                [c.state == "done" for c in line.order_id.picking_ids]
+            ) and all(
+                [c.product_uom_qty == c.qty_delivered for c in (
+                    line.order_id.order_line
+                )]
+            ):
+                line.order_id.update_line_qty = False
