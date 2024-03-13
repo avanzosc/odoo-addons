8e861b3a93f1b1fe6383ffe39ee39468e0b01fc3
Alfredo <alfredodelafuente@avanzosc.es>
2024-03-13 10:20:44 +0100

    [ADD] acs_visits_ext: In visit object new fiels: Visit type, and person.

---
+++ acs_visits_ext/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2020 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Acs Visits Ext",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "acs_visits",
+    ],
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "category": "Project",
+    "data": [
+        "security/ir.model.access.csv",
+        "views/visit_type_views.xml",
+        "views/visit_visit_views.xml",
+    ],
+    "installable": True,
+}
