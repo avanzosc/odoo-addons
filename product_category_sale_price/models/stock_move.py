968fab6b4a4ed5d5a936eae7c21be2692c4e4d19
Unai Beristain <unaiberistain@avanzosc.es>
2024-03-18 13:49:18 +0100

    [ADD] product_category_sale_price and product_alternative_sale_code

---
+++ product_category_sale_price/models/stock_move.py
@@ -0,0 +1,13 @@
+# Copyright 2020 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+from odoo import models, api
+
+
+class StockMove(models.Model):
+    _inherit = 'stock.move'
+
+    @api.multi
+    def product_price_update_before_done(self, forced_qty=None):
+        return super(StockMove, self.with_context(
+            update_base_cost=True)).product_price_update_before_done(
+                forced_qty=forced_qty)
