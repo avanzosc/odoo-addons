5f06b78baf3fe954459e95bd996ae8479f7a9cc5
Alfredo <alfredodelafuente@avanzosc.es>
2024-03-18 07:36:05 +0100

    [IMP] acs_visits_ext: Mostrar "Planificar actividad" en formulario de visitas.

--- acs_visits_ext/__manifest__.py
+++ acs_visits_ext/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
     "name": "Acs Visits Ext",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.1.0",
     "license": "AGPL-3",
     "depends": [
         "acs_visits",
