f594fda8a4ab5a16c1cc44c6bf327941a06164ff
Unai Beristain <unaiberistain@avanzosc.es>
2024-03-18 16:30:31 +0100

    [ADD] modules

---
+++ sale_stock_location_oin/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright 2019 Odoo IT now <http://www.odooitnow.com/>
+# See LICENSE file for full copyright and licensing details.
+#
+##############################################################################
+
+from . import models
