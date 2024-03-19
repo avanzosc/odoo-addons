79f2cd4c69d1e15682bdbabdc42e97af07656f9e
Alfredo <alfredodelafuente@avanzosc.es>
2024-03-19 07:46:07 +0100

    [ADD] stock_account_lineal: In "Inventory Valuation" new fields: Lineal.

---
+++ stock_account_lineal/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2024 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Stock Account Lineal",
+    "version": "14.0.1.0.0",
+    "category": "Inventory/Inventory",
+    "license": "AGPL-3",
+    "author": "Avanzosc",
+    "website": "https://www.avanzosc.es",
+    "depends": [
+        "stock_account",
+        "product_template_lineal",
+    ],
+    "data": [
+        "views/stock_valuation_layer_views.xml",
+    ],
+    "installable": True,
+}
