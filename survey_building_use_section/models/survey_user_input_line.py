0c77a0599517312c387aeb4b18b00db0edb2b7ab
Unai Beristain <unaiberistain@avanzosc.es>
2024-03-21 09:39:17 +0100

    [IMP] logging

--- survey_building_use_section/models/survey_user_input_line.py
+++ survey_building_use_section/models/survey_user_input_line.py
@@ -1,6 +1,8 @@
 # Copyright 2024 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from odoo import api, fields, models
+import logging
+
 _logger = logging.getLogger(__name__)
 
 
