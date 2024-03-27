f0240a2535d658d0e1249b82a192128b992a406f
Tu Nombre <you@example.com>
2024-03-27 14:42:44 +0100

    [14.0][IMP] custom_move_line_report: context in action.

---
+++ custom_move_line_report/models/stock_picking.py
@@ -0,0 +1,11 @@
+# Copyright 2023 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import api, fields, models
+
+
+class StockPicking(models.Model):
+    _inherit = "stock.picking"
+
+    show_in_report = fields.Boolean(
+        string="Show in Report",
+        default=True)
