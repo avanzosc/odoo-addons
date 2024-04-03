6a40a395d592806aaa5f460609d55b3ad98c0012
Alfredo <alfredodelafuente@avanzosc.es>
2024-04-03 11:12:33 +0200

    [ADD] account_move_hr_expense_sheet_employee: Journal entries with employee from employee's expense.

---
+++ account_move_hr_expense_sheet_employee/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2024 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Account Move Hr Expense Sheet Employee",
+    "version": "14.0.1.0.0",
+    "category": "Inventory/Purchase",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "account",
+        "hr_expense",
+    ],
+    "data": [
+        "views/account_move_views.xml",
+    ],
+    'installable': True,
+}
