4742406916d1d0105bd0447ee2c7bc2c318c3b25
Tu Nombre <you@example.com>
2024-04-04 14:06:29 +0200

    [14.0][IMP] batch_liquidation_report: New report.

--- batch_liquidation_report/__manifest__.py
+++ batch_liquidation_report/__manifest__.py
@@ -14,6 +14,7 @@
         "report_qweb_element_page_visibility",
     ],
     "data": [
+        "security/ir.model.access.csv",
         "data/ticket_paperformat.xml",
         "report/liquidation_account_move_report.xml",
         "report/liquidation_cost_report.xml",
@@ -23,6 +24,9 @@
         "report/sale_order_report.xml",
         "report/purchase_order_report.xml",
         "report/account_move_report.xml",
+        "report/stock_by_breeding_xlsx.xml",
+        "views/stock_picking_batch_view.xml",
+        "wizard/stock_by_breeding_wizard_view.xml",
     ],
     "installable": True,
 }
---
+++ batch_liquidation_report/wizard/stock_by_breeding_wizard.py
@@ -0,0 +1,33 @@
+# Copyright 2024 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import models, fields
+
+
+class StockByBreedingWizard(models.TransientModel):
+    _name = "stock.by.breeding.wizard"
+    _description = "Wizard to see the actual stock of breedings"
+
+    meat_cost = fields.Float(
+        string="Meat Cost",
+        required=True,
+    )
+    date = fields.Date(
+        string="Date",
+        required=True,
+        default=fields.Date.today(),
+    )
+
+    def button_create_report(self):
+        data = {}
+        if self.date:
+            data.update({"date": self.date})
+        if self.meat_cost:
+            data.update({"meat_cost": self.meat_cost})
+        if "active_ids" in self.env.context:
+            objects = self.env["stock.picking.batch"].browse(
+                self.env.context.get("active_ids")
+            )
+            data.update({"objects": objects.ids})
+        return self.env.ref(
+            "batch_liquidation_report.report_stock_by_breeding_xlsx"
+        ).report_action(self, data=data)
