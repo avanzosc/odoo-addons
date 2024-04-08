25b487b5215fc60e3c9eba242d5cff91d8ab91c3
Unai Beristain <unaiberistain@avanzosc.es>
2024-04-08 16:07:34 +0200

    [IMP] Add more loggers

--- survey_building_use_section/controllers/main.py
+++ survey_building_use_section/controllers/main.py
@@ -102,8 +102,6 @@ class Survey(Survey):
 
     def _prepare_survey_data(self, survey_sudo, answer_sudo, **post):
         res = super()._prepare_survey_data(survey_sudo, answer_sudo, **post)
-        pprint_string = pprint.pformat(res, indent=4)
-        _logger.info(f"2024okdeb - Contenido de res pprint:{pprint_string}")
         _logger.info(f"2024okdeb - Contenido de res:{res}")
         return res
 
@@ -153,3 +151,92 @@ class Survey(Survey):
             'survey_navigation': request.env['ir.qweb']._render('survey.survey_navigation', survey_data),
             'background_image_url': background_image_url
         }
+
+
+    def _prepare_survey_data(self, survey_sudo, answer_sudo, **post):
+        """ This method prepares all the data needed for template rendering, in function of the survey user input state.
+            :param post:
+                - previous_page_id : come from the breadcrumb or the back button and force the next questions to load
+                                     to be the previous ones. """
+        _logger.info(f"2024okdeb - Answer sudo _prepare_survey_data: {answer_sudo}")
+
+        data = {
+            'is_html_empty': is_html_empty,
+            'survey': survey_sudo,
+            'answer': answer_sudo,
+            'breadcrumb_pages': [{
+                'id': page.id,
+                'title': page.title,
+            } for page in survey_sudo.page_ids],
+            'format_datetime': lambda dt: format_datetime(request.env, dt, dt_format=False),
+            'format_date': lambda date: format_date(request.env, date)
+        }
+        if survey_sudo.questions_layout != 'page_per_question':
+            triggering_answer_by_question, triggered_questions_by_answer, selected_answers = answer_sudo._get_conditional_values()
+            data.update({
+                'triggering_answer_by_question': {
+                    question.id: triggering_answer_by_question[question].id for question in triggering_answer_by_question.keys()
+                    if triggering_answer_by_question[question]
+                },
+                'triggered_questions_by_answer': {
+                    answer.id: triggered_questions_by_answer[answer].ids
+                    for answer in triggered_questions_by_answer.keys()
+                },
+                'selected_answers': selected_answers.ids
+            })
+
+        if not answer_sudo.is_session_answer and survey_sudo.is_time_limited and answer_sudo.start_datetime:
+            data.update({
+                'server_time': fields.Datetime.now(),
+                'timer_start': answer_sudo.start_datetime.isoformat(),
+                'time_limit_minutes': survey_sudo.time_limit
+            })
+
+        page_or_question_key = 'question' if survey_sudo.questions_layout == 'page_per_question' else 'page'
+
+        # Bypass all if page_id is specified (comes from breadcrumb or previous button)
+        if 'previous_page_id' in post:
+            previous_page_or_question_id = int(post['previous_page_id'])
+            new_previous_id = survey_sudo._get_next_page_or_question(answer_sudo, previous_page_or_question_id, go_back=True).id
+            page_or_question = request.env['survey.question'].sudo().browse(previous_page_or_question_id)
+            data.update({
+                page_or_question_key: page_or_question,
+                'previous_page_id': new_previous_id,
+                'has_answered': answer_sudo.user_input_line_ids.filtered(lambda line: line.question_id.id == new_previous_id),
+                'can_go_back': survey_sudo._can_go_back(answer_sudo, page_or_question),
+            })
+            return data
+
+        if answer_sudo.state == 'in_progress':
+            if answer_sudo.is_session_answer:
+                next_page_or_question = survey_sudo.session_question_id
+            else:
+                next_page_or_question = survey_sudo._get_next_page_or_question(
+                    answer_sudo,
+                    answer_sudo.last_displayed_page_id.id if answer_sudo.last_displayed_page_id else 0)
+
+                if next_page_or_question:
+                    data.update({
+                        'survey_last': survey_sudo._is_last_page_or_question(answer_sudo, next_page_or_question)
+                    })
+
+            if answer_sudo.is_session_answer and next_page_or_question.is_time_limited:
+                data.update({
+                    'timer_start': survey_sudo.session_question_start_time.isoformat(),
+                    'time_limit_minutes': next_page_or_question.time_limit / 60
+                })
+
+            data.update({
+                page_or_question_key: next_page_or_question,
+                'has_answered': answer_sudo.user_input_line_ids.filtered(lambda line: line.question_id == next_page_or_question),
+                'can_go_back': survey_sudo._can_go_back(answer_sudo, next_page_or_question),
+            })
+            if survey_sudo.questions_layout != 'one_page':
+                data.update({
+                    'previous_page_id': survey_sudo._get_next_page_or_question(answer_sudo, next_page_or_question.id, go_back=True).id
+                })
+        elif answer_sudo.state == 'done' or answer_sudo.survey_time_limit_reached:
+            # Display success message
+            return self._prepare_survey_finished_values(survey_sudo, answer_sudo)
+
+        return data
