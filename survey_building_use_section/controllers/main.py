79892114382666dfd598e591972e4944d83cdef2
Unai Beristain <unaiberistain@avanzosc.es>
2024-04-08 15:56:49 +0200

    [IMP] Add more loggers

--- survey_building_use_section/controllers/main.py
+++ survey_building_use_section/controllers/main.py
@@ -107,9 +107,49 @@ class Survey(Survey):
         _logger.info(f"2024okdeb - Contenido de res:{res}")
         return res
 
+
     def _prepare_question_html(self, survey_sudo, answer_sudo, **post):
-        _logger.info(f"2024okdeb - Contenido de res2 survey_sudo:{survey_sudo}")
-        _logger.info(f"2024okdeb - Contenido de res2 answer_sudo:{answer_sudo}")
-        res = super()._prepare_question_html(survey_sudo, answer_sudo, **post)
-        _logger.info(f"2024okdeb - Contenido de res2:{res}")
-        return res
+        """ Survey page navigation is done in AJAX. This function prepare the 'next page' to display in html
+        and send back this html to the survey_form widget that will inject it into the page.
+        Background url must be given to the caller in order to process its refresh as we don't have the next question
+        object at frontend side."""
+        survey_data = self._prepare_survey_data(survey_sudo, answer_sudo, **post)
+
+        _logger.info(f"2024okdeb - Contenido de survey_data:{survey_data}")
+
+        if answer_sudo.state == 'done':
+            survey_content = request.env['ir.qweb']._render('survey.survey_fill_form_done', survey_data)
+        else:
+            survey_content = request.env['ir.qweb']._render('survey.survey_fill_form_in_progress', survey_data)
+
+        survey_progress = False
+        if answer_sudo.state == 'in_progress' and not survey_data.get('question', request.env['survey.question']).is_page:
+            if survey_sudo.questions_layout == 'page_per_section':
+                page_ids = survey_sudo.page_ids.ids
+                survey_progress = request.env['ir.qweb']._render('survey.survey_progression', {
+                    'survey': survey_sudo,
+                    'page_ids': page_ids,
+                    'page_number': page_ids.index(survey_data['page'].id) + (1 if survey_sudo.progression_mode == 'number' else 0)
+                })
+            elif survey_sudo.questions_layout == 'page_per_question':
+                page_ids = (answer_sudo.predefined_question_ids.ids
+                            if not answer_sudo.is_session_answer and survey_sudo.questions_selection == 'random'
+                            else survey_sudo.question_ids.ids)
+                survey_progress = request.env['ir.qweb']._render('survey.survey_progression', {
+                    'survey': survey_sudo,
+                    'page_ids': page_ids,
+                    'page_number': page_ids.index(survey_data['question'].id)
+                })
+
+        background_image_url = survey_sudo.background_image_url
+        if 'question' in survey_data:
+            background_image_url = survey_data['question'].background_image_url
+        elif 'page' in survey_data:
+            background_image_url = survey_data['page'].background_image_url
+
+        return {
+            'survey_content': survey_content,
+            'survey_progress': survey_progress,
+            'survey_navigation': request.env['ir.qweb']._render('survey.survey_navigation', survey_data),
+            'background_image_url': background_image_url
+        }
