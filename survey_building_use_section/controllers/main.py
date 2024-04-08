5132b524604c32954cf2ac9d9a43c42b0f4f9cd9
Unai Beristain <unaiberistain@avanzosc.es>
2024-04-08 15:50:15 +0200

    [IMP] Add more loggers

--- survey_building_use_section/controllers/main.py
+++ survey_building_use_section/controllers/main.py
@@ -187,120 +187,119 @@ class Survey(Survey):
         _logger.info(f"2024okdeb - Contenido de res2 survey_sudo:{survey_sudo}")
         _logger.info(f"2024okdeb - Contenido de res2 answer_sudo:{answer_sudo}")
 
-
         return self._prepare_question_html(survey_sudo, answer_sudo)
 
 
 
-    def _get_access_data(self, survey_token, answer_token, ensure_token=True, check_partner=True):
-        """ Get back data related to survey and user input, given the ID and access
-        token provided by the route.
+    # def _get_access_data(self, survey_token, answer_token, ensure_token=True, check_partner=True):
+    #     """ Get back data related to survey and user input, given the ID and access
+    #     token provided by the route.
 
-         : param ensure_token: whether user input existence should be enforced or not(see ``_check_validity``)
-         : param check_partner: whether the partner of the target answer should be checked (see ``_check_validity``)
-        """
-        survey_sudo, answer_sudo = request.env['survey.survey'].sudo(), request.env['survey.user_input'].sudo()
-        has_survey_access, can_answer = False, False
+    #      : param ensure_token: whether user input existence should be enforced or not(see ``_check_validity``)
+    #      : param check_partner: whether the partner of the target answer should be checked (see ``_check_validity``)
+    #     """
+    #     survey_sudo, answer_sudo = request.env['survey.survey'].sudo(), request.env['survey.user_input'].sudo()
+    #     has_survey_access, can_answer = False, False
 
-        validity_code = self._check_validity(survey_token, answer_token, ensure_token=ensure_token, check_partner=check_partner)
+    #     validity_code = self._check_validity(survey_token, answer_token, ensure_token=ensure_token, check_partner=check_partner)
         
-        _logger.info(f"2024okdeb - Contenido de validity_code:{validity_code}")
+    #     _logger.info(f"2024okdeb - Contenido de validity_code:{validity_code}")
 
-        if validity_code != 'survey_wrong':
-            survey_sudo, answer_sudo = self._fetch_from_access_token(survey_token, answer_token)
-            try:
-                survey_user = survey_sudo.with_user(request.env.user)
-                survey_user.check_access_rights(self, 'read', raise_exception=True)
-                survey_user.check_access_rule(self, 'read')
-            except:
-                pass
-            else:
-                has_survey_access = True
-            can_answer = bool(answer_sudo)
-            if not can_answer:
-                can_answer = survey_sudo.access_mode == 'public'
+    #     if validity_code != 'survey_wrong':
+    #         survey_sudo, answer_sudo = self._fetch_from_access_token(survey_token, answer_token)
+    #         try:
+    #             survey_user = survey_sudo.with_user(request.env.user)
+    #             survey_user.check_access_rights(self, 'read', raise_exception=True)
+    #             survey_user.check_access_rule(self, 'read')
+    #         except:
+    #             pass
+    #         else:
+    #             has_survey_access = True
+    #         can_answer = bool(answer_sudo)
+    #         if not can_answer:
+    #             can_answer = survey_sudo.access_mode == 'public'
 
-        return {
-            'survey_sudo': survey_sudo,
-            'answer_sudo': answer_sudo,
-            'has_survey_access': has_survey_access,
-            'can_answer': can_answer,
-            'validity_code': validity_code,
-        }
+    #     return {
+    #         'survey_sudo': survey_sudo,
+    #         'answer_sudo': answer_sudo,
+    #         'has_survey_access': has_survey_access,
+    #         'can_answer': can_answer,
+    #         'validity_code': validity_code,
+    #     }
 
-    def _check_validity(self, survey_token, answer_token, ensure_token=True, check_partner=True):
-        """ Check survey is open and can be taken. This does not checks for
-        security rules, only functional / business rules. It returns a string key
-        allowing further manipulation of validity issues
+    # def _check_validity(self, survey_token, answer_token, ensure_token=True, check_partner=True):
+    #     """ Check survey is open and can be taken. This does not checks for
+    #     security rules, only functional / business rules. It returns a string key
+    #     allowing further manipulation of validity issues
 
-         * survey_wrong: survey does not exist;
-         * survey_auth: authentication is required;
-         * survey_closed: survey is closed and does not accept input anymore;
-         * survey_void: survey is void and should not be taken;
-         * token_wrong: given token not recognized;
-         * token_required: no token given although it is necessary to access the
-           survey;
-         * answer_deadline: token linked to an expired answer;
+    #      * survey_wrong: survey does not exist;
+    #      * survey_auth: authentication is required;
+    #      * survey_closed: survey is closed and does not accept input anymore;
+    #      * survey_void: survey is void and should not be taken;
+    #      * token_wrong: given token not recognized;
+    #      * token_required: no token given although it is necessary to access the
+    #        survey;
+    #      * answer_deadline: token linked to an expired answer;
 
-        :param ensure_token: whether user input existence based on given access token
-          should be enforced or not, depending on the route requesting a token or
-          allowing external world calls;
+    #     :param ensure_token: whether user input existence based on given access token
+    #       should be enforced or not, depending on the route requesting a token or
+    #       allowing external world calls;
 
-        :param check_partner: Whether we must check that the partner associated to the target
-          answer corresponds to the active user.
-        """
-        survey_sudo, answer_sudo = self._fetch_from_access_token(survey_token, answer_token)
+    #     :param check_partner: Whether we must check that the partner associated to the target
+    #       answer corresponds to the active user.
+    #     """
+    #     survey_sudo, answer_sudo = self._fetch_from_access_token(survey_token, answer_token)
 
-        _logger.info(f"2024okdeb - Contenido de survey_sudo:{survey_sudo}")
-        _logger.info(f"2024okdeb - Contenido de survey_token:{survey_token}")
-        _logger.info(f"2024okdeb - Contenido de answer_sudo:{answer_sudo}")
-        _logger.info(f"2024okdeb - Contenido de answer_token:{answer_token}")
-
-
-        if not survey_sudo.exists():
-            return 'survey_wrong'
-
-        if answer_token and not answer_sudo:
-            return 'token_wrong'
-
-        if not answer_sudo and ensure_token:
-            return 'token_required'
-        if not answer_sudo and survey_sudo.access_mode == 'token':
-            return 'token_required'
-
-        if survey_sudo.users_login_required and request.env.user._is_public():
-            return 'survey_auth'
-
-        if not survey_sudo.active and (not answer_sudo or not answer_sudo.test_entry):
-            return 'survey_closed'
-
-        if (not survey_sudo.page_ids and survey_sudo.questions_layout == 'page_per_section') or not survey_sudo.question_ids:
-            return 'survey_void'
-
-        if answer_sudo and check_partner:
-            if request.env.user._is_public() and answer_sudo.partner_id and not answer_token:
-                # answers from public user should not have any partner_id; this indicates probably a cookie issue
-                return 'answer_wrong_user'
-            if not request.env.user._is_public() and answer_sudo.partner_id != request.env.user.partner_id:
-                # partner mismatch, probably a cookie issue
-                return 'answer_wrong_user'
-
-        if answer_sudo and answer_sudo.deadline and answer_sudo.deadline < datetime.now():
-            return 'answer_deadline'
-
-        return True
+    #     _logger.info(f"2024okdeb - Contenido de survey_sudo:{survey_sudo}")
+    #     _logger.info(f"2024okdeb - Contenido de survey_token:{survey_token}")
+    #     _logger.info(f"2024okdeb - Contenido de answer_sudo:{answer_sudo}")
+    #     _logger.info(f"2024okdeb - Contenido de answer_token:{answer_token}")
 
 
-    def _fetch_from_access_token(self, survey_token, answer_token):
-        """ Check that given token matches an answer from the given survey_id.
-        Returns a sudo-ed browse record of survey in order to avoid access rights
-        issues now that access is granted through token. """
-        survey_sudo = request.env['survey.survey'].with_context(active_test=False).sudo().search([('access_token', '=', survey_token)])
-        if not answer_token:
-            answer_sudo = request.env['survey.user_input'].sudo()
-        else:
-            answer_sudo = request.env['survey.user_input'].sudo().search([
-                ('survey_id', '=', survey_sudo.id),
-                ('access_token', '=', answer_token)
-            ], limit=1)
-        return survey_sudo, answer_sudo
+    #     if not survey_sudo.exists():
+    #         return 'survey_wrong'
+
+    #     if answer_token and not answer_sudo:
+    #         return 'token_wrong'
+
+    #     if not answer_sudo and ensure_token:
+    #         return 'token_required'
+    #     if not answer_sudo and survey_sudo.access_mode == 'token':
+    #         return 'token_required'
+
+    #     if survey_sudo.users_login_required and request.env.user._is_public():
+    #         return 'survey_auth'
+
+    #     if not survey_sudo.active and (not answer_sudo or not answer_sudo.test_entry):
+    #         return 'survey_closed'
+
+    #     if (not survey_sudo.page_ids and survey_sudo.questions_layout == 'page_per_section') or not survey_sudo.question_ids:
+    #         return 'survey_void'
+
+    #     if answer_sudo and check_partner:
+    #         if request.env.user._is_public() and answer_sudo.partner_id and not answer_token:
+    #             # answers from public user should not have any partner_id; this indicates probably a cookie issue
+    #             return 'answer_wrong_user'
+    #         if not request.env.user._is_public() and answer_sudo.partner_id != request.env.user.partner_id:
+    #             # partner mismatch, probably a cookie issue
+    #             return 'answer_wrong_user'
+
+    #     if answer_sudo and answer_sudo.deadline and answer_sudo.deadline < datetime.now():
+    #         return 'answer_deadline'
+
+    #     return True
+
+
+    # def _fetch_from_access_token(self, survey_token, answer_token):
+    #     """ Check that given token matches an answer from the given survey_id.
+    #     Returns a sudo-ed browse record of survey in order to avoid access rights
+    #     issues now that access is granted through token. """
+    #     survey_sudo = request.env['survey.survey'].with_context(active_test=False).sudo().search([('access_token', '=', survey_token)])
+    #     if not answer_token:
+    #         answer_sudo = request.env['survey.user_input'].sudo()
+    #     else:
+    #         answer_sudo = request.env['survey.user_input'].sudo().search([
+    #             ('survey_id', '=', survey_sudo.id),
+    #             ('access_token', '=', answer_token)
+    #         ], limit=1)
+    #     return survey_sudo, answer_sudo
