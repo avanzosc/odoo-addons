0e83be066f2350fe6830583b63cee76b43b1097c
oihane <oihanecruce@gmail.com>
2024-04-09 16:47:00 +0200

    [IMP] res_partner_company_change: write properties for destination company (#2480)

--- res_partner_company_change/models/res_partner_company_change.py
+++ res_partner_company_change/models/res_partner_company_change.py
@@ -78,31 +78,39 @@ class ResPartnerCompanyChange(models.Model):
         })
         return action_dict
 
-    def action_import_file(self):
-        if not self.data:
-            self.import_line_ids.unlink()
-            lines = []
-            contacts = self.env["res.partner"].search([
-                ("company_id", "=", self.company_origin_id.id),
-                ("country_id", "=", self.country_id.id),
-            ]).filtered(lambda c: c.company_type == "company")
-            childs = self.env["res.partner"].search([
-                ("parent_id", "in", contacts.ids)
-            ])
-            contacts += childs
-            for contact in distinct(contacts):
-                line_data = {
-                    "import_id": self.id,
-                    "partner_id": contact.id,
-                    "property_account_position_id": (
-                        self.property_account_position_id.id
-                    ),
-                }
-                lines.append((0, 0, line_data))
-            self.import_line_ids = lines
-            return lines
-        else:
-            return super(ResPartnerCompanyChange, self).action_import_file()
+    def _read_file(self):
+        if self.data:
+            return super()._read_file()
+        lines = []
+        partner_sudo = self.sudo().env["res.partner"]
+        contacts = partner_sudo.search([
+            ("company_id", "=", self.company_origin_id.id),
+            ("country_id", "=", self.country_id.id),
+            ('is_company', '=', True)
+        ])
+        contacts |= partner_sudo.search([("parent_id", "in", contacts.ids)])
+        for contact in contacts:
+            origin_contact = contact.with_context(
+                force_company=self.company_origin_id.id)
+            line_data = {
+                "import_id": self.id,
+                "partner_id": origin_contact.id,
+                "property_account_position_id": (
+                    self.property_account_position_id.id
+                ),
+                "customer_payment_mode_name":
+                    origin_contact.customer_payment_mode_id.name,
+                "supplier_payment_mode_name":
+                    origin_contact.supplier_payment_mode_id.name,
+                "delivery_carrier_name":
+                    origin_contact.property_delivery_carrier_id.name,
+                "payment_term_name":
+                    origin_contact.property_payment_term_id.name,
+                "supplier_payment_term_name":
+                    origin_contact.property_supplier_payment_term_id.name,
+            }
+            lines.append((0, 0, line_data))
+        return lines
 
 
 class ResPartnerCompanyChangeLine(models.Model):
@@ -127,6 +135,11 @@ class ResPartnerCompanyChangeLine(models.Model):
         string="Fiscal Position",
         comodel_name="account.fiscal.position",
     )
+    customer_payment_mode_name = fields.Char()
+    supplier_payment_mode_name = fields.Char()
+    delivery_carrier_name = fields.Char()
+    payment_term_name = fields.Char()
+    supplier_payment_term_name = fields.Char()
     customer_payment_mode_id = fields.Many2one(
         string="Customer Payment Mode",
         comodel_name="account.payment.mode",
@@ -147,59 +160,92 @@ class ResPartnerCompanyChangeLine(models.Model):
         string="Supplier Payment Term",
         comodel_name="account.payment.term",
     )
+    current_partner_company_id = fields.Many2one(
+        comodel_name="res.company",
+        related="partner_id.company_id",
+    )
 
     def _action_validate(self):
         self.ensure_one()
         update_values = super()._action_validate()
         log_infos = []
-        customer_payment_mode = supplier_payment_mode = delivery_carrier = (
-            property_payment_term) = property_supplier_payment_term = False
-        if self.partner_id.customer_payment_mode_id:
+        customer_payment_mode = supplier_payment_mode = delivery_carrier = False
+        property_payment_term = property_supplier_payment_term = False
+        partner_company = self.partner_id.with_context(
+            force_company=self.import_id.company_dest_id.id)
+        customer_payment_mode_name = (self.customer_payment_mode_name or
+                                      partner_company.customer_payment_mode_id.name)
+        if customer_payment_mode_name:
             customer_payment_mode, log_info_customer_payment_mode = (
-                self._check_customer_payment_mode()
+                self._check_customer_payment_mode(customer_payment_mode_name)
             )
             if log_info_customer_payment_mode:
                 log_infos.append(log_info_customer_payment_mode)
-        if self.partner_id.supplier_payment_mode_id:
+        supplier_payment_mode_name = (self.supplier_payment_mode_name or
+                                      partner_company.supplier_payment_mode_id.name)
+        if supplier_payment_mode_name:
             supplier_payment_mode, log_info_supplier_payment_mode = (
-                self._check_supplier_payment_mode()
+                self._check_supplier_payment_mode(supplier_payment_mode_name)
             )
             if log_info_supplier_payment_mode:
                 log_infos.append(log_info_supplier_payment_mode)
-        if self.partner_id.property_delivery_carrier_id:
+        delivery_carrier_name = (self.delivery_carrier_name or
+                                 partner_company.property_delivery_carrier_id.name)
+        if delivery_carrier_name:
             delivery_carrier, log_info_delivery_carrier = (
-                self._check_delivery_carrier()
+                self._check_delivery_carrier(delivery_carrier_name)
             )
             if log_info_delivery_carrier:
                 log_infos.append(log_info_delivery_carrier)
-        if self.partner_id.property_payment_term_id:
+        payment_term_name = (self.payment_term_name or
+                             partner_company.property_payment_term_id.name)
+        if payment_term_name:
             property_payment_term, log_info_property_payment_term = (
-                self._check_property_payment_term()
+                self._check_property_payment_term(payment_term_name)
             )
             if log_info_property_payment_term:
                 log_infos.append(log_info_property_payment_term)
-        if self.partner_id.property_supplier_payment_term_id:
+        supplier_payment_term_name = (
+            self.supplier_payment_term_name or
+            partner_company.property_supplier_payment_term_id.name
+        )
+        if supplier_payment_term_name:
             (
                 property_supplier_payment_term,
                 log_info_property_supplier_payment_term
-            ) = self._check_property_supplier_payment_term()
+            ) = self._check_property_supplier_payment_term(supplier_payment_term_name)
             if log_info_property_supplier_payment_term:
                 log_infos.append(log_info_property_supplier_payment_term)
         state = "error" if log_infos else "pass"
         action = "update" if state != "error" else "nothing"
         update_values.update(
             {
+                "customer_payment_mode_name": (
+                    customer_payment_mode_name or self.customer_payment_mode_name
+                ),
                 "customer_payment_mode_id": (
                     customer_payment_mode and customer_payment_mode.id
+                ),
+                "supplier_payment_mode_name": (
+                    supplier_payment_mode_name or self.supplier_payment_mode_name
                 ),
                 "supplier_payment_mode_id": (
                     supplier_payment_mode and supplier_payment_mode.id
                 ),
+                "delivery_carrier_name": (
+                    delivery_carrier_name or self.delivery_carrier_name
+                ),
                 "property_delivery_carrier_id": (
                     delivery_carrier and delivery_carrier.id
                 ),
+                "payment_term_name": (
+                    payment_term_name or self.payment_term_name
+                ),
                 "property_payment_term_id": (
                     property_payment_term and property_payment_term.id
+                ),
+                "supplier_payment_term_name": (
+                    supplier_payment_term_name or self.supplier_payment_term_name
                 ),
                 "property_supplier_payment_term_id": (
                     property_supplier_payment_term
@@ -216,7 +262,6 @@ class ResPartnerCompanyChangeLine(models.Model):
         update_values = super()._action_process()
         state = self.state
         if self.action == "update":
-            self = self.with_context(force_company_id=self.import_id.company_dest_id.id)
             self._update_company()
             self._update_values()
             state = "done"
@@ -227,15 +272,16 @@ class ResPartnerCompanyChangeLine(models.Model):
         )
         return update_values
 
-    def _check_customer_payment_mode(self):
+    def _check_customer_payment_mode(self, payment_mode_name):
         self.ensure_one()
         log_info = ""
-        customer_payment_mode_obj = self.env["account.payment.mode"]
+        customer_payment_mode_obj = self.sudo().env["account.payment.mode"]
         search_domain = [
-            ("name", "=", self.partner_id.customer_payment_mode_id.name),
-            ("company_id", "=", self.import_id.company_dest_id.id)
+            ("name", "=", payment_mode_name),
+            ("company_id", "=", self.import_id.company_dest_id.id),
+            ("payment_type", "=", "inbound"),
         ]
-        customer_payment_mode = customer_payment_mode_obj.sudo().search(
+        customer_payment_mode = customer_payment_mode_obj.search(
             search_domain
         )
         if not customer_payment_mode:
@@ -247,15 +293,16 @@ class ResPartnerCompanyChangeLine(models.Model):
             )
         return customer_payment_mode and customer_payment_mode[:1], log_info
 
-    def _check_supplier_payment_mode(self):
+    def _check_supplier_payment_mode(self, supplier_payment_mode_name):
         self.ensure_one()
         log_info = ""
-        supplier_payment_mode_obj = self.env["account.payment.mode"]
+        supplier_payment_mode_obj = self.sudo().env["account.payment.mode"]
         search_domain = [
-            ("name", "=", self.partner_id.supplier_payment_mode_id.name),
-            ("company_id", "=", self.import_id.company_dest_id.id)
+            ("name", "=", supplier_payment_mode_name),
+            ("company_id", "=", self.import_id.company_dest_id.id),
+            ("payment_type", "=", "outbound"),
         ]
-        supplier_payment_mode = supplier_payment_mode_obj.sudo().search(
+        supplier_payment_mode = supplier_payment_mode_obj.search(
             search_domain
         )
         if not supplier_payment_mode:
@@ -267,15 +314,15 @@ class ResPartnerCompanyChangeLine(models.Model):
             )
         return supplier_payment_mode and supplier_payment_mode[:1], log_info
 
-    def _check_delivery_carrier(self):
+    def _check_delivery_carrier(self, delivery_carrier_name):
         self.ensure_one()
         log_info = ""
-        delivery_carrier_obj = self.env["delivery.carrier"]
+        delivery_carrier_obj = self.sudo().env["delivery.carrier"]
         search_domain = [
-            ("name", "=", self.partner_id.property_delivery_carrier_id.name),
+            ("name", "=", delivery_carrier_name),
             ("company_id", "=", self.import_id.company_dest_id.id)
         ]
-        delivery_carrier = delivery_carrier_obj.sudo().search(search_domain)
+        delivery_carrier = delivery_carrier_obj.search(search_domain)
         if not delivery_carrier:
             log_info = _("No delivery carrier found.")
         elif len(delivery_carrier) > 1:
@@ -285,15 +332,15 @@ class ResPartnerCompanyChangeLine(models.Model):
             )
         return delivery_carrier and delivery_carrier[:1], log_info
 
-    def _check_property_payment_term(self):
+    def _check_property_payment_term(self, payment_term_name):
         self.ensure_one()
         log_info = ""
-        property_payment_term_obj = self.env["account.payment.term"]
+        payment_term_obj = self.sudo().env["account.payment.term"]
         search_domain = [
-            ("name", "=", self.partner_id.property_payment_term_id.name),
+            ("name", "=", payment_term_name),
             ("company_id", "=", self.import_id.company_dest_id.id)
         ]
-        property_payment_term = property_payment_term_obj.sudo().search(
+        property_payment_term = payment_term_obj.search(
             search_domain
         )
         if not property_payment_term:
@@ -305,19 +352,15 @@ class ResPartnerCompanyChangeLine(models.Model):
             )
         return property_payment_term and property_payment_term[:1], log_info
 
-    def _check_property_supplier_payment_term(self):
+    def _check_property_supplier_payment_term(self, payment_term_name):
         self.ensure_one()
         log_info = ""
-        property_supplier_payment_term_obj = self.env["account.payment.term"]
+        payment_term_obj = self.sudo().env["account.payment.term"]
         search_domain = [
-            ("name", "=", (
-                self.partner_id.property_supplier_payment_term_id.name
-            )),
+            ("name", "=", payment_term_name),
             ("company_id", "=", self.import_id.company_dest_id.id)
         ]
-        property_supplier_payment_term = (
-            property_supplier_payment_term_obj.sudo().search(search_domain)
-        )
+        property_supplier_payment_term = payment_term_obj.search(search_domain)
         if not property_supplier_payment_term:
             log_info = _("No property supplier payment term found.")
         elif len(property_supplier_payment_term) > 1:
@@ -336,12 +379,12 @@ class ResPartnerCompanyChangeLine(models.Model):
                 "company_ids": [(4, self.import_id.company_dest_id.id)],
                 "company_id": self.import_id.company_dest_id.id,
             })
-        self.partner_id.sudo().company_id = self.import_id.company_dest_id.id
 
     def _update_values(self):
         self.ensure_one()
         vals = self._get_update_values()
-        self.partner_id.sudo().write(vals)
+        self.sudo().with_context(
+            force_company=self.import_id.company_dest_id.id).partner_id.write(vals)
 
     def _get_update_values(self):
         return {
@@ -354,4 +397,5 @@ class ResPartnerCompanyChangeLine(models.Model):
             "property_supplier_payment_term_id": (
                 self.property_supplier_payment_term_id.id
             ),
+            "company_id": self.import_id.company_dest_id.id,
         }
