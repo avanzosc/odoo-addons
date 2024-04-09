0813032de0497530069d383aacdfa50712ed38ea
Unai Beristain <unaiberistain@avanzosc.es>
2024-04-09 09:32:30 +0200

    [IMP] Remove unnecessary loggers

--- survey_building_use_section/controllers/main.py
+++ survey_building_use_section/controllers/main.py
@@ -24,22 +24,16 @@ class Survey(Survey):
             triggering_question_obj = request.env['survey.question'].browse(triggering_question_id)
 
             for question in res.qcontext['survey'].question_and_page_ids:
-                _logger.info(f"2024okdeb - in for:  {res.qcontext['survey'].question_and_page_ids}")
-
                 if question.sequence > triggering_question_obj.sequence:                        
                     triggered_question = question
-                    _logger.info(f"2024okdeb - in question.sequence > triggering_question_obj.sequence:  {triggered_question}")
-
                     # If a question has no normative show it no matter the normative and
                     # remove the is conditional filter
                     if not triggered_question.question_normative_ids:
                         triggered_question.write({
                             'is_conditional': False,
                         })
-                        _logger.info(f"2024okdeb - 'is_conditional': False,: {triggered_question}")
                         
                     else:
-                        
                         # All other questions must be conditional. If they are not conditional they will display 
                         # no matter the condition
                         triggered_question.write({
@@ -87,7 +81,6 @@ class Survey(Survey):
                         and answer.value == normative.name):
                         
                         _logger.info(f"2024okdeb - Condition evaluated: {normative.start_year} <= {res.qcontext['answer'].inspected_building_id.service_start_date.year} < {normative.end_year} - Normative name: {normative.name} - Answer value: {answer.value}")
-
                         
                         # Check if a record already exists for these conditions
                         existing_user_input_line = request.env['survey.user_input.line'].search([
@@ -115,154 +108,7 @@ class Survey(Survey):
                         else:
                             _logger.info(f"2024okdeb - Survey exists: {existing_user_input_line}")
 
-                        
                         selected_answers.append(answer)                        
 
         return request.render('survey.survey_page_fill',
             self._prepare_survey_data(res.qcontext['survey'], res.qcontext['answer'], **post))
-
-
-
-    def _prepare_survey_data(self, survey_sudo, answer_sudo, **post):
-        res = super()._prepare_survey_data(survey_sudo, answer_sudo, **post)
-        _logger.info(f"2024okdeb - Contenido de res:{res}")
-        return res
-
-
-    def _prepare_question_html(self, survey_sudo, answer_sudo, **post):
-        """ Survey page navigation is done in AJAX. This function prepare the 'next page' to display in html
-        and send back this html to the survey_form widget that will inject it into the page.
-        Background url must be given to the caller in order to process its refresh as we don't have the next question
-        object at frontend side."""
-        survey_data = self._prepare_survey_data(survey_sudo, answer_sudo, **post)
-
-        _logger.info(f"2024okdeb - Contenido de survey_data:{survey_data}")
-
-        if answer_sudo.state == 'done':
-            survey_content = request.env['ir.qweb']._render('survey.survey_fill_form_done', survey_data)
-        else:
-            survey_content = request.env['ir.qweb']._render('survey.survey_fill_form_in_progress', survey_data)
-
-        survey_progress = False
-        if answer_sudo.state == 'in_progress' and not survey_data.get('question', request.env['survey.question']).is_page:
-            if survey_sudo.questions_layout == 'page_per_section':
-                page_ids = survey_sudo.page_ids.ids
-                survey_progress = request.env['ir.qweb']._render('survey.survey_progression', {
-                    'survey': survey_sudo,
-                    'page_ids': page_ids,
-                    'page_number': page_ids.index(survey_data['page'].id) + (1 if survey_sudo.progression_mode == 'number' else 0)
-                })
-            elif survey_sudo.questions_layout == 'page_per_question':
-                page_ids = (answer_sudo.predefined_question_ids.ids
-                            if not answer_sudo.is_session_answer and survey_sudo.questions_selection == 'random'
-                            else survey_sudo.question_ids.ids)
-                survey_progress = request.env['ir.qweb']._render('survey.survey_progression', {
-                    'survey': survey_sudo,
-                    'page_ids': page_ids,
-                    'page_number': page_ids.index(survey_data['question'].id)
-                })
-
-        background_image_url = survey_sudo.background_image_url
-        if 'question' in survey_data:
-            background_image_url = survey_data['question'].background_image_url
-        elif 'page' in survey_data:
-            background_image_url = survey_data['page'].background_image_url
-
-        return {
-            'survey_content': survey_content,
-            'survey_progress': survey_progress,
-            'survey_navigation': request.env['ir.qweb']._render('survey.survey_navigation', survey_data),
-            'background_image_url': background_image_url
-        }
-
-
-    def _prepare_survey_data(self, survey_sudo, answer_sudo, **post):
-        """ This method prepares all the data needed for template rendering, in function of the survey user input state.
-            :param post:
-                - previous_page_id : come from the breadcrumb or the back button and force the next questions to load
-                                     to be the previous ones. """
-        _logger.info(f"2024okdeb - Answer sudo _prepare_survey_data: {answer_sudo}")
-
-        _logger.info(f"2024okdeb - Answer sudo input line.ids: {answer_sudo.user_input_line_ids}")
-
-
-        data = {
-            'is_html_empty': is_html_empty,
-            'survey': survey_sudo,
-            'answer': answer_sudo,
-            'breadcrumb_pages': [{
-                'id': page.id,
-                'title': page.title,
-            } for page in survey_sudo.page_ids],
-            'format_datetime': lambda dt: format_datetime(request.env, dt, dt_format=False),
-            'format_date': lambda date: format_date(request.env, date)
-        }
-        if survey_sudo.questions_layout != 'page_per_question':
-            triggering_answer_by_question, triggered_questions_by_answer, selected_answers = answer_sudo._get_conditional_values()
-            data.update({
-                'triggering_answer_by_question': {
-                    question.id: triggering_answer_by_question[question].id for question in triggering_answer_by_question.keys()
-                    if triggering_answer_by_question[question]
-                },
-                'triggered_questions_by_answer': {
-                    answer.id: triggered_questions_by_answer[answer].ids
-                    for answer in triggered_questions_by_answer.keys()
-                },
-                'selected_answers': selected_answers.ids
-            })
-
-        if not answer_sudo.is_session_answer and survey_sudo.is_time_limited and answer_sudo.start_datetime:
-            data.update({
-                'server_time': fields.Datetime.now(),
-                'timer_start': answer_sudo.start_datetime.isoformat(),
-                'time_limit_minutes': survey_sudo.time_limit
-            })
-
-        page_or_question_key = 'question' if survey_sudo.questions_layout == 'page_per_question' else 'page'
-
-        # Bypass all if page_id is specified (comes from breadcrumb or previous button)
-        if 'previous_page_id' in post:
-            previous_page_or_question_id = int(post['previous_page_id'])
-            new_previous_id = survey_sudo._get_next_page_or_question(answer_sudo, previous_page_or_question_id, go_back=True).id
-            page_or_question = request.env['survey.question'].sudo().browse(previous_page_or_question_id)
-            data.update({
-                page_or_question_key: page_or_question,
-                'previous_page_id': new_previous_id,
-                'has_answered': answer_sudo.user_input_line_ids.filtered(lambda line: line.question_id.id == new_previous_id),
-                'can_go_back': survey_sudo._can_go_back(answer_sudo, page_or_question),
-            })
-            return data
-
-        if answer_sudo.state == 'in_progress':
-            if answer_sudo.is_session_answer:
-                next_page_or_question = survey_sudo.session_question_id
-            else:
-                next_page_or_question = survey_sudo._get_next_page_or_question(
-                    answer_sudo,
-                    answer_sudo.last_displayed_page_id.id if answer_sudo.last_displayed_page_id else 0)
-
-                if next_page_or_question:
-                    data.update({
-                        'survey_last': survey_sudo._is_last_page_or_question(answer_sudo, next_page_or_question)
-                    })
-
-            if answer_sudo.is_session_answer and next_page_or_question.is_time_limited:
-                data.update({
-                    'timer_start': survey_sudo.session_question_start_time.isoformat(),
-                    'time_limit_minutes': next_page_or_question.time_limit / 60
-                })
-
-            data.update({
-                page_or_question_key: next_page_or_question,
-                'has_answered': answer_sudo.user_input_line_ids.filtered(lambda line: line.question_id == next_page_or_question),
-                'can_go_back': survey_sudo._can_go_back(answer_sudo, next_page_or_question),
-            })
-            if survey_sudo.questions_layout != 'one_page':
-                data.update({
-                    'previous_page_id': survey_sudo._get_next_page_or_question(answer_sudo, next_page_or_question.id, go_back=True).id
-                })
-        elif answer_sudo.state == 'done' or answer_sudo.survey_time_limit_reached:
-            # Display success message
-            return self._prepare_survey_finished_values(survey_sudo, answer_sudo)
-
-        return data
