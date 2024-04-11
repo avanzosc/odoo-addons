2eae278c9761ea8749bf40c94b59ce935b830653
Unai Beristain <unaiberistain@avanzosc.es>
2024-04-11 14:47:59 +0200

    [IMP] survey_question_answer po

--- survey_building_use_section/models/survey_question_normative.py
+++ survey_building_use_section/models/survey_question_normative.py
@@ -30,32 +30,14 @@ class SurveyQuestionNormative(models.Model):
         inverse_name="question_normative_id",
         string="Question Articles",
     )
-    start_year = fields.Integer(
-        string="Start Year",
-        copy=False,
-    )
-    end_year = fields.Integer(
-        string="End Year",
-        copy=False,
-    )
     start_date = fields.Date(
         string="Start Date",
-        compute="_compute_start_date",
         store=True,
         copy=False,
     )
     end_date = fields.Date(
         string="End Date",
-        compute="_compute_end_date",
         store=True,
         copy=False,
     )
-
-    def init(self):
-        super(SurveyQuestionNormative, self).init()
-        for record in self.search([]):
-            if record.start_year and not record.start_date:
-                record.start_date = fields.Date.from_string(str(record.start_year) + '-01-01')
-            if record.end_year and not record.end_date:
-                record.end_date = fields.Date.from_string(str(record.end_year) + '-12-31')
 
