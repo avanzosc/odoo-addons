7aedf338c90d1e01fc1147838505ff50999fefb6
Unai Beristain <unaiberistain@avanzosc.es>
2024-04-11 15:05:26 +0200

    [IMP] survey_question_answer po

--- survey_building_use_section/models/survey_question_normative.py
+++ survey_building_use_section/models/survey_question_normative.py
@@ -1,8 +1,5 @@
 # Copyright 2024 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-import logging
-
-_logger = logging.getLogger(__name__)
 
 from odoo import fields, models, api
 
