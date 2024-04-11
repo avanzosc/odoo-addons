fd2028503648b0b664c882ab092a8d6d76bfc603
Unai Beristain <unaiberistain@avanzosc.es>
2024-04-11 15:22:15 +0200

    [IMP] survey_question_answer domain

--- survey_building_use_section/models/survey_question_normative.py
+++ survey_building_use_section/models/survey_question_normative.py
@@ -22,7 +22,7 @@ class SurveyQuestionNormative(models.Model):
         string="Error Text",
         copy=False,
     )
-    normative_question_article_ids = fields.One2many(
+    normative_question_article_ids = fields.Many2many(
         "survey.question.article",
         inverse_name="question_normative_id",
         string="Question Articles",
