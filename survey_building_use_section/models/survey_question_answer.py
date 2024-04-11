f895667fa3f5d0fcba124ea207fbe64abe075e4c
Unai Beristain <unaiberistain@avanzosc.es>
2024-04-11 15:41:49 +0200

    [IMP] survey_question_answer domain

--- survey_building_use_section/models/survey_question_answer.py
+++ survey_building_use_section/models/survey_question_answer.py
@@ -14,13 +14,6 @@ class SurveyQuestionAnswer(models.Model):
     )
     
     related_article_ids = fields.Many2many(
-        'survey.question.article', string='Related Articles'
+        'survey.question.article', string='Related Articles',
+        related='question_id.question_normative_ids.normative_question_article_ids'
     )
-
-    # Método para calcular los artículos relacionados
-    def _compute_related_article_ids(self):
-        for answer in self:
-            if answer.question_id:  # Asegurarse de que hay una pregunta asociada
-                normative_article_ids = answer.question_id.question_normative_ids.mapped('normative_question_article_ids')
-                answer.related_article_ids = normative_article_ids
-
