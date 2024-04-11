7a1588ee5730306f03a9be8773a7eea1f990d523
Unai Beristain <unaiberistain@avanzosc.es>
2024-04-11 14:35:50 +0200

    [IMP] survey_question_answer po

--- survey_building_use_section/models/survey_question_normative.py
+++ survey_building_use_section/models/survey_question_normative.py
@@ -1,6 +1,6 @@
 # Copyright 2024 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import fields, models, api
+from odoo import fields, models
 
 
 class SurveyQuestionNormative(models.Model):
@@ -26,7 +26,6 @@ class SurveyQuestionNormative(models.Model):
         inverse_name="question_normative_id",
         string="Question Articles",
     )
-
     start_year = fields.Integer(
         string="Start Year",
         copy=False,
@@ -37,19 +36,32 @@ class SurveyQuestionNormative(models.Model):
     )
     start_date = fields.Date(
         string="Start Date",
-        compute="_compute_start_date",
+        copy=False,
     )
     end_date = fields.Date(
         string="End Date",
-        compute="_compute_end_date",
+        copy=False,
     )
+    
+    def write(self, values):
+        if 'start_year' in values:
+            start_year = values['start_year']
+            start_date = fields.Date.from_string(str(start_year) + '-01-01')
+            values['start_date'] = start_date
+        if 'end_year' in values:
+            end_year = values['end_year']
+            end_date = fields.Date.from_string(str(end_year) + '-12-31')
+            values['end_date'] = end_date
+        return super(SurveyQuestionNormative, self).write(values)
 
-    def _compute_start_date(self):
-        for record in self:
-            if record.start_year:
-                record.start_date = fields.Date.from_string(str(record.start_year) + '-01-01')
-
-    def _compute_end_date(self):
-        for record in self:
-            if record.end_year:
-                record.end_date = fields.Date.from_string(str(record.end_year) + '-12-31')
+    @api.model
+    def create(self, values):
+        if 'start_year' in values:
+            start_year = values['start_year']
+            start_date = fields.Date.from_string(str(start_year) + '-01-01')
+            values['start_date'] = start_date
+        if 'end_year' in values:
+            end_year = values['end_year']
+            end_date = fields.Date.from_string(str(end_year) + '-12-31')
+            values['end_date'] = end_date
+        return super(SurveyQuestionNormative, self).create(values)
