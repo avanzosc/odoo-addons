f127f34af2cc07131dda5ee1748a511aa55db9b9
Unai Beristain <unaiberistain@avanzosc.es>
2024-04-12 08:54:04 +0200

    [IMP] Improvemet add api

--- survey_building_use_section/models/survey_question_answer.py
+++ survey_building_use_section/models/survey_question_answer.py
@@ -1,6 +1,6 @@
 # Copyright 2024 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class SurveyQuestionAnswer(models.Model):
