bb6d0b96c24098436b2a90dc3d47192cb3ac84ab
Unai Beristain <unaiberistain@avanzosc.es>
2024-04-12 09:55:02 +0200

    [IMP] Add js script

--- survey_building_use_section/static/src/js/th_articles_width.js
+++ survey_building_use_section/static/src/js/th_articles_width.js
@@ -2,6 +2,7 @@ odoo.define('survey_building_use_section.th_articles_width', function (require) 
     "use strict";
 
     var core = require('web.core');
+    var SurveyForm = require('survey.form'); // Importar el módulo survey.form
 
     // Función para verificar si la URL contiene el parámetro model=survey
     function checkURLParameter() {
@@ -29,9 +30,11 @@ odoo.define('survey_building_use_section.th_articles_width', function (require) 
                         console.log("Current style for question_article_ids:", currentStyle);
                         // Verificar si el estilo actual está definido
                         if (currentStyle) {
+                            // Agregar 500px al estilo actual
                             thElements[i].style.width = (parseInt(thElements[i].style.width) + 500) + 'px';
                             console.log("Adjusted width for question_article_ids:", thElements[i].style.width);
                         } else {
+                            // Si el estilo no está definido, establecer un nuevo estilo con 500px de ancho
                             thElements[i].style.width = '500px';
                             console.log("Set new width for question_article_ids:", thElements[i].style.width);
                         }
@@ -41,10 +44,12 @@ odoo.define('survey_building_use_section.th_articles_width', function (require) 
                         console.log("Current style for value:", currentStyle);
                         // Verificar si el estilo actual está definido
                         if (currentStyle) {
+                            // Quitar 400px del ancho actual
                             thElements[i].style.width = (parseInt(thElements[i].style.width) - 400) + 'px';
                             console.log("Adjusted width for value:", thElements[i].style.width);
                         } else {
-                            thElements[i].style.width = '400px'; // Set default width if style not defined
+                            // Si el estilo no está definido, establecer un nuevo estilo con 400px menos de ancho
+                            thElements[i].style.width = '100px'; // Set default width if style not defined
                             console.log("Set new width for value:", thElements[i].style.width);
                         }
                     }
