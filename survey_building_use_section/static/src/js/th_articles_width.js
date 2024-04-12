e807e68e312c8bdf9be99adef892403255ad7b0e
Unai Beristain <unaiberistain@avanzosc.es>
2024-04-12 09:33:05 +0200

    [IMP] Add js script

--- survey_building_use_section/static/src/js/th_articles_width.js
+++ survey_building_use_section/static/src/js/th_articles_width.js
@@ -6,32 +6,47 @@ odoo.define('survey_building_use_section.th_articles_width', function (require) 
     // Función para verificar si la URL contiene el parámetro model=survey
     function checkURLParameter() {
         var urlParams = new URLSearchParams(window.location.search);
-        return urlParams.has('model') && urlParams.get('model') === 'survey';
+        return urlParams.has('model') && urlParams.get('model').startsWith('survey');
     }
 
     // Verificar si la URL contiene el parámetro model=survey
     if (checkURLParameter()) {
-        // Obtener todos los elementos th en la página
-        var thElements = document.getElementsByTagName('th');
+        // Agregar event listener para el evento click en los elementos <tr>
+        document.addEventListener('click', function(event) {
+            var target = event.target;
+            // Verificar si el elemento clickeado es un <tr>
+            if (target.tagName === 'TR') {
+                // Obtener todos los elementos th en la página
+                var thElements = document.getElementsByTagName('th');
 
-        // Iterar sobre los elementos th
-        for (var i = 0; i < thElements.length; i++) {
-            // Verificar si el atributo data-name es "question_article_ids"
-            if (thElements[i].getAttribute('data-name') === "question_article_ids") {
-                // Obtener el estilo actual del elemento th
-                var currentStyle = thElements[i].getAttribute('style');
-                // Verificar si el estilo actual está definido
-                if (currentStyle) {
-                    // Agregar 500px al estilo actual
-                    thElements[i].setAttribute('style', currentStyle + ' width: 876px;');
-                } else {
-                    // Si el estilo no está definido, establecer un nuevo estilo con 500px de ancho
-                    thElements[i].setAttribute('style', 'width: 876px;');
+                // Iterar sobre los elementos th
+                for (var i = 0; i < thElements.length; i++) {
+                    // Verificar si el atributo data-name es "question_article_ids" o "value"
+                    if (thElements[i].getAttribute('data-name') === "question_article_ids") {
+                        // Obtener el estilo actual del elemento th
+                        var currentStyle = thElements[i].getAttribute('style');
+                        // Verificar si el estilo actual está definido
+                        if (currentStyle) {
+                            // Agregar 500px al estilo actual
+                            thElements[i].setAttribute('style', currentStyle + ' width: 876px;');
+                        } else {
+                            // Si el estilo no está definido, establecer un nuevo estilo con 500px de ancho
+                            thElements[i].setAttribute('style', 'width: 876px;');
+                        }
+                    } else if (thElements[i].getAttribute('data-name') === "value") {
+                        // Obtener el estilo actual del elemento th
+                        var currentStyle = thElements[i].getAttribute('style');
+                        // Verificar si el estilo actual está definido
+                        if (currentStyle) {
+                            // Quitar 400px del ancho actual
+                            thElements[i].setAttribute('style', currentStyle + ' width: 353px;');
+                        } else {
+                            // Si el estilo no está definido, establecer un nuevo estilo con 400px menos de ancho
+                            thElements[i].setAttribute('style', 'width: 353px;');
+                        }
+                    }
                 }
-                // Detener el bucle después de encontrar el primer elemento que cumpla con la condición
-                break;
             }
-        }
+        });
     }
 });
-
