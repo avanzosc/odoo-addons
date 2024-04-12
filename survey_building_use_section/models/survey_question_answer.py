5112067e7e9856bbf786e6bbc6f2c38e196f80e0
Unai Beristain <unaiberis@gmail.com>
2024-04-12 20:01:57 +0200

    [IMP] Add second api depends

--- survey_building_use_section/models/survey_question_answer.py
+++ survey_building_use_section/models/survey_question_answer.py
@@ -7,32 +7,35 @@ class SurveyQuestionAnswer(models.Model):
     _inherit = "survey.question.answer"
 
     notes = fields.Text(
-        string="Note", help="Error Text", copy=False,
+        string="Note",
+        help="Error Text",
+        copy=False,
     )
-    survey_id = fields.Many2one(
-        'survey.survey',
-        related="question_id.survey_id"
-    )
+    survey_id = fields.Many2one("survey.survey", related="question_id.survey_id")
     matrix_survey_id = fields.Many2one(
-        'survey.survey',
-        related="matrix_question_id.survey_id"
+        "survey.survey", related="matrix_question_id.survey_id"
     )
     question_article_ids = fields.Many2many(
-        'survey.question.article', 
-        string='Articles',
-        relation='survey_question_answer_article_rel', 
-        column1='question_answer_id', 
-        column2='article_id'  
+        "survey.question.article",
+        string="Articles",
+        relation="survey_question_answer_article_rel",
+        column1="question_answer_id",
+        column2="article_id",
     )
     related_article_filter_ids = fields.Many2many(
-        'survey.question.article',
+        "survey.question.article",
         string="Related Articles",
-        compute='_compute_related_article_filter_ids',
-        store=True
+        compute="_compute_related_article_filter_ids",
+        store=True,
     )
 
-    @api.depends('matrix_question_id.question_normative_ids.related_article_ids')
+    @api.depends(
+        "matrix_question_id.question_normative_ids.related_article_ids",
+        "matrix_question_id.question_normative_ids",
+    )
     def _compute_related_article_filter_ids(self):
         for record in self:
-            related_articles = record.matrix_question_id.question_normative_ids.mapped('related_article_ids')
+            related_articles = record.matrix_question_id.question_normative_ids.mapped(
+                "related_article_ids"
+            )
             record.related_article_filter_ids = [(6, 0, related_articles.ids)]
