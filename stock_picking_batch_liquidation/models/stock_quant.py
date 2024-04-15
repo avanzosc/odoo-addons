587e20d370cc3f36d270ab16141c01e1d75e2dd5
Tu Nombre <you@example.com>
2024-04-15 16:15:02 +0200

    [14.0][IMP] stock_picking_batch_liquidation: Add some related fields.

---
+++ stock_picking_batch_liquidation/models/stock_quant.py
@@ -0,0 +1,13 @@
+# Copyright 2024 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import fields, models
+
+
+class StockQuant(models.Model):
+    _inherit = "stock.quant"
+
+    move_type_id = fields.Many2one(
+        string="Move Type",
+        comodel_name="move.type",
+        related="product_id.categ_id.move_type_id",
+        store=True)
