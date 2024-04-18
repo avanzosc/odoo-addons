f33d48be033f76e975d4666cacc4788d46ed8f4d
Alfredo <alfredodelafuente@avanzosc.es>
2024-04-18 08:31:05 +0200

    [ADD] alfredo_reports: Customized report.

---
+++ alaia_reports/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2024 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Alaia Reports",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "sale",
+    ],
+    "author": "AvanzOSC",
+    "website": "https://bitbucket.org/centropuertas/centropuertas/",
+    "category": "Custom",
+    "data": [
+        "report/sale_order_report.xml",
+    ],
+    "installable": True,
+}
