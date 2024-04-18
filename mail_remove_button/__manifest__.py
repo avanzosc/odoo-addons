c5531307f029e718c95ea46a3eb8aedfaa34a818
Unai Beristain <unaiberistain@avanzosc.es>
2024-04-18 11:17:05 +0200

    [ADD] mail_remove_button
    [IMP] manifest and Readme improvements
    
    [FIX] Error improvement
    
    [FIX] Error improvement
    
    [FIX] Error improvement
    
    [FIX] Error improvement

---
+++ mail_remove_button/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2024 Unai Beristain - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Remove Mail Button",
+    "version": "14.0.1.0.0",
+    "category": "Custom",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": ["mail"],
+    "data": [
+        "views/custom_mail_templates.xml",
+    ],
+    "installable": True,
+}
