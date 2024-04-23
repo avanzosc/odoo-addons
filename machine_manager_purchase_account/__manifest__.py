38f4b5ff2d643dc9e6685683f3d33e607e681d04
Alfredo <alfredodelafuente@avanzosc.es>
2024-04-23 12:25:25 +0200

    [ADD] machine_manager_purchase_account: In "Holds Machine Records" object, new field: "Purchase Invoice".

---
+++ machine_manager_purchase_account/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright 2024 Alfredo de la Fuente - AvanzOSC
+# Copyright 2015 Daniel Campos - AvanzOSC
+# Copyright 2015 Esther Martín - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Machine Manager Purchase Account",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "OdooMRP team, "
+              "AvanzOSC, "
+              "Serv. Tecnol. Avanzados - Pedro M. Baeza",
+    "contributors": [
+        "Daniel Campos <danielcampos@avanzosc.es>",
+        "Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>",
+        "Ana Juaristi <ajuaristio@gmail.com>",
+        "Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>",
+        "Esther Martín <esthermartin@avanzosc.es>",
+    ],
+    "website": "http://avanzosc.com",
+    "depends": [
+        "account",
+        "machine_manager_purchase",
+    ],
+    "category": "Machinery Management",
+    "data": [
+        "views/machine_views.xml",
+    ],
+    "installable": True,
+    "auto_install": True,
+}
