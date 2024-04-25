0233516c5b792f2ef5d15aaaa30567839f3c8e38
Unai Beristain <unaiberistain@avanzosc.es>
2024-04-25 15:50:20 +0200

    [IMP] Add fields and translations

--- res_partner_building_use_section/models/res_partner.py
+++ res_partner_building_use_section/models/res_partner.py
@@ -1,46 +1,38 @@
 # Copyright 2024 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import fields, models, api, _
+from odoo import fields, models, api
+
 
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
     building_use_id = fields.Many2one(
+        string="Building use",
         comodel_name="building.use",
-        string=_("Building use"),
         copy=False,
     )
     building_section_ids = fields.One2many(
+        string="Building Section/Area",
         comodel_name="building.section",
         inverse_name="partner_id",
-        string=_("Building Section/Area"),
         copy=False,
     )
     service_start_date = fields.Date(
-        string=_("Service Start Date"),
+        string="Service Start Date",
         copy=False,
     )
     service_end_date = fields.Date(
-        string=_("Service End Date"),
+        string="Service End Date",
         copy=False,
     )
 
-    maintainer_id = fields.Many2one(
-        comodel_name="res.partner",
-        string=_("Maintainer")
-    )
-    installer_id = fields.Many2one(
-        comodel_name="res.partner",
-        string=_("Installer")
-    )
-    administrator_id = fields.Many2one(
-        comodel_name="res.partner",
-        string=_("Administrator")
-    )
+    maintainer_id = fields.Many2one("res.partner", string="Maintainer")
+    installer_id = fields.Many2one("res.partner", string="Installer")
+    administrator_id = fields.Many2one("res.partner", string="Administrator")
 
     normativas_ids = fields.Many2many(
-        comodel_name="survey.question.normative",
-        string=_("Normativas"),
+        "survey.question.normative",
+        string="Normativas",
         compute="_compute_normativas_ids",
     )
 
