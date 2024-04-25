aba0d54772272a5e977e3993c990be7ec0dd8396
Unai Beristain <unaiberistain@avanzosc.es>
2024-04-25 16:03:39 +0200

    [IMP] Add fields and translations

--- res_partner_building_use_section/models/res_partner.py
+++ res_partner_building_use_section/models/res_partner.py
@@ -1,28 +1,28 @@
 # Copyright 2024 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import fields, models, api
+from odoo import fields, models, api, _
 
 
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
     building_use_id = fields.Many2one(
-        string="Building use",
+        string=_("Building use"),
         comodel_name="building.use",
         copy=False,
     )
     building_section_ids = fields.One2many(
-        string="Building Section/Area",
+        string=_("Building Section/Area"),
         comodel_name="building.section",
         inverse_name="partner_id",
         copy=False,
     )
     service_start_date = fields.Date(
-        string="Service Start Date",
+        string=_("Service Start Date"),
         copy=False,
     )
     service_end_date = fields.Date(
-        string="Service End Date",
+        string=_("Service End Date"),
         copy=False,
     )
     
@@ -37,13 +37,13 @@ class ResPartner(models.Model):
         help=_("Membership number of the individual contact."),
     )
 
-    maintainer_id = fields.Many2one("res.partner", string="Maintainer")
-    installer_id = fields.Many2one("res.partner", string="Installer")
-    administrator_id = fields.Many2one("res.partner", string="Administrator")
+    maintainer_id = fields.Many2one(_("Maintainer"), "res.partner")
+    installer_id = fields.Many2one(_("Installer"), "res.partner")
+    administrator_id = fields.Many2one(_("Administrator"), "res.partner")
 
     normativas_ids = fields.Many2many(
         "survey.question.normative",
-        string="Normativas",
+        string=_("Normativas"),
         compute="_compute_normativas_ids",
     )
 
