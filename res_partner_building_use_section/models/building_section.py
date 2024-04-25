5d6fd8073644b8dd4825225f089ee3bd5e830bff
Unai Beristain <unaiberistain@avanzosc.es>
2024-04-25 16:04:53 +0200

    [IMP] Add fields and translations

--- res_partner_building_use_section/models/building_section.py
+++ res_partner_building_use_section/models/building_section.py
@@ -22,13 +22,13 @@ class BuildingSection(models.Model):
     # Project
     project_title = fields.Char(string=_("Project Title"))
     project_author_id = fields.Many2one(string=_("Project Author"), comodel_name="res.partner")
-    project_author_degree = fields.Char(string=_("Project Author Degree"), related="project_author_id.membership_number")
-    project_author_license = fields.Char(string=_("Project Author License"), related="project_author_id.colegiado_numero")
+    project_author_degree = fields.Char(string=_("Project Author Degree"), related="project_author_id.degree_title")
+    project_author_license = fields.Char(string=_("Project Author License"), related="project_author_id.membership_number")
     project_approved_date = fields.Date(string=_("Project Approved Date"))
 
     # Certificate of Final Work Direction
     dof_author_id = fields.Many2one(string=_("Director of Works Author"), comodel_name="res.partner")
-    dof_author_degree = fields.Char(string=_("Director of Works Author Degree"), related="dof_author_id.membership_number")
-    dof_author_license = fields.Char(string=_("Director of Works Author License"), related="dof_author_id.colegiado_numero")
+    dof_author_degree = fields.Char(string=_("Director of Works Author Degree"), related="dof_author_id.degree_title")
+    dof_author_license = fields.Char(string=_("Director of Works Author License"), related="dof_author_id.membership_number")
     dof_approved_date = fields.Date(string=_("Director of Works Approved Date"))
 
