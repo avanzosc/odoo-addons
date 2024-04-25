313cf882ac386466fbce6189adce796a2c6189f4
Unai Beristain <unaiberistain@avanzosc.es>
2024-04-25 15:54:02 +0200

    [IMP] Add fields and translations

--- res_partner_building_use_section/models/building_section.py
+++ res_partner_building_use_section/models/building_section.py
@@ -21,7 +21,7 @@ class BuildingSection(models.Model):
     # Project
     project_title = fields.Char(string=_("Project Title"))
     project_author_id = fields.Many2one(string=_("Project Author"), comodel_name="res.partner")
-    project_author_degree = fields.Char(string=_("Project Author Degree"), related="project_author_id.degree_title")
+    project_author_degree = fields.Char(string=_("Project Author Degree"), related="project_author_id.membership_number")
     project_author_license = fields.Char(string=_("Project Author License"), related="project_author_id.colegiado_numero")
     project_approved_date = fields.Date(string=_("Project Approved Date"))
 
