e37166b56eb799bc2a70e49f59877d6a9b7da27a
Unai Beristain <unaiberistain@avanzosc.es>
2024-04-26 10:09:07 +0200

    [IMP] Improve PR

--- survey_building_type_section/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import res_partner
-from . import survey_question_answer
-from . import survey_question_normative
-from . import survey_question
-from . import survey_survey
-from . import survey_user_input
-from . import survey_user_input_line
