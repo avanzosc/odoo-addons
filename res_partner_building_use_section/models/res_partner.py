03d6f2c646f8d3a48a3beea4a6639ebe9b73c22a
Unai Beristain <unaiberistain@avanzosc.es>
2024-04-26 10:23:21 +0200

    [IMP] Improve PR

---
+++ res_partner_building_use_section/models/res_partner.py
@@ -0,0 +1,30 @@
+# Copyright 2024 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    building_use_id = fields.Many2one(
+        string="Building use",
+        comodel_name="building.use",
+        copy=False,
+    )
+    building_section_ids = fields.One2many(
+        string="Building Section/Area",
+        comodel_name="building.section",
+        inverse_name="partner_id",
+        copy=False,
+    )
+    service_start_date = fields.Date(
+        copy=False,
+    )
+    service_end_date = fields.Date(
+        copy=False,
+    )
+    maintainer_id = fields.Many2one(comodel_name="res.partner", string="Maintainer")
+    installer_id = fields.Many2one(comodel_name="res.partner", string="Installer")
+    administrator_id = fields.Many2one(
+        comodel_name="res.partner", string="Administrator"
+    )
