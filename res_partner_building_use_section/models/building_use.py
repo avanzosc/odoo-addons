e8d3c099715ff7bdb546fe4919ec5afd12e7476b
Unai Beristain <unaiberistain@avanzosc.es>
2024-04-26 10:16:42 +0200

    [IMP] Improve PR

---
+++ res_partner_building_use_section/models/building_use.py
@@ -0,0 +1,15 @@
+# Copyright 2024 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import fields, models
+
+
+class BuildingUse(models.Model):
+    _name = "building.use"
+    _description = "Building use"
+
+    name = fields.Char(
+        string="Description",
+        required=True,
+        copy=False,
+    )
+    is_industrial = fields.Boolean(string="Industrial")
