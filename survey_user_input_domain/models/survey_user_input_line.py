62c37fad967f40c6c4c7209a8c64ee177c299bc4
Unai Beristain <unaiberistain@avanzosc.es>
2024-05-02 15:36:43 +0200

    [IMP] Improve pre-commit errors

--- survey_user_input_domain/models/survey_user_input_line.py
+++ survey_user_input_domain/models/survey_user_input_line.py
@@ -1,40 +1,36 @@
-from odoo import models, fields, api
+# Copyright 2024 Unai Beristain - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import api, fields, models
+
 
 class SurveyUserInputLine(models.Model):
-    _inherit = 'survey.user_input.line'
+    _inherit = "survey.user_input.line"
 
-    # Define el campo calculado
     calculated_matrix_question_id = fields.Many2one(
-        'survey.question.answer', 
-        string='Matrix Question ID',
-        compute='_compute_matrix_question_id',
-        store=True  # Almacenar el valor calculado en la base de datos
+        comodel_name="survey.question",
+        string="Matrix Question ID",
+        compute="_compute_matrix_question_id",
+        store=True,
     )
-    
-    # Define el campo calculado
     calculated_suggested_answer_question_id = fields.Many2one(
-        'survey.question.answer', 
-        string='Suggested Answer ID',
-        compute='_compute_suggested_answer_id',
-        store=True  # Almacenar el valor calculado en la base de datos
+        comodel_name="survey.question",
+        string="Suggested Answer ID",
+        compute="_compute_suggested_answer_id",
+        store=True,
     )
 
-
-    # Método de cálculo para el campo calculado
-    @api.depends('matrix_row_id')
+    @api.depends("matrix_row_id", "matrix_row_id.matrix_question_id")
     def _compute_matrix_question_id(self):
         for line in self:
-            if line.matrix_row_id:
-                line.calculated_matrix_question_id = line.matrix_row_id.matrix_question_id.id
-            else:
-                line.calculated_matrix_question_id = False
+            line.calculated_matrix_question_id = (
+                line.matrix_row_id.matrix_question_id if line.matrix_row_id else False
+            )
 
-
-    # Método de cálculo para el campo calculado
-    @api.depends('suggested_answer_id')
+    @api.depends("suggested_answer_id", "suggested_answer_id.question_id")
     def _compute_suggested_answer_id(self):
         for line in self:
-            if line.question_id:
-                line.calculated_suggested_answer_question_id = line.suggested_answer_id.question_id.id
-            else:
-                line.calculated_suggested_answer_question_id = False
+            line.calculated_suggested_answer_question_id = (
+                line.suggested_answer_id.question_id
+                if line.suggested_answer_id
+                else False
+            )
