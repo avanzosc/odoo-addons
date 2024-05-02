8b017e5f01f8dafdfe5f49608fdff933b07de115
unaiberis <146723346+unaiberis@users.noreply.github.com>
2024-05-02 15:36:02 +0200

    [16.0][MIG] product_editable_tree_menu: Migration to 16.0 (#2277)
    * [ADD] product_editable_tree_menu: In Sales - Products, new menu "Products editable tree". (#1508)
    
    * [IMP] product_editable_tree_menu: remove dependency (#1543)
    
    * [IMP] product_editable_tree_menu: standar price editable in list (#1571)
    
    * [IMP] product_editable_tree_menu: pre-commit stuff
    
    * [IMP] product_editable_tree_menu: pre-commit execution
    
    * [MIG] product_editable_tree_menu: Migration to 16.0
    
    ---------
    
    Co-authored-by: alfredoavanzosc <alfredodelafuente@avanzosc.es>
    Co-authored-by: oihane <oihanecruce@gmail.com>

---
+++ product_editable_tree_menu/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2022 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Product Editable Tree Menu",
+    "version": "16.0.1.0.0",
+    "category": "Sales/Sales",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": [
+        "product",
+        "sale",
+        "sales_team",
+    ],
+    "data": [
+        "views/product_template_views.xml",
+    ],
+    "installable": True,
+}
