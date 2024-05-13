f72c57fd17e95edaa316ab87557e5c9a74796bb2
Alfredo <alfredodelafuente@avanzosc.es>
2024-05-13 12:09:39 +0200

    [ADD] res_partner_tecnology: New object "Tecnology" for partners.

---
+++ res_partner_technology/models/res_partner_technology.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+from odoo import fields, models
+
+
+class ResPartnerTechnology(models.Model):
+    _name = "res.partner.technology"
+    _description = "Customer technology"
+    _order = "name"
+
+    name = fields.Char(
+        string="Description", required=True, copy=False
+    )
