dc8a9a3448f2cfb6e195cab196aaabd4f15a0bcc
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2024-05-17 10:49:35 +0200

    [ADD] machine_manager_purchase_account: In "Holds Machine Records" object, new field: "Purchase Invoice". (#2509)

---
+++ machine_manager_purchase_account/models/machine.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015 Daniel Campos - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+from odoo import models, fields
+
+
+class Machine(models.Model):
+    _inherit = "machine"
+
+    purch_inv_id = fields.Many2one(
+        string="Purchase Invoice", comodel_name="account.move"
+    )
