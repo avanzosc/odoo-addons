5488eb55d57e4c64c1896108990f17b0f0f86e5c
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2024-05-17 12:15:10 +0200

    [ADD] res_partner_market: New object "Market" for partner. (#2547)
    Co-authored-by: Ana Juaristi <ajuaristio@gmail.com>

---
+++ res_partner_market/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Res Partner Market",
+    "summary": "Market For Customers",
+    "version": "16.0.1.0.0",
+    "category": "Sales/CRM",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "contributors": [
+        "Ana Juaristi <anajuaristi@avanzosc.es>",
+        "Alfredo de la Fuente <alfredodelafuente@avanzosc.es>",
+    ],
+    "depends": [
+        "contacts",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/res_partner_market_views.xml",
+        "views/res_partner_views.xml",
+    ],
+    "installable": True,
+}
