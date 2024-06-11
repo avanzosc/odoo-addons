4ee2a3dea64c027ecf9b84af9fc2a4841eae38c8
Alfredo <alfredodelafuente@avanzosc.es>
2024-06-11 13:19:47 +0200

    [ADD] purchase_order_line_seller: In purchase order line new field "Supplier Pricelist".

---
+++ purchase_order_line_seller/models/product_supplierinfo.py
@@ -0,0 +1,21 @@
+# Copyright 2024 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+from odoo import models
+
+
+class ProductSupplierinfo(models.Model):
+    _inherit = "product.supplierinfo"
+
+    def name_get(self):
+        result = []
+        for supplierinfo in self:
+            product_name = (
+                supplierinfo.product_name if supplierinfo.product_name else
+                supplierinfo.name.name)
+            if supplierinfo.product_code:
+                name = u"[{}] {}".format(
+                    supplierinfo.product_code, product_name)
+            else:
+                name = u"{}".format(product_name)
+            result.append((supplierinfo.id, name))
+        return result
