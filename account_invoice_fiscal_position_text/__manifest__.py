5b59bf8d9c09a464e3900c04cbcc8aa0f459d4cf
manolo <software@klo.es>
2024-06-18 11:53:52 +0200

    [IMP] account_invoice_fiscal_position_text: black, isort, prettier

--- account_invoice_fiscal_position_text/__manifest__.py
+++ account_invoice_fiscal_position_text/__manifest__.py
@@ -2,18 +2,15 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Account Invoice Fiscal Position Text',
-    'version': '12.0.1.0.0',
+    "name": "Account Invoice Fiscal Position Text",
+    "version": "12.0.1.0.0",
     "category": "Invoicing Management",
-    'author':  "AvanzOSC",
-    'license': "AGPL-3",
-    'website': 'http://www.avanzosc.es',
-    'depends': [
-        "base",
-        "account"
+    "author": "AvanzOSC",
+    "license": "AGPL-3",
+    "website": "http://www.avanzosc.es",
+    "depends": ["base", "account"],
+    "data": [
+        "views/account_invoice_fiscal_position_text_view.xml",
     ],
-    'data': [
-        'views/account_invoice_fiscal_position_text_view.xml',
-        ],
-    'installable': True
+    "installable": True,
 }
