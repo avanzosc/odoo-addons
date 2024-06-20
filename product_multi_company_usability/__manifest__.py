a70bcdc56752c7d9d2fdb5f3050f77e92cff1878
Tu Nombre <you@example.com>
2024-06-20 07:49:27 +0200

    [12.0][BKP] product_multi_company_usability: Backport to 12.0

--- product_multi_company_usability/__manifest__.py
+++ product_multi_company_usability/__manifest__.py
@@ -2,13 +2,13 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Product Multi Company Usability",
-    'version': '14.0.1.0.0',
+    'version': '12.0.1.0.0',
     "category": "Inventory",
     "license": "AGPL-3",
     "author": "AvanzOSC",
     "website": "https://github.com/avanzosc/odoo-addons",
     "depends": [
-        "stock",
+        "product",
     ],
     "data": [
         "security/product_company_security.xml",
