cb9a19a8ad00ce6fe8a4e914f65d44ca36915b82
alfredo <alfredodelafuente@avanzosc.es>
2024-06-21 09:08:13 +0200

    [ADD] account_analytic_line_show_archived: In account analytic line new field "no_archived"

---
+++ account_analytic_line_show_archived/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# (c) 2024 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Account Analytic Line Show Archived",
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "contributors": [
+        "Ana Juaristi <anajuaristi@avanzosc.es>",
+        "Alfredo de la Fuente <alfredodelafuente@avanzosc.es",
+    ],
+    "category": "Hidden/Dependency",
+    "depends": [
+        "analytic",
+    ],
+    "data": [
+        "views/account_analytic_line_views.xml",
+        ],
+    "installable": True,
+}
