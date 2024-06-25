dd972dbdc1d0d02a5d4caff432865e895d737507
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-06-25 15:27:43 +0200

    [IMP] pre-commit stuff

--- machine_manager_purchase_account/models/machine.py
+++ machine_manager_purchase_account/models/machine.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Daniel Campos - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from odoo import models, fields
+from odoo import fields, models
 
 
 class Machine(models.Model):
