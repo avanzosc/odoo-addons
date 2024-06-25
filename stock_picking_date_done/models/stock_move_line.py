066cd8a251b37a4ac3f046125318b1d25051562e
oihane <oihanecruce@gmail.com>
2024-06-25 15:33:04 +0200

    [IMP] pre-commit stuff (#2627)

--- stock_picking_date_done/models/stock_move_line.py
+++ stock_picking_date_done/models/stock_move_line.py
@@ -4,13 +4,14 @@ from odoo import models
 
 
 class StockMoveLine(models.Model):
-    _inherit = 'stock.move.line'
+    _inherit = "stock.move.line"
 
     def write(self, vals):
-        result = super(StockMoveLine, self).write(vals)
-        if ('with_custom_date_done' not in self.env.context and 'date' in vals):
+        result = super().write(vals)
+        if "with_custom_date_done" not in self.env.context and "date" in vals:
             for line in self:
                 if line.picking_id.custom_date_done:
-                    line.with_context(with_custom_date_done=True).date = (
-                        line.picking_id.custom_date_done)
+                    line.with_context(
+                        with_custom_date_done=True
+                    ).date = line.picking_id.custom_date_done
         return result
