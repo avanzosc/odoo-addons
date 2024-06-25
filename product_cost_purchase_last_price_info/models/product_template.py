83dd3c55016d51bfd447cd1f60beff3f5c92625d
oihane <oihanecruce@gmail.com>
2024-06-25 15:33:04 +0200

    [IMP] pre-commit stuff (#2627)

--- product_cost_purchase_last_price_info/models/product_template.py
+++ product_cost_purchase_last_price_info/models/product_template.py
@@ -1,6 +1,6 @@
 # Copyright 2023 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import api, models, fields
+from odoo import api, fields, models
 
 
 class ProductTemplate(models.Model):
@@ -8,45 +8,50 @@ class ProductTemplate(models.Model):
 
     last_purchase_price_company_currency = fields.Float(
         compute="_compute_last_purchase_price_company_currency",
-        string="LPP in company currency", store=True, copy=False
+        string="LPP in company currency",
+        store=True,
+        copy=False,
     )
     average_price_value = fields.Float(
-        compute="_compute_average_price_value",
-        string="Average price value"
+        compute="_compute_average_price_value", string="Average price value"
     )
     last_purchase_value = fields.Float(
-        compute="_compute_last_purchase_value",
-        string="Last purchase value"
+        compute="_compute_last_purchase_value", string="Last purchase value"
     )
     net_purchase_price = fields.Float(
-        compute="_compute_net_purchase_price",
-        string="Net purchase price")
+        compute="_compute_net_purchase_price", string="Net purchase price"
+    )
 
-    @api.depends("product_variant_ids",
-                 "product_variant_ids.last_purchase_price_company_currency")
+    @api.depends(
+        "product_variant_ids",
+        "product_variant_ids.last_purchase_price_company_currency",
+    )
     def _compute_last_purchase_price_company_currency(self):
         for template in self:
             last_purchase_price_company_currency = 0
             if template.product_variant_ids:
                 variant = template.product_variant_ids[0]
                 last_purchase_price_company_currency = (
-                    variant.last_purchase_price_company_currency)
+                    variant.last_purchase_price_company_currency
+                )
             template.last_purchase_price_company_currency = (
-                last_purchase_price_company_currency)
+                last_purchase_price_company_currency
+            )
 
     def _compute_average_price_value(self):
         for template in self:
             template.average_price_value = (
-                template.qty_available * template.standard_price)
+                template.qty_available * template.standard_price
+            )
 
     def _compute_last_purchase_value(self):
         for template in self:
             last_purchase_value = 0
             if len(template.product_variant_ids) == 1:
                 variant = template.product_variant_ids[0]
-                last_purchase_value = (
-                    template.qty_available *
-                    round(variant.last_purchase_price_company_currency, 2))
+                last_purchase_value = template.qty_available * round(
+                    variant.last_purchase_price_company_currency, 2
+                )
             template.last_purchase_value = last_purchase_value
 
     def _compute_net_purchase_price(self):
@@ -54,7 +59,7 @@ class ProductTemplate(models.Model):
             net_purchase_price = 0
             if len(template.product_variant_ids) == 1:
                 variant = template.product_variant_ids[0]
-                net_purchase_price = (
-                    variant.qty_available *
-                    round(variant.last_purchase_net_unit_price, 2))
+                net_purchase_price = variant.qty_available * round(
+                    variant.last_purchase_net_unit_price, 2
+                )
             template.net_purchase_price = net_purchase_price
--- product_cost_purchase_last_price_info/models/stock_quant.py
+++ product_cost_purchase_last_price_info/models/stock_quant.py
@@ -1,7 +1,6 @@
 # Copyright 2023 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import api, models, fields
-from odoo.tools.float_utils import float_is_zero
+from odoo import api, fields, models
 
 
 class StockQuant(models.Model):
@@ -10,37 +9,45 @@ class StockQuant(models.Model):
     last_purchase_price_company_currency = fields.Float(
         string="LPP in company currency",
         related="product_id.last_purchase_price_company_currency",
-        groups="stock.group_stock_manager", store=True, copy=False,
+        groups="stock.group_stock_manager",
+        store=True,
+        copy=False,
     )
     purchase_value = fields.Monetary(
-        compute="_compute_purchase_value", string="Last purchase price value",
-        groups="stock.group_stock_manager", store=True, copy=False,
+        compute="_compute_purchase_value",
+        string="Last purchase price value",
+        groups="stock.group_stock_manager",
+        store=True,
+        copy=False,
     )
     standard_price = fields.Float(
         string="Product Price",
-        related="product_id.standard_price", store=True, copy=False,
-        groups="stock.group_stock_manager"
+        related="product_id.standard_price",
+        store=True,
+        copy=False,
+        groups="stock.group_stock_manager",
     )
-    value = fields.Monetary(
-        string="Product cost price value"
-    )
+    value = fields.Monetary(string="Product cost price value")
     value_to_pivot = fields.Monetary(
-        string="Product cost price value", related="value", store=True,
-        copy=False)
+        string="Product cost price value", related="value", store=True, copy=False
+    )
 
-    @api.depends("location_id", "quantity",
-                 "product_id",
-                 "product_id.last_purchase_price_company_currency")
+    @api.depends(
+        "location_id",
+        "quantity",
+        "product_id",
+        "product_id.last_purchase_price_company_currency",
+    )
     def _compute_purchase_value(self):
         for quant in self:
             if not quant.location_id:
                 quant.purchase_value = 0
                 return
-            if not quant.location_id._should_be_valued() or\
-                    (quant.owner_id and quant.owner_id !=
-                     quant.company_id.partner_id):
+            if not quant.location_id._should_be_valued() or (
+                quant.owner_id and quant.owner_id != quant.company_id.partner_id
+            ):
                 quant.sudo().purchase_value = 0
                 continue
-            quant.sudo().purchase_value = (
-                quant.quantity *
-                round(quant.product_id.last_purchase_price_company_currency, 2))
+            quant.sudo().purchase_value = quant.quantity * round(
+                quant.product_id.last_purchase_price_company_currency, 2
+            )
