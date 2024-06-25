c5557f5fdd8d993fa459ac865b7866de21b16425
oihane <oihanecruce@gmail.com>
2024-06-25 15:31:14 +0200

    [IMP] pre-commit stuff (#2626)

--- stock_move_cost/__manifest__.py
+++ stock_move_cost/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "Stock Move Cost",
     "version": "16.0.1.0.0",
     "category": "Inventory/Inventory",
-    "website": "https://avanzosc.es/",
+    "website": "https://github.com/avanzosc/odoo-addons",
     "author": "AvanzOSC",
     "license": "AGPL-3",
     "depends": [
