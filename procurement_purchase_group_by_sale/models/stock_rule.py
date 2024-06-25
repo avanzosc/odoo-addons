1b0c54b99a921823a609077845a189789332adc6
oihane <oihanecruce@gmail.com>
2024-06-25 15:33:04 +0200

    [IMP] pre-commit stuff (#2627)

--- procurement_purchase_group_by_sale/models/stock_rule.py
+++ procurement_purchase_group_by_sale/models/stock_rule.py
@@ -7,17 +7,14 @@ class StockRule(models.Model):
     _inherit = "stock.rule"
 
     def _make_po_get_domain(self, company_id, values, partner):
-        domain = super(StockRule, self)._make_po_get_domain(
-            company_id, values, partner)
+        domain = super()._make_po_get_domain(company_id, values, partner)
         if "sale_origin" in self.env.context:
-            domain += (
-                ("origin", "=", self.env.context.get("sale_origin")),
-            )
-        if ("from_orderpoint" in self.env.context and
-            self.env.context.get("from_orderpoint", False) and
-            "orderpoint_id" in values and
-                values.get("orderpoint_id", False)):
-            domain += (
-                ("origin", "=", "New reordering rule"),
-            )
+            domain += (("origin", "=", self.env.context.get("sale_origin")),)
+        if (
+            "from_orderpoint" in self.env.context
+            and self.env.context.get("from_orderpoint", False)
+            and "orderpoint_id" in values
+            and values.get("orderpoint_id", False)
+        ):
+            domain += (("origin", "=", "New reordering rule"),)
         return domain
