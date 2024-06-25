15b4d46d1d80c292b4308a999eecca73f4e061fa
oihane <oihanecruce@gmail.com>
2024-06-25 15:33:04 +0200

    [IMP] pre-commit stuff (#2627)

--- stock_production_lot_fleet_vehicle/models/fleet_vehicle_model.py
+++ stock_production_lot_fleet_vehicle/models/fleet_vehicle_model.py
@@ -1,10 +1,11 @@
 # Copyright 2021 Berezi - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import models, fields
+from odoo import fields, models
 
 
 class FleetVehicleModel(models.Model):
-    _inherit = 'fleet.vehicle.model'
+    _inherit = "fleet.vehicle.model"
 
     type_id = fields.Many2one(
-        string='Vehicle type', comodel_name='fleet.vehicle.model.type')
+        string="Vehicle type", comodel_name="fleet.vehicle.model.type"
+    )
