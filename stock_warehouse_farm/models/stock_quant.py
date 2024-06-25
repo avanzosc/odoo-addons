fdf8971e2b24be8673a81ea57271c6b50c42a240
oihane <oihanecruce@gmail.com>
2024-06-25 15:33:04 +0200

    [IMP] pre-commit stuff (#2627)

--- stock_warehouse_farm/models/stock_quant.py
+++ stock_warehouse_farm/models/stock_quant.py
@@ -1,23 +1,26 @@
 # Copyright 2022 Berezi Amubieta - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import models, fields
+from odoo import fields, models
 
 
 class StockQuant(models.Model):
-    _inherit = 'stock.quant'
+    _inherit = "stock.quant"
 
     categ_id = fields.Many2one(
-        string='Product Category',
-        comodel_name='product.category',
-        related='product_id.categ_id',
-        store=True)
+        string="Product Category",
+        comodel_name="product.category",
+        related="product_id.categ_id",
+        store=True,
+    )
     category_type_id = fields.Many2one(
-        string='Product Section',
-        comodel_name='category.type',
-        related='categ_id.type_id',
-        store=True)
+        string="Product Section",
+        comodel_name="category.type",
+        related="categ_id.type_id",
+        store=True,
+    )
     location_type_id = fields.Many2one(
-        string='Location Section',
-        comodel_name='category.type',
-        related='location_id.type_id',
-        store=True)
+        string="Location Section",
+        comodel_name="category.type",
+        related="location_id.type_id",
+        store=True,
+    )
