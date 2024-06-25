99396697056521ce48203d5a32a04ae39d5408ac
oihane <oihanecruce@gmail.com>
2024-06-25 15:33:04 +0200

    [IMP] pre-commit stuff (#2627)

--- custom_descarga/models/stock_move.py
+++ custom_descarga/models/stock_move.py
@@ -7,14 +7,16 @@ class StockMove(models.Model):
     _inherit = "stock.move"
 
     download_unit = fields.Integer(
-        string="Units",
-        compute="_compute_download_unit",
-        store=True)
+        string="Units", compute="_compute_download_unit", store=True
+    )
 
     @api.depends("move_line_ids", "move_line_ids.download_unit")
     def _compute_download_unit(self):
         for line in self:
             line.download_unit = 0
             if line.move_line_ids:
-                line.download_unit = sum(line.move_line_ids.filtered(
-                    lambda c: c.state == "done").mapped("download_unit"))
+                line.download_unit = sum(
+                    line.move_line_ids.filtered(lambda c: c.state == "done").mapped(
+                        "download_unit"
+                    )
+                )
--- custom_descarga/models/stock_move_line.py
+++ custom_descarga/models/stock_move_line.py
@@ -1,6 +1,6 @@
 # Copyright 2022 Berezi Amubieta - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import api, fields, models
+from odoo import api, models
 
 
 class StockMoveLine(models.Model):
