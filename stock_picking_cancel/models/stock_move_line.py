5e4ce06ba13ae7f857e8cbcfc8578f8f4c4ff120
oihane <oihanecruce@gmail.com>
2024-06-25 15:33:04 +0200

    [IMP] pre-commit stuff (#2627)

--- stock_picking_cancel/models/stock_move_line.py
+++ stock_picking_cancel/models/stock_move_line.py
@@ -1,50 +1,56 @@
 # Copyright 2022 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import models, _
+from odoo import _, models
 from odoo.exceptions import UserError
 
 
 class StockMoveLine(models.Model):
-    _inherit = 'stock.move.line'
+    _inherit = "stock.move.line"
 
     def _refresh_quants_by_picking_cancelation(self):
-        quant_obj = self.env['stock.quant']
-        cond = self._quant_condition_for_picking_cancelation(
-            self.location_dest_id)
+        quant_obj = self.env["stock.quant"]
+        cond = self._quant_condition_for_picking_cancelation(self.location_dest_id)
         quant = quant_obj.search(cond)
         if len(quant) > 1:
             raise UserError(
-                _("More than 1 quant found for move line with id: {}, and "
-                  "condition: {}").format(self.id, cond))
+                _(
+                    "More than 1 quant found for move line with id: {}, and "
+                    "condition: {}"
+                ).format(self.id, cond)
+            )
         if quant:
             quant.sudo().quantity = quant.quantity - self.qty_done
-        cond = self._quant_condition_for_picking_cancelation(
-            self.location_id)
+        cond = self._quant_condition_for_picking_cancelation(self.location_id)
         quant = quant_obj.search(cond)
         if len(quant) > 1:
             raise UserError(
-                _("More than 1 quant found for move line with id: {}, and "
-                  "condition: {}").format(self.id, cond))
+                _(
+                    "More than 1 quant found for move line with id: {}, and "
+                    "condition: {}"
+                ).format(self.id, cond)
+            )
         if quant:
             quant.sudo().quantity = quant.quantity + self.qty_done
 
     def _quant_condition_for_picking_cancelation(self, location):
-        cond = [('product_id', '=', self.product_id.id),
-                ('location_id', '=', location.id)]
+        cond = [
+            ("product_id", "=", self.product_id.id),
+            ("location_id", "=", location.id),
+        ]
         if location.company_id:
-            ('company_id', '=', location.company_id.id)
+            ("company_id", "=", location.company_id.id)
         if self.lot_id:
-            cond.append(('lot_id', '=', self.lot_id.id))
+            cond.append(("lot_id", "=", self.lot_id.id))
         else:
-            cond.append(('lot_id', '=', False))
+            cond.append(("lot_id", "=", False))
         if self.package_id:
-            cond.append(('package_id', '=', self.package_id.id))
+            cond.append(("package_id", "=", self.package_id.id))
         if self.result_package_id:
-            cond.append(('package_id', '=', self.result_package_id.id))
+            cond.append(("package_id", "=", self.result_package_id.id))
         if not self.package_id and not self.result_package_id:
-            cond.append(('package_id', '=', False))
+            cond.append(("package_id", "=", False))
         if self.owner_id:
-            cond.append(('owner_id', '=', self.owner_id.id))
+            cond.append(("owner_id", "=", self.owner_id.id))
         else:
-            cond.append(('owner_id', '=', False))
+            cond.append(("owner_id", "=", False))
         return cond
