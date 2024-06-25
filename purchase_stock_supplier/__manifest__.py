c9d244b04dcd29e193be7b1838345188ef63c654
oihane <oihanecruce@gmail.com>
2024-06-25 15:33:04 +0200

    [IMP] pre-commit stuff (#2627)

--- purchase_stock_supplier/__manifest__.py
+++ purchase_stock_supplier/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Purchase Stock Supplier",
-    'version': '14.0.1.0.0',
+    "version": "14.0.1.0.0",
     "author": "Avanzosc",
     "category": "Inventory/Purchase",
     "website": "https://github.com/avanzosc/odoo-addons",
@@ -13,5 +13,5 @@
     "data": [
         "views/stock_warehouse_orderpoint_views.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
