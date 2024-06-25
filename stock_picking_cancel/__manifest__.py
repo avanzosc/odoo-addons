2fe4360da4a905b290e69678a8dc985b3fe2f823
oihane <oihanecruce@gmail.com>
2024-06-25 15:33:04 +0200

    [IMP] pre-commit stuff (#2627)

--- stock_picking_cancel/__manifest__.py
+++ stock_picking_cancel/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Stock Picking Cancel",
-    'version': '14.0.1.0.0',
+    "version": "14.0.1.0.0",
     "category": "Inventory/Inventory",
     "license": "AGPL-3",
     "author": "AvanzOSC",
@@ -13,6 +13,6 @@
     "data": [
         "views/stock_picking_views.xml",
     ],
-    'installable': True,
-    'auto_install': False,
+    "installable": True,
+    "auto_install": False,
 }
