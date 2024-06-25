e4c1f5275cd64757e93bf6c42ea8e3c07c2db933
oihane <oihanecruce@gmail.com>
2024-06-25 15:33:04 +0200

    [IMP] pre-commit stuff (#2627)

--- custom_descarga_report/__manifest__.py
+++ custom_descarga_report/__manifest__.py
@@ -8,12 +8,7 @@
     "license": "AGPL-3",
     "author": "AvanzOSC",
     "website": "http://www.avanzosc.es",
-    "depends": [
-        "web",
-        "custom_saca",
-        "custom_descarga",
-        "res_company_stamp"
-    ],
+    "depends": ["web", "custom_saca", "custom_descarga", "res_company_stamp"],
     "data": [
         "report/saca_report.xml",
         "report/cleaning_certificate_report.xml",
