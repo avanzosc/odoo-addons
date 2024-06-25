434572ae7905b332def553706f8bb45f53b4b4ac
oihane <oihanecruce@gmail.com>
2024-06-25 15:33:04 +0200

    [IMP] pre-commit stuff (#2627)

--- stock_production_lot_state/models/stock_production_lot.py
+++ stock_production_lot_state/models/stock_production_lot.py
@@ -1,16 +1,19 @@
 # Copyright 2022 Berezi Amubieta - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import models, fields
+from odoo import fields, models
 
 
 class StockProductionLot(models.Model):
-    _inherit = 'stock.production.lot'
+    _inherit = "stock.production.lot"
 
     state = fields.Selection(
-        [('new', 'New'),
-         ('active', 'Active'),
-         ('inactive', 'Inactive'),
-         ('finished', 'Finished')],
+        [
+            ("new", "New"),
+            ("active", "Active"),
+            ("inactive", "Inactive"),
+            ("finished", "Finished"),
+        ],
         string="State",
-        default='new',
-        copy=False)
+        default="new",
+        copy=False,
+    )
