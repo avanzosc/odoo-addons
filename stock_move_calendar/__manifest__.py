ed6326f301cc323e52e450b5749a27e9cd4f02bd
Tu Nombre <you@example.com>
2024-06-28 09:33:59 +0200

    [16.0][ADD] stock_move_calendar: CAlendar view in move and move lines.

---
+++ stock_move_calendar/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2024 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Stock Move Calendar",
+    "version": "16.0.1.0.0",
+    "category": "Inventory/Inventory",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        "views/stock_move_view.xml",
+        "views/stock_move_line_view.xml",
+    ],
+    "installable": True,
+}
