3eef653c61560097d3fab1a12e8db58d00100c53
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-06-28 09:12:12 +0200

    [IMP] account_banking_mandate_usability: pre-commit stuff

--- account_banking_mandate_usability/__manifest__.py
+++ account_banking_mandate_usability/__manifest__.py
@@ -7,7 +7,7 @@
     "category": "Banking addons",
     "license": "AGPL-3",
     "author": "AvanzOSC",
-    "website": "http://www.avanzosc.es",
+    "website": "https://github.com/avanzosc/odoo-addons",
     "depends": [
         "account_banking_mandate",
         "account_banking_sepa_direct_debit",
