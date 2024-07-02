d2443a6f0ef894ab413eccd839c1aee40675646e
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-07-02 19:45:47 +0200

    [IMP] event_attendee_form: pre-commit stuff

--- event_attendee_form/__manifest__.py
+++ event_attendee_form/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "Event Partner Form",
     "license": "AGPL-3",
     "author": "AvanzOSC",
-    "website": "http://www.avanzosc.es",
+    "website": "https://github.com/avanzosc/odoo-addons",
     "category": "website",
     "version": "14.0.1.0.0",
     "depends": ["event_registration_student", "website_event"],
