2f42464b46aa193d6d70ea042fff17351b3774b2
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-07-02 16:10:47 +0200

    [IMP] calendar_event_description: pre-commit stuff

--- calendar_event_description/__manifest__.py
+++ calendar_event_description/__manifest__.py
@@ -3,7 +3,8 @@
 {
     "name": "Calendar Event Description",
     "version": "14.0.1.1.0",
-    "author": "Avanzosc",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
     "category": "Sales",
     "depends": [
         "calendar",
