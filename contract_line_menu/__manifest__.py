9a1fb951579b12d5664324ea32f26d7e554c44e2
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-07-02 19:34:11 +0200

    [IMP] contract_line_menu: pre-commit stuff

--- contract_line_menu/__manifest__.py
+++ contract_line_menu/__manifest__.py
@@ -3,7 +3,8 @@
 {
     "name": "Contract Line Menu",
     "version": "14.0.1.0.0",
-    "author": "Avanzosc",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
     "category": "Sales/CRM",
     "depends": [
         "contract",
