732ebe0b59943f4b6ef45fcfe2e61d3ca408b6a8
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-07-02 12:58:50 +0200

    [IMP] portal_wizard_extension: pre-commit stuff

--- portal_wizard_extension/__manifest__.py
+++ portal_wizard_extension/__manifest__.py
@@ -2,14 +2,14 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
     "name": "Portal Wizard Extension",
-    "version": "12.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "depends": [
         "base",
         "portal",
     ],
     "author": "AvanzOSC",
-    "website": "http://www.avanzosc.es",
+    "website": "https://github.com/avanzosc/odoo-addons",
     "category": "Custom",
     "data": [
         "views/res_users_view.xml",
