9f01cb559a8ea8013d4fedc36ab527a31da87a13
unaiberis <146723346+unaiberis@users.noreply.github.com>
2024-07-02 14:57:25 +0200

    [14.0][ADD] Add module product_classify_fields (#2631)
    * [ADD] Add module product_classify_fields
    
    * [ADD] Add module product_classify_fields
    
    ---------
    
    Co-authored-by: Ana Juaristi <ajuaristio@gmail.com>

---
+++ product_classify_fields/models/product_classify_fields.py
@@ -0,0 +1,29 @@
+from odoo import fields, models
+
+
+class ProductSeries(models.Model):
+    _name = "product.series"
+    _description = "Product Series"
+
+    name = fields.Char(string="Series", required=True)
+
+
+class ProductModel(models.Model):
+    _name = "product.model"
+    _description = "Product Model"
+
+    name = fields.Char(string="Model", required=True)
+
+
+class ProductApplication(models.Model):
+    _name = "product.application"
+    _description = "Product Application"
+
+    name = fields.Char(string="Application", required=True)
+
+
+class ProductFamily(models.Model):
+    _name = "product.family"
+    _description = "Product Family"
+
+    name = fields.Char(string="Family", required=True)
