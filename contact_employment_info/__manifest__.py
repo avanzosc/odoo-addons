b750163465c1cc20ecee993122b7dd31b9454790
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-07-02 16:11:45 +0200

    [IMP] contact_employment_info: pre-commit stuff

--- contact_employment_info/__manifest__.py
+++ contact_employment_info/__manifest__.py
@@ -3,7 +3,8 @@
 {
     "name": "Contact employment info",
     "version": "14.0.1.0.0",
-    "author": "Avanzosc",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
     "category": "Sales/CRM",
     "depends": ["contacts"],
     "data": [
