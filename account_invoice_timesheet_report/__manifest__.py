5c2015e280510c530e2a5d80a09878ad3f85a6bf
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-07-02 15:59:09 +0200

    [IMP] account_invoice_timesheet_report: pre-commit stuff

--- account_invoice_timesheet_report/__manifest__.py
+++ account_invoice_timesheet_report/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Account Invoice Timesheet Report",
     "version": "14.0.1.0.0",
-    "author": "Avanzosc",
+    "author": "AvanzOSC",
     "website": "https://github.com/avanzosc/odoo-addons",
     "category": "Accounting/Accounting",
     "depends": [
