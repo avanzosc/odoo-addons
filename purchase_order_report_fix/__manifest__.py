c5d1eb8523554b78b8e7b2c913ace0ec5ccefce9
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-07-03 12:02:59 +0200

    [IMP] purchase_order_report_fix: pre-commit stuff

--- purchase_order_report_fix/__manifest__.py
+++ purchase_order_report_fix/__manifest__.py
@@ -5,7 +5,7 @@
     "version": "14.0.1.0.0",
     "author": "Avanzosc",
     "category": "Inventory",
-    "website": "http://www.avanzosc.es",
+    "website": "https://github.com/avanzosc/odoo-addons",
     "depends": ["purchase", "purchase_stock"],
     "data": [
         "reports/purchase_order_templates.xml",
