621ef6676286d57049373a5bbaa039035878d9c6
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-07-03 10:14:01 +0200

    [IMP] partner_academic_background: pre-commit stuff

--- partner_academic_background/__manifest__.py
+++ partner_academic_background/__manifest__.py
@@ -3,7 +3,8 @@
 {
     "name": "Partner academic background",
     "version": "14.0.1.0.0",
-    "author": "Avanzosc",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
     "category": "Sales/CRM",
     "depends": ["contacts", "crm_phonecall"],
     "data": [
