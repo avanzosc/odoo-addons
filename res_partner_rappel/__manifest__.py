a990579579e8d2147f7b72ad6c7292ba90d9335f
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-07-03 12:04:56 +0200

    [IMP] res_partner_rappel: pre-commit stuff

--- res_partner_rappel/__manifest__.py
+++ res_partner_rappel/__manifest__.py
@@ -5,7 +5,7 @@
     "version": "14.0.1.0.0",
     "author": "Avanzosc",
     "category": "Contacts",
-    "website": "http://www.avanzosc.es",
+    "website": "https://github.com/avanzosc/odoo-addons",
     "depends": [
         "contacts",
         "account",
