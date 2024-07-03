84f564a4b4138d62817a5861d3b2e697b5c8fae2
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-07-03 11:35:21 +0200

    [IMP] event_track_replanification: pre-commit stuff

--- event_track_replanification/__manifest__.py
+++ event_track_replanification/__manifest__.py
@@ -6,7 +6,7 @@
     "category": "Marketing/Events",
     "license": "AGPL-3",
     "author": "AvanzOSC",
-    "website": "http://www.avanzosc.es",
+    "website": "https://github.com/avanzosc/odoo-addons",
     "depends": ["website_event_track", "event_schedule", "event_create_track"],
     "data": [
         "views/event_event_views.xml",
