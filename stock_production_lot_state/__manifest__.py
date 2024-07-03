180cd5b29284abd0a13fbdaf0ac83f480eed6b83
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-07-03 10:10:52 +0200

    [IMP] stock_production_lot_state: pre-commit stuff

--- stock_production_lot_state/__manifest__.py
+++ stock_production_lot_state/__manifest__.py
@@ -3,7 +3,8 @@
 {
     "name": "Stock Production Lot State",
     "version": "14.0.1.0.0",
-    "author": "Avanzosc",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
     "category": "Inventory",
     "depends": [
         "stock",
