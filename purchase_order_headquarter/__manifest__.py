16a79a740fa2a42a7b68654d1d53ecb9bd1da1f5
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-07-03 12:02:42 +0200

    [IMP] purchase_order_headquarter: pre-commit stuff

--- purchase_order_headquarter/__manifest__.py
+++ purchase_order_headquarter/__manifest__.py
@@ -6,7 +6,7 @@
     "category": "Inventory/Purchase",
     "license": "AGPL-3",
     "author": "AvanzOSC",
-    "website": "http://www.avanzosc.es",
+    "website": "https://github.com/avanzosc/odoo-addons",
     "depends": [
         "res_partner_headquarter",
         "purchase",
