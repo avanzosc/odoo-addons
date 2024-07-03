d279798481c561f7cda9b6d5b0dda764b19b4979
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-07-03 11:34:29 +0200

    [IMP] event_track_calendar_report: pre-commit stuff

--- event_track_calendar_report/__manifest__.py
+++ event_track_calendar_report/__manifest__.py
@@ -6,7 +6,7 @@
     "category": "Project",
     "license": "AGPL-3",
     "author": "AvanzOSC",
-    "website": "http://www.avanzosc.es",
+    "website": "https://github.com/avanzosc/odoo-addons",
     "depends": ["calendar", "website_event_track"],
     "data": [
         "security/ir.model.access.csv",
