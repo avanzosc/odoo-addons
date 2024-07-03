95026323bfa16393af97e2313966b502a9cadb8b
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-07-03 11:34:37 +0200

    [IMP] event_track_cancel_billability: pre-commit stuff

--- event_track_cancel_billability/__manifest__.py
+++ event_track_cancel_billability/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "Event Track Cancel Billability",
     "version": "14.0.1.0.0",
     "author": "Avanzosc",
-    "website": "http://www.avanzosc.es",
+    "website": "https://github.com/avanzosc/odoo-addons",
     "category": "Marketing/Events",
     "depends": [
         "event_track_cancel_reason",
