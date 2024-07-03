589259f795862940aa58154c7b2ab60d29e97eef
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-07-03 12:16:08 +0200

    [IMP] stock_orderpoint_generation: pre-commit stuff

--- stock_orderpoint_generation/__manifest__.py
+++ stock_orderpoint_generation/__manifest__.py
@@ -8,7 +8,7 @@
         "stock",
     ],
     "author": "AvanzOSC",
-    "website": "http://www.avanzosc.es",
+    "website": "https://github.com/avanzosc/odoo-addons",
     "category": "Warehouse Management",
     "data": [
         "security/ir.model.access.csv",
