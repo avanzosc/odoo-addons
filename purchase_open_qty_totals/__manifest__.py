6657ddf7e3da30b9414a1b93bd79a6eefc434f21
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-07-03 10:42:52 +0200

    [IMP] purchase_open_qty_totals: pre-commit stuff

--- purchase_open_qty_totals/__manifest__.py
+++ purchase_open_qty_totals/__manifest__.py
@@ -6,7 +6,7 @@
     "category": "Purchases",
     "license": "AGPL-3",
     "author": "AvanzOSC",
-    "website": "http://www.avanzosc.es/odoo-addons",
+    "website": "https://github.com/avanzosc/odoo-addons",
     "depends": ["purchase_open_qty"],
     "data": ["views/purchase_order_view.xml"],
     "installable": True,
