4b7b0fbec99b3e9326e1c7bbe0cfe6f1d0563c1c
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-07-03 10:13:48 +0200

    [IMP] partner_commercial_by_language: pre-commit stuff

--- partner_commercial_by_language/__manifest__.py
+++ partner_commercial_by_language/__manifest__.py
@@ -6,7 +6,7 @@
     "version": "14.0.2.0.0",
     "license": "AGPL-3",
     "author": "AvanzOSC",
-    "website": "https://www.avanzosc.es",
+    "website": "https://github.com/avanzosc/odoo-addons",
     "depends": [
         "base",
         "sales_team",
