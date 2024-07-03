9302a9be3137c6d1b699df9e1db1cab0d7113adb
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-07-03 10:52:18 +0200

    [IMP] stock_move_line_divide: pre-commit stuff

--- stock_move_line_divide/__manifest__.py
+++ stock_move_line_divide/__manifest__.py
@@ -2,9 +2,9 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Stock Move Line Divide",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Avanzosc",
-    "website": "http://www.avanzosc.es",
+    "website": "https://github.com/avanzosc/odoo-addons",
     "category": "Inventory",
     "depends": [
         "stock",
