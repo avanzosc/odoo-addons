3512aab7a0b818dc0ed357e4e34df07feb26b43c
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-07-03 10:04:33 +0200

    [IMP] stock_quant_change_owner: pre-commit stuff

--- stock_quant_change_owner/__manifest__.py
+++ stock_quant_change_owner/__manifest__.py
@@ -3,9 +3,9 @@
 {
     "name": "Stock Quant Change Owner",
     "version": "14.0.1.0.0",
-    "author": "Avanzosc",
+    "author": "AvanzOSC",
     "category": "Inventory",
-    "website": "http://www.avanzosc.es",
+    "website": "https://github.com/avanzosc/odoo-addons",
     "depends": [
         "stock",
     ],
