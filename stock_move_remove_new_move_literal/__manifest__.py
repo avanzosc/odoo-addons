4b1b4b5456fcc7a4dcfe077adb73b467ac43a43d
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-07-03 12:15:47 +0200

    [IMP] stock_move_remove_new_move_literal: pre-commit stuff

--- stock_move_remove_new_move_literal/__manifest__.py
+++ stock_move_remove_new_move_literal/__manifest__.py
@@ -6,7 +6,7 @@
     "category": "Inventory/Inventory",
     "author": "AvanzOSC",
     "license": "AGPL-3",
-    "website": "https://www.avanzosc.es",
+    "website": "https://github.com/avanzosc/odoo-addons",
     "depends": [
         "stock",
     ],
