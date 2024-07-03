67040410183ffaf6e7458f82ba145305413119f9
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-07-03 11:48:30 +0200

    [IMP] product_template_year_consumption: pre-commit stuff

--- product_template_year_consumption/__manifest__.py
+++ product_template_year_consumption/__manifest__.py
@@ -3,7 +3,8 @@
 {
     "name": "Product Template Year Consumption",
     "version": "14.0.1.1.0",
-    "author": "Avanzosc",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
     "category": "Stock",
     "depends": [
         "stock",
