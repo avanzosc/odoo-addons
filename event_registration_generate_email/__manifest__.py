b46cecca8e1d5285b1cca1f44418d1a2d54ae2ca
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-07-03 11:31:52 +0200

    [IMP] event_registration_generate_email: pre-commit stuff

--- event_registration_generate_email/__manifest__.py
+++ event_registration_generate_email/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "Event Registration Generate Email",
     "license": "AGPL-3",
     "author": "AvanzOSC",
-    "website": "http://www.avanzosc.es",
+    "website": "https://github.com/avanzosc/odoo-addons",
     "category": "website",
     "version": "14.0.1.0.0",
     "depends": ["website", "website_event"],
