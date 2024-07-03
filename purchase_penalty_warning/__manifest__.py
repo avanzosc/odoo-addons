66e2e33e1fc945639c49188afa4aedd99a1ff641
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-07-03 12:03:31 +0200

    [IMP] purchase_penalty_warning: pre-commit stuff

--- purchase_penalty_warning/__manifest__.py
+++ purchase_penalty_warning/__manifest__.py
@@ -4,7 +4,8 @@
     "name": "Purchase Penalty Warning",
     "version": "14.0.1.0.0",
     "category": "Inventory/Purchase",
-    "author": "https://avanzosc.es/",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
     "license": "AGPL-3",
     "depends": [
         "base_penalty_warning",
