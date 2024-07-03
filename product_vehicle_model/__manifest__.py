13ed3b0660b2d90ebf6807b04f2a753d1d7cf887
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-07-03 11:48:52 +0200

    [IMP] product_vehicle_model: pre-commit stuff

--- product_vehicle_model/__manifest__.py
+++ product_vehicle_model/__manifest__.py
@@ -3,7 +3,8 @@
 {
     "name": "Product Vehicle Model",
     "version": "14.0.1.0.0",
-    "author": "Avanzosc",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
     "category": "Sales/CRM",
     "depends": ["fleet_extension"],
     "data": [
