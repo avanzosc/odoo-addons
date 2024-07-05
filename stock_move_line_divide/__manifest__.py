5c689d0d88625cffab042675be256d951a6ad9a5
Tu Nombre <you@example.com>
2024-07-05 07:56:16 +0200

    [MIG] stock_move_line_divide: Migration to 16.0

--- stock_move_line_divide/__manifest__.py
+++ stock_move_line_divide/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Stock Move Line Divide",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Avanzosc",
     "website": "https://github.com/avanzosc/odoo-addons",
     "category": "Inventory",
