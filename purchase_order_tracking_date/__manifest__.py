c90a46f6602ddf954af493d14e89a358b0e080a7
Unai Beristain <unaiberis@gmail.com>
2024-07-09 11:44:39 +0200

    [ADD] purchase_tracking_time_date

---
+++ purchase_order_tracking_date/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Purchase Order Tracking Date",
+    "version": "12.0.1.0.0",
+    "summary": "Adds a purchase tracking tab to Purchase Orders",
+    "category": "Purchases",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": ["purchase"],
+    "data": [
+        "views/purchase_order_view.xml",
+        "views/purchase_order_carrier_view.xml",
+        "security/ir.model.access.csv",
+    ],
+    "installable": True,
+}
