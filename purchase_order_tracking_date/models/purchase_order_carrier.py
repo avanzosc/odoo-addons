da6c6f1fee410525ae509d54d0a4c32a0607632d
Unai Beristain <unaiberis@gmail.com>
2024-07-10 12:21:24 +0200

    [IMP] purchase_order_tracking_date: Improve .po after module name change

--- purchase_order_tracking_date/models/purchase_order_carrier.py
+++ purchase_order_tracking_date/models/purchase_order_carrier.py
@@ -1,14 +1,13 @@
-from odoo import models, fields
+from odoo import fields, models
+
 
 class Carrier(models.Model):
-    _name = 'purchase.order.carrier'
-    _description = 'Carrier for Purchase Orders'
+    _name = "purchase.order.carrier"
+    _description = "Carrier for Purchase Orders"
 
-    _sql_constraints = [
-        ('name_unique', 'UNIQUE(name)', 'Carrier name must be unique!')
-    ]
+    _sql_constraints = [("name_unique", "UNIQUE(name)", "Carrier name must be unique!")]
 
     name = fields.Char(
-        string="Carrier Name", 
+        string="Carrier Name",
         required=True,
     )
