83767a167e6bf58d91318074e28bf7872b73df16
Alfredo <alfredodelafuente@avanzosc.es>
2024-07-23 21:12:14 +0200

    [16.0][MIG] stock_picking_warning: Migration to 16.0

--- stock_picking_warning/models/product_template.py
+++ stock_picking_warning/models/product_template.py
@@ -1,23 +1,32 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from openerp import models, fields
+from odoo import _, fields, models
 
-WARNING_MESSAGE = [('no-message', 'No Message'),
-                   ('warning', 'Warning'),
-                   ('block', 'Blocking Message')]
+WARNING_MESSAGE = [
+    ("no-message", _("No Message")),
+    ("warning", _("Warning")),
+    ("block", _("Blocking Message")),
+]
 
 
 class ProductTemplate(models.Model):
-    _inherit = 'product.template'
+    _inherit = "product.template"
 
     out_picking_warn = fields.Selection(
-        selection=WARNING_MESSAGE, required=True, string='Out picking line',
-        default='no-message')
+        selection=WARNING_MESSAGE,
+        required=True,
+        string="Out picking line",
+        default="no-message",
+        copy=False,
+    )
     out_picking_warn_msg = fields.Text(
-        string='Message for out picking line')
+        string="Message for out picking line", copy=False
+    )
     in_picking_warn = fields.Selection(
-        selection=WARNING_MESSAGE, required=True, string='In picking line',
-        default='no-message')
-    in_picking_warn_msg = fields.Text(
-        string='Message for in picking line')
+        selection=WARNING_MESSAGE,
+        required=True,
+        string="In picking line",
+        default="no-message",
+        copy=False,
+    )
+    in_picking_warn_msg = fields.Text(string="Message for in picking line", copy=False)
