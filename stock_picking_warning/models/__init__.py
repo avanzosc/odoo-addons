dadbe926d3a362c2e0b9328488727baee1826b18
Alfredo <alfredodelafuente@avanzosc.es>
2024-07-23 21:12:14 +0200

    [16.0][MIG] stock_picking_warning: Migration to 16.0

--- stock_picking_warning/models/__init__.py
+++ stock_picking_warning/models/__init__.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-# Copyright 2019 Alfredo de la Fuente - AvanzOSC
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 from . import product_template
-from . import stock
+from . import stock_picking
+from . import stock_move
