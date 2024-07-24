02af638e2bd0ab491b10bc415ed9c3f055c1a1da
Alfredo <alfredodelafuente@avanzosc.es>
2024-07-24 11:42:21 +0200

    [14.0][ADD] stock_move_line_hide_fields: In picking hide fields in "detailled operations", if the product "tracking" is equal to "By unique serial number".

---
+++ stock_move_line_hide_fields/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Stock Move Line Hide Fields",
+    "version": "14.0.1.0.0",
+    "category": "Inventory/Inventory",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        "views/stock_move_line_views.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
