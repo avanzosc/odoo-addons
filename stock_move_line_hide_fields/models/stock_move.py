14917dff8b66c7bf4e50a54f2520ea3c49b3d1a2
Alfredo <alfredodelafuente@avanzosc.es>
2024-07-24 11:42:21 +0200

    [14.0][ADD] stock_move_line_hide_fields: In picking hide fields in "detailled operations", if the product "tracking" is equal to "By unique serial number".

---
+++ stock_move_line_hide_fields/models/stock_move.py
@@ -0,0 +1,16 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import models
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    def action_show_details(self):
+        self.ensure_one()
+        result = super(StockMove, self).action_show_details()
+        if self.product_id.tracking and self.product_id.tracking == "serial":
+            result["context"]["show_destination_location"] = False
+            result["context"]["show_qty_done"] = False
+            result["context"]["show_product_uom"] = False
+        return result
