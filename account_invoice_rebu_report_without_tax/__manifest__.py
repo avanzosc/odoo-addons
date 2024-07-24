f74220db8eb528f832f2bbd797ede965e280e344
Alfredo <alfredodelafuente@avanzosc.es>
2024-07-24 09:53:53 +0200

    [14.0][ADD] account_invoice_rebu_report_without_tax: If invoice is rebu, in report do not show tax.

---
+++ account_invoice_rebu_report_without_tax/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Account Invoice Rebu Report Without Tax",
+    "version": "14.0.1.0.0",
+    "category": "Accounting/Accounting",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": ["account_invoice_report_without_tax_breakdown", "l10n_es_account_rebu"],
+    "data": [
+        "reports/account_invoice_report.xml",
+    ],
+    "installable": True,
+}
