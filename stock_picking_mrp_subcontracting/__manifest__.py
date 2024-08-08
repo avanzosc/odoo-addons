b59d2ead01307053d1408d1ffa38bd6ec31fe5ce
Alfredo <alfredodelafuente@avanzosc.es>
2024-08-08 14:31:01 +0200

    [16.0][ADD] stock_picking_mrp_subcontracting: In picking show "Manufacturing Order".

---
+++ stock_picking_mrp_subcontracting/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2024 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Stock Picking Mrp Subcontracting",
+    "version": "16.0.1.0.0",
+    "category": "Sales",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": ["stock", "mrp_subcontracting"],
+    "excludes": [],
+    "data": [
+        "views/stock_picking_views.xml",
+    ],
+    "installable": True,
+    "auto_install": True,
+}
