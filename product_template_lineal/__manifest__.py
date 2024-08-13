6b45dd3139288e448e8ce5aa37ca0831269214ce
Alfredo <alfredodelafuente@avanzosc.es>
2024-08-13 10:50:34 +0200

    [16.0][IMP] product_template_lineal: New field "Lineal" in "stock.quant".

--- product_template_lineal/__manifest__.py
+++ product_template_lineal/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Product Template Lineal",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.1.0",
     "author": "Avanzosc",
     "website": "https://github.com/avanzosc/odoo-addons",
     "category": "Inventory",
@@ -16,6 +16,7 @@
         "views/stock_picking_views.xml",
         "views/stock_move_line_views.xml",
         "views/stock_move_views.xml",
+        "views/stock_quant_views.xml",
     ],
     "license": "AGPL-3",
     "installable": True,
