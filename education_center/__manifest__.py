2de0dd2b2190b70ae519d519edb125b12bd6406f
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-08-22 16:23:37 +0200

    [FIX] education_center: wrong version

--- education_center/__manifest__.py
+++ education_center/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Education Center",
-    "version": "14.0.1.1.0",
+    "version": "16.0.1.0.0",
     "author": "AvanzOSC",
     "website": "https://github.com/avanzosc/odoo-addons",
     "category": "Sales/CRM",
