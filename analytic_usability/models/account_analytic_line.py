d0670e1aa6ecd3767adbcac76a28f5e9644f3b4b
Unai Beristain <unaiberis@gmail.com>
2024-09-10 16:57:42 +0200

    [IMP] account_usability: Improve account_partner_id field

--- analytic_usability/models/account_analytic_line.py
+++ analytic_usability/models/account_analytic_line.py
@@ -8,7 +8,7 @@ from odoo import api, fields, models
 class AccountAnalyticLine(models.Model):
     _inherit = "account.analytic.line"
 
-    partner_id = fields.Many2one(
+    account_partner_id = fields.Many2one(
         "res.partner",
         string="Partner",
         related="account_id.partner_id",
