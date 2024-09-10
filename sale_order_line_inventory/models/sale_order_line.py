710493a53145d4ba3c0790f71c16b6e2c062096f
Unai Beristain <unaiberis@gmail.com>
2024-09-10 14:47:13 +0200

    [IMP] sale_order_line_inventory: Improve link to inventory

--- sale_order_line_inventory/models/sale_order_line.py
+++ sale_order_line_inventory/models/sale_order_line.py
@@ -8,8 +8,13 @@ class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
     def show_product_inventory(self):
-        result = self.env.ref("stock.action_view_quants").read()[0]
-        result.update(
-            {"context": {"search_default_product_tmpl_id": self.product_id.id}}
+        self.ensure_one()
+        action = self.env.ref("stock.action_view_quants").read()[0]
+        action.update(
+            {
+                "context": {
+                    "search_default_product_id": self.product_id.product_tmpl_id.id
+                }
+            }
         )
-        return result
+        return action
