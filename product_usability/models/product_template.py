ce053cdb67748ebebe5fe0c64e2721a32887e485
Unai Beristain <unaiberis@gmail.com>
2024-09-11 15:09:39 +0200

    [IMP] product_usability

--- product_usability/models/product_template.py
+++ product_usability/models/product_template.py
@@ -6,13 +6,6 @@ from odoo import api, fields, models
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    consumed_last_twelve_months = fields.Float(
-        digits="Product Unit of Measure",
-        compute="_compute_consumed_last_twelve_months",
-    )
-    months_with_stock = fields.Integer(
-        compute="_compute_months_with_stock",
-    )
     main_seller_id = fields.Many2one(
         string="Main Seller",
         comodel_name="res.partner",
@@ -37,22 +30,6 @@ class ProductTemplate(models.Model):
         related="categ_id.parent_id",
         store=True,
     )
-
-    def _compute_consumed_last_twelve_months(self):
-        for template in self:
-            consumed_last_twelve_months = 0
-            if len(template.product_variant_ids) == 1:
-                consumed_last_twelve_months = template.product_variant_ids[
-                    0
-                ].consumed_last_twelve_months
-            template.consumed_last_twelve_months = consumed_last_twelve_months
-
-    def _compute_months_with_stock(self):
-        for template in self:
-            months_with_stock = 0
-            if len(template.product_variant_ids) == 1:
-                months_with_stock = template.product_variant_ids[0].months_with_stock
-            template.months_with_stock = months_with_stock
 
     @api.depends("seller_ids")
     def _compute_main_seller(self):
