31439880cfccba871e661be2f808fe4e4e3f2bd4
Tu Nombre <you@example.com>
2024-09-11 12:24:08 +0200

    [14.0][ADD] purchase_order_amount_by_group: Purchase order report with amount grouped by tax.

---
+++ purchase_order_amount_by_group/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2024 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Purchase Order Amount By Group",
+    "version": "14.0.1.0.0",
+    "category": "Purchase",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": [
+        "purchase",
+    ],
+    "data": [
+        "report/purchase_order_report.xml",
+    ],
+    "installable": True,
+}
