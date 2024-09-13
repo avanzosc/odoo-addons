46ce57e6b22f6228c38df20c17eaa1174bb897a6
Tu Nombre <you@example.com>
2024-09-13 09:36:17 +0200

    [14.0][ADD] account_invoice_report_grouped_by_picking_refund_extension: Also the sections and notes are going to print.

---
+++ account_invoice_report_grouped_by_picking_refund_extension/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2024 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Account Invoice Report Grouped By Picking Refund Extension",
+    "version": "14.0.1.0.0",
+    "author": "AvanzOSC",
+    "category": "Invoices & Payments",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": ["account_invoice_report_grouped_by_picking_refund"],
+    "data": [],
+    "license": "AGPL-3",
+    "installable": True,
+}
