7907878bdd7518d2169523ac363e308af271720e
Unai Beristain <unaiberis@gmail.com>
2024-09-16 16:41:59 +0200

    [IMP] Improve branch: Move website_sale from odoo-addons to sale-addons

--- website_sale_product_external_link/models/product_template.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models
-
-
-class ProductTemplate(models.Model):
-    _inherit = "product.template"
-
-    website_link = fields.Char(
-        help="Website page. Example: http://www.example.com",
-    )
