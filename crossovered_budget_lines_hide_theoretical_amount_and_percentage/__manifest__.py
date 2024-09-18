b8eefc587ab43cdf7f9db049920e1952e7642457
Unai Beristain <unaiberis@gmail.com>
2024-09-18 16:28:56 +0200

    [ADD] crossovered_budget_lines_hide_theoretical_amount_and_percentage

---
+++ crossovered_budget_lines_hide_theoretical_amount_and_percentage/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Crossovered Budget Lines Hide Theoretical Amount and Percentage",
+    "version": "16.0.1.0.0",
+    "author": "Avanzosc",
+    "summary": "Hide theoretical amount and percentage columns in crossovered.budget.lines.",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "license": "LGPL-3",
+    "depends": ["account_budget_oca"],
+    "data": ["views/crossovered_budget_lines_view.xml"],
+    "installable": True,
+    "application": False,
+}
