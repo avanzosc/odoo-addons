4e1632063dcb44127ea854f416164822b1e682ed
Alfredo <alfredodelafuente@avanzosc.es>
2024-09-18 11:25:35 +0200

    [16.0][ADD] purchase_requisition_second_description: In "Purchase Requisition" new field: "Description".

---
+++ purchase_requisition_second_description/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2024 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Purchase Requisition Second Description",
+    "version": "16.0.1.0.0",
+    "category": "Inventory/Purchase",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": [
+        "purchase_requisition",
+    ],
+    "data": [
+        "report/purchase_requisition_report.xml",
+        "views/purchase_requisition_views.xml",
+    ],
+    "installable": True,
+}
