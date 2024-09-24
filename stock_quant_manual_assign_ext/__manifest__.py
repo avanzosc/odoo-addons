76e130117139a40f75b9ae6aafef7f011885fc86
Alfredo <alfredodelafuente@avanzosc.es>
2024-09-24 13:17:59 +0200

    [16.0][ADD] stock_quant_manual_assign_ext: When reserving quants manually, delete the old move lines, those that have no quantities left.

---
+++ stock_quant_manual_assign_ext/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2024 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Stock Quant Manual Assign Ext",
+    "version": "16.0.1.0.0",
+    "author": "Avanzosc",
+    "category": "Warehouse",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": ["stock_quant_manual_assign"],
+    "data": [],
+    "license": "AGPL-3",
+    "installable": True,
+}
