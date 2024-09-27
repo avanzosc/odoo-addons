8058c3dfadee31660a3e311c0425d2a02947b6ee
Tu Nombre <you@example.com>
2024-09-27 11:28:39 +0200

    [16.0][MIG] stock_move_line_package_dimension: Migration to 16.0

--- stock_move_line_package_dimension/__manifest__.py
+++ stock_move_line_package_dimension/__manifest__.py
@@ -2,16 +2,11 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Stock Move Line Package Dimension",
-    "version": "14.0.2.0.0",
+    "version": "16.0.2.0.0",
     "author": "AvanzOSC",
     "website": "https://github.com/avanzosc/odoo-addons",
     "category": "Inventory",
-    "depends": [
-        "stock",
-        "delivery",
-        "product_packaging_dimension",
-        "stock_picking_package_usability",
-    ],
+    "depends": ["stock_picking_package_usability"],
     "data": ["views/stock_move_line_views.xml", "views/product_packaging_views.xml"],
     "license": "AGPL-3",
     "installable": True,
