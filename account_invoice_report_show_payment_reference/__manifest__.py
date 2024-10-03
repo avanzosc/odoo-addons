4575dd8fafdcb46251fd30e4cd8ea9f0e7662ade
Unai Beristain <unaiberis@gmail.com>
2024-10-03 14:51:10 +0200

    [MIG] account_invoice_report_show_payment_reference: Migration to 16.0

--- account_invoice_report_show_payment_reference/__manifest__.py
+++ account_invoice_report_show_payment_reference/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Account Invoice Report Show Payment Reference",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Banking addons",
     "license": "AGPL-3",
     "author": "AvanzOSC",
