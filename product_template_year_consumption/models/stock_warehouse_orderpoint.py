7d98a80b0fe2bda19641c6dfeee1f70ca02dc002
Unai Beristain <unaiberis@gmail.com>
2024-10-10 12:57:46 +0200

    [IMP] product_template_year_consumption: Add two new fields sales_count and months_with_stock_sales_count

--- product_template_year_consumption/models/stock_warehouse_orderpoint.py
+++ product_template_year_consumption/models/stock_warehouse_orderpoint.py
@@ -12,3 +12,21 @@ class StockWarehouseOrderpoint(models.Model):
     months_with_stock = fields.Integer(
         string="Months with stock", related="product_id.months_with_stock"
     )
+    sales_count = fields.Float(
+        string="Sales Count", related="product_id.sales_count", store=True
+    )
+    months_with_stock_sales_count = fields.Float(
+        string="Months with Stock (Sales Count)",
+        compute="_compute_months_with_stock_sales_count",
+    )
+
+    def _compute_months_with_stock_sales_count(self):
+        for orderpoint in self:
+            months_with_stock = 0
+            consumed_last_twelve_months = orderpoint.consumed_last_twelve_months
+            if consumed_last_twelve_months:
+                virtual_available = orderpoint.product_id.virtual_available
+                months_with_stock = virtual_available / (
+                    consumed_last_twelve_months / 12
+                )
+            orderpoint.months_with_stock_sales_count = months_with_stock
