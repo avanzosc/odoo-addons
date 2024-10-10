f6b42539bf1e53a781b1363925a7a37a6bb7c129
Tu Nombre <you@example.com>
2024-10-10 10:26:53 +0200

    [14.0][ADD] account_invoice_report_grouped_by_picking_out_refund: When the invoice is out refund, it will print the invoice lines with corresponding quantity and sign.

---
+++ account_invoice_report_grouped_by_picking_out_refund/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2024 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Account Invoice Report Grouped By Picking Out Refund",
+    "version": "14.0.1.0.0",
+    "category": "Accounting & Finance",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": [
+        "stock_picking_invoice_link",
+        "account_invoice_report_grouped_by_picking",
+    ],
+    "data": [],
+    "installable": True,
+}
