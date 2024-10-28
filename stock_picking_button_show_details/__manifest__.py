adb4731605317c2b1185cdb379f74eea202d9f7f
Alfredo <alfredodelafuente@avanzosc.es>
2024-10-28 08:33:15 +0100

    [16.0][MIG] stock_picking_button_show_details: Migration to 16.0

--- stock_picking_button_show_details/__manifest__.py
+++ stock_picking_button_show_details/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Stock Picking Button Show Details",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "AvanzOSC",
     "website": "https://github.com/avanzosc/odoo-addons",
