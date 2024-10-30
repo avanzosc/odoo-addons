e64f64e8312d339a5e554bdc4dcdae090ec615dc
Berezi <berezi@avanzosc.home>
2024-10-30 14:37:11 +0100

    [ADD][14.0] account_asset_line_usability: Take some fields from assets to asset lines.

---
+++ account_asset_line_usability/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2024 Berezu Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Account Asset Line Usability",
+    "version": "14.0.1.0.0",
+    "category": "Accounting & Finance",
+    "license": "AGPL-3",
+    "author": "Avanzosc",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": [
+        "account_asset_line_menu",
+    ],
+    "data": [
+        "views/account_asset_line_views.xml",
+    ],
+    "installable": True,
+}
