03e7891b97f83596e4d25e6e93c339db50f6aee3
Berezi <berezi@avanzosc.home>
2024-11-07 15:48:13 +0100

    [14.0][ADD] account_invoice_paid_data: Pay fields in invoice.

---
+++ account_invoice_paid_data/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2023 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Account invoice paid data",
+    "version": "14.0.1.1.0",
+    "category": "Hidden",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": ["account", "account_move_template"],
+    "excludes": [],
+    "data": [
+        "views/account_move_views.xml",
+    ],
+    "installable": True,
+}
