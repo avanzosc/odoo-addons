ed5912965721ab4bd1a739d810324444f0c21dc5
Berezi <berezi@avanzosc.home>
2024-11-14 11:55:06 +0100

    [1430][IMP] custom_p: Put received quantity in purcase order line quantity.

---
+++ custom_p/models/account_move_line.py
@@ -0,0 +1,10 @@
+# Copyright 2024 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
+
+    out_refund_from_invoice = fields.Boolean(default=False)
