ecd93ac8318606cd242d6b351331fa3599df7a8f
Berezi <berezi@avanzosc.home>
2024-11-19 15:57:44 +0100

    [16.0][ADD] analytic_cleaning_database_operations: Cleaning all analytic operations from database.

---
+++ analytic_cleaning_database_operations/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2024 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Analytic Cleaning Database Operations",
+    "version": "16.0.1.0.0",
+    "category": "Analytic",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": [
+        "cleaning_database_operations",
+        "analytic",
+    ],
+    "data": [
+        "views/cleaning_database_view.xml",
+    ],
+    "installable": True,
+    "auto_install": True,
+}
---
+++ analytic_cleaning_database_operations/models/cleaning_database.py
@@ -0,0 +1,13 @@
+# Copyright 2024 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import models
+
+
+class CleaningDatabase(models.Model):
+    _inherit = "cleaning.database"
+
+    def action_delete_analytic_operations(self):
+        self.env.cr.execute(
+            "DELETE FROM account_analytic_line WHERE company_id in %s",
+            [tuple(self.company_ids.ids)],
+        )
