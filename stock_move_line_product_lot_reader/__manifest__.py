775d74e2fc2933c4b22fa602e8f304e2e81bac13
Berezi <berezi@avanzosc.home>
2024-11-20 14:12:36 +0100

    [14.0][BKP] stock_move_line_product_lot_reader: In detailed operations of pickings new field "Reader", to read with a reader   the "product", or "product lot", separated by a space

---
+++ stock_move_line_product_lot_reader/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2024 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Stock Move Line Product Lot Reader",
+    "version": "14.0.1.0.0",
+    "author": "Avanzosc",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "category": "Inventory",
+    "license": "AGPL-3",
+    "depends": [
+        "stock",
+    ],
+    "data": ["views/stock_move_line_views.xml"],
+    "installable": True,
+}
