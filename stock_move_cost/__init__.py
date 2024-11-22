43d79e6c91e45072bca1c10b4fca2bdfa9d03cae
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-11-22 14:00:21 +0100

    [MIG] stock_move_cost: Backport to 14.0

--- stock_move_cost/__init__.py
+++ stock_move_cost/__init__.py
@@ -21,34 +21,38 @@ def _post_install_put_cost_in_move_lines(cr, registry):
     cr.execute(
         """
         UPDATE stock_move_line
-        SET price_unit_cost = (select stock_lot.purchase_price
-                               from stock_lot
-                               where stock_lot.id = stock_move_line.lot_id)
-        WHERE lot_id is not null
+           SET price_unit_cost = (
+                   SELECT stock_production_lot.purchase_price_unit
+                     FROM stock_production_lot
+                    WHERE stock_production_lot.id = stock_move_line.lot_id)
+        WHERE lot_id IS NOT null
         """
     )
 
     cr.execute(
         """
         UPDATE stock_move_line
-        SET cost = price_unit_cost * qty_done;
+           SET cost = stock_move_line.price_unit_cost * stock_move_line.qty_done;
         """
     )
 
     cr.execute(
         """
         UPDATE stock_move
-        SET cost = (select sum(stock_move_line.cost)
-                    from   stock_move_line
-                    where  stock_move_line.move_id = stock_move.id);
+           SET cost = (
+                SELECT sum(stock_move_line.cost)
+                  FROM stock_move_line
+                 WHERE stock_move_line.move_id = stock_move.id);
         """
     )
 
     cr.execute(
         """
         UPDATE stock_move
-        SET price_unit_cost = cost / quantity_done
-        where cost > 0
-          and quantity_done > 0;
+           SET price_unit_cost = stock_move.cost / (
+                   SELECT sum(stock_move_line.qty_done)
+                     FROM stock_move_line
+                    WHERE stock_move_line.move_id = stock_move.id)
+         WHERE cost > 0;
         """
     )
