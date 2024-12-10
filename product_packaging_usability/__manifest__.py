b17b76508b674ff8d1b74a121551264250ff0b82
Berezi <berezi@avanzosc.home>
2024-12-10 12:06:24 +0100

    [14.0][IMP] product_packaging_usability: Add search view in product packaging.

---
+++ product_packaging_usability/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2024 Berezi Amubieta - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Product Packaging Usability",
+    "version": "14.0.1.0.0",
+    "category": "Product",
+    "license": "AGPL-3",
+    "author": "Avanzosc",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": ["product"],
+    "data": [
+        "views/product_packaging_views.xml",
+    ],
+    "installable": True,
+}
