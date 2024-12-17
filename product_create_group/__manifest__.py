13f2014c56bce59727d3b4af4c77d9e92363ec3f
Berezi <berezi@avanzosc.home>
2024-12-17 12:33:18 +0100

    [14.0][ADD] product_create_group: Access to create products.

---
+++ product_create_group/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2024 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Products Create Group",
+    "version": "14.0.1.0.0",
+    "category": "Product Management",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": ["product", "sale", "purchase", "mrp"],
+    "data": [
+        "data/create_product_group.xml",
+    ],
+    "installable": True,
+}
