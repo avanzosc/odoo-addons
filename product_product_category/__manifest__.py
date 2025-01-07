e2086d1149d2ff3b16bfe06f1cb15fcb579c7a9d
Unai Beristain <unaiberis@gmail.com>
2025-01-07 10:07:16 +0100

    [IMP] product_product_category: Remove usability files

--- product_product_category/__manifest__.py
+++ product_product_category/__manifest__.py
@@ -7,10 +7,7 @@
     "license": "LGPL-3",
     "depends": ["product"],
     "data": [
-        "views/account_move_views.xml",
         "views/product_product_views.xml",
-        "views/stock_move_views.xml",
-        "views/stock_move_line_views.xml",
     ],
     "installable": True,
     "application": False,
