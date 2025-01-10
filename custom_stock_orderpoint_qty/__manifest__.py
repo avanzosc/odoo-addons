b81d11c89313f253aa4a53e32dfea6e2496414b4
Berezi <berezi@avanzosc.home>
2025-01-10 09:56:02 +0100

    [14.0][ADD] custom_stock_orderpoint_qty: Remove compute to quantity to order and create a cron to recalculate.

---
+++ custom_stock_orderpoint_qty/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2025 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Custom Stock Orderpoint Qty",
+    "version": "14.0.1.0.0",
+    "category": "Stock",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        "data/ir_cron_data.xml",
+    ],
+    "installable": True,
+}
