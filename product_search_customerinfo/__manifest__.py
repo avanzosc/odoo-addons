f6471458058b8585e228f3943b4ab826709cd937
Unai Beristain <unaiberis@gmail.com>
2025-01-17 12:55:28 +0100

    [MIG] product_search_customerinfo: Migration to 16.0

--- product_search_customerinfo/__manifest__.py
+++ product_search_customerinfo/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Product Search Customerinfo",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Sales Management",
     "license": "AGPL-3",
     "author": "AvanzOSC",
