58af7aa3c36ea9c3f10a405cdceb46f703ca4536
Unai Beristain <unaiberis@gmail.com>
2025-01-22 10:51:17 +0100

    [IMP] stock_move_qty_by_packaging_report: Divide module in two - functionality and report

--- stock_move_qty_by_packaging_report/__manifest__.py
+++ stock_move_qty_by_packaging_report/__manifest__.py
@@ -1,7 +1,7 @@
 # Copyright 2023 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
-    "name": "Stock Move Qty By Packaging",
+    "name": "Stock Move Qty By Packaging Report",
     "version": "16.0.1.0.0",
     "category": "Hidden",
     "license": "AGPL-3",
@@ -9,15 +9,12 @@
     "website": "https://github.com/avanzosc/odoo-addons",
     "depends": [
         "stock",
-        "sale_order_line_qty_by_packaging",
-        "stock_move_line_force_done",
-        "product_packaging_palet",
-        "uom",
+        "stock_move_qty_by_packaging",
     ],
     "excludes": [],
     "data": [
-        "views/stock_picking_views.xml",
-        "views/stock_move_line_views.xml",
+        "report/deliveryslip_report.xml",
+        "report/stockpicking_operations_report.xml",
     ],
     "installable": True,
 }
