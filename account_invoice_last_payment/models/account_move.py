fa31206448bf8f16976bbe71827ebc3de7d94814
Oihane Crucelaegui <oihanecruce@gmail.com>
2025-01-23 14:03:57 +0100

    [IMP] account_invoice_last_payment: extend depends

--- account_invoice_last_payment/models/account_move.py
+++ account_invoice_last_payment/models/account_move.py
@@ -12,7 +12,7 @@ class AccountMove(models.Model):
         store=True,
     )
 
-    @api.depends("line_ids")
+    @api.depends("line_ids", "payment_status")
     def _compute_last_payment_date(self):
         for move in self:
             if not move.is_invoice():
