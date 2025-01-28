2d2e4fe5a4951a4c2a49edfaa92a015e60e452a2
Alfredo <alfredodelafuente@avanzosc.es>
2025-01-28 09:13:01 +0100

    [16.0][ADD] stock_picking_report_valued_ext: "Valued" field is specific to the picking and not related to the partner.

---
+++ stock_picking_report_valued_ext/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2025 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Stock Picking Report Valued Ext",
+    "version": "16.0.1.0.0",
+    "author": "Avanzosc",
+    "category": "Inventory/Inventory",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": ["stock_picking_report_valued"],
+    "data": ["views/stock_picking_view.xml"],
+    "license": "AGPL-3",
+    "installable": True,
+}
