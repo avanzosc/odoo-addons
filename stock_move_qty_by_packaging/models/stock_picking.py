a77d9b952c0468df25962cca6f381d9873ba42e2
Berezi <berezi@avanzosc.home>
2025-01-31 12:26:51 +0100

    [16.0][IMP] stock_move_qty_by_packaging: Change dependencies.

--- stock_move_qty_by_packaging/models/stock_picking.py
+++ stock_move_qty_by_packaging/models/stock_picking.py
@@ -10,15 +10,6 @@ class StockPicking(models.Model):
         result = super().button_force_done_detailed_operations()
         for picking in self:
             for line in picking.move_line_ids_without_package:
-                if line.move_id and line.move_id.sale_line_id:
-                    if line.move_id.sale_line_id.product_packaging_id:
-                        line.product_packaging_id = (
-                            line.move_id.sale_line_id.product_packaging_id.id
-                        )
-                        line.product_packaging_qty = (
-                            line.move_id.sale_line_id.product_packaging_qty
-                        )
-                    if line.move_id.sale_line_id.palet_id:
-                        line.palet_id = line.move_id.sale_line_id.palet_id.id
-                        line.palet_qty = line.move_id.sale_line_id.palet_qty
+                if line.move_id and line.move_id.product_packaging_id:
+                    line.product_packaging_id = line.move_id.product_packaging_id.id
         return result
