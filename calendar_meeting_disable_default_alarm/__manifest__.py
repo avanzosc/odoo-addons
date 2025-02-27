f4a20cce518ea8c1ad0db33436d68a6330dc1bd4
Unai Beristain <unaiberis@gmail.com>
2025-02-27 12:47:42 +0100

    [ADD] calendar_meeting_disable_alarm_custom: Disable alarm for new partner_ids

---
+++ calendar_meeting_disable_default_alarm/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Calendar Meeting Disable Default Alarm",
+    "version": "14.0.1.0.0",
+    "category": "Calendar",
+    "author": "Avanzosc",
+    "license": "LGPL-3",
+    "depends": ["calendar"],
+    "data": [],
+    "installable": True,
+    "application": False,
+    "website": "https://github.com/avanzosc/odoo-addons",
+}
