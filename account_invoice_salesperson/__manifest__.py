b777ecb8fb9dd34e3075da35a2b3aeca66df4c09
Alfredo <alfredodelafuente@avanzosc.es>
2025-03-04 08:43:40 +0100

    [16.0][ADD] account_invoice_salesperson: Add the "Salesperson" field to the newly generated invoice.

---
+++ account_invoice_salesperson/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2025 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Account Invoice Salesperson",
+    "version": "14.0.1.0.0",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "category": "Custom",
+    "license": "AGPL-3",
+    "depends": [
+        "account",
+    ],
+    "data": [],
+    "installable": True,
+}
