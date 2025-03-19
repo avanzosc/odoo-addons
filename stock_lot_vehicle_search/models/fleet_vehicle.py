0727948e947496c98eb0ef03e5e41ae7338f7064
Unai Beristain <unaiberis@gmail.com>
2025-03-19 11:09:25 +0100

    [IMP] stock_lot_vehicle_search: Simplify name_search

--- stock_lot_vehicle_search/models/fleet_vehicle.py
+++ stock_lot_vehicle_search/models/fleet_vehicle.py
@@ -9,21 +9,10 @@ class FleetVehicle(models.Model):
     _inherit = "fleet.vehicle"
 
     @api.model
-    def name_search(self, name="", args=None, operator="ilike", limit=100):
-        """
-        Search for vehicles by name or lot_id.name if lot_id exists.
-        """
-        result = super().name_search(
-            name=name, args=args, operator=operator, limit=limit
-        )
-        if not name or not hasattr(self, "lot_id"):
-            return result
-
-        vehicles = self.search(
-            ["|", ("name", operator, name), ("lot_id.name", operator, name)]
-            + (args or []),
-            limit=limit,
-        )
-        result_ids = {r[0] for r in result}
-        result += [(v.id, v.name) for v in vehicles if v.id not in result_ids]
-        return result
+    def name_search(self, name, args=None, operator="ilike", limit=100):
+        args = args or []
+        domain = []
+        if name:
+            domain = ["|", ("name", operator, name), ("lot_id.name", operator, name)]
+        vehicles = self.search(domain + args, limit=limit)
+        return vehicles.name_get()
