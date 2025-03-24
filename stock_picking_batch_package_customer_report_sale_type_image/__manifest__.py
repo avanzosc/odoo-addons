bd66defaa44820923513c898758274bad7997d8d
Alfredo <alfredodelafuente@avanzosc.es>
2025-03-24 13:52:30 +0100

    [16.0][ADD] stock_picking_batch_package_customer_report_sale_type_image: Print logo labels depending on the type of sales order.

---
+++ stock_picking_batch_package_customer_report_sale_type_image/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2025 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Stock Picking Batch Package Customer Report Sale Type Image",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "category": "Inventory",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": [
+        "stock_picking_batch_package_customer_report",
+        "stock_picking_batch_package_sale_type_image",
+    ],
+    "data": ["report/stock_picking_batch_package_label.xml"],
+    "installable": True,
+    "auto_install": True,
+}
