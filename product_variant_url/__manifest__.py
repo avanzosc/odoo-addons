2f4fc6c5f6ba9627a558f7309963720fc0c623d7
Unai Beristain <unaiberis@gmail.com>
2025-04-29 14:30:08 +0200

    [ADD] New module product_variant_url

---
+++ product_variant_url/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Product Variant URL",
+    "version": "12.0.1.0.0",
+    "category": "Product",
+    "summary": "Add and show a URL on product variants",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "depends": [
+        "product",
+    ],
+    "data": [
+        "views/product_template_views.xml",
+    ],
+    "installable": True,
+}
