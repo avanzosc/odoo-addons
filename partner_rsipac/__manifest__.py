1d873ba89b72fae3862786878a546a8f1e894e54
Oihane Crucelaegui <oihanecruce@gmail.com>
2025-04-29 14:30:49 +0200

    [MIG] partner_rsipac: Migration to 16.0

--- partner_rsipac/__manifest__.py
+++ partner_rsipac/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Partner RSIPAC number",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Sales/CRM",
     "license": "AGPL-3",
     "author": "AvanzOSC",
@@ -11,7 +11,6 @@
     "depends": [
         "base",
     ],
-    "excludes": [],
     "data": [
         "views/res_partner_view.xml",
         "views/res_company_view.xml",
