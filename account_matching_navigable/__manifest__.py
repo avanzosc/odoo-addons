7e7411520b258c7d3ef5ccc0de5792ca16887871
Alfredo <alfredodelafuente@avanzosc.es>
2025-05-14 11:59:19 +0200

    [16.0][ADD] account_matching_navigable: In account move line tree view new button "Matching" to view "Reconciled lines".

---
+++ account_matching_navigable/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2025 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Account Matching Navigable",
+    "version": "16.0.1.0.0",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "license": "AGPL-3",
+    "category": "Accounting/Accounting",
+    "summary": "Makes the matching_number field navigable in accounting lines",
+    "depends": ["account"],
+    "data": ["views/account_move_line_views.xml"],
+    "installable": True,
+    "application": False,
+}
