ddb90cc950bd1fea5ebcb2f18c293c8a5ca03f2d
luciaavanzosc <luciaavanzosc@gmail.com>
2025-05-20 14:00:19 +0200

    [14.0][ADD] account_due_date_fixed: Show the expiration date as a fixed date instead of remaining days.

---
+++ account_due_date_fixed/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2025 Lucía Echeverría - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Account Due Date Fixed",
+    "version": "14.0.1.0.0",
+    "summary": "Show the expiration date as a fixed date instead of remaining days.",
+    "category": "Accounting",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "license": "AGPL-3",
+    "depends": ["account"],
+    "data": [
+        "views/account_invoice_view.xml",
+    ],
+    "installable": True,
+}
