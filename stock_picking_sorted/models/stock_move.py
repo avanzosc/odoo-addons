c2a3a9365613f956fa79cdf933bf9ab03f149a2a
Alfredo <alfredodelafuente@avanzosc.es>
2025-06-02 12:08:03 +0200

    [16.0][MIG] stock_picking_sorted: Migration to 16.0

--- stock_picking_sorted/models/stock_move.py
+++ stock_picking_sorted/models/stock_move.py
@@ -1,29 +1,22 @@
 # Copyright 2020 Mikel Arregi Etxaniz - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class StockMove(models.Model):
     _inherit = "stock.move"
-    # Use compute instead of related because it raises access error even using related_sudo
 
     categ_id = fields.Many2one(
+        string="Product Category",
         comodel_name="product.category",
-        compute="_compute_categ_id",
-        compute_sudo=True,
+        related="product_id.categ_id",
+        store=True,
+        copy=False,
     )
     product_brand_id = fields.Many2one(
+        string="Product Brand",
         comodel_name="product.brand",
-        compute="_compute_product_brand_id",
+        related="product_id.product_brand_id",
         store=True,
+        copy=False,
     )
-
-    @api.depends("product_id", "product_id.categ_id")
-    def _compute_categ_id(self):
-        for move in self:
-            move.categ_id = move.product_id.categ_id
-
-    @api.depends("product_id", "product_id.product_brand_id")
-    def _compute_product_brand_id(self):
-        for move in self:
-            move.product_brand_id = move.product_id.product_brand_id
