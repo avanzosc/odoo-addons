31fafeb0f91d0739214411e840b77617fb38be30
Alfredo <alfredodelafuente@avanzosc.es>
2025-06-03 08:19:27 +0200

    [16.0][MIG] stock_picking_sorted: Migration to 16.0

--- stock_picking_sorted/__manifest__.py
+++ stock_picking_sorted/__manifest__.py
@@ -1,8 +1,8 @@
 # Copyright 2020 Mikel Arregi Etxaniz - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
-    "name": "Order picking tree",
-    "version": "12.0.1.0.0",
+    "name": "Stock Picking Sorted",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "depends": [
         "sale_stock",
@@ -13,6 +13,7 @@
     "category": "Inventory",
     "data": [
         "views/stock_picking_view.xml",
+        "views/stock_move_view.xml",
         "report/report_stock_picking_operations.xml",
     ],
     "installable": True,
