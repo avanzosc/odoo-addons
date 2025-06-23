a45d30750c529f65ff4659fdf756c84dbcba52a1
Alfredo <alfredodelafuente@avanzosc.es>
2025-06-23 12:32:30 +0200

    [16.0][ADD] account_financial_risk_stock: In partner new field "Block picking in case of exceeded risk".

---
+++ account_financial_risk_stock/models/res_partner.py
@@ -0,0 +1,11 @@
+# Copyright 2025 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    block_picking_risk = fields.Boolean(
+        string="Block picking in case of exceeded risk", default=False, copy=False
+    )
