9ea1fa6e8129b223b124fa880d4393d1586b2c36
Ane <aneavanzosc@gmail.com>
2025-06-27 08:03:24 +0200

    add license and move .po file to i18 folder

--- stock_move_line_product_lot_reader/models/stock_move_line.py
+++ stock_move_line_product_lot_reader/models/stock_move_line.py
@@ -1,3 +1,6 @@
+# Copyright 2024 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
 from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 
