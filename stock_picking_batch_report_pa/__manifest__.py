a53c686b43621190f538be621459d003c36880d0
luciaavanzosc <luciaavanzosc@gmail.com>
2025-07-02 15:09:49 +0200

    [14.0][ADD] stock_picking_batch_report_pa: Picking batch report grouped by customer and date.

---
+++ stock_picking_batch_report_pa/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2025 Lucía Echeverría - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Stock Picking Batch Report PA",
+    "summary": "Picking batch report grouped by customer and date.",
+    "version": "14.0.1.0.0",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "category": "Inventory/Inventory",
+    "license": "AGPL-3",
+    "depends": ["stock_picking_batch", "stock_move_qty_by_packaging"],
+    "data": [
+        "report/report.xml",
+    ],
+    "installable": True,
+}
