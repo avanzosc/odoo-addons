0009b8b324c69cc2a1a04bcfa5a8940c23651db7
Alfredo <alfredodelafuente@avanzosc.es>
2025-07-21 13:20:47 +0200

    [ADD] purchase_open_qty_fix: In purchases, fix search for the quantity to be received and the quantity to be invoiced.

---
+++ purchase_open_qty_fix/models/purchase_order.py
@@ -0,0 +1,25 @@
+# Copyright 2025 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+from odoo import api, models
+
+
+class PurchaseOrder(models.Model):
+    _inherit = "purchase.order"
+
+    @api.model
+    def _search_qty_to_invoice(self, operator, value):
+        value = float(value) if value else 0.0
+        po_line_obj = self.env["purchase.order.line"]
+        cond = [("qty_to_invoice", operator, value)]
+        po_lines = po_line_obj.search(cond)
+        orders = po_lines.mapped("order_id")
+        return [("id", "in", orders.ids)]
+
+    @api.model
+    def _search_qty_to_receive(self, operator, value):
+        value = float(value) if value else 0.0
+        po_line_obj = self.env["purchase.order.line"]
+        cond = [("qty_to_receive", operator, value)]
+        po_lines = po_line_obj.search(cond)
+        orders = po_lines.mapped("order_id")
+        return [("id", "in", orders.ids)]
