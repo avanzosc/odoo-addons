2085a89672a359b0f3f38b556d73194378c54599
Alfredo <alfredodelafuente@avanzosc.es>
2025-07-21 12:36:52 +0200

    [16.0][MIG] purchase_last_price_info_discount: Migration to 16.0

--- purchase_last_price_info_discount/models/product_template.py
+++ purchase_last_price_info_discount/models/product_template.py
@@ -12,7 +12,6 @@ class ProductTemplate(models.Model):
         compute="_compute_last_purchase_line_id_info",
     )
     last_purchase_net_unit_price = fields.Float(
-        string="Last Purchase Net Unit Price",
         compute="_compute_last_purchase_line_id_info",
     )
 
@@ -21,10 +20,13 @@ class ProductTemplate(models.Model):
         result = super()._compute_last_purchase_line_id_info()
         for item in self:
             item.last_purchase_discount = item.last_purchase_line_id.discount
-            if item.last_purchase_line_id.product_qty:
+            if item.last_purchase_line_id.discount:
                 item.last_purchase_net_unit_price = (
-                    item.last_purchase_line_id.price_subtotal
-                    / item.last_purchase_line_id.product_qty
+                    item.last_purchase_line_id.price_unit
+                    - (
+                        item.last_purchase_line_id.price_unit
+                        * (item.last_purchase_line_id.discount / 100)
+                    )
                 )
             else:
                 item.last_purchase_net_unit_price = item.last_purchase_price
