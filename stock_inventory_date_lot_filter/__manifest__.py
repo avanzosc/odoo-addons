cc24afdb93c7b2da3af4f6007f91d95e3e0ddf52
luciaavanzosc <luciaavanzosc@gmail.com>
2025-08-05 10:50:22 +0200

    [14.0][ADD] stock_inventory_date_lot_filter: Extend inventory adjustments with filters by quant creation date and lot name substring.

---
+++ stock_inventory_date_lot_filter/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2025 Lucía Echeverría - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Stock Inventory Date Lot Filter",
+    "summary": "Optional date and lot filter in stock inventory.",
+    "version": "14.0.1.0.0",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "category": "Inventory",
+    "license": "AGPL-3",
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        "views/stock_inventory_view.xml",
+    ],
+    "installable": True,
+}
