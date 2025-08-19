86179c6419fccb0a86c89aeb412202064318dff9
Oihane Crucelaegui <oihanecruce@gmail.com>
2025-08-19 14:14:57 +0200

    [IMP] custom_breeding_apps: add rvd to saca lines

--- custom_breeding_apps/models/__init__.py
+++ custom_breeding_apps/models/__init__.py
@@ -9,6 +9,7 @@ from . import stock_picking_batch
 from . import stock_location
 from . import stock_quant
 from . import stock_move
+from . import saca_line
 from . import sale_order
 from . import sale_order_line
 from . import purchase_order
