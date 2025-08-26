375ed09819005b4a52758ab70198b98baba1c80e
Alfredo <alfredodelafuente@avanzosc.es>
2025-08-26 14:17:46 +0200

    [18.0][MIG] crossovered_budget_lines_notes: Migration to 18.0

--- crossovered_budget_lines_notes/__manifest__.py
+++ crossovered_budget_lines_notes/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
     "name": "Crossovered Budget Lines Notes",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Accounting",
     "license": "AGPL-3",
     "author": "AvanzOSC",
