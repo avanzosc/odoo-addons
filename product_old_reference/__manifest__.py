2cf6b59e59053ababae4a388735aa23788a7cdc8
Alfredo <alfredodelafuente@avanzosc.es>
2025-08-27 07:38:50 +0200

    [18.0][MIG] product_old_reference: Migration to 18.0

--- product_old_reference/__manifest__.py
+++ product_old_reference/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Product Old Reference",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Sales/Sales",
     "author": "AvanzOSC",
     "website": "https://github.com/avanzosc/odoo-addons",
