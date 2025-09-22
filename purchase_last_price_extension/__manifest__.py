a547af6231b0d1d977d8776eed8801dbbadd281e
AnerAvanzosc <aneravanzosc@gmail.com>
2025-09-22 15:27:22 +0200

    [MIG] purchase_last_price_extension: Migration to 18.0

--- purchase_last_price_extension/__manifest__.py
+++ purchase_last_price_extension/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Purchase Last Price Extension",
-    "version": "12.0.2.0.0",
+    "version": "18.0.1.0.0",
     "category": "Purchase",
     "license": "AGPL-3",
     "author": "AvanzOSC",
@@ -12,7 +12,6 @@
         "purchase_last_price_info",
         "purchase_order_line_input",
     ],
-    "excludes": [],
     "data": [
         "views/purchase_order_line_view.xml",
     ],
