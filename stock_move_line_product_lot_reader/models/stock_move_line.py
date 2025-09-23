b3a81fc4cfebfa9a5c2fdc485b19e706c62b5f37
Ane <aneavanzosc@gmail.com>
2025-09-23 14:59:56 +0200

    Fix merge conflict in stock_move_line.py

--- stock_move_line_product_lot_reader/models/stock_move_line.py
+++ stock_move_line_product_lot_reader/models/stock_move_line.py
@@ -1,5 +1,5 @@
 # Copyright 2024 Alfredo de la Fuente - AvanzOSC
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+# License  AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 
