6093094601ff569fc5e8bba8b942bfe6e49d95bc
luciaavanzosc <luciaavanzosc@gmail.com>
2025-10-07 10:40:33 +0200

    [14.0][IMP] custom_breeding_apps: Bring lot average price to move lines' average price when doing manual stock adjustments.

--- custom_breeding_apps/models/stock_move.py
+++ custom_breeding_apps/models/stock_move.py
@@ -8,3 +8,17 @@ class StockMove(models.Model):
 
     standard_price = fields.Float(digits="Standard Cost Decimal Precision")
     date = fields.Datetime(string="Date")
+
+    def _action_done(self, cancel_backorder=False):
+        res = super()._action_done(cancel_backorder)
+        for move in self:
+            if (
+                move.location_id.usage == "inventory"
+                or move.location_dest_id.usage == "inventory"
+            ) and not move.inventory_id:
+                for line in move.move_line_ids:
+                    if line.lot_id:
+                        line.standard_price = line.lot_id.average_price
+                    else:
+                        line.standard_price = line.product_id.standard_price or 0.0
+        return res
