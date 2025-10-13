de258a6215124a6c877cd60c4f9c9e02ac6a5343
luciaavanzosc <luciaavanzosc@gmail.com>
2025-10-13 14:25:59 +0200

    [14.0][ADD] stock_quant_purchase_fifo_valuation: Export XLSX from selected Quants: non‑traceable vs purchase lines (FIFO with adjustment).

--- stock_quant_purchase_fifo_valuation/__manifest__.py
+++ stock_quant_purchase_fifo_valuation/__manifest__.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
 {
     "name": "Stock Quant Purchase FIFO Valuation (Not Traceable)",
     "version": "14.0.1.0.0",
     "category": "Inventory",
-    "summary": "Export XLSX from selected Quants: non‑traceable vs purchase lines (FIFO with adjustment).",
+    "summary": "Export XLSX from selected Quants: non‑traceable vs purchase lines.",
     "author": "AvanzOSC",
     "license": "AGPL-3",
     "website": "https://github.com/avanzosc/odoo-addons",
@@ -13,5 +12,3 @@
     ],
     "installable": True,
 }
-
-
