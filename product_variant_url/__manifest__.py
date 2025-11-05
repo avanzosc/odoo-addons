d27fb7c01b55eb4cdebe3f2ae8ec4b4388d68804
Alfredo <alfredodelafuente@avanzosc.es>
2025-11-05 08:39:29 +0100

    [18.0][MIG] product_variant_url: Migration to 18.0

--- product_variant_url/__manifest__.py
+++ product_variant_url/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Product Variant URL",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Product",
     "summary": "Add and show a URL on product variants",
     "license": "AGPL-3",
