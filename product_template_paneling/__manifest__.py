23daf92e85f0c2a4eefd85342599dd5764938682
Alfredo <alfredodelafuente@avanzosc.es>
2025-11-12 12:45:14 +0100

    [16.0][ADD] product_template_paneling: In product template new field "Paneling".

---
+++ product_template_paneling/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2025 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Product Template Paneling",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["product", "stock"],
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "category": "Sales/Sales",
+    "data": [
+        "security/ir.model.access.csv",
+        "views/product_paneling_views.xml",
+        "views/product_template_views.xml",
+    ],
+    "installable": True,
+}
