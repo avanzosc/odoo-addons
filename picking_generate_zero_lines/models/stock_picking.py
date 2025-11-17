20f7125905414b5dc5b908887bfa82f4db713c42
luciaavanzosc <luciaavanzosc@gmail.com>
2025-11-17 08:26:03 +0100

    [14.0][FIX] picking_generate_zero_lines: Allow creating lines without lot but block quant creation.

---
+++ picking_generate_zero_lines/models/stock_picking.py
@@ -0,0 +1,31 @@
+# Copyright 2024 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import _, models
+from odoo.exceptions import UserError
+
+
+class StockPicking(models.Model):
+    _inherit = "stock.picking"
+
+    def button_validate(self):
+        zero_moves = self.move_ids_without_package.filtered(
+            lambda c: c.quantity_done == 0
+        )
+        for move in zero_moves:
+            if move.product_id.tracking != "none" and move.move_line_ids.filtered(
+                lambda ml: ml.qty_done > 0 and not (ml.lot_id or ml.lot_name)
+            ):
+                raise UserError(
+                    _("You need to supply a Lot/Serial number for product %s")
+                    % (move.product_id.display_name)
+                )
+            move.write(
+                {
+                    "state": "done",
+                    "product_uom_qty": move.quantity_done,
+                }
+            )
+            if not move.move_line_ids:
+                self.env["stock.move.line"].create(move._prepare_move_line_vals())
+        result = super(StockPicking, self).button_validate()
+        return result
