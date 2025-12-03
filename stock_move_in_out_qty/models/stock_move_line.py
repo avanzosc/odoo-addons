3ce9b77f7dbe74e57bf2a69a2e1fb0a3fa545dde
aneravanzosc <aneravanzosc@gmail.com>
2025-12-03 16:07:13 +0100

    [18.0][MIG] stock_move_in_out_qty: Migration to 18.0

--- stock_move_in_out_qty/models/stock_move_line.py
+++ stock_move_in_out_qty/models/stock_move_line.py
@@ -1,5 +1,3 @@
-# Copyright 2023 Berezi Amubieta - AvanzOSC
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from odoo import api, fields, models
 
 
@@ -12,19 +10,19 @@ class StockMoveLine(models.Model):
     )
     dif_qty = fields.Float(string="Difference", compute="_compute_dif_qty", store=True)
 
-    @api.depends("qty_done", "location_id", "location_id.usage")
+    @api.depends("quantity", "location_id", "location_id.usage")  
     def _compute_in_qty(self):
         for line in self:
             line.in_qty = 0
             if line.location_id and line.location_id.usage != "internal":
-                line.in_qty = line.qty_done
+                line.in_qty = line.quantity  
 
-    @api.depends("qty_done", "location_dest_id", "location_dest_id.usage")
+    @api.depends("quantity", "location_dest_id", "location_dest_id.usage")  
     def _compute_out_qty(self):
         for line in self:
             line.out_qty = 0
             if line.location_dest_id and (line.location_dest_id.usage) != ("internal"):
-                line.out_qty = line.qty_done * (-1)
+                line.out_qty = line.quantity * (-1)  
 
     @api.depends("in_qty", "out_qty")
     def _compute_dif_qty(self):
