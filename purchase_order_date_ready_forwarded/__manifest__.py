77a005537b517ddc42336d5160a77dd573b4c3f4
Eñaut Alberdi <enautavanzosc@gmail.com>
2026-01-13 15:22:14 +0100

    [18.0][MIG] purchase_order_date_ready_forwarded: Migration to 18.0

--- purchase_order_date_ready_forwarded/__manifest__.py
+++ purchase_order_date_ready_forwarded/__manifest__.py
@@ -1,9 +1,12 @@
 # Copyright 2020 Adrian Revilla - AvanzOSC
+# Copyright 2025 Eñaut Alberdi - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Purchase Order Date Ready Fordwarded",
-    "version": "12.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Purchases",
+    "summary": """Adds ready, forwarded, delivery and arrival dates 
+    to purchase orders for enhanced supply chain tracking""",
     "license": "AGPL-3",
     "author": "AvanzOSC",
     "website": "http://www.avanzosc.es",
