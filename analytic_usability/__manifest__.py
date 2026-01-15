11ac4177a4769b47e12f26b83ce2846fcfad7539
Ane <aneavanzosc@gmail.com>
2026-01-15 08:20:51 +0100

    [18.0][MIG] analytic_usability: Migration to 18.0

--- analytic_usability/__manifest__.py
+++ analytic_usability/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Analytic Usability",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "AvanzOSC",
     "website": "https://github.com/avanzosc/odoo-addons",
