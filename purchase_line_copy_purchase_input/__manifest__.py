e652e027db495f2f3df745d9bd73ec4121f206ce
Alfredo <alfredodelafuente@avanzosc.es>
2026-01-15 13:01:32 +0100

    [IMP] purchase_line_copy_purchase_input: pre-commit auto fixes

--- purchase_line_copy_purchase_input/__manifest__.py
+++ purchase_line_copy_purchase_input/__manifest__.py
@@ -4,16 +4,12 @@
     "name": "Glue module for purchase_order_line_input and purchase_line_copy",
     "version": "12.0.1.0.0",
     "license": "AGPL-3",
-    "depends": [
-        "purchase_order_line_input", "purchase_order_line_copy"
-    ],
+    "depends": ["purchase_order_line_input", "purchase_order_line_copy"],
     "author": "AvanzOSC",
-    "website": "http://www.avanzosc.es",
+    "website": "https://github.com/avanzosc/odoo-addons",
     "category": "",
-    "data": [
-        "views/purchase_order_line_view.xml"
-    ],
-    'demo': [],
-    'installable': True,
-    'auto_install': True,
+    "data": ["views/purchase_order_line_view.xml"],
+    "demo": [],
+    "installable": True,
+    "auto_install": True,
 }
