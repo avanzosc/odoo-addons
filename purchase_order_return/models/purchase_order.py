49a8c7f149c3e152d6ff8dcf4b1cdd569b54f5a0
luciaavanzosc <luciaavanzosc@gmail.com>
2026-01-21 10:52:03 +0100

    [14.0][IMP] purchase_order_return: New purchase order return management.

--- purchase_order_return/models/purchase_order.py
+++ purchase_order_return/models/purchase_order.py
@@ -1,116 +1,132 @@
 # Copyright 2024 Berezi Amubieta - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import _, models
-from odoo.exceptions import ValidationError
+from odoo import SUPERUSER_ID, _, models
+from odoo.exceptions import UserError, ValidationError
 
 
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
-    def button_confirm_pickings(self):
-        result = super(PurchaseOrder, self).button_confirm_pickings()
-        for line in self.order_line:
-            if (
-                line.product_id
-                and (line.product_id.tracking != "none")
-                and not (line.lot_id)
-                and line.return_qty
-            ):
-                raise ValidationError(
-                    _("The product {} has not lot").format(line.product_id.name)
-                )
-        return result
-
-    def button_return_picking(self):
-        if not self.picking_ids and any(
-            [line.product_uom_qty != 0 for line in self.order_line]
-        ):
-            raise ValidationError(_("First you have to do the delivery."))
-        elif not self.picking_ids and any(
-            [line.return_qty != 0 for line in self.order_line]
-        ):
-            if not self.picking_type_id:
-                raise ValidationError(_("You must enter the picking type."))
-            elif not self.picking_type_id.return_picking_type_id:
-                raise ValidationError(
-                    _("You must enter the return picking type of picking" " type.")
-                )
-            else:
-                self.button_confirm()
-                entry_picking = self.picking_ids[:1]
-                entry_picking.button_force_done_detailed_operations()
-                pick_type = self.picking_type_id
-                picking = self.env["stock.picking"].create(
-                    {
-                        "partner_id": self.partner_id.id,
-                        "picking_type_id": pick_type.return_picking_type_id.id,
-                        "location_id": (
-                            pick_type.return_picking_type_id.default_location_src_id.id
-                            or self.env.ref("stock.stock_location_customers").id
-                        ),
-                        "location_dest_id": (
-                            pick_type.return_picking_type_id.default_location_dest_id.id
-                            or self.env.ref("stock.stock_location_suppliers").id
-                        ),
-                        "company_id": self.company_id.id,
-                        "origin": entry_picking.origin,
-                    }
-                )
-                for line in self.order_line:
-                    line._prepare_stock_moves(picking)
-                for line in entry_picking.move_line_ids_without_package:
-                    move = self.env["stock.move"].create(
-                        {
-                            "purchase_line_id": line.move_id.purchase_line_id.id,
-                            "picking_id": picking.id,
-                            "product_id": line.product_id.id,
-                            "name": line.move_id.name,
-                            "product_uom": line.product_uom_id.id,
-                            "product_uom_qty": line.move_id.purchase_line_id.return_qty,
-                            "location_id": picking.location_id.id,
-                            "location_dest_id": picking.location_dest_id.id,
-                            "move_orig_ids": [(4, line.move_id.id)],
-                            "origin_returned_move_id": line.move_id.id,
-                            "to_refund": True,
-                            "move_line_ids": [
-                                (
-                                    0,
-                                    0,
-                                    {
-                                        "picking_id": picking.id,
-                                        "product_id": line.product_id.id,
-                                        "product_uom_id": line.product_uom_id.id,
-                                        "qty_done": line.move_id.purchase_line_id.return_qty,
-                                        "location_id": picking.location_id.id,
-                                        "location_dest_id": picking.location_dest_id.id,
-                                        "lot_id": line.move_id.purchase_line_id.lot_id.id,
-                                    },
-                                )
-                            ],
-                        }
+    def button_validate_everything(self):
+        for order in self:
+            for line in order.order_line:
+                if (
+                    line.return_qty > 0
+                    and line.product_id.tracking != "none"
+                    and not line.lot_id
+                ):
+                    raise ValidationError(
+                        _("The product {} has not lot").format(line.product_id.name)
                     )
-                    line.move_id.write(
-                        {
-                            "move_dest_ids": [(4, move.id)],
-                            "returned_move_ids": [(4, move.id)],
-                        }
-                    )
-                picking.group_id = self.group_id.id
-                picking.action_confirm()
-                result = picking.button_validate()
-                entry_picking.action_cancel()
-                entry_picking.sudo().unlink()
-                return result
-        else:
-            picking_act = super(PurchaseOrder, self).button_return_picking()
-            picking = self.env["stock.picking"].browse(picking_act.get("res_id"))
-            if len(picking) == 1:
-                for line in picking.move_line_ids_without_package:
-                    if line.move_id.purchase_line_id:
-                        line.qty_done = line.move_id.purchase_line_id.return_qty
-                if not any([line.return_qty != 0 for line in self.order_line]):
-                    return picking_act
-                else:
+            if order.state in ["draft", "sent"]:
+                order.button_confirm()
+            pickings = order.picking_ids.filtered(
+                lambda p: p.state not in ("done", "cancel")
+            )
+            if pickings:
+                for picking in pickings:
                     picking.action_confirm()
-                    return picking.button_validate()
+                    action = picking.button_validate()
+                    if isinstance(action, dict):
+                        return action
+        return True
+
+    def _prepare_picking(self, picking_type, location_id, location_dest_id):
+        if not self.group_id:
+            self.group_id = self.group_id.create(
+                {"name": self.name, "partner_id": self.partner_id.id}
+            )
+        if not self.partner_id.property_stock_supplier.id:
+            raise UserError(
+                _("You must set a Vendor Location for this partner %s")
+                % self.partner_id.name
+            )
+        return {
+            "picking_type_id": picking_type.id,
+            "partner_id": self.partner_id.id,
+            "user_id": False,
+            "date": self.date_order,
+            "origin": self.name,
+            "location_id": location_id,
+            "location_dest_id": location_dest_id,
+            "company_id": self.company_id.id,
+        }
+
+    def _process_picking(
+        self,
+        lines,
+        existing_pickings,
+        picking_type,
+        location_id,
+        location_dest_id,
+        is_return=False,
+    ):
+        StockPicking = self.env["stock.picking"]
+        if not lines:
+            return False
+        if not existing_pickings:
+            res = self._prepare_picking(picking_type, location_id, location_dest_id)
+            picking = StockPicking.with_user(SUPERUSER_ID).create(res)
+        else:
+            picking = existing_pickings[0]
+        if is_return:
+            moves = lines._create_stock_return_moves(picking)
+        else:
+            moves = lines._create_stock_moves(picking)
+        moves = moves.filtered(
+            lambda m: m.state not in ("done", "cancel")
+        )._action_confirm()
+        seq = 0
+        for move in sorted(moves, key=lambda m: m.date):
+            seq += 5
+            move.sequence = seq
+        moves._action_assign()
+        picking.message_post_with_view(
+            "mail.message_origin_link",
+            values={"self": picking, "origin": self},
+            subtype_id=self.env.ref("mail.mt_note").id,
+        )
+        return picking
+
+    def _create_picking(self):
+        for order in self.filtered(lambda po: po.state in ("purchase", "done")):
+            if not any(
+                product.type in ["product", "consu"]
+                for product in order.order_line.product_id
+            ):
+                continue
+            order = order.with_company(order.company_id)
+            entry_pickings = order.picking_ids.filtered(
+                lambda p: p.state not in ("done", "cancel")
+                and p.picking_type_id == order.picking_type_id
+            )
+            return_pickings = order.picking_ids.filtered(
+                lambda p: p.state not in ("done", "cancel")
+                and p.picking_type_id == order.picking_type_id.return_picking_type_id
+            )
+            entry_lines = order.order_line.filtered(
+                lambda l: (l.product_uom_qty - l.qty_received) > 0
+            )
+            return_lines = order.order_line.filtered(
+                lambda l: (l.return_qty - abs(l.qty_received)) > 0
+            )
+            location_dest = order._get_destination_location()
+            location_id = order.partner_id.property_stock_supplier.id
+            picking_type = order.picking_type_id
+            order._process_picking(
+                entry_lines,
+                entry_pickings,
+                picking_type=picking_type,
+                location_id=location_id,
+                location_dest_id=location_dest,
+            )
+            order._process_picking(
+                return_lines,
+                return_pickings,
+                picking_type=picking_type.return_picking_type_id,
+                location_id=location_dest,
+                location_dest_id=location_id,
+                is_return=True,
+            )
+        return True
--- purchase_order_return/models/purchase_order_line.py
+++ purchase_order_return/models/purchase_order_line.py
@@ -1,7 +1,9 @@
 # Copyright 2024 Berezi Amubieta - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
+from odoo.tools.float_utils import float_compare
 
 
 class PurchaseOrderLine(models.Model):
@@ -21,23 +23,181 @@ class PurchaseOrderLine(models.Model):
             self.product_qty = 0
         return result
 
-    @api.model
-    def create(self, values):
-        line = super().create(values)
-        if values.get("return_qty", False):
-            done_picking = line.order_id.picking_ids.filtered(
-                lambda c: c.state == "done"
+    def _ensure_picking_for_line(
+        self, line, qty, picking_type, location_id, location_dest_id, to_refund
+    ):
+        if qty <= 0:
+            return
+        picking = line.order_id.picking_ids.filtered(
+            lambda p: p.state not in ("done", "cancel")
+            and p.picking_type_id == picking_type
+        )[:1]
+        if not picking:
+            vals = line.order_id._prepare_picking(
+                picking_type, location_id, location_dest_id
             )
-            if done_picking:
-                for move in line.move_ids:
-                    if move.picking_id != done_picking[:1]:
-                        picking = move.picking_id
-                        move.picking_id = done_picking[:1].id
-                        move.state = "done"
-                        if not move.move_line_ids:
-                            self.env["stock.move.line"].create(
-                                move._prepare_move_line_vals()
-                            )
-                        if not picking.move_ids_without_package:
-                            picking.unlink()
-        return line
+            picking = self.env["stock.picking"].create(vals)
+        moves = picking.move_ids_without_package.filtered(
+            lambda m: m.purchase_line_id == line and m.state not in ("done", "cancel")
+        )
+        if moves:
+            moves.write(
+                {
+                    "product_uom_qty": qty,
+                    "restrict_lot_id": line.lot_id.id if line.lot_id else False,
+                }
+            )
+        else:
+            moves = self.env["stock.move"].create(
+                {
+                    "name": line.name or line.product_id.display_name,
+                    "product_id": line.product_id.id,
+                    "product_uom_qty": qty,
+                    "product_uom": line.product_uom.id,
+                    "picking_id": picking.id,
+                    "location_id": location_id,
+                    "location_dest_id": location_dest_id,
+                    "company_id": line.company_id.id,
+                    "purchase_line_id": line.id,
+                    "restrict_lot_id": line.lot_id.id if line.lot_id else False,
+                    "origin": line.order_id.name,
+                    "group_id": line.order_id.group_id.id,
+                    "state": "draft",
+                }
+            )
+            if to_refund:
+                moves.write(
+                    {
+                        "to_refund": True,
+                    }
+                )
+
+    def _create_stock_return_moves(self, picking):
+        StockMove = self.env["stock.move"]
+        moves = StockMove.browse()
+        for line in self:
+            qty = line.return_qty - abs(line.qty_received)
+            if qty <= 0:
+                continue
+            moves |= StockMove.create(
+                {
+                    "name": line.name or line.product_id.display_name,
+                    "product_id": line.product_id.id,
+                    "product_uom_qty": qty,
+                    "product_uom": line.product_uom.id,
+                    "picking_id": picking.id,
+                    "location_id": line.order_id._get_destination_location(),
+                    "location_dest_id": line.order_id.partner_id.property_stock_supplier.id,
+                    "company_id": line.order_id.company_id.id,
+                    "purchase_line_id": line.id,
+                    "restrict_lot_id": line.lot_id.id if line.lot_id else False,
+                    "to_refund": True,
+                    "origin": line.order_id.name,
+                    "group_id": line.order_id.group_id.id,
+                    "state": "draft",
+                }
+            )
+        return moves
+
+    def _create_or_update_picking(self):
+        for line in self:
+            if not (line.product_id and line.product_id.type in ("product", "consu")):
+                continue
+            if (
+                float_compare(
+                    line.product_qty, line.qty_received, line.product_uom.rounding
+                )
+                < 0
+                and line.product_qty > 0
+            ):
+                raise UserError(
+                    _(
+                        "You cannot decrease the ordered quantity below\n"
+                        "the received quantity. Create a return first."
+                    )
+                )
+            if (
+                float_compare(
+                    line.return_qty, abs(line.qty_received), line.product_uom.rounding
+                )
+                < 0
+                and line.return_qty > 0
+            ):
+                raise UserError(
+                    _(
+                        "You cannot decrease the return quantity below\n"
+                        "the already returned quantity. Create a reception first."
+                    )
+                )
+            if (
+                float_compare(
+                    line.product_qty, line.qty_invoiced, line.product_uom.rounding
+                )
+                == -1
+                and line.invoice_lines
+            ):
+                line.invoice_lines[0].move_id.activity_schedule(
+                    "mail.mail_activity_data_warning",
+                    note=_(
+                        "The quantities on your purchase order indicate less\n"
+                        "than billed. You should ask for a refund."
+                    ),
+                )
+            self._ensure_picking_for_line(
+                line=line,
+                qty=line.product_uom_qty - line.qty_received,
+                picking_type=line.order_id.picking_type_id,
+                location_id=line.order_id.partner_id.property_stock_supplier.id,
+                location_dest_id=line.order_id._get_destination_location(),
+                to_refund=False,
+            )
+            self._ensure_picking_for_line(
+                line=line,
+                qty=line.return_qty - abs(line.qty_received),
+                picking_type=line.order_id.picking_type_id.return_picking_type_id,
+                location_id=line.order_id._get_destination_location(),
+                location_dest_id=line.order_id.partner_id.property_stock_supplier.id,
+                to_refund=True,
+            )
+
+    def write(self, values):
+        lines = self.filtered(lambda l: l.order_id.state == "purchase")
+        prev_product_qty = {line.id: line.product_uom_qty for line in lines}
+        prev_return_qty = {line.id: line.return_qty for line in lines}
+        res = super().write(values)
+        for line in lines:
+            if "product_uom_qty" in values:
+                if (
+                    float_compare(
+                        prev_product_qty[line.id],
+                        line.product_uom_qty,
+                        precision_rounding=line.product_uom.rounding,
+                    )
+                    != 0
+                ):
+                    line._create_or_update_picking()
+            if "return_qty" in values:
+                if (
+                    float_compare(
+                        prev_return_qty[line.id],
+                        line.return_qty,
+                        precision_rounding=line.product_uom.rounding,
+                    )
+                    != 0
+                ):
+                    line._create_or_update_picking()
+        return res
+
+    @api.depends(
+        "move_ids.state",
+        "move_ids.scrapped",
+        "move_ids.product_uom_qty",
+        "move_ids.product_uom",
+        "return_qty",
+        "move_ids.quantity_done",
+    )
+    def _compute_qty_received(self):
+        super(PurchaseOrderLine, self)._compute_qty_received()
+        for line in self:
+            if line.return_qty and line.return_qty > 0:
+                line.product_qty = line.qty_received
