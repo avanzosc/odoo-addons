c6cef88488073ec4a71c664d9e30f1fbb2382a12
Eñaut Alberdi <enautavanzosc@gmail.com>
2026-01-21 16:22:21 +0100

    [14.0][FIX] stock_employee: Assign employees to stock picking and quant packages

--- stock_employee/models/stock_move_line.py
+++ stock_employee/models/stock_move_line.py
@@ -11,15 +11,20 @@ class StockMoveLine(models.Model):
 
     @api.onchange("employee_id")
     def _onchange_employee(self):
-
+        
         for line in self:
             if line.employee_id and line.package_id:
                 line.package_id.employee_id = line.employee_id
 
     def _action_done(self):
-
+       
         res = super()._action_done()
         for line in self.exists():
-            if line.employee_id and line.package_id and not line.package_id.employee_id:
+            if (
+                line.employee_id
+                and line.package_id
+                and not line.package_id.employee_id
+            ):
                 line.package_id.employee_id = line.employee_id
         return res
+
