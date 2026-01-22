3904992f80303c119439d86a4bc58160d4a3d529
aneravanzosc <aneravanzosc@gmail.com>
2026-01-22 14:31:53 +0100

    [14.0][ADD] agreement_penalty_type: link penalties and custom pricing to agreements

--- agreement_penalty_type/models/agreement_penalty_type.py
+++ agreement_penalty_type/models/agreement_penalty_type.py
@@ -1,7 +1,7 @@
 # Copyright 2026 Aner Arregi - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class AgreementPenaltyType(models.Model):
@@ -19,6 +19,14 @@ class AgreementPenaltyType(models.Model):
         string="Penalty Type",
         required=True,
     )
+    product_id = fields.Many2one(
+        comodel_name="product.product",
+        related="penalty_type_id.product_id",
+        string="Product",
+        readonly=True,
+    )
+    penalty_price = fields.Float(string="Penalty Price", digits=(16, 2))
+
     number = fields.Integer(string="Duration", default=1)
     term = fields.Selection(
         selection=[
@@ -35,3 +43,9 @@ class AgreementPenaltyType(models.Model):
         default=0,
     )
     notes = fields.Text(string="Notes")
+
+    @api.onchange("penalty_type_id")
+    def _onchange_penalty_type_id(self):
+        """Trae el precio de venta del producto al seleccionar el tipo"""
+        if self.penalty_type_id and self.penalty_type_id.product_id:
+            self.penalty_price = self.penalty_type_id.product_id.list_price
