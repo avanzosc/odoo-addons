3eede28e030aa04926a0b68c12aa5224f1fce34e
aneravanzosc <aneravanzosc@gmail.com>
2026-01-26 15:47:50 +0100

    [IMP] : pre-commit auto fixes

--- stock_usability/models/stock_quant.py
+++ stock_usability/models/stock_quant.py
@@ -2,7 +2,10 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from odoo import fields, models
 
+
 class StockQuant(models.Model):
     _inherit = "stock.quant"
 
-    tracking = fields.Selection(related='product_id.tracking', store=True, readonly=True)
+    tracking = fields.Selection(
+        related="product_id.tracking", store=True, readonly=True
+    )
