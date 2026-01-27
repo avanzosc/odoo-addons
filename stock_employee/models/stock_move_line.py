09918593d0d98f25d57ce05387144bb3aa3ba610
Eñaut Alberdi <enautavanzosc@gmail.com>
2026-01-27 15:33:40 +0100

    [14.0][FIX] stock_employee: Assign employees to stock picking and quant packages

--- stock_employee/models/stock_move_line.py
+++ stock_employee/models/stock_move_line.py
@@ -9,17 +9,28 @@ class StockMoveLine(models.Model):
         string="Employee",
     )
 
-    @api.onchange("employee_id")
-    def _onchange_employee(self):
-
+    def _sync_employee_to_package(self):
         for line in self:
-            if line.employee_id and line.package_id:
-                line.package_id.employee_id = line.employee_id
+            package = line.result_package_id or line.package_id
+            if line.employee_id and package and package.employee_id != line.employee_id:
+                package.employee_id = line.employee_id
+
+    @api.onchange("employee_id", "result_package_id", "package_id")
+    def _onchange_employee(self):
+        self._sync_employee_to_package()
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        records = super().create(vals_list)
+        records._sync_employee_to_package()
+        return records
+
+    def write(self, vals):
+        res = super().write(vals)
+        self._sync_employee_to_package()
+        return res
 
     def _action_done(self):
-
         res = super()._action_done()
-        for line in self.exists():
-            if line.employee_id and line.package_id and not line.package_id.employee_id:
-                line.package_id.employee_id = line.employee_id
+        self.exists()._sync_employee_to_package()
         return res
