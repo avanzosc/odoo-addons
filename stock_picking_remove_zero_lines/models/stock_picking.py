60f78b0ed1dde63d8db0a817a88347fc9cd7e9b3
luciaavanzosc <luciaavanzosc@gmail.com>
2026-02-20 09:11:48 +0100

    [16.0][ADD] stock_picking_remove_zero_lines: Remove move lines with 0 quantity on picking validation.

--- stock_picking_remove_zero_lines/models/stock_picking.py
+++ stock_picking_remove_zero_lines/models/stock_picking.py
@@ -2,13 +2,14 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 from odoo import models
 
+
 class StockPicking(models.Model):
-    _inherit = 'stock.picking'
+    _inherit = "stock.picking"
 
     def button_validate(self):
         for picking in self:
             lines_to_remove = picking.move_line_ids.filtered(
-                lambda l: l.qty_done == 0 and l.state not in ('done', 'cancel')
+                lambda line: line.qty_done == 0 and line.state not in ("done", "cancel")
             )
             if lines_to_remove:
                 lines_to_remove.unlink()
