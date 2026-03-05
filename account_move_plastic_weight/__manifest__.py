9e10a943a8947d65a26c7cf364cab5003172e866
luciaavanzosc <luciaavanzosc@gmail.com>
2026-03-05 15:46:11 +0100

    [16.0][ADD] account_move_plastic_weight: Shows total weight of recycled and non-recycled plastic on invoices.

--- account_move_plastic_weight/__manifest__.py
+++ account_move_plastic_weight/__manifest__.py
@@ -2,19 +2,18 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
     "name": "Account Move Plastic Weight",
-    "summary":"Shows total weight of recycled and non-recycled plastic on invoices",
+    "summary": "Shows total weight of recycled and non-recycled plastic on invoices",
     "version": "16.0.1.0.0",
     "category": "Accounting/Localizations",
     "license": "AGPL-3",
     "author": "AvanzOSC",
     "website": "https://github.com/avanzosc/odoo-addons",
     "depends": [
-        'account',
-        'l10n_es_aeat_mod592',
+        "account",
+        "l10n_es_aeat_mod592",
     ],
     "data": [
-        'views/account_move_views.xml',
+        "views/account_move_views.xml",
     ],
     "installable": True,
 }
-
--- account_move_plastic_weight/models/account_move.py
+++ account_move_plastic_weight/models/account_move.py
@@ -1,6 +1,7 @@
 # Copyright 2026 Lucía Echeverría - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from odoo import models, fields
+from odoo import api, fields, models
+
 
 class AccountMove(models.Model):
     _inherit = "account.move"
@@ -9,18 +10,25 @@ class AccountMove(models.Model):
         string="Total non-recycled plastic (kg)",
         store=True,
         copy=True,
-        help="Sum of the weight of non-recycled plastic (subject to IPNR) of all products on the invoice.",
+        help="Sum of the weight of non-recycled plastic (subject to IPNR) "
+        "of all products on the invoice.",
     )
     x_plastic_weight_recyclable_total = fields.Float(
         string="Total recycled plastic (kg)",
         store=True,
         copy=True,
-        help="Sum of the weight of recycled plastic (NOT subject to IPNR) of all products on the invoice.",
+        help="Sum of the weight of recycled plastic (NOT subject to IPNR) "
+        "of all products on the invoice.",
     )
 
     def _compute_plastic_weight_totals(self):
         for move in self:
-            if move.move_type not in ("out_invoice", "out_refund", "in_invoice", "in_refund"):
+            if move.move_type not in (
+                "out_invoice",
+                "out_refund",
+                "in_invoice",
+                "in_refund",
+            ):
                 continue
 
             total_non_recyclable = 0.0
@@ -37,11 +45,15 @@ class AccountMove(models.Model):
                     continue
 
                 try:
-                    qty = line.product_uom_id._compute_quantity(qty, line.product_id.uom_id)
+                    qty = line.product_uom_id._compute_quantity(
+                        qty, line.product_id.uom_id
+                    )
                 except Exception:
                     qty = line.quantity
 
-                weight_non_recyclable = line.product_id.plastic_weight_non_recyclable or 0.0
+                weight_non_recyclable = (
+                    line.product_id.plastic_weight_non_recyclable or 0.0
+                )
                 weight_total = line.product_id.plastic_tax_weight or 0.0
                 weight_recyclable = max(weight_total - weight_non_recyclable, 0.0)
 
@@ -52,16 +64,20 @@ class AccountMove(models.Model):
                 total_non_recyclable *= -1.0
                 total_recyclable *= -1.0
 
-            move.write({
-                "x_plastic_weight_non_recyclable_total": round(total_non_recyclable, 6),
-                "x_plastic_weight_recyclable_total": round(total_recyclable, 6),
-            })
-            
-    def write(self, vals):
-        res = super().write(vals)
-        if "invoice_line_ids" in vals:
-            self._compute_plastic_weight_totals()
-        return res
+            move.write(
+                {
+                    "x_plastic_weight_non_recyclable_total": round(
+                        total_non_recyclable, 6
+                    ),
+                    "x_plastic_weight_recyclable_total": round(total_recyclable, 6),
+                }
+            )
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        moves = super().create(vals_list)
+        moves._compute_plastic_weight_totals()
+        return moves
 
     def action_post(self):
         res = super().action_post()
