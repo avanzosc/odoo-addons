dd53002db8920e48e34695ad15b6e22bfa0c640d
Eñaut Alberdi <enautavanzosc@gmail.com>
2026-03-13 09:09:16 +0100

    [18.0][MIG] purchase_open_qty_fix: Migration to 18.0

--- purchase_open_qty_fix/__manifest__.py
+++ purchase_open_qty_fix/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
     "name": "Purchase Open Qty Fix",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.1",
     "category": "Purchases",
     "license": "AGPL-3",
     "author": "Avanzosc",
--- purchase_open_qty_fix/models/purchase_order.py
+++ purchase_open_qty_fix/models/purchase_order.py
@@ -1,10 +1,23 @@
 # Copyright 2025 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from odoo import api, models
+from odoo import api, fields, models
 
 
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
+
+    qty_to_invoice = fields.Float(
+        compute="_compute_qty_to_invoice",
+        search="_search_qty_to_invoice",
+        string="Qty to Bill",
+        default=0.0,
+    )
+    qty_to_receive = fields.Float(
+        compute="_compute_qty_to_receive",
+        search="_search_qty_to_receive",
+        string="Qty to Receive",
+        default=0.0,
+    )
 
     @api.model
     def _search_qty_to_invoice(self, operator, value):
