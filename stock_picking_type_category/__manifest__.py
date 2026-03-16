88c48424bb394ca594e770dd541e8933516eeedc
Berezi Amubieta <bereziamubieta@avancosc.es>
2026-03-16 16:13:51 +0100

    [18.0][MIG] stock_move_line_divide: Migration to 18.0

--- stock_picking_type_category/__manifest__.py
+++ stock_picking_type_category/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Stock Picking Category Type",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "MRP",
     "license": "AGPL-3",
     "author": "AvanzOSC",
