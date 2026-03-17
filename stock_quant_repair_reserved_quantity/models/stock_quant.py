24484a37df97b246c96cce5a1b382d3cc53e745a
Eñaut Alberdi <enautavanzosc@gmail.com>
2026-03-17 11:46:24 +0100

    [18.0][MIG] stock_quant_repair_reserved_quantity: Migration to 18.0

--- stock_quant_repair_reserved_quantity/models/stock_quant.py
+++ stock_quant_repair_reserved_quantity/models/stock_quant.py
@@ -1,6 +1,7 @@
 # Copyright 2024 Berezi Amubieta - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from odoo import models
+from odoo.tools.float_utils import float_is_zero
 
 
 class StockQuant(models.Model):
@@ -14,16 +15,18 @@ class StockQuant(models.Model):
                 ("location_id", "=", quant.location_id.id),
                 ("lot_id", "=", quant.lot_id.id),
                 ("package_id", "=", quant.package_id.id),
+                ("owner_id", "=", quant.owner_id.id),
                 ("state", "=", "assigned"),
             ]
             moves = move_obj.search(domain)
-            reserved_qty = (
-                sum(moves.mapped("product_uom_qty")) - quant.reserved_quantity
-            )
+            reserved_qty = sum(moves.mapped("quantity_product_uom")) - quant.reserved_quantity
+            if float_is_zero(reserved_qty, precision_rounding=quant.product_id.uom_id.rounding):
+                continue
             quant.sudo()._update_reserved_quantity(
                 quant.product_id,
                 quant.location_id,
                 reserved_qty,
                 lot_id=quant.lot_id,
                 package_id=quant.package_id,
+                owner_id=quant.owner_id,
             )
