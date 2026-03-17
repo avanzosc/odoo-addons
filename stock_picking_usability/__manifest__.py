ec59be51d4170d4ce914176bb93170b82120f141
Eñaut Alberdi <enautavanzosc@gmail.com>
2026-03-17 14:50:42 +0100

    [18.0][MIG] stock_picking_usability: Migration to 18.0

--- stock_picking_usability/__manifest__.py
+++ stock_picking_usability/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Stock Picking Usability",
-    "version": "14.0.2.1.0",
+    "version": "18.0.1.0.0",
     "category": "Hidden/Tools",
     "license": "AGPL-3",
     "author": "AvanzOSC",
