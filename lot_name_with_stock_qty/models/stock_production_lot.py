6b020ac5a091a258b00676855d63a66571192898
Eñaut Alberdi <enautavanzosc@gmail.com>
2026-03-17 13:06:09 +0100

    [18.0][MIG] lot_name_with_stock_qty: Migration to 18.0

--- lot_name_with_stock_qty/models/stock_production_lot.py
+++ lot_name_with_stock_qty/models/stock_production_lot.py
@@ -5,38 +5,45 @@ from odoo.tools import float_repr, float_round
 
 
 class StockProductionLot(models.Model):
-    _inherit = "stock.production.lot"
+    _inherit = "stock.lot"
 
-    def name_get(self):
-        result = []
-        rounding = self.env["decimal.precision"].precision_get(
+    @api.depends("name", "product_qty")
+    @api.depends_context("default_location_id")
+    def _compute_display_name(self):
+        precision = self.env["decimal.precision"].precision_get(
             "Product Unit of Measure"
         )
+        fallback_rounding = 10**-precision
         for lot in self:
-            name = "{} ({})".format(
-                lot.name,
+            precision_rounding = lot.product_uom_id.rounding or fallback_rounding
+            lot.display_name = "{} ({})".format(
+                lot.name or "",
                 float_repr(
-                    float_round(lot.product_qty, precision_digits=rounding), rounding
+                    float_round(lot.product_qty, precision_rounding=precision_rounding),
+                    precision,
                 ),
             )
-            result.append((lot.id, name))
-        return result
 
-    @api.depends("quant_ids", "quant_ids.quantity")
+    @api.depends(
+        "quant_ids",
+        "quant_ids.quantity",
+        "quant_ids.location_id",
+        "quant_ids.location_id.usage",
+        "quant_ids.location_id.company_id",
+    )
+    @api.depends_context("default_location_id")
     def _product_qty(self):
         super()._product_qty()
         location_id = self.env.context.get("default_location_id")
-        if location_id:
-            for lot in self:
-                # We only care for the quants in internal or transit locations.
-                quants = lot.quant_ids.filtered(
-                    lambda q: q.location_id.id == location_id
-                    and (
-                        q.location_id.usage == "internal"
-                        or (
-                            q.location_id.usage == "transit"
-                            and q.location_id.company_id
-                        )
-                    )
+        if not location_id:
+            return
+        for lot in self:
+            # We only care for the quants in internal or transit locations.
+            quants = lot.quant_ids.filtered(
+                lambda q: q.location_id.id == location_id
+                and (
+                    q.location_id.usage == "internal"
+                    or (q.location_id.usage == "transit" and q.location_id.company_id)
                 )
-                lot.product_qty = sum(quants.mapped("quantity"))
+            )
+            lot.product_qty = sum(quants.mapped("quantity"))
