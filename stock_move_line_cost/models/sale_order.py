c260d023090ace2c9a811adfce9d0b5c06806412
Eñaut Alberdi <enautavanzosc@gmail.com>
2026-03-23 15:13:12 +0100

    [18.0][MIG] stock_move_line_cost: Migration to 18.0

--- stock_move_line_cost/models/sale_order.py
+++ stock_move_line_cost/models/sale_order.py
@@ -8,7 +8,6 @@ class SaleOrder(models.Model):
 
     def action_confirm(self):
         result = super().action_confirm()
-        for picking in self.picking_ids:
-            for move in picking.move_ids_without_package:
-                move._onchange_sale_line_id()
+        moves = self.mapped("picking_ids.move_ids")
+        moves._set_standard_price_from_source(force=True)
         return result
--- stock_move_line_cost/models/stock_move.py
+++ stock_move_line_cost/models/stock_move.py
@@ -6,56 +6,47 @@ from odoo import api, fields, models
 class StockMove(models.Model):
     _inherit = "stock.move"
 
-    def _default_standard_price(self):
-        result = 0
-        if self.sale_line_id and self.sale_line_id.price_unit:
-            result = self.sale_line_id.price_unit
-        elif self.purchase_line_id and self.purchase_line_id.price_unit:
-            result = self.purchase_line_id.price_unit
-        elif self.product_id:
-            result = self.product_id.standard_price
-        return result
+    standard_price = fields.Float(string="Cost", digits="Product Price")
+    amount = fields.Float(compute="_compute_amount", store=True, digits="Product Price")
 
-    standard_price = fields.Float(string="Cost", default=_default_standard_price)
-    amount = fields.Float(string="Amount")
+    @api.depends("standard_price", "quantity")
+    def _compute_amount(self):
+        for move in self:
+            move.amount = move.standard_price * move.quantity
 
-    @api.onchange("sale_line_id")
-    def _onchange_sale_line_id(self):
+    def _get_standard_price_from_source(self):
+        self.ensure_one()
         if self.sale_line_id:
-            self.standard_price = self.sale_line_id.price_unit
-            self.onchange_standard_price()
-
-    @api.onchange("purchase_line_id")
-    def _onchange_purchase_line_id(self):
+            return self.sale_line_id.price_unit
         if self.purchase_line_id:
-            self.standard_price = self.purchase_line_id.price_unit
-            for line in self.move_line_ids:
-                line.standard_price = self.standard_price
-                line.onchange_standard_price()
-            self.onchange_standard_price()
+            return self.purchase_line_id.price_unit
+        if self.product_id:
+            return self.product_id.standard_price
+        return 0.0
 
-    @api.onchange("product_id")
-    def _onchange_product_id(self):
-        if self.sale_line_id and self.sale_line_id.price_unit:
-            self.standard_price = self.sale_line_id.price_unit
-        elif self.purchase_line_id and self.purchase_line_id.price_unit:
-            self.standard_price = self.purchase_line_id.price_unit
-        elif self.product_id and self.product_id.standard_price:
-            self.standard_price = self.product_id.standard_price
+    def _set_standard_price_from_source(self, force=False):
+        for move in self:
+            if force or not move.standard_price:
+                move.standard_price = move._get_standard_price_from_source()
 
-    @api.onchange("standard_price", "quantity_done")
-    def onchange_standard_price(self):
-        if self.standard_price:
-            self.amount = self.standard_price * self.quantity_done
+    @api.model_create_multi
+    def create(self, vals_list):
+        moves = super().create(vals_list)
+        for move, vals in zip(moves, vals_list):
+            if "standard_price" not in vals:
+                move._set_standard_price_from_source()
+        return moves
 
-    @api.depends(
-        "move_line_ids.qty_done",
-        "move_line_ids.product_uom_id",
-        "move_line_nosuggest_ids.qty_done",
-        "picking_type_id",
-    )
-    def _quantity_done_compute(self):
-        result = super()._quantity_done_compute()
-        for line in self:
-            line.onchange_standard_price()
+    def write(self, vals):
+        update_from_source = (
+            "standard_price" not in vals
+            and any(key in vals for key in ("sale_line_id", "purchase_line_id", "product_id"))
+        )
+        result = super().write(vals)
+        if update_from_source:
+            self._set_standard_price_from_source(force=True)
         return result
+
+    @api.onchange("sale_line_id", "purchase_line_id", "product_id")
+    def _onchange_standard_price_source(self):
+        self._set_standard_price_from_source(force=True)
