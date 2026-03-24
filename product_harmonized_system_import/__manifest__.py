551088d7e4be5caa6bf90c1e5bb096c754472d19
Eñaut Alberdi <enautavanzosc@gmail.com>
2026-03-24 13:48:30 +0100

    [18.0][MIG] product_harmonized_system_import: Migration to 18.0

--- product_harmonized_system_import/__manifest__.py
+++ product_harmonized_system_import/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Product Harmonized System Import",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Hidden/Tools",
     "license": "AGPL-3",
     "author": "AvanzOSC",
