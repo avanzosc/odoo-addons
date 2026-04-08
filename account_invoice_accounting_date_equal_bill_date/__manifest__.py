8bf91a0863a58226a71e99d6a46eea41c8fcadf6
Ane <aneavanzosc@gmail.com>
2026-04-08 14:35:41 +0200

    [18.0][MIG] account_invoice_accounting_date_equal_bill_date: Migration to 18.0

--- account_invoice_accounting_date_equal_bill_date/__manifest__.py
+++ account_invoice_accounting_date_equal_bill_date/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Account Invoice Accounting Date Equal Bill Date",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Invoices & Payments",
     "license": "AGPL-3",
     "author": "AvanzOSC",
