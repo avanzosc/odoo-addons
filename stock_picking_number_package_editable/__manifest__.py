823b381c54427e2e47988deeae036f6e33393fd1
Alfredo <alfredodelafuente@avanzosc.es>
2026-04-16 10:56:39 +0200

    [16.0][ADD] stock_picking_number_package_editable: In picking field "Number of Packages" editable.

---
+++ stock_picking_number_package_editable/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2026 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Stock Picking Number Package Editable",
+    "version": "16.0.1.0.0",
+    "author": "Avanzosc",
+    "category": "Delivery",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "license": "AGPL-3",
+    "depends": ["delivery_package_number"],
+    "data": [
+        "views/stock_picking_views.xml",
+    ],
+    "installable": True,
+}
