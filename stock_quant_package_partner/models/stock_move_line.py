f99c85139e4117ef365c01298d668a70dcb68411
luciaavanzosc <luciaavanzosc@gmail.com>
2026-04-29 13:39:02 +0200

    [16.0][FIX] stock_quant_package_partner: Fix error while validating pickings.

--- stock_quant_package_partner/models/stock_move_line.py
+++ stock_quant_package_partner/models/stock_move_line.py
@@ -5,9 +5,13 @@ class StockMoveLine(models.Model):
     _inherit = "stock.move.line"
 
     def _action_done(self):
+        pending = [
+            (ml.package_id, ml.move_id.picking_id.partner_id)
+            for ml in self
+            if ml.package_id and ml.move_id.picking_id and not ml.package_id.partner_id
+        ]
         res = super()._action_done()
-        for ml in self:
-            if ml.package_id and ml.move_id.picking_id and not ml.package_id.partner_id:
-                ml.package_id.partner_id = ml.move_id.picking_id.partner_id
-
+        for package, partner in pending:
+            if not package.partner_id:
+                package.partner_id = partner
         return res
