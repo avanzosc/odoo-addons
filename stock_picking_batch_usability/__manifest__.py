fe7cfc5573444a53bef25f224a288a20acc847e3
Eñaut Alberdi <enautavanzosc@gmail.com>
2026-05-06 12:53:50 +0200

    [18.0][MIG] stock_picking_batch_usability: Migration to 18.0

--- stock_picking_batch_usability/__manifest__.py
+++ stock_picking_batch_usability/__manifest__.py
@@ -2,12 +2,11 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Stock Picking Batch Usability",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Avanzosc",
     "category": "Inventory",
     "website": "https://github.com/avanzosc/odoo-addons",
     "depends": ["stock_picking_batch"],
-    "data": ["views/stock_picking_batch_view.xml"],
     "license": "AGPL-3",
     "installable": True,
 }
--- stock_picking_batch_usability/models/stock_picking_batch.py
+++ stock_picking_batch_usability/models/stock_picking_batch.py
@@ -1,45 +1,23 @@
 # Copyright 2022 Berezi Amubieta - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import api, fields, models
+from odoo import api, models
 
 
 class StockPickingBatch(models.Model):
     _inherit = "stock.picking.batch"
 
-    in_progress = fields.Boolean(string="Is in progress", default=False)
-    is_done = fields.Boolean(string="Is done", default=False)
-
     @api.depends("company_id", "picking_type_id", "state")
     def _compute_allowed_picking_ids(self):
         result = super()._compute_allowed_picking_ids()
         for batch in self:
-            allowed_pickings = batch.allowed_picking_ids
-            domain = [("state", "=", "done")]
+            domain = [
+                ("company_id", "=", batch.company_id.id),
+                ("state", "=", "done"),
+            ]
             if batch.picking_type_id:
-                domain += [("picking_type_id", "=", batch.picking_type_id.id)]
-            new_pickings = self.env["stock.picking"].search(domain)
-            allowed_pickings += new_pickings
-            batch.allowed_picking_ids = allowed_pickings
-        return result
-
-    def action_confirm(self):
-        result = super().action_confirm()
-        self.in_progress = True
-        return result
-
-    def action_done(self):
-        result = super().action_done()
-        self.is_done = True
-        return result
-
-    @api.depends("picking_ids", "picking_ids.state", "in_progress", "is_done")
-    def _compute_state(self):
-        result = super()._compute_state()
-        for batch in self:
-            if batch.in_progress is False and (batch.is_done) is False:
-                batch.state = "draft"
-            if batch.in_progress is True and batch.is_done is False:
-                batch.state = "in_progress"
-            if batch.in_progress is True and batch.is_done is True:
-                batch.state = "done"
+                domain.append(("picking_type_id", "=", batch.picking_type_id.id))
+            done_pickings = self.env["stock.picking"].search(
+                domain, order="date_done desc", limit=1000
+            )
+            batch.allowed_picking_ids |= done_pickings
         return result
