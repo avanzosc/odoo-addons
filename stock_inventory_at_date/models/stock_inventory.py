35743f170ce1f14e24e4b0082cdba37ac6bf55d3
Eñaut Alberdi <enautavanzosc@gmail.com>
2026-05-08 09:23:20 +0200

    [18.0][MIG] stock_inventory_at_date: Migration to 18.0

--- stock_inventory_at_date/models/stock_inventory.py
+++ stock_inventory_at_date/models/stock_inventory.py
@@ -6,24 +6,27 @@ from odoo import fields, models
 class StockInventory(models.Model):
     _inherit = "stock.inventory"
 
-    def _default_aacounting_date(self):
-        result = fields.Datetime.now()
-        return result
+    accounting_date = fields.Datetime(default=fields.Datetime.now, copy=False)
 
-    accounting_date = fields.Datetime(
-        string="Date", default=_default_aacounting_date, copy=False
-    )
-
-    def action_open_inventory_lines(self):
-        result = super().action_open_inventory_lines()
+    def action_view_inventory_adjustment(self):
+        result = super().action_view_inventory_adjustment()
+        result.setdefault("context", {})
         result["context"].update({"inventory_date": self.accounting_date})
         return result
 
-    def action_validate(self):
-        result = super().action_validate()
-        if self.accounting_date:
-            for move in self.move_ids:
-                move.date = self.accounting_date
-                for line in move.move_line_ids:
-                    line.date = self.accounting_date
+    def action_state_to_in_progress(self):
+        for rec in self.filtered("accounting_date"):
+            rec.date = rec.accounting_date
+        result = super().action_state_to_in_progress()
+        for rec in self.filtered("accounting_date"):
+            rec.stock_quant_ids.filtered(
+                lambda quant, rec=rec: quant.current_inventory_id == rec
+            ).write({"inventory_date": rec.accounting_date})
+        return result
+
+    def action_state_to_done(self):
+        result = super().action_state_to_done()
+        for rec in self.filtered("accounting_date"):
+            rec.stock_move_ids.write({"date": rec.accounting_date})
+            rec.stock_move_ids.mapped("move_id").write({"date": rec.accounting_date})
         return result
