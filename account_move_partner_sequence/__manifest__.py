88bcde19b3557998f09a21e011cf9e03f1891247
Eñaut Alberdi <enautavanzosc@gmail.com>
2026-05-08 10:43:05 +0200

    [18.0][MIG] account_move_partner_sequence: Migration to 18.0

--- account_move_partner_sequence/__manifest__.py
+++ account_move_partner_sequence/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Account Move Partner Sequence",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Analytic",
     "license": "AGPL-3",
     "author": "AvanzOSC",
