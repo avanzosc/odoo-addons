12ce9dfab4e5705cb439469cbe5ce733cc24db76
Eñaut Alberdi <enautavanzosc@gmail.com>
2026-05-22 10:57:27 +0200

    [18.0][MIG] stock_move_line_product_lot_reader: Migration to 18.0

--- stock_move_line_product_lot_reader/__manifest__.py
+++ stock_move_line_product_lot_reader/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Stock Move Line Product Lot Reader",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Avanzosc",
     "website": "https://github.com/avanzosc/odoo-addons",
     "category": "Inventory",
