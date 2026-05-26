9c69b3165e8f24c8fd3623727ee368f4c721728c
Eñaut Alberdi <enautavanzosc@gmail.com>
2026-05-26 15:11:55 +0200

    [18.0][MIG] purchase_requisiton_line_usability: Migration to 18.0

--- purchase_requisition_line_usability/models/purchase_requisition_line.py
+++ purchase_requisition_line_usability/models/purchase_requisition_line.py
@@ -18,41 +18,35 @@ class PurchaseRequisitionLine(models.Model):
         related="requisition_id.user_id",
         store=True,
     )
-    type_id = fields.Many2one(
-        string="Purchase Requisition Type",
-        comodel_name="purchase.requisition.type",
-        related="requisition_id.type_id",
-        store=True,
-    )
     price_unit = fields.Float(digits="Price Unit Decimal Precision")
-    shipping_cost = fields.Float(
-        string="Shipping Cost", digits="Price Unit Decimal Precision"
-    )
-    date_end = fields.Datetime(
+    shipping_cost = fields.Float(digits="Price Unit Decimal Precision")
+    date_end = fields.Date(
         string="Date End", related="requisition_id.date_end", store=True
     )
     ordering_date = fields.Date(
-        string="Ordering Date", related="requisition_id.ordering_date", store=True
+        string="Ordering Date", related="requisition_id.date_start", store=True
     )
     schedule_date = fields.Date(
-        string="Schedule Date", related="requisition_id.schedule_date", store=True
+        string="Schedule Date", related="requisition_id.date_end", store=True
     )
     state = fields.Selection(string="State", related="requisition_id.state", store=True)
     origin = fields.Char(
-        string="Origin Document", related="requisition_id.origin", store=True
+        string="Origin Document", related="requisition_id.reference", store=True
     )
     dif_qty = fields.Float(string="Pending", compute="_compute_dif_qty")
 
     @api.depends("requisition_id.purchase_ids.state")
     def _compute_ordered_qty(self):
-        super()._compute_ordered_qty()
+        res = super()._compute_ordered_qty()
         for line in self:
             total = 0.0
+            line_product = line.product_id
             for po in line.requisition_id.purchase_ids.filtered(
                 lambda purchase_order: purchase_order.state in ["purchase", "done"]
             ):
                 for po_line in po.order_line.filtered(
-                    lambda order_line: order_line.product_id == line.product_id
+                    lambda order_line, product=line_product: order_line.product_id
+                    == product
                 ):
                     if po_line.product_uom != line.product_uom_id:
                         total += po_line.product_uom._compute_quantity(
@@ -61,6 +55,7 @@ class PurchaseRequisitionLine(models.Model):
                     else:
                         total += po_line.product_qty
             line.qty_ordered = total
+        return res
 
     @api.depends("product_qty", "qty_ordered")
     def _compute_dif_qty(self):
