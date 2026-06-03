96a6ce175baa37bb4d1c31d003f8078c589ac3bb
aneravanzosc <aneravanzosc@gmail.com>
2026-06-03 13:43:11 +0200

    [18.0][MIG] purchase_sale_intercompany_usability: Migration to 18.0

--- purchase_sale_intercompany_usability/__manifest__.py
+++ purchase_sale_intercompany_usability/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Purchase Sale Inter Company Usability",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Purchase Management",
     "license": "AGPL-3",
     "author": "AvanzOSC",
