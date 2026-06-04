f7e6a8a09238bfe3ecca4a9edda0ce990c8fd2ff
aneravanzosc <aneravanzosc@gmail.com>
2026-06-04 09:10:11 +0200

    [18.0][MIG] product_create_group: Migration to 18.0

--- product_create_group/__manifest__.py
+++ product_create_group/__manifest__.py
@@ -3,12 +3,17 @@
 
 {
     "name": "Products Create Group",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Product Management",
     "license": "AGPL-3",
     "author": "AvanzOSC",
     "website": "https://github.com/avanzosc/odoo-addons",
-    "depends": ["product", "sale", "purchase", "mrp"],
+    "depends": [
+        "product",
+        "sale",
+        "purchase",
+        "mrp",
+    ],
     "data": [
         "data/create_product_group.xml",
     ],
--- product_create_group/models/ir_model_access.py
+++ product_create_group/models/ir_model_access.py
@@ -1,38 +1,32 @@
 # Copyright 2024 Berezi Amubieta - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
+import logging
+
 from odoo import api, models
+
+_logger = logging.getLogger(__name__)
 
 
 class IrModelAccess(models.Model):
     _inherit = "ir.model.access"
 
-    @api.model
+    @api.private
     def init(self):
+        accesses = self.search(
+            [
+                ("model_id", "=", "product.template"),
+                "|",
+                ("perm_create", "=", True),
+                ("perm_unlink", "=", True),
+            ]
+        )
         try:
-            group_product_tmpl = self.env.ref(
-                "product_create_group.access_create_product_tmpl"
+            accesses -= self.env.ref("product_create_group.access_create_product_tmpl")
+        except ValueError:
+            _logger.debug(
+                "product_create_group ACL xmlid not yet available; "
+                "stripping create/unlink on all existing product.template "
+                "access records."
             )
-            if group_product_tmpl:
-                templates = (
-                    self.env["ir.model.access"].search(
-                        [
-                            ("model_id", "=", "product.template"),
-                            "|",
-                            ("perm_create", "=", True),
-                            ("perm_unlink", "=", True),
-                        ]
-                    )
-                    - group_product_tmpl
-                )
-        except Exception:
-            templates = self.env["ir.model.access"].search(
-                [
-                    ("model_id", "=", "product.template"),
-                    "|",
-                    ("perm_create", "=", True),
-                    ("perm_unlink", "=", True),
-                ]
-            )
-        for tmpl in templates:
-            tmpl.write({"perm_create": False, "perm_unlink": False})
+        accesses.write({"perm_create": False, "perm_unlink": False})
