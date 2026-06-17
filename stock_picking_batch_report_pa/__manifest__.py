aea3eccd710dd377c57cc087c82b166aea7209db
Eñaut Alberdi <enautavanzosc@gmail.com>
2026-06-17 10:18:28 +0200

    [18.0][MIG] stock_picking_batch_report_pa: Migration to 18.0

--- stock_picking_batch_report_pa/__manifest__.py
+++ stock_picking_batch_report_pa/__manifest__.py
@@ -3,12 +3,16 @@
 {
     "name": "Stock Picking Batch Report PA",
     "summary": "Picking batch report grouped by customer and date.",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "AvanzOSC",
     "website": "https://github.com/avanzosc/odoo-addons",
     "category": "Inventory/Inventory",
     "license": "AGPL-3",
-    "depends": ["stock_picking_batch", "stock_move_qty_by_packaging"],
+    "depends": [
+        "stock_picking_batch",
+        "stock_move_qty_by_packaging",
+        "custom_mrp_line_cost",
+    ],
     "data": [
         "report/report.xml",
     ],
