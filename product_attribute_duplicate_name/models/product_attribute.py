8cda570d097f97d98e70d71c793ecde250064963
Ane <aneavanzosc@gmail.com>
2026-07-15 07:36:15 +0200

    add copy suffix to duplicated attributes

--- product_attribute_duplicate_name/models/product_attribute.py
+++ product_attribute_duplicate_name/models/product_attribute.py
@@ -1,12 +1,16 @@
 # Copyright 2026 Ane Gurruchaga - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import models
+from odoo import _, models
 
 
 class ProductAttribute(models.Model):
     _inherit = "product.attribute"
 
-    _sql_constraints = [
-        ("number_uniq", "CHECK(name IS NOT NULL)", "Attribute name is required!"),
-    ]
+    def copy_data(self, default=None):
+        default = dict(default or {})
+        vals_list = super().copy_data(default=default)
+        if "name" not in default:
+            for attribute, vals in zip(self, vals_list, strict=False):
+                vals["name"] = _("%s (copy)", attribute.name)
+        return vals_list
