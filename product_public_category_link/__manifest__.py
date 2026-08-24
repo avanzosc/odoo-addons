0341c4691d9a90289ef3c07462517ba379892505
Lucía Echeverría <luciaavanzosc@gmail.com>
2026-08-24 08:13:41 +0200

    [18.0][ADD] product_public_category_link: Sync internal categories with public categories.

---
+++ product_public_category_link/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2025 Aner Arregi - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Product Public Category Link",
+    "version": "18.0.1.0.0",
+    "category": "Product",
+    "summary": "Sync internal categories with public categories",
+    "depends": ["product", "website_sale"],
+    "data": [
+        "views/product_category_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "author": "AvanzOSC",
+    "license": "AGPL-3",
+    "website": "https://github.com/avanzosc/odoo-addons",
+}
