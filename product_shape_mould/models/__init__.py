b07341d7077e462167fd343d42a6d8e0b7454839
Ane <aneavanzosc@gmail.com>
2026-09-08 13:54:29 +0200

    [18.0][ADD] product_shape_mould: add management of product shapes, moulds and templates

---
+++ product_shape_mould/models/__init__.py
@@ -0,0 +1,7 @@
+# Copyright 2026 Ane Gurruchaga - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from . import product_mould
+from . import product_template
+from . import product_shape
+from . import stock_lot
