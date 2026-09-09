499d8125d94d650981975462c16dbd4a4c7ef8ed
Alfredo <alfredodelafuente@avanzosc.es>
2026-09-09 13:59:46 +0200

    [18.0][MIG] stock_picking_batch_shortcut: Migration to 18.0

--- stock_picking_batch_shortcut/__manifest__.py
+++ stock_picking_batch_shortcut/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
     "name": "Stock Picking Batch Shorcut",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "AvanzOSC",
     "website": "https://github.com/avanzosc/odoo-addons",
     "category": "Inventory",
