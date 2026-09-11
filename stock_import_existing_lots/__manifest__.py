f197c0b84bbee173fb3e4ce30774a07bd0e942c3
Lucía Echeverría <luciaavanzosc@gmail.com>
2026-09-11 11:54:32 +0200

    [18.0][ADD] stock_import_existing_lots: Always show the 'Import Lots' button and let it search existing lots when the picking type doesn't allow creating new ones

---
+++ stock_import_existing_lots/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2026 Lucía Echeverría - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Stock Import Existing Lots",
+    "summary": "Always show the 'Import Lots' button and let it search "
+    "existing lots when the picking type doesn't allow creating new ones",
+    "version": "18.0.1.0.0",
+    "category": "Inventory/Inventory",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": [
+        "stock",
+    ],
+    "installable": True,
+}
