e9002f567c584a83682a64c8c63df4591b9d01fd
Alfredo <alfredodelafuente@avanzosc.es>
2026-09-16 10:52:04 +0200

    [18.0][MIG] purchase_stock_supplier: Migration to 18.0

--- purchase_stock_supplier/models/stock_warehouse_orderpoint.py
+++ purchase_stock_supplier/models/stock_warehouse_orderpoint.py
@@ -6,14 +6,26 @@ from odoo import api, models
 class StockWarehouseOrderpoint(models.Model):
     _inherit = "stock.warehouse.orderpoint"
 
+    @api.onchange("route_id")
+    def _onchange_route_id(self):
+        result = super()._onchange_route_id()
+        if self.product_id and self.route_id:
+            self.get_default_supplier()
+        return result
+
     @api.model
     def create(self, vals):
-        orderpoint = super().create(vals)
-        if not orderpoint.supplier_id:
+        orderpoints = super().create(vals)
+        for orderpoint in orderpoints.filtered(lambda z: not z.supplier_id):
             orderpoint.get_default_supplier()
-        return orderpoint
+        return orderpoints
 
     def get_default_supplier(self):
-        for orderpoint in self.filtered(lambda x: x.product_id):
+        for orderpoint in self.filtered(
+            lambda x: x.product_id
+            and x.route_id
+            and x.route_id.rule_ids
+            and x.route_id.rule_ids[0].action == "buy"
+        ):
             if orderpoint.product_id.seller_ids:
                 orderpoint.supplier_id = orderpoint.product_id.seller_ids[0].id
