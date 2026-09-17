d6e388320e4f344d8b84730c31fbb80ba56d8d7a
Berezi Amubieta <bereziamubieta@avancosc.es>
2026-09-17 14:38:14 +0200

    [14.0][FIX] stock_quant_valuation: Remove custom module depends.

--- stock_quant_valuation/__manifest__.py
+++ stock_quant_valuation/__manifest__.py
@@ -8,7 +8,10 @@
     "license": "AGPL-3",
     "author": "AvanzOSC",
     "website": "https://github.com/avanzosc/odoo-addons",
-    "depends": ["purchase_last_price_info", "custom_mrp_descarga"],
-    "data": ["views/stock_quant_view.xml"],
+    "depends": [
+        "purchase_last_price_info",
+        "stock_production_lot_purchase_cost",
+    ],
+    "data": ["views/stock_quant_view.xml", "views/stock_production_lot_view.xml"],
     "installable": True,
 }
--- stock_quant_valuation/models/__init__.py
+++ stock_quant_valuation/models/__init__.py
@@ -1,1 +1,2 @@
+from . import stock_production_lot
 from . import stock_quant
