51f1ec4240121479c521ef128f028577a46c1877
Ane <aneavanzosc@gmail.com>
2026-09-21 13:17:26 +0200

    [18.0][IMP] product_shape_mould: validate shape attribute values

--- product_shape_mould/models/product_attribute_value.py
+++ product_shape_mould/models/product_attribute_value.py
@@ -1,7 +1,8 @@
 # Copyright 2026 Ana Juaristi - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class ProductAttributeValue(models.Model):
@@ -22,3 +23,58 @@ class ProductAttributeValue(models.Model):
         string="Mould",
         store=True,
     )
+
+    @api.onchange("shape_id")
+    def _onchange_shape_id(self):
+        for value in self:
+            value._update_shape_values()
+
+    @api.constrains("attribute_id", "shape_id")
+    def _check_shape_id(self):
+        for value in self:
+            if not value.attribute_id.is_shape:
+                continue
+            if not value.shape_id:
+                raise ValidationError(
+                    _("Shape is required for shape attribute values.")
+                )
+            domain = [
+                ("id", "!=", value.id),
+                ("attribute_id", "=", value.attribute_id.id),
+                ("shape_id", "=", value.shape_id.id),
+            ]
+            if self.search_count(domain):
+                raise ValidationError(
+                    _("This shape is already used by another attribute value.")
+                )
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        vals_list = [self._prepare_shape_values(vals) for vals in vals_list]
+        return super().create(vals_list)
+
+    def write(self, vals):
+        for value in self:
+            super(ProductAttributeValue, value).write(value._prepare_shape_values(vals))
+        return True
+
+    def _prepare_shape_values(self, vals):
+        vals = dict(vals)
+        attribute = self.attribute_id
+        if vals.get("attribute_id"):
+            attribute = self.env["product.attribute"].browse(vals["attribute_id"])
+        shape = self.shape_id
+        if vals.get("shape_id"):
+            shape = self.env["product.shape"].browse(vals["shape_id"])
+        if attribute.is_shape and shape:
+            vals["name"] = shape.name
+            if "code" in self._fields:
+                vals["code"] = shape.name
+        return vals
+
+    def _update_shape_values(self):
+        self.ensure_one()
+        if self.shape_id:
+            self.name = self.shape_id.name
+            if "code" in self._fields:
+                self.code = self.shape_id.name
