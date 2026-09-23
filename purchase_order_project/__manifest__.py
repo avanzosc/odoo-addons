453abbd3efdeec2a28c76fec6c8f9b95e9bf2be1
Alfredo <alfredodelafuente@avanzosc.es>
2026-09-23 09:22:04 +0200

    [18.0][IMP] purchase_order_project: Post-migration step for purchase orders to load the project from the analytical account.

--- purchase_order_project/__manifest__.py
+++ purchase_order_project/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
     "name": "Purchase Order Project",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.1.0",
     "category": "Inventory/Purchase",
     "license": "AGPL-3",
     "author": "AvanzOSC",
